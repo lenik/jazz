5319c9476159f4fc8f20f3f5af748a326a485b8d
Lenik <pub.lenik@bodz.net>
2005-07-30 12:12:32 +0000

    the include path is still a problem

--- zlw/common-php/make.bat
+++ zlw/common-php/make.bat
@@ -1,10 +1,13 @@
 @echo off
 
-if "%M4PAPTH%"=="" set M4PATH=%~dp0..\common-m4
+if "%M4PAPTH%"=="" set M4PATH=%~dp0..\common-m4:%~dp0..\config
 
 echo configure smtp api ...
 smtp.m4 > smtp.php
 
 echo configure string utilities ...
 string.m4 > string.php
+
+echo configure default DBI connection ...
+dbi.m4 > dbi.php
 
