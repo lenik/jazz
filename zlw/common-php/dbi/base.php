8e91ebe646e4281979f540d08ca0748b9156670d
Lenik <pub.lenik@bodz.net>
2005-07-31 10:52:27 +0000

    moved sql-related string operations from string.php to here.

--- zlw/common-php/dbi/base.php
+++ zlw/common-php/dbi/base.php
@@ -3,8 +3,11 @@
  *
  * Database Access Interface
  * 
- * $Id: base.php,v 1.3 2005-07-31 04:32:49 dansei Exp $
+ * $Id: base.php,v 1.4 2005-07-31 10:52:27 dansei Exp $
  * $Log: not supported by cvs2svn $
+ * Revision 1.3  2005/07/31 04:32:49  dansei
+ * iter.1-1
+ *
  * Revision 1.2  2005/07/31 02:30:35  dansei
  * includes fix and dev. pack
  *
@@ -45,6 +48,62 @@ class DBI_base {
             $this->free_result($rs); 
         }
         return $result; 
+    }
+    
+    /* import_assoc(this, fetch_assoc()) */
+    function import($assoc, $ignores = NULL) {
+        if ($assoc) {
+            foreach ($assoc as $member=>$value) {
+                if (is_null($value)) continue; 
+                if ($ignores)
+                    if ($ignores[$member]) continue; 
+                $this->$member = $value; 
+            }
+        }
+    }
+    
+    function build_sql_value($val) {
+        if (is_string($val))
+            return Q($this->escape_string($val)); 
+        return $val; 
+    }
+    
+    /* key=value, key=value, ... */
+    function build_pairs($assoc, $ignores = NULL, $separator = ', ', $delim = '=') {
+        $pairs = ''; 
+        foreach ($assoc as $k=>$v) {
+            if (substr($k, 0, 1) == '_') continue; 
+            if (is_null($v)) continue; 
+            if ($ignores)
+                if ($ignores[$k]) continue; 
+            if ($pairs)
+                $pairs .= $separator; 
+            $pairs .= "$k$delim".$this->build_sql_value($v); 
+        }
+        return $pairs; 
+    }
+    
+    function build_debug_info($assoc, $ignores = NULL) {
+        return build_pairs($assoc, $ignores, "\n", " = "); 
+    }
+    
+    /* (key, key, ...) values (value, value, ...) */
+    function build_insert_values($assoc, $ignores = NULL) {
+        $keys = ''; 
+        $values = ''; 
+        foreach ($assoc as $k=>$v) {
+            if (substr($k, 0, 1) == '_') continue; 
+            if (is_null($v)) continue; 
+            if ($ignores)
+                if ($ignores[$k]) continue; 
+            if ($keys) {
+                $keys .= ', '; 
+                $values .= ', '; 
+            }
+            $keys .= $k; 
+            $values .= $this->build_sql_value($v); 
+        }
+        return "($keys) values($values)"; 
     }
 }
 ?>
