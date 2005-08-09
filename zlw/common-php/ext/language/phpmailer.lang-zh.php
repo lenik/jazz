a4b79158a382c65aa1890dca76d48c27d3e4e722
Lenik <pub.lenik@bodz.net>
2005-08-09 01:25:39 +0000

    language files imported

---
+++ zlw/common-php/ext/language/phpmailer.lang-zh.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHPMailer language file.  
+ * Chinese Version (简体中文)
+ * By Xima Lenik (谢继雷)
+ * This file is written in UTF-8. 
+ */
+
+$PHPMAILER_LANG = array();
+
+$PHPMAILER_LANG["provide_address"] = '至少需要一个邮件地址.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' 发送方法不支持。';
+$PHPMAILER_LANG["execute"] = '无法执行: ';
+$PHPMAILER_LANG["instantiate"] = '邮件功能不可用: ';
+$PHPMAILER_LANG["authenticate"] = 'SMTP 错误: 验证失败。';
+$PHPMAILER_LANG["from_failed"] = '以下发送者地址无效: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP 错误: 以下收件人无效：';
+$PHPMAILER_LANG["data_not_accepted"] = 'SMTP 错误: 数据未接受。';
+$PHPMAILER_LANG["connect_host"] = 'SMTP 错误: 不能连接到 SMTP 服务器。';
+$PHPMAILER_LANG["file_access"] = '文件不能访问: ';
+$PHPMAILER_LANG["file_open"] = '文件不能打开: ';
+$PHPMAILER_LANG["encoding"] = '位置编码: ';
+?>
