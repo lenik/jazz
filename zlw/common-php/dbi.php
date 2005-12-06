c0489b2a42640b7fbe114b590b6dbffa4132aa40
Lenik <pub.lenik@bodz.net>
2005-12-06 07:04:35 +0000

    rename sql_connect to phpx_connect

--- zlw/common-php/dbi.php
+++ zlw/common-php/dbi.php
@@ -7,7 +7,7 @@ require '_Phpfixes.php';
 _RequireOnce('string.php'); 
 _RequireOnce('dbi/' . DBI_DIALECT . '.php'); 
 
-function sql_connect($persist = true, $debug = false) {
+function phpx_connect($persist = true, $debug = false) {
     $dbi = new phpx_dbi(DBI_HOST, DBI_USER, DBI_PASSWORD, DBI_DATABASE, 
         true, $persist, $debug); 
     return $dbi; 
