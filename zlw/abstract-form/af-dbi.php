746f630770cba9b20588442a1dc4485e5e7abf96
Lenik <pub.lenik@bodz.net>
2005-12-06 15:06:28 +0000

    add query_as_input, query_as_table for everyday works.

--- zlw/abstract-form/af-dbi.php
+++ zlw/abstract-form/af-dbi.php
@@ -2,31 +2,97 @@
 require '_Phpfixes.php'; 
 _RequireOnce('http.php'); 
 
-function phpx_af_table($dbi, $name, $sql, $primary_key = 'id', 
-                       $methods = 'insert:delete:modify', 
-                       $hint = NULL, $page_size = -1, $page = 0) {
-    if ($hint == NULL) {
+function phpx_af_error($desc, $code = NULL, $source = NULL) {
+    $props = ''; 
+    if (! is_null($code)) {
+        $props .= " code=\"" . htmlspecialchars($code) . "\""; 
+    }
+    if (! is_null($source)) {
+        $props .= " source=\"" . htmlspecialchars($source) . "\""; 
+    }
+    echo "<af:error $props>" . htmlspecialchars($desc) . "</af:error>\n"; 
+}
+
+function phpx_af_special_hints($hint) {
+    if ($hint == '*this*') {
         $hint = phpx_this_url(); 
         if ($pos = strpos($hint, '?')) {
             $hint = substr($hint, 0, $pos); 
         }
     }
-    $keys = explode(':', $primary_key); 
-    
-    echo "<af:table name='$name'>\n"; 
-    foreach (explode(':', $methods) as $method) {
-        echo "<af:method name='$method' hint='$path'/>\n"; 
+    return $hint; 
+}
+
+function phpx_af_query_as_input($dbi, $sql, $readonly_fields = '', 
+                                $update_method = 'update', $name = '', $hint = '') {
+    $result = $dbi->_query($sql); 
+    if (! $result) {
+        $code = $dbi->_errno(); 
+        $desc = $dbi->_error(); 
+        phpx_af_error($desc, $code); 
+        return false; 
     }
     
+    $hint = phpx_af_special_hints($hint); 
+    $readonly_fields = explode(':', $readonly_fields); 
+    
+    echo "<af:form"; 
+    if ($name)
+        echo " name=\"" . htmlspecialchars($name) . "\""; 
+    if ($hint)
+        echo " hint=\"" . htmlspecialchars($hint) . "\""; 
+    echo ">\n"; 
+    
+    $nfields = $dbi->_num_fields($result); 
+    $row = $dbi->_fetch_row($result); 
+    for ($i = 0; $i < $nfields; $i++) {
+        $field = $dbi->_field_name($result, $i); 
+        $value = htmlspecialchars($row[$i]); 
+        echo "<af:input name=\"$field\" init=\"" . htmlspecialchars($value) . "\""; 
+        if (in_array($field, $readonly_fields))
+            echo " read-only=\"1\""; 
+        echo "/>\n"; 
+    }
+    echo "<af:method name=\"" . htmlspecialchars($update_method) . "\"/>\n"; 
+    echo "</af:form>\n"; 
+    $dbi->_free_result($result); 
+    return true; 
+}
+
+function phpx_af_query_as_table($dbi, $sql, $primary_key = 'id', 
+                                $methods = 'delete:modify', $name = '', $hint = '', 
+                                $page_size = -1, $page = 0) {
     $result = $dbi->_query($sql); 
+    if (! $result) {
+        $code = $dbi->_errno(); 
+        $desc = $dbi->_error(); 
+        phpx_af_error($desc, $code); 
+        return false; 
+    }
+    
+    $hint = phpx_af_special_hints($hint); 
+    $keys = explode(':', $primary_key); 
+    
+    echo "<af:table"; 
+    if ($name)
+        echo " name=\"" . htmlspecialchars($name) . "\""; 
+    echo ">\n"; 
+    
+    foreach (explode(':', $methods) as $method) {
+        echo "<af:method name=\"" . htmlspecialchars($method) . "\""; 
+        if ($hint)
+            echo " hint=\"" . htmlspecialchars($hint) . "\""; 
+        echo "/>\n"; 
+    }
+    
     $nfields = $dbi->_num_fields($result); 
     $fields = array(); 
     for ($i = 0; $i < $nfields; $i++) {
         $field = $dbi->_field_name($result, $i); 
         $fields[] = $field; 
-        echo "<af:column name='$field'"; 
+        echo "<af:column name=\"$field\""; 
         if (in_array($field, $keys)) {
-            echo " primary-key='true'"; 
+            echo " primary-key=\"true\""; 
         }
         echo "/>\n"; 
     }
@@ -35,11 +101,13 @@ function phpx_af_table($dbi, $name, $sql, $primary_key = 'id',
         echo "<af:row>\n"; 
         for ($i = 0; $i < $nfields; $i++) {
             $field = $fields[$i]; 
-            echo "<$field>$row[$i]</$field>\n"; 
+            echo "<$field>" . htmlspecialchars($row[$i]) . "</$field>\n"; 
         }
         echo "</af:row>\n"; 
     }
     echo "</af:table>\n"; 
+    $dbi->_free_result($result); 
+    return true; 
 }
 
 ?>
