bce0ec42f9136c88c56fb364b559e27ff8cc27fd
Lenik <pub.lenik@bodz.net>
2005-12-12 02:09:29 +0000

    dev, test ok

--- zlw/abstract-form/html-view.js
+++ zlw/abstract-form/html-view.js
@@ -1,4 +1,33 @@
 
+/* Transaction (Submit Once) */
+function zlw_af_form_submit(ev) {
+    var form = ev.currentTarget; 
+    while (form != null) {
+        // assert form.tagName; 
+        if (form.tagName.toLowerCase() == "form")
+            break; 
+        form = form.parentElement; 
+    }
+    
+    // assert form
+
+    // checking constraints
+    var formId = form.id; 
+    
+    var formValidate = eval("form_" + formId + "_validate"); 
+    if (formValidate != null)
+        if (! formValidate())
+            return false; 
+    
+    // disable all controls
+    for (var i in form.elements) {
+		var element = form.elements[i]; 
+        element.disabled = true; 
+    }
+    return true; 
+}
+
+
 /* Constraints */
 
 function zlw_af_cons_and(a, b) {
