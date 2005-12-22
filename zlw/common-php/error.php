3c17e56f129d2b7bbd9bdf79454c13202e5069f2
Lenik <pub.lenik@bodz.net>
2005-12-22 06:08:01 +0000

    Added to common-php: error.php, graph.php

---
+++ zlw/common-php/error.php
@@ -0,0 +1,144 @@
+<?php
+
+require_once dirname(__FILE__) . '/lang.php'; 
+require_once dirname(__FILE__) . '/graph.php'; 
+
+# [type [.name] ] user-friendly [: detail...]
+# name is used for internationalization. 
+global $PHPX_ERROR_FORMAT; 
+$PHPX_ERROR_FORMAT = '/^\s*(?:\[(\w+)(?:\.(\w+))\]\s*)?(.+?)(?::\s+(.+))?$/'; 
+
+class phpx_error {
+    var $time; 
+    var $provider; 
+    var $type; 
+    var $name; 
+    var $text; 
+    var $detail; 
+    var $source; 
+    var $cause; 
+    
+    function phpx_error($provider, $cause = NULL) {
+        $this->time = time(); 
+        $this->provider = $provider; 
+        if ($cause != NULL) {
+            $this->cause = $cause; 
+            phpx_or($this, $cause); 
+        }
+    }
+    
+    function summary($detail = false) {
+        $buf = $this->type; 
+        if ($this->name)
+            $buf .= '.' . $this->name; 
+        if ($buf)
+            $buf = "[$buf] "; 
+        $buf .= $this->text; 
+        if ($detail && $this->detail)
+            $buf .= ': ' . $this->detail; 
+        return $buf; 
+    }
+}
+
+class phpx_error_manager extends phpx_node {
+    var $errors; 
+    var $mark; 
+    
+    function phpx_error_manager($provider = NULL) {
+        global $PHPX_EM_NEXT; 
+        if ($provider == NULL)
+            $provider = 'EM/' . $PHPX_EM_NEXT++; 
+        $this->name = $provider; 
+        phpx_em_register($this); 
+    }
+    
+    function process($summary, $source = NULL, $cause = NULL) {
+        global $PHPX_ERROR_FORMAT; 
+        assert($summary != NULL); 
+        if (! preg_match($PHPX_ERROR_FORMAT, $summary, $matches))
+            die("Illegal error-summary syntax: $summary"); 
+        $type = $matches[1]; 
+        $name = $matches[2]; 
+        $text = $matches[3]; 
+        $detail = $matches[4]; 
+        
+        $e = new phpx_error($this->name, $cause); 
+        $e->source = $source; 
+        $e->text = $text; 
+        if ($type)) $e->type = $type; 
+        if ($name)) $e->name = $name; 
+        if ($detail)) $e->detail = $detail; 
+        
+        $errors[] = &$e; 
+        return handler($e); 
+    }
+    
+    function handler(&$error) {
+        # Default Handler. 
+        error_log($error->summary(true)); 
+    }
+    
+    function mark() {
+        return $this->mark = sizeof($this->errors); 
+    }
+    
+    function select($cut = false, $mark = NULL) {
+        if ($mark == NULL)
+            $mark = $this->mark; 
+        if ($mark == NULL) {
+            $mark = sizeof($this->errors) - 1; 
+            if ($mark < 0) $mark = 0; 
+        }
+        if ($cut)
+            return array_splice($this->errors, $mark); 
+        else
+            return array_slice($this->errors, $mark); 
+    }
+    
+    function dump() {
+        foreach ($this->errors as $error) {
+            # dumping...
+        }
+    }
+}
+
+global $PHPX_EM_REGISTRY; 
+$PHPX_EM_REGISTRY = new phpx_graph(); 
+
+function phpx_error_manager_register($manager) {
+    global $PHPX_EM_REGISTRY; 
+    return $PHPX_EM_REGISTRY->add($manager); 
+}
+
+function phpx_error_manager_unregister($manager) {
+    global $PHPX_EM_REGISTRY; 
+    return $PHPX_EM_REGISTRY->remove($manager); 
+}
+
+function &phpx_error_manager_get($provider) {
+    global $PHPX_EM_REGISTRY; 
+    return $PHPX_EM_REGISTRY->get($provider); 
+}
+
+function phpx_error_manager_connect_leaves($term_manager) {
+    global $PHPX_EM_REGISTRY; 
+    return $PHPX_EM_REGISTRY->connect_leaves($term_manager); 
+}
+
+function phpx_error_manager_connect($from, $to) {
+    global $PHPX_EM_REGISTRY; 
+    return $PHPX_EM_REGISTRY->connect($from, $to); 
+}
+
+function phpx_error_dispatch($provider, $name = NULL, $type = 'error', $text = NULL, 
+        $source = NULL, $next = NULL) {
+    $next = $provider; 
+    while ($next != NULL) {
+        $em = phpx_error_manager_get($provider); 
+        $error = new phpx_error(); 
+        $em->add($error); 
+        $next = phpx_error_manager_connect($next); 
+    }
+}
+
+?>
