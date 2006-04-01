2aec21c0c8887c2108cbd10f73d40f541b382bf6
Lenik <pub.lenik@bodz.net>
2006-04-01 16:12:18 +0000

    Added common-php ormutil to support ORM.

---
+++ zlw/common-php/ormutil/test/config.php
@@ -0,0 +1,7 @@
+<?php
+    define('DBI_DIALECT', 'mysql'); 
+    define('DBI_HOST', 'localhost'); 
+    define('DBI_USER', 'root'); 
+    define('DBI_PASSWORD', 'l.'); 
+    define('DBI_DATABASE', 'test'); 
+?>
---
+++ zlw/common-php/ormutil/test/showtree.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once dirname(__FILE__) . '/config.php'; 
+require_once dirname(__FILE__) . '/../../lang.php'; 
+require_once dirname(__FILE__) . '/../../error.php'; 
+require_once dirname(__FILE__) . '/../../dbi/' . DBI_DIALECT . '.php'; 
+require_once dirname(__FILE__) . '/../../error.php'; 
+require_once dirname(__FILE__) . '/../isa.php'; 
+
