5341a104552e3d1d45930c16f55386809a75c2ed
Lenik <pub.lenik@bodz.net>
2006-04-01 14:13:19 +0000

    abstract-form: test data.

---
+++ zlw/common-php/test/t-byref-2.php
@@ -0,0 +1,45 @@
+<?php
+
+# complex object ref
+
+class input {
+    var $name; 
+    var $value; 
+    function input($name, $value) {
+        $this->name = $name; 
+        $this->value = $value; 
+    }
+}
+
+class form {
+    var $inputs; 
+    function add_input(&$input) {
+        $name = $input->name;
+        $old = &$this->inputs[$name];
+        if (is_null($old)) {
+            $this->inputs[$name] = &$input;
+            return 1; 
+        }
+        if (! is_array($old)) {
+            $new = array(&$old); 
+            $this->inputs[$name] = &$new; 
+            $old = &$this->inputs[$name]; 
+            
+        }
+        $old[] = &$input; 
+        return sizeof($old); 
+    }
+}
+
+$form = new form(); 
+$a = new input('a', '1'); 
+$b1 = new input('b', '1'); 
+$b2 = new input('b', '2'); 
+$c = new input('c', '1'); 
+$form->add_input($a); 
+$form->add_input($b1); 
+$form->add_input($b2); 
+$form->add_input($c); 
+var_dump($form);
+
+?>
