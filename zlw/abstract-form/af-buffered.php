cc1ad31eab85fd273b31fbab0fb1043649b0cfac
Lenik <pub.lenik@bodz.net>
2006-04-22 13:05:01 +0000

    renamed from af-buf.php to af-buffered.php adapt to the changes of af-mode

--- zlw/abstract-form/af-buffered.php
+++ zlw/abstract-form/af-buffered.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once dirname(__FILE__) . "/af-object.php";
-require_once dirname(__FILE__) . "/af-dbi.php";
 require_once dirname(__FILE__) . '/af-mode.php'; 
 
 class zlw_af_mode_buffered extends zlw_af_mode_base {
@@ -11,47 +9,30 @@ class zlw_af_mode_buffered extends zlw_af_mode_base {
         $this->xmlns = $xmlns;
     }
     
-    function process($o) {
-        if (isset($this->form))
-            $this->form->add($o);
-        elseif (isset($this->section))
-            $this->section->add($o);
-        elseif (isset($this->document))
-            $this->document->add($o);
-        return $o; 
+    function output($output) {
+        $this->context()->add($object);
     }
     
-    function error_handler(&$error) {
-        if (isset($this->document)) {
-            echo $this->process($error); 
-        } else {
-            $doc = new zlw_af_document('Error', null, new zlw_af_section(
-                    'first', null, null, $error));
-            echo $this->process($doc); 
-        }
+    function on_error(&$error) {
+        $this->output($error); 
         exit; 
     }
     
-    function form_end() {
-        $form = parent::form_end();
-        return $this->process($form); 
-    }
-
-    function section_end() {
-        $section = parent::section_end();
-        return $this->process($section); 
-    }
-
-    function document_end() {
-        $document = parent::document_end();
+    function end_document() {
+        $document = parent::end_document();
         phpx_xml_mode();
         echo phpx_xml_value($document, $this->xmlns);
     }
-}
-
-function zlw_af_buf_mode($xmlns = '') {
-    global $ZLW_AF_MODE; 
-    $ZLW_AF_MODE = new zlw_af_mode_buffered($xmlns); 
+    
+    function end_section() {
+        $section = parent::end_section();
+        return $this->output($section); 
+    }
+    
+    function end_form() {
+        $form = parent::form_end();
+        return $this->output($form); 
+    }
 }
 
 ?>
