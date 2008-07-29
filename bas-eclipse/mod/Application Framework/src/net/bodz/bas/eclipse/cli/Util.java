3a892f450a2c4065d6a7a3918351d3147edd3c35
Lenik <pub.lenik@bodz.net>
2008-07-29 12:50:51 +0000

    initial bas-eclipse.

---
+++ bas-eclipse/mod/Application Framework/src/net/bodz/bas/eclipse/cli/Util.java
@@ -0,0 +1,20 @@
+package net.bodz.bas.eclipse.cli;
+
+import java.util.Map;
+
+import org.eclipse.equinox.app.IApplicationContext;
+
+class Util {
+
+    private static final String[] EMPTY_ARGS = {};
+
+    @SuppressWarnings("unchecked")
+    public static String[] getArguments(IApplicationContext context) {
+        Map arguments = context.getArguments();
+        Object _args = arguments.get(IApplicationContext.APPLICATION_ARGS);
+        if (_args == null)
+            return EMPTY_ARGS;
+        return (String[]) _args;
+    }
+
+}
