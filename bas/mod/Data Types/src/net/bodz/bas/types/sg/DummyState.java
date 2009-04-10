ec510aee79b14b9f9d9b74049d564cc9f37c1595
Lenik <pub.lenik@bodz.net>
2009-04-10 10:54:22 +0000

    Added Data Types/..State*, Matrix*; Datbase Abstraction.

---
+++ bas/mod/Data Types/src/net/bodz/bas/types/sg/DummyState.java
@@ -0,0 +1,15 @@
+package net.bodz.bas.types.sg;
+
+public class DummyState extends StateImpl {
+
+    private static final long serialVersionUID = 283780226489973729L;
+
+    public DummyState() {
+        super(null);
+    }
+
+    public DummyState(StateGraph graph) {
+        super(graph);
+    }
+
+}
---
+++ bas/mod/Data Types/src/net/bodz/bas/types/sg/StateGraph.java
@@ -0,0 +1,38 @@
+package net.bodz.bas.types.sg;
+
+import java.io.Serializable;
+
+/**
+ * State Context
+ */
+public interface StateGraph extends Serializable {
+
+    Object context();
+
+    State current();
+
+    boolean isAccepted();
+
+    boolean isErrored();
+
+    State recv(Object message);
+
+    void jump(State state);
+
+    void push(State state);
+
+    State pop();
+
+    // registry of named states
+    // a group of states could find each other thru the registry.
+    // key is compared by "equals".
+
+    State get(Object key);
+
+    Object find(State state);
+
+    void add(Object key, State state);
+
+    void remove(Object key);
+
+}
