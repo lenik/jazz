47a8c18b44c1ed24f4c0e6a171d6bfcb0a0ad0bc
Lenik <pub.lenik@bodz.net>
2010-01-15 06:36:53 +0000

    separate XxxBits implementations to public classes

---
+++ bas/layer-1/bas-bits/src/main/java/net/bodz/bas/bits/Byte_leBits.java
@@ -0,0 +1,69 @@
+package net.bodz.bas.bits;
+
+public class Byte_leBits
+        extends AbstractBits {
+
+    private byte[] bytes;
+    private int bits;
+
+    public Byte_leBits(int bits) {
+        this(new byte[units(bits, Byte.SIZE)], bits);
+    }
+
+    public Byte_leBits(byte... bytes) {
+        this(bytes, bytes.length * Byte.SIZE);
+    }
+
+    public Byte_leBits(byte[] bytes, int bits) {
+        assert bits <= bytes.length * Byte.SIZE;
+        this.bytes = bytes;
+        this.bits = bits;
+    }
+
+    public Byte_leBits(byte[] bytes, int bitOff, int bits) {
+        if (bitOff == 0) {
+            this.bytes = bytes;
+            this.bits = bits;
+        } else {
+            this.bytes = new byte[units(bits, Byte.SIZE)];
+            this.bits = bits;
+            set(bytes, bitOff, 0, bits);
+        }
+    }
+
+    public static final byte[] SET;
+    public static final byte[] CLEAR;
+    static {
+        SET = new byte[] {
+        //
+                (byte) 0x01, (byte) 0x02, (byte) 0x04, (byte) 0x08, //
+                (byte) 0x10, (byte) 0x20, (byte) 0x40, (byte) 0x80, };
+        CLEAR = new byte[] {
+        //
+                (byte) 0xfe, (byte) 0xfd, (byte) 0xfb, (byte) 0xf7, //
+                (byte) 0xef, (byte) 0xdf, (byte) 0xbf, (byte) 0x7f, };
+    }
+
+    @Override
+    public int size() {
+        return bits;
+    }
+
+    @Override
+    public boolean test(int bitIndex) {
+        int off = bitIndex / Byte.SIZE;
+        bitIndex %= Byte.SIZE;
+        return (bytes[off] & SET[bitIndex]) != 0;
+    }
+
+    @Override
+    public void set(int bitIndex, boolean value) {
+        int off = bitIndex / Byte.SIZE;
+        bitIndex %= Byte.SIZE;
+        if (value)
+            bytes[off] |= SET[bitIndex];
+        else
+            bytes[off] &= CLEAR[bitIndex];
+    }
+
+}
--- bas/layer-1/bas-io/src/main/java/net/bodz/bas/io/out/BitOut.java
+++ bas/layer-1/bas-io/src/main/java/net/bodz/bas/io/out/BitOut.java
@@ -2,30 +2,34 @@ package net.bodz.bas.io.out;
 
 import java.io.IOException;
 
+import net.bodz.bas.bits.Byte_leBits;
 import net.bodz.bas.bits.ISimpleBits;
-import net.bodz.bas.bits.IBits.BytevLE;
-import net.bodz.bas.bits.IBits.IntvLE;
+import net.bodz.bas.bits.Int_leBits;
 
-public abstract class BitOut implements IBitOut {
+public abstract class BitOut
+        implements IBitOut {
 
     @Override
-    public void write(ISimpleBits bits) throws IOException {
+    public void write(ISimpleBits bits)
+            throws IOException {
         int size = bits.size();
         for (int i = 0; i < size; i++)
             _write(bits.test(i));
     }
 
-    public abstract void _write(boolean bit) throws IOException;
+    public abstract void _write(boolean bit)
+            throws IOException;
 
     /**
      * In little-endian version.
      * 
      * @throws IOException
      * 
-     * @see BytevLE
+     * @see Byte_leBits
      */
-    public void print(byte[] bytes, int bitOff, int bits) throws IOException {
-        write(new BytevLE(bytes, bitOff, bits));
+    public void print(byte[] bytes, int bitOff, int bits)
+            throws IOException {
+        write(new Byte_leBits(bytes, bitOff, bits));
     }
 
     /**
@@ -33,10 +37,11 @@ public abstract class BitOut implements IBitOut {
      * 
      * @throws IOException
      * 
-     * @see IntvLE
+     * @see Int_leBits
      */
-    public void print(int[] ints, int bitOff, int bits) throws IOException {
-        write(new IntvLE(ints, bitOff, bits));
+    public void print(int[] ints, int bitOff, int bits)
+            throws IOException {
+        write(new Int_leBits(ints, bitOff, bits));
     }
 
 }
