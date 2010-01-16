c7e74eed81d5d9c0e305394fafaf39d4cde106bc
Lenik <pub.lenik@bodz.net>
2010-01-16 03:35:17 +0000

    move -collection to layer-1

--- bas/layer-1/bas-lang/src/main/java/net/bodz/bas/lang/IQueryable.java
+++ bas/layer-1/bas-lang/src/main/java/net/bodz/bas/lang/IQueryable.java
@@ -2,7 +2,16 @@ package net.bodz.bas.lang;
 
 public interface IQueryable {
 
-    Object query(String specificationId);
+    /**
+     * @return <code>null</code> If no available implementation exists for the
+     *         <code>specificationId</code>.
+     * @throws NullPointerException
+     *             If <code>specificationId</code> is <code>null</code>.
+     * @throws QueryException
+     *             If query is failed.
+     */
+    Object query(String specificationId)
+            throws QueryException;
 
     /**
      * The returnd value should be exactly the same to the return value of {@link #query(String)}
@@ -14,7 +23,17 @@ public interface IQueryable {
      *            <code>specificationType</code> , but one may return the result which is instance
      *            of <code>specificationType</code>. Return <code>null</code> if the specification
      *            type doesn't available.
+     * @return <code>null</code> If no available implementation exists for the
+     *         <code>specificationType</code>.
+     * @throws NullPointerException
+     *             If <code>specificationType</code> is <code>null</code>.
+     * @throws ClassCastException
+     *             If the returned implementation can't be isn't instanceof specifiec
+     *             <code>specificationType</code>.
+     * @throws QueryException
+     *             If query is failed.
      */
-    <T> T query(Class<T> specificationType);
+    <T> T query(Class<T> specificationType)
+            throws QueryException;
 
 }
