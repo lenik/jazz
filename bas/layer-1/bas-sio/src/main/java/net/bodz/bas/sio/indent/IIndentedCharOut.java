81976fc9b885a4417f41c394f565be4fded0453a
Lenik <pub.lenik@bodz.net>
2010-02-11 16:16:30 +0000

    Text indention default implementation.

---
+++ bas/layer-1/bas-sio/src/main/java/net/bodz/bas/sio/indent/IIndentedCharOut.java
@@ -0,0 +1,8 @@
+package net.bodz.bas.sio.indent;
+
+import net.bodz.bas.sio.ILineCharOut;
+
+public interface IIndentedCharOut
+        extends ILineCharOut, ITextIndention {
+
+}
--- bas/layer-1/bas-sio/src/main/java/net/bodz/bas/sio/indent/IIndention.java
+++ bas/layer-1/bas-sio/src/main/java/net/bodz/bas/sio/indent/IIndention.java
@@ -4,10 +4,18 @@ public interface IIndention {
 
     int getIndentLevel();
 
+    /**
+     * @throws IllegalArgumentException
+     *             If <code>indentLevel</code> is negative.
+     */
     void setIndentLevel(int indentLevel);
 
     void increaseIndentLevel();
 
+    /**
+     * @throws IllegalStateException
+     *             If can't unindent any more.
+     */
     void decreaseIndentLevel();
 
 }
