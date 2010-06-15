1945ab43304c58526ea1028df8f3df1f7da5c240
Lenik <pub.lenik@bodz.net>
2010-06-15 09:39:48 +0000

    add T[] to simplify usage. using ArrayWrappers.wrap().

--- bas/layer-2/bas-combina/src/test/java/net/bodz/bas/combina/PermutationTest.java
+++ bas/layer-2/bas-combina/src/test/java/net/bodz/bas/combina/PermutationTest.java
@@ -1,5 +1,6 @@
 package net.bodz.bas.combina;
 
+import static net.bodz.bas.collection.array.AbstractArrayWrapper.wrap;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -17,39 +18,39 @@ import org.junit.Test;
 
 public class PermutationTest {
 
-    static LogTerm logger = LogTerms.get(1);
     int count = 0;
 
-    public PermutationTest() {
-        logger.setLevel(LogTerm.INFO);
+    static void debugf(String format, Object... args) {
+        String mesg = String.format(format, args);
+        System.out.println(mesg);
     }
 
     @Test
     public void testIterate() {
-        final String orig = "654321"; 
+        final String orig = "654321";
         final char[] array = orig.toCharArray();
         final Set<String> instances = new HashSet<String>();
 
-        Permutation.iterate(array, new Proc1<char[]>() {
+        Permutation.iterate(wrap(array), new Proc1<char[]>() {
             @Override
             public void exec(char[] inst) {
                 count++;
                 // System.out.println(cc);
                 String s = new String(inst);
-                assertEquals("iter-" + count, orig.length(), inst.length); 
+                assertEquals("iter-" + count, orig.length(), inst.length);
                 instances.add(s);
+                // System.out.println(s);
                 int pos = s.indexOf('1');
-                s = s.replaceAll("[^1]", "0");  
+                s = s.replaceAll("[^1]", "0");
                 int bin = Integer.parseInt(s, 2);
-                Terminal dbg = logger.debug();
-                dbg.f("%8d - %8d - %8d\n", count, pos, bin); 
+                debugf("%8d - %8d - %8d", count, pos, bin);
             }
         });
         int expectedCount = IntMath.fac(orig.length()).intValue();
         assert expectedCount == 720;
-        assertEquals("iterates", expectedCount, count); 
-        assertEquals("unified", expectedCount, instances.size()); 
-        assertEquals("modified", orig, new String(array)); 
+        assertEquals("iterates", expectedCount, count);
+        assertEquals("unified", expectedCount, instances.size());
+        assertEquals("modified", orig, new String(array));
     }
 
     static class TestPermEval {
@@ -61,89 +62,90 @@ public class PermutationTest {
             this.dst = new char[src.length];
         }
 
-        String eval(Integer ord) {
-            Permutation.perm(ord, src, dst);
-            return new String(dst);
+        String eval(int ord) {
+            Permutation.perm(ord, wrap(src), wrap(dst));
+            return String.valueOf(dst);
         }
 
-        void o(Integer ord, String expected) {
-            Permutation.perm(ord, src, dst);
-            String actual = new String(dst);
+        void o(int ord, String expected) {
+            Permutation.perm(ord, wrap(src), wrap(dst));
+            String actual = String.valueOf(dst);
             assertEquals(expected, actual);
         }
     }
 
     @Test
     public void testPerm1() {
-        TestPermEval d = new TestPermEval("1".toCharArray()); // 
-        d.o(0, "1"); // 
+        TestPermEval d = new TestPermEval("1".toCharArray()); //
+        d.o(0, "1"); //
     }
 
     @Test
     public void testPerm2() {
-        TestPermEval d = new TestPermEval("12".toCharArray()); // 
-        d.o(0, "12"); // 
-        d.o(1, "21"); // 
+        TestPermEval d = new TestPermEval("12".toCharArray()); //
+        d.o(0, "12"); //
+        d.o(1, "21"); //
     }
 
     @Test
     public void testPerm3() {
-        TestPermEval d = new TestPermEval("123".toCharArray()); // 
-        d.o(0 * 2 + 0, "123"); // 1 
-        d.o(0 * 2 + 1, "132"); // 
-        d.o(1 * 2 + 0, "213"); // 
-        d.o(1 * 2 + 1, "231"); // 
-        d.o(2 * 2 + 0, "312"); // 
-        d.o(2 * 2 + 1, "321"); // 
+        TestPermEval d = new TestPermEval("123".toCharArray()); //
+        d.o(0 * 2 + 0, "123"); // 1
+        d.o(0 * 2 + 1, "132"); //
+        d.o(1 * 2 + 0, "213"); //
+        d.o(1 * 2 + 1, "231"); //
+        d.o(2 * 2 + 0, "312"); //
+        d.o(2 * 2 + 1, "321"); //
         // cycles
-        d.o(3 * 6 + 0 * 2 + 0, "123"); // 
-        d.o(3 * 6 + 0 * 2 + 1, "132"); // 
-        d.o(3 * 6 + 1 * 2 + 0, "213"); //  
-        d.o(3 * 6 + 1 * 2 + 1, "231"); //  
-        d.o(3 * 6 + 2 * 2 + 0, "312"); // 11 
-        d.o(3 * 6 + 2 * 2 + 1, "321"); // 
+        d.o(3 * 6 + 0 * 2 + 0, "123"); //
+        d.o(3 * 6 + 0 * 2 + 1, "132"); //
+        d.o(3 * 6 + 1 * 2 + 0, "213"); //
+        d.o(3 * 6 + 1 * 2 + 1, "231"); //
+        d.o(3 * 6 + 2 * 2 + 0, "312"); // 11
+        d.o(3 * 6 + 2 * 2 + 1, "321"); //
     }
 
     @Test
     public void testPerm4() {
-        TestPermEval d = new TestPermEval("1234".toCharArray()); // 
-        d.o(0 * 6 + 0 * 2 + 0, "1234"); // 1 
-        d.o(0 * 6 + 0 * 2 + 1, "1243"); // 
-        d.o(0 * 6 + 1 * 2 + 0, "1324"); // 
-        d.o(0 * 6 + 1 * 2 + 1, "1342"); // 
-        d.o(0 * 6 + 2 * 2 + 0, "1423"); // 
-        d.o(0 * 6 + 2 * 2 + 1, "1432"); // 
+        TestPermEval d = new TestPermEval("1234".toCharArray()); //
+        d.o(0 * 6 + 0 * 2 + 0, "1234"); // 1
+        d.o(0 * 6 + 0 * 2 + 1, "1243"); //
+        d.o(0 * 6 + 1 * 2 + 0, "1324"); //
+        d.o(0 * 6 + 1 * 2 + 1, "1342"); //
+        d.o(0 * 6 + 2 * 2 + 0, "1423"); //
+        d.o(0 * 6 + 2 * 2 + 1, "1432"); //
 
-        d.o(1 * 6 + 0 * 2 + 0, "2134"); // 
-        d.o(1 * 6 + 0 * 2 + 1, "2143"); // 
-        d.o(1 * 6 + 1 * 2 + 0, "2314"); // 
-        d.o(1 * 6 + 1 * 2 + 1, "2341"); // 
-        d.o(1 * 6 + 2 * 2 + 0, "2413"); // 11 
-        d.o(1 * 6 + 2 * 2 + 1, "2431"); // 
+        d.o(1 * 6 + 0 * 2 + 0, "2134"); //
+        d.o(1 * 6 + 0 * 2 + 1, "2143"); //
+        d.o(1 * 6 + 1 * 2 + 0, "2314"); //
+        d.o(1 * 6 + 1 * 2 + 1, "2341"); //
+        d.o(1 * 6 + 2 * 2 + 0, "2413"); // 11
+        d.o(1 * 6 + 2 * 2 + 1, "2431"); //
 
-        d.o(2 * 6 + 0 * 2 + 0, "3124"); //  
-        d.o(2 * 6 + 0 * 2 + 1, "3142"); // 
-        d.o(2 * 6 + 1 * 2 + 0, "3214"); // 
-        d.o(2 * 6 + 1 * 2 + 1, "3241"); // 
-        d.o(2 * 6 + 2 * 2 + 0, "3412"); // 
-        d.o(2 * 6 + 2 * 2 + 1, "3421"); // 
+        d.o(2 * 6 + 0 * 2 + 0, "3124"); //
+        d.o(2 * 6 + 0 * 2 + 1, "3142"); //
+        d.o(2 * 6 + 1 * 2 + 0, "3214"); //
+        d.o(2 * 6 + 1 * 2 + 1, "3241"); //
+        d.o(2 * 6 + 2 * 2 + 0, "3412"); //
+        d.o(2 * 6 + 2 * 2 + 1, "3421"); //
 
-        d.o(3 * 6 + 0 * 2 + 0, "4123"); // 
-        d.o(3 * 6 + 0 * 2 + 1, "4132"); // 
-        d.o(3 * 6 + 1 * 2 + 0, "4213"); // 21 
-        d.o(3 * 6 + 1 * 2 + 1, "4231"); // 
-        d.o(3 * 6 + 2 * 2 + 0, "4312"); // 
-        d.o(3 * 6 + 2 * 2 + 1, "4321"); // 
+        d.o(3 * 6 + 0 * 2 + 0, "4123"); //
+        d.o(3 * 6 + 0 * 2 + 1, "4132"); //
+        d.o(3 * 6 + 1 * 2 + 0, "4213"); // 21
+        d.o(3 * 6 + 1 * 2 + 1, "4231"); //
+        d.o(3 * 6 + 2 * 2 + 0, "4312"); //
+        d.o(3 * 6 + 2 * 2 + 1, "4321"); //
     }
 
     @Test
-    public void testPerm5() throws Throwable {
-        TestPermEval d = new TestPermEval("12345".toCharArray()); 
+    public void testPerm5()
+            throws Throwable {
+        TestPermEval d = new TestPermEval("12345".toCharArray());
         int last = 0;
         for (int i = 0; i < 120; i++) {
             String perm = (String) d.eval(i);
             int num = Integer.parseInt(perm);
-            assertTrue("increase", num > last); 
+            assertTrue("increase", num > last);
             last = num;
         }
     }
@@ -157,7 +159,7 @@ public class PermutationTest {
 
         public void o(String input, int expected) {
             char[] dst = input.toCharArray();
-            int actual = Permutation.ord(src, dst);
+            int actual = Permutation.ord(wrap(src), wrap(dst));
             assertEquals(expected, actual);
         }
     }
@@ -165,67 +167,67 @@ public class PermutationTest {
     @Test
     public void testOrd1() {
         TestPermOrd d = new TestPermOrd("1".toCharArray());
-        d.o("1", 0); // 
+        d.o("1", 0); //
     }
 
     @Test
     public void testOrd2() {
-        TestPermOrd d = new TestPermOrd("12".toCharArray()); // 
-        d.o("12", 0); // 
-        d.o("21", 1); // 
+        TestPermOrd d = new TestPermOrd("12".toCharArray()); //
+        d.o("12", 0); //
+        d.o("21", 1); //
     }
 
     @Test
     public void testOrd3() {
-        TestPermOrd d = new TestPermOrd("123".toCharArray()); // 
-        d.o("123", 0 * 2 + 0); // 1 
-        d.o("132", 0 * 2 + 1); // 
-        d.o("213", 1 * 2 + 0); // 
-        d.o("231", 1 * 2 + 1); // 
-        d.o("312", 2 * 2 + 0); // 
-        d.o("321", 2 * 2 + 1); // 
+        TestPermOrd d = new TestPermOrd("123".toCharArray()); //
+        d.o("123", 0 * 2 + 0); // 1
+        d.o("132", 0 * 2 + 1); //
+        d.o("213", 1 * 2 + 0); //
+        d.o("231", 1 * 2 + 1); //
+        d.o("312", 2 * 2 + 0); //
+        d.o("321", 2 * 2 + 1); //
     }
 
     @Test
     public void testOrd4() {
-        TestPermOrd d = new TestPermOrd("1234".toCharArray()); // 
-        d.o("1234", 0 * 6 + 0 * 2 + 0); // 1 
-        d.o("1243", 0 * 6 + 0 * 2 + 1); // 
-        d.o("1324", 0 * 6 + 1 * 2 + 0); // 
-        d.o("1342", 0 * 6 + 1 * 2 + 1); // 
-        d.o("1423", 0 * 6 + 2 * 2 + 0); // 
-        d.o("1432", 0 * 6 + 2 * 2 + 1); // 
+        TestPermOrd d = new TestPermOrd("1234".toCharArray()); //
+        d.o("1234", 0 * 6 + 0 * 2 + 0); // 1
+        d.o("1243", 0 * 6 + 0 * 2 + 1); //
+        d.o("1324", 0 * 6 + 1 * 2 + 0); //
+        d.o("1342", 0 * 6 + 1 * 2 + 1); //
+        d.o("1423", 0 * 6 + 2 * 2 + 0); //
+        d.o("1432", 0 * 6 + 2 * 2 + 1); //
 
-        d.o("2134", 1 * 6 + 0 * 2 + 0); // 
-        d.o("2143", 1 * 6 + 0 * 2 + 1); // 
-        d.o("2314", 1 * 6 + 1 * 2 + 0); // 
-        d.o("2341", 1 * 6 + 1 * 2 + 1); // 
-        d.o("2413", 1 * 6 + 2 * 2 + 0); // 11 
-        d.o("2431", 1 * 6 + 2 * 2 + 1); // 
+        d.o("2134", 1 * 6 + 0 * 2 + 0); //
+        d.o("2143", 1 * 6 + 0 * 2 + 1); //
+        d.o("2314", 1 * 6 + 1 * 2 + 0); //
+        d.o("2341", 1 * 6 + 1 * 2 + 1); //
+        d.o("2413", 1 * 6 + 2 * 2 + 0); // 11
+        d.o("2431", 1 * 6 + 2 * 2 + 1); //
 
-        d.o("3124", 2 * 6 + 0 * 2 + 0); //  
-        d.o("3142", 2 * 6 + 0 * 2 + 1); // 
-        d.o("3214", 2 * 6 + 1 * 2 + 0); // 
-        d.o("3241", 2 * 6 + 1 * 2 + 1); // 
-        d.o("3412", 2 * 6 + 2 * 2 + 0); // 
-        d.o("3421", 2 * 6 + 2 * 2 + 1); // 
+        d.o("3124", 2 * 6 + 0 * 2 + 0); //
+        d.o("3142", 2 * 6 + 0 * 2 + 1); //
+        d.o("3214", 2 * 6 + 1 * 2 + 0); //
+        d.o("3241", 2 * 6 + 1 * 2 + 1); //
+        d.o("3412", 2 * 6 + 2 * 2 + 0); //
+        d.o("3421", 2 * 6 + 2 * 2 + 1); //
 
-        d.o("4123", 3 * 6 + 0 * 2 + 0); // 
-        d.o("4132", 3 * 6 + 0 * 2 + 1); // 
-        d.o("4213", 3 * 6 + 1 * 2 + 0); // 21 
-        d.o("4231", 3 * 6 + 1 * 2 + 1); // 
-        d.o("4312", 3 * 6 + 2 * 2 + 0); // 
-        d.o("4321", 3 * 6 + 2 * 2 + 1); // 
+        d.o("4123", 3 * 6 + 0 * 2 + 0); //
+        d.o("4132", 3 * 6 + 0 * 2 + 1); //
+        d.o("4213", 3 * 6 + 1 * 2 + 0); // 21
+        d.o("4231", 3 * 6 + 1 * 2 + 1); //
+        d.o("4312", 3 * 6 + 2 * 2 + 0); //
+        d.o("4321", 3 * 6 + 2 * 2 + 1); //
     }
 
     @Test
     public void testIterOrd() {
-        final char[] src = "1234".toCharArray(); 
+        final char[] src = "1234".toCharArray();
         final List<Integer> ords = new ArrayList<Integer>();
-        Permutation.iterate(src, new Proc1<char[]>() {
+        Permutation.iterate(wrap(src), new Proc1<char[]>() {
             @Override
             public void exec(char[] inst) {
-                int ord = Permutation.ord(src, inst);
+                int ord = Permutation.ord(wrap(src), wrap(inst));
                 ords.add(ord);
             }
         });
