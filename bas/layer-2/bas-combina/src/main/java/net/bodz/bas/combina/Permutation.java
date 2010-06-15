fd5858fc7ba54650c1cdb1bdca2a9b735ee5b9a7
Lenik <pub.lenik@bodz.net>
2010-06-15 09:39:48 +0000

    add T[] to simplify usage. using ArrayWrappers.wrap().

--- bas/layer-2/bas-combina/src/main/java/net/bodz/bas/combina/Permutation.java
+++ bas/layer-2/bas-combina/src/main/java/net/bodz/bas/combina/Permutation.java
@@ -1,10 +1,10 @@
 package net.bodz.bas.combina;
 
-import java.lang.reflect.Array;
-
 import net.bodz.bas.closure.alt.Proc1;
+import net.bodz.bas.collection.array.ArrayWrapper;
 import net.bodz.bas.collection.array.IArrayWrapper;
 import net.bodz.bas.collection.list.IntSList;
+import net.bodz.bas.exceptions.IllegalUsageException;
 import net.bodz.bas.exceptions.OutOfDomainException;
 
 /**
@@ -12,6 +12,11 @@ import net.bodz.bas.exceptions.OutOfDomainException;
  */
 public class Permutation {
 
+    /**
+     * In-place iterate core implementation.
+     * 
+     * The array after full-iterated will be reversed.
+     */
     private static <A, E> void _iterate(IArrayWrapper<A, E> array, int off, int len, Proc1<A> visitor) {
         if (len == 1) {
             visitor.exec(array.getArray());
@@ -25,13 +30,33 @@ public class Permutation {
         }
     }
 
+    public static <T> void iterate(T[] array, Proc1<T[]> closure) {
+        ArrayWrapper<T> _array = ArrayWrapper.wrap(array);
+        iterate(_array, closure);
+    }
+
     public static <A, E> void iterate(IArrayWrapper<A, E> array, Proc1<A> closure) {
         _iterate(array.copy(), 0, array.length(), closure);
+    }
+
+    public static <T> void iterateInPlace(T[] array, Proc1<T[]> closure) {
+        ArrayWrapper<T> _array = ArrayWrapper.wrap(array);
+        iterateInPlace(_array, closure);
     }
 
     public static <A, E> void iterateInPlace(IArrayWrapper<A, E> array, Proc1<A> closure) {
         _iterate(array, 0, array.length(), closure);
         array.reverse();
+    }
+
+    public static <T> void perm(int ord, T[] array, T[] output) {
+        if (array == null)
+            throw new NullPointerException("array");
+        if (output == null)
+            throw new NullPointerException("output");
+        ArrayWrapper<T> _array = ArrayWrapper.wrap(array);
+        ArrayWrapper<T> _sample = ArrayWrapper.wrap(output);
+        perm(ord, _array, _sample);
     }
 
     public static <A, E> void perm(int ord, IArrayWrapper<A, E> array, IArrayWrapper<A, E> output) {
@@ -55,37 +80,41 @@ public class Permutation {
         }
     }
 
-    public static <A, E> void perm(int ord, IArrayWrapper<A, E> src, IArrayWrapper<A, E> dst) {
-        int srclen = Array.getLength(src);
-        int dstlen = Array.getLength(dst);
-        perm(ord, src, 0, srclen, dst, 0, dstlen);
+    public static <T> int ord(T[] array, T[] sample) {
+        if (array == null)
+            throw new NullPointerException("array");
+        if (sample == null)
+            throw new NullPointerException("sample");
+        ArrayWrapper<T> _array = ArrayWrapper.wrap(array);
+        ArrayWrapper<T> _sample = ArrayWrapper.wrap(sample);
+        return ord(_array, _sample);
     }
 
-    public static <A, E> int ord(IArrayWrapper<A, E> src, IArrayWrapper<A, E> dst) {
-        int srclen = src.length();
-        int dstlen = dst.length();
+    public static <A, E> int ord(IArrayWrapper<A, E> array, IArrayWrapper<A, E> sample) {
+        if (array == null)
+            throw new NullPointerException("array");
+        if (sample == null)
+            throw new NullPointerException("sample");
+        int len = array.length();
+        int sampleLen = sample.length();
+        if (len != sampleLen)
+            throw new IllegalUsageException("the two array of different length");
 
-        IntSList candidates = new IntSList(srclen);
+        IntSList candidates = new IntSList(len);
         int ord = 0;
-        for (int i = 0; i < dstlen; i++) {
-            assert candidates.size() == srclen - i;
+        for (int i = 0; i < sampleLen; i++) {
+            assert candidates.size() == len - i;
 
-            Object val = dst.get(i);
-            int srcindex = src.indexOf(src, val);
+            E val = sample.get(i);
+            int srcindex = array.indexOf(val);
             int mod = candidates.indexOf(srcindex);
-            if (mod == -1 || mod >= srclen)
-                throw new OutOfDomainException("dst[" + (dstoff + i) + "]");
+            if (mod == -1 || mod >= len)
+                throw new OutOfDomainException("dst[" + i + "]");
             candidates.removeAbsolute(srcindex);
 
-            ord = ord * (srclen - i) + mod;
+            ord = ord * (len - i) + mod;
         }
         return ord;
-    }
-
-    public static <A> int ord(A src, A dst) {
-        int srclen = Array.getLength(src);
-        int dstlen = Array.getLength(dst);
-        return ord(src, 0, srclen, dst, 0, dstlen);
     }
 
 }
