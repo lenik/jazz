0622cc2f52acecd4b19ebf41632fd2cddb2f5360
Lenik <pub.lenik@bodz.net>
2010-07-03 09:42:50 +0000

    AnnotationUtil.java: add getDeclaredAnnotation helper
    prepare for XA/externalized-annotations

---
+++ bas/layer-1/bas-annotation/src/main/java/net/bodz/bas/annotation/xa/XClass.java
@@ -0,0 +1,48 @@
+package net.bodz.bas.annotation.xa;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.bodz.bas.exceptions.NotImplementedException;
+
+public class XClass {
+
+    private final Class<?> declaringType;
+
+    public XClass(Class<?> declaringType) {
+        if (declaringType == null)
+            throw new NullPointerException("declaringType");
+        this.declaringType = declaringType;
+    }
+
+    public Class<?> getDeclaringType() {
+        return declaringType;
+    }
+
+    public <A extends Annotation> A getAnnotation(Class<A> annotationType) {
+        throw new NotImplementedException();
+    }
+
+    public <A extends Annotation> A getFieldAnnotation(Field field, Class<A> annotationType) {
+        throw new NotImplementedException();
+    }
+
+    public <A extends Annotation> A getMethodAnnotation(Method method, Class<A> annotationType) {
+        throw new NotImplementedException();
+    }
+
+    private static Map<Class<?>, XClass> parsedTypes = new HashMap<Class<?>, XClass>();
+
+    public static XClass getXClass(Class<?> type) {
+        XClass xClass = parsedTypes.get(type);
+        if (xClass == null) {
+            xClass = new XClass(type);
+            parsedTypes.put(type, xClass);
+        }
+        return xClass;
+    }
+
+}
--- bas/layer-1/bas-annotation/src/test/java/net/bodz/bas/annotation/util/AnnotationsTest.java
+++ bas/layer-1/bas-annotation/src/test/java/net/bodz/bas/annotation/util/AnnotationsTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.lang.annotation.Annotation;
 
-import net.bodz.bas.sio.BCharOut;
-
 import org.junit.Test;
 
 @XyzHint("TeST")
@@ -17,11 +15,10 @@ public class AnnotationsTest {
 
     @Test
     public void testDumpAnnotationMap() {
-        BCharOut buffer = new BCharOut();
-        AnnotationDump.dumpAnnotationMap(AnnotationsTest.class, buffer);
+        String text = AnnotationDump.dumpAnnotationMap(AnnotationsTest.class);
         // @net.bodz.bas.annotations.Author(value=[TeST])
-        assertEquals(annPrefix(XyzHint.class) + "(value=[TeST])\n", // 
-                buffer.toString());
+        assertEquals(annPrefix(XyzHint.class) + "(value=[TeST])\n", //
+                text);
     }
 
 }
