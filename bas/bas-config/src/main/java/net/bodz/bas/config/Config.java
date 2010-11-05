6fd8e3bbd5e4d227e6362177d5505fb271a5a1d9
Lenik <pub.lenik@bodz.net>
2010-11-05 00:18:03 +0000

    default r/w block size = 4096.

---
+++ bas/bas-config/src/main/java/net/bodz/bas/config/Config.java
@@ -0,0 +1,11 @@
+package net.bodz.bas.config;
+
+public class Config {
+
+    private Config() {
+    }
+
+    public static int readBlockSize = 4096;
+    public static int writeBlockSize = 4096;
+
+}
