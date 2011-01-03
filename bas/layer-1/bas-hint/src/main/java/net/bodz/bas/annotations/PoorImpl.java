7fe8ce6066da32f0f34f4ac2b012346b3985ad42
Lenik <pub.lenik@bodz.net>
2011-01-03 06:48:20 +0000

    add @PoorImpl ann.

---
+++ bas/layer-1/bas-hint/src/main/java/net/bodz/bas/annotations/PoorImpl.java
@@ -0,0 +1,17 @@
+package net.bodz.bas.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A poor-implemented method should be optimized by concrete classes.
+ */
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.METHOD, ElementType.TYPE })
+public @interface PoorImpl {
+
+}
