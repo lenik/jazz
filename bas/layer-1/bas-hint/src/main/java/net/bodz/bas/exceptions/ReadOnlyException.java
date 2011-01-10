02ca252e546f62e5c6fadd8375fe918f34ccffa3
Lenik <pub.lenik@bodz.net>
2011-01-10 21:26:39 +0800

    Change class ReadOnlyException  to be inherits from RuntimeException.

--- bas/layer-1/bas-hint/src/main/java/net/bodz/bas/exceptions/ReadOnlyException.java
+++ bas/layer-1/bas-hint/src/main/java/net/bodz/bas/exceptions/ReadOnlyException.java
@@ -1,7 +1,7 @@
 package net.bodz.bas.exceptions;
 
 public class ReadOnlyException
-        extends UnsupportedOperationException {
+        extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
