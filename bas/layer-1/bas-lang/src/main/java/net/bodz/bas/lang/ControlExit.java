2301992d7e2a0d189aa7c9790898306d0cf3de30
Lenik <pub.lenik@bodz.net>
2011-01-17 02:35:36 +0800

    Re-add the Control/Break/Continue classes. Because there's no alternative.

---
+++ bas/layer-1/bas-lang/src/main/java/net/bodz/bas/lang/ControlExit.java
@@ -0,0 +1,24 @@
+package net.bodz.bas.lang;
+
+public class ControlExit
+        extends Control {
+
+    private static final long serialVersionUID = 1L;
+
+    public ControlExit() {
+        super();
+    }
+
+    public ControlExit(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ControlExit(String message) {
+        super(message);
+    }
+
+    public ControlExit(Throwable cause) {
+        super(cause);
+    }
+
+}
