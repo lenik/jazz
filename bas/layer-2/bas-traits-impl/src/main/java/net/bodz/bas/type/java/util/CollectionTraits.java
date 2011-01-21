38a54bac6f67af406f7d978fb9b31aab7e96f853
Lenik <pub.lenik@bodz.net>
2011-01-22 00:02:32 +0800

    Add collection traits.

---
+++ bas/layer-2/bas-traits-impl/src/main/java/net/bodz/bas/type/java/util/CollectionTraits.java
@@ -0,0 +1,39 @@
+package net.bodz.bas.type.java.util;
+
+import java.util.Collection;
+
+import net.bodz.bas.lang.QueryException;
+import net.bodz.bas.traits.AbstractCommonTraits;
+import net.bodz.bas.traits.IParser;
+import net.bodz.bas.traits.ISampleGenerator;
+
+public class CollectionTraits<T>
+        extends AbstractCommonTraits<Collection<T>> {
+
+    @SuppressWarnings("unchecked")
+    public CollectionTraits(Class<T> valueType) {
+        super((Class<Collection<T>>) (Class<?>) Collection.class);
+    }
+
+    @Override
+    public Object query(String specificationId) {
+        return super.query(specificationId);
+    }
+
+    @Override
+    public Object query(Object specification)
+            throws QueryException {
+        return super.query(specification);
+    }
+
+    @Override
+    protected Object query(int traitsIndex) {
+        switch (traitsIndex) {
+        case IParser.traitsIndex:
+        case ISampleGenerator.traitsIndex:
+            return this;
+        }
+        return null;
+    }
+
+}
