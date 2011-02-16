323b4af35c78c47fd076968087e7b1624cec05c3
Lenik <pub.lenik@bodz.net>
2011-02-16 21:56:01 +0800

    Rewrite MultiplexedLogSink.

--- bas/layer-7/bas-program/src/main/java/net/bodz/bas/util/MultiplexedLogSink.java
+++ bas/layer-7/bas-program/src/main/java/net/bodz/bas/util/MultiplexedLogSink.java
@@ -4,37 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.bodz.bas.log.AbstractLogSink;
+import net.bodz.bas.log.ILogEntry;
 import net.bodz.bas.log.ILogSink;
 
 public class MultiplexedLogSink
         extends AbstractLogSink {
 
-    private List<ILogSink> terminals;
+    private List<ILogSink> sinks;
 
-    public MultiplexedLogSink(ILogSink... terminals) {
-        this.terminals = new ArrayList<ILogSink>(terminals.length);
-        for (ILogSink t : terminals)
-            this.terminals.add(t);
+    public MultiplexedLogSink(ILogSink... sinks) {
+        this.sinks = new ArrayList<ILogSink>(sinks.length);
+        for (ILogSink sink : sinks)
+            this.sinks.add(sink);
     }
 
-    public boolean add(ILogSink terminal) {
-        return terminals.add(terminal);
+    public boolean add(ILogSink sink) {
+        return sinks.add(sink);
     }
 
-    public boolean remove(ILogSink terminal) {
-        return terminals.remove(terminal);
-    }
-
-    @Override
-    public void f(String format, Object... args) {
-        for (ILogSink t : terminals)
-            t.f(format, args);
+    public boolean remove(ILogSink sink) {
+        return sinks.remove(sink);
     }
 
     @Override
-    public void p(String s) {
-        for (ILogSink t : terminals)
-            t.p(s);
+    public void log(ILogEntry entry) {
+        for (ILogSink sink : sinks)
+            sink.log(entry);
+    }
+
+    @Override
+    public void logMessage(Object message) {
+        for (ILogSink sink : sinks)
+            sink.logMessage(message);
+    }
+
+    @Override
+    public void logException(Object message, Throwable exception) {
+        for (ILogSink sink : sinks)
+            sink.logException(message, exception);
     }
 
 }
