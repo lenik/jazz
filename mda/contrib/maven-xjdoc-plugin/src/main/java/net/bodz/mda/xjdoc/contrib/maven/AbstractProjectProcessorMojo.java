92c2e8d7be9043a004ffdd0b43c866bfb2fb305d
Lenik <pub.lenik@bodz.net>
2012-04-17 17:24:51 +0800

    Prepare xjdoc m2 plugin.

---
+++ mda/contrib/maven-xjdoc-plugin/src/main/java/net/bodz/mda/xjdoc/contrib/maven/AbstractProjectProcessorMojo.java
@@ -0,0 +1,43 @@
+package net.bodz.mda.xjdoc.contrib.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.project.MavenProject;
+
+public abstract class AbstractProjectProcessorMojo
+        extends AbstractMojo {
+
+    /**
+     * The current Maven project.
+     * 
+     * @parameter default-value="${project}"
+     * @readonly
+     * @required
+     */
+    private MavenProject project;
+
+    /**
+     * The Java version for which to generate source code. Default value is <code>1.5</code> for
+     * plugin version 2.6+ and <code>1.4</code> in older versions.
+     * 
+     * @parameter expression="${jdkVersion}"
+     * @since 2.4
+     */
+    private String jdkVersion;
+
+    protected MavenProject getProject() {
+        return project;
+    }
+
+    protected void setProject(MavenProject project) {
+        this.project = project;
+    }
+
+    protected String getJdkVersion() {
+        return jdkVersion;
+    }
+
+    protected void setJdkVersion(String jdkVersion) {
+        this.jdkVersion = jdkVersion;
+    }
+
+}
