1a663824e5cdee593f6cfffa6797ba1099ddc6d3
Lenik <pub.lenik@bodz.net>
2012-04-17 17:24:51 +0800

    Prepare xjdoc m2 plugin.

--- mda/contrib/maven-xjdoc-plugin/src/main/java/net/bodz/mda/xjdoc/contrib/maven/ClassDocBuilderMojo.java
+++ mda/contrib/maven-xjdoc-plugin/src/main/java/net/bodz/mda/xjdoc/contrib/maven/ClassDocBuilderMojo.java
@@ -1,8 +1,33 @@
 package net.bodz.mda.xjdoc.contrib.maven;
 
-import org.apache.maven.plugin.AbstractMojo;
+import java.io.File;
+import java.io.IOException;
+
+import javax.free.FinalNegotiation;
+import javax.free.ICharOut;
+import javax.free.INegotiation;
+import javax.free.IStreamOutputTarget;
+import javax.free.JavaioFile;
+import javax.free.NegotiationException;
+import javax.free.NegotiationParameter;
+import javax.free.NotImplementedException;
+import javax.free.OutputStreamTarget;
+import javax.free.UnexpectedException;
+
+import net.bodz.bas.text.flatf.FlatfOutput;
+import net.bodz.mda.xjdoc.conv.ClassDocBuilder;
+import net.bodz.mda.xjdoc.meta.IXjLanguage;
+import net.bodz.mda.xjdoc.meta.JavadocXjLang;
+import net.bodz.mda.xjdoc.model.ClassDoc;
+import net.bodz.mda.xjdoc.util.TypeNameContext;
+
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+
+import com.thoughtworks.qdox.JavaDocBuilder;
+import com.thoughtworks.qdox.model.ClassLibrary;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaSource;
 
 /**
  * Builder .classdoc from Java source files.
@@ -11,14 +36,14 @@ import org.apache.maven.plugin.MojoFailureException;
  * @phase generate-resources
  */
 public class ClassDocBuilderMojo
-        extends AbstractMojo {
+        extends AbstractResourceGeneratorMojo {
 
     /**
      * The extension name used to generate classdoc resource files.
      * 
      * @parameter expression="${classdoc.extension}"
      */
-    String extension = "ff";
+    String extension = "classdoc";
 
     /**
      * Xjdoc language name.
@@ -34,9 +59,85 @@ public class ClassDocBuilderMojo
      */
     boolean testResources;
 
+    /**
+     * Add template attributes for missing elements.
+     * 
+     * @parameter expression="${classdoc.missingDoc}"
+     */
+    String missingDoc;
+
     @Override
     public void execute()
             throws MojoExecutionException, MojoFailureException {
+
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        ClassLibrary classLibrary = new ClassLibrary(classLoader);
+
+        JavaDocBuilder javaDocBuilder = new JavaDocBuilder(classLibrary);
+
+        File srcRoot = getSourceDirectory();
+        File outRoot = getOutputDirectory();
+        javaDocBuilder.addSourceTree(srcRoot);
+
+        for (JavaSource jsource : javaDocBuilder.getSources()) {
+            String packageName = jsource.getPackageName();
+            File outDir = outRoot == null ? null : new File(outRoot, packageName.replace('.', '/'));
+
+            TypeNameContext sourceFileImports = new TypeNameContext(packageName);
+            for (String importFqcn : jsource.getImports())
+                sourceFileImports.importTypeName(importFqcn);
+
+            IXjLanguage sourceLang = getLang(langName, sourceFileImports);
+
+            for (JavaClass jclass : jsource.getClasses()) {
+                ClassDocBuilder builder = new ClassDocBuilder(sourceLang, sourceFileImports);
+                // builder.setCreateClassImports(true);
+                ClassDoc classDoc = builder.buildClass(jclass);
+                // builder.setMissingDoc(missingDoc);
+
+                String fqcn = jclass.getFullyQualifiedName();
+                if (!fqcn.startsWith(packageName + "."))
+                    throw new UnexpectedException("Class FQCN doesn't starts with package: " + fqcn);
+                String baseName = fqcn.substring(packageName.length() + 1);
+                baseName = baseName.replace('.', '$') + "." + extension;
+                File classDocFile = outDir == null ? null : new File(outDir, baseName);
+
+                TypeNameContext classImports = classDoc.getOrCreateImports();
+                IXjLanguage classLang = getLang(langName, classImports);
+                INegotiation negotiation = new FinalNegotiation(//
+                        new NegotiationParameter(//
+                                IXjLanguage.class, classLang // JavadocXjLang.getInstance()
+                        ));
+
+                IStreamOutputTarget outTarget;
+                if (classDocFile == null) {
+                    outTarget = new OutputStreamTarget(System.out);
+                    System.out.println("FILE: " + baseName);
+                } else {
+                    outTarget = new JavaioFile(classDocFile).toTarget();
+                }
+                // outTarget.setCharset("utf-8");
+                try {
+                    ICharOut charOut = outTarget.newCharOut();
+                    FlatfOutput ffOut = new FlatfOutput(charOut);
+                    classDoc.writeObject(ffOut, negotiation);
+                    charOut.flush();
+                } catch (IOException e) {
+                    throw new MojoExecutionException(e.getMessage(), e);
+                } catch (NegotiationException e) {
+                    throw new MojoExecutionException(e.getMessage(), e);
+                }
+            }
+        }
+    }
+
+    // static Map<String, IXjLanguage> langMap;
+
+    public IXjLanguage getLang(String langName, TypeNameContext typeNameContext) {
+        if (langName.equals("javadoc"))
+            return new JavadocXjLang(typeNameContext);
+        else
+            throw new NotImplementedException("lang: " + langName);
     }
 
 }
---
+++ mda/contrib/maven-xjdoc-plugin/src/test/java/net/bodz/mda/xjdoc/contrib/maven/ClassDocBuilderMojoTest.java
@@ -0,0 +1,89 @@
+package net.bodz.mda.xjdoc.contrib.maven;
+
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.DefaultComponentRepository;
+import org.junit.Test;
+
+public class ClassDocBuilderMojoTest
+        extends AbstractMojoTestCase {
+
+    @Override
+    protected void setUp()
+            throws Exception {
+        super.setUp();
+    }
+
+    @SuppressWarnings("unchecked")
+    static <T> T getField(Object obj, String fieldName)
+            throws ReflectiveOperationException {
+        Object val = getField(obj, obj.getClass(), fieldName);
+        return (T) val;
+    }
+
+    static Object getField(Object obj, Class<?> clazz, String fieldName)
+            throws ReflectiveOperationException {
+        try {
+            Field field = clazz.getDeclaredField(fieldName);
+            field.setAccessible(true);
+            return field.get(obj);
+        } catch (NoSuchFieldException e) {
+            Class<?> superclass = clazz.getSuperclass();
+            if (superclass == null)
+                throw e;
+            else
+                return getField(obj, superclass, fieldName);
+        }
+    }
+
+    static void dumpTextMap(Map<?, ?> map, String prefix) {
+        List<String> keys = new ArrayList<String>((Collection<? extends String>) map.keySet());
+        Collections.sort(keys);
+        for (String key : keys) {
+            System.out.printf("%s%s = %s\n", prefix, key, map.get(key));
+        }
+    }
+
+    static void dumpTextMap2(Map<?, ?> map2) {
+        List<String> keys2 = new ArrayList<String>((Collection<? extends String>) map2.keySet());
+        Collections.sort(keys2);
+        for (String key2 : keys2) {
+            Map<?, ?> map1 = (Map<?, ?>) map2.get(key2);
+            System.out.printf("%s:\n", key2);
+            dumpTextMap(map1, "    ");
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void testVars()
+            throws Exception {
+        File testPom = new File(getBasedir(), "src/test/resources/unit/dumpff/test1.xml");
+        assertTrue(testPom.exists());
+
+        PlexusContainer container = getContainer();
+        DefaultComponentRepository repo = getField(container, "componentRepository");
+
+        System.out.println("Component Descriptor Maps:");
+        dumpTextMap2((Map<?, ?>) getField(repo, "componentDescriptorMaps"));
+        System.out.println();
+
+        ClassDocBuilderMojo mojo = (ClassDocBuilderMojo) lookupMojo("build", testPom);
+        MavenProject project = mojo.getProject();
+
+        assertNotNull(mojo);
+        System.out.println("Src dir = " + mojo.getSourceDirectory());
+        System.out.println("Out dir = " + mojo.getOutputDirectory());
+        mojo.execute();
+    }
+
+}
