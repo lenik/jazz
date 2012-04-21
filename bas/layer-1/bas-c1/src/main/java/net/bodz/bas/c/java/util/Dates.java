111ca146bf4ad09c6defa171dc6fe2e2679b709c
Lenik <pub.lenik@bodz.net>
2012-04-21 12:25:11 +0800

    Moved -cdata/Dates to -c1.

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/java/util/Dates.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/java/util/Dates.java
@@ -1,11 +1,8 @@
-package net.bodz.bas.cdata.config;
+package net.bodz.bas.c.java.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
-/**
- * @test {@link DatesTest}
- */
 public class Dates {
 
     public static final DateFormat sysDateFormat;
@@ -14,8 +11,12 @@ public class Dates {
 
     /** 1980-12-31 */
     public static final DateFormat YYYY_MM_DD;
+    /** 80-12-31 */
+    public static final DateFormat YY_MM_DD;
     /** 12/31/1980 */
     public static final DateFormat MM_DD_YYYY;
+    /** 12/31/80 */
+    public static final DateFormat MM_DD_YY;
     /** 19801231 */
     public static final DateFormat YYYYMMDD;
     /** 801231 */
@@ -26,27 +27,34 @@ public class Dates {
     /** 173059 */
     public static final DateFormat HHMMSS;
 
+    /** 1980-12-31 17:30:59 */
+    public static final DateFormat D10T8;
+    /** 17:30:59 12/31/1980 */
+    public static final DateFormat T8D10;
+    /** 19801231-173059 */
+    public static final DateFormat D8T6;
+    /** 801231-173059 */
+    public static final DateFormat D6T6;
+
     static {
         sysDateFormat = DateFormat.getDateInstance();
         sysTimeFormat = DateFormat.getTimeInstance();
         sysDateTimeFormat = DateFormat.getDateTimeInstance();
 
         YYYY_MM_DD = getFormat("yyyy-MM-dd");
+        YY_MM_DD = getFormat("yy-MM-dd");
         MM_DD_YYYY = getFormat("MM/dd/yyyy");
+        MM_DD_YY = getFormat("MM/dd/yy");
         YYYYMMDD = getFormat("yyyyMMdd");
         YYMMDD = getFormat("yyMMdd");
 
         HH_MM_SS = getFormat("hh:mm:ss");
         HHMMSS = getFormat("hhmmss");
-    }
 
-    public static DateFormat dateFormat;
-    public static DateFormat timeFormat;
-    public static DateFormat dateTimeFormat;
-    static {
-        dateFormat = YYYY_MM_DD;
-        timeFormat = HH_MM_SS;
-        dateTimeFormat = getFormat("yyyy-MM-dd hh:mm:ss");
+        D10T8 = getFormat("yyyy-MM-dd hh:mm:ss");
+        T8D10 = getFormat("hh:mm:ss MM/dd/yyyy");
+        D8T6 = getFormat("yyyyMMdd-hhmmss");
+        D6T6 = getFormat("yyMMdd-hhmmss");
     }
 
     public static DateFormat getFormat(String dateFormatString) {
