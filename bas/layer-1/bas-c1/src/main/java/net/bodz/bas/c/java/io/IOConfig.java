c6d2a48dcb0c5e86b21475a30960baf61ad930ae
Lenik <pub.lenik@bodz.net>
2012-04-22 23:36:26 +0800

    Renamed cdata.Config to c..io.IOConfig.

---
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/java/io/IOConfig.java
@@ -0,0 +1,8 @@
+package net.bodz.bas.c.java.io;
+
+public interface IOConfig {
+
+    int readBlockSize = 4096;
+    int writeBlockSize = 4096;
+
+}
--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/cdata/config/Config.java
+++
@@ -1,8 +0,0 @@
-package net.bodz.bas.cdata.config;
-
-public class Config {
-
-    public static int readBlockSize = 4096;
-    public static int writeBlockSize = 4096;
-
-}
