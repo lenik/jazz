e54bc411a3a22b39116050c0143bd2cd77023ef7
Lenik <pub.lenik@bodz.net>
2012-05-14 17:15:39 +0800

    Refactor Map2VariantMap > IDecorator. And the impl is mutable.

--- bas/layer-2/bas-script/src/main/java/net/bodz/bas/variant/map/Map2VariantMap.java
+++ bas/layer-2/bas-script/src/main/java/net/bodz/bas/variant/map/Map2VariantMap.java
@@ -4,87 +4,99 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
+import net.bodz.bas.model.IDecorator;
+
 public class Map2VariantMap<K>
-        extends AbstractVariantMap<K> {
+        extends AbstractVariantMap<K>
+        implements IDecorator<Map<K, Object>> {
 
-    private final Map<K, Object> proxy;
+    private Map<K, Object> impl;
 
-    public Map2VariantMap(Map<K, Object> proxy) {
-        if (proxy == null)
-            throw new NullPointerException("proxy");
-        this.proxy = proxy;
+    public Map2VariantMap(Map<K, Object> impl) {
+        if (impl == null)
+            throw new NullPointerException("impl");
+        this.impl = impl;
+    }
+
+    @Override
+    public Map<K, Object> getImplementation() {
+        return impl;
+    }
+
+    public void setImplementation(Map<K, Object> impl) {
+        this.impl = impl;
     }
 
     @Override
     public boolean containsKey(Object key) {
-        return proxy.containsKey(key);
+        return impl.containsKey(key);
     }
 
     @Override
     public Object get(Object key) {
-        return proxy.get(key);
+        return impl.get(key);
     }
 
     @Override
     public Set<K> keySet() {
-        return proxy.keySet();
+        return impl.keySet();
     }
 
     // Map implementation.
 
     @Override
     public int size() {
-        return proxy.size();
+        return impl.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return proxy.isEmpty();
+        return impl.isEmpty();
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return proxy.containsValue(value);
+        return impl.containsValue(value);
     }
 
     @Override
     public Object put(K key, Object value) {
-        return proxy.put(key, value);
+        return impl.put(key, value);
     }
 
     @Override
     public Object remove(Object key) {
-        return proxy.remove(key);
+        return impl.remove(key);
     }
 
     @Override
     public void putAll(Map<? extends K, ? extends Object> m) {
-        proxy.putAll(m);
+        impl.putAll(m);
     }
 
     @Override
     public void clear() {
-        proxy.clear();
+        impl.clear();
     }
 
     @Override
     public Collection<Object> values() {
-        return proxy.values();
+        return impl.values();
     }
 
     @Override
     public Set<java.util.Map.Entry<K, Object>> entrySet() {
-        return proxy.entrySet();
+        return impl.entrySet();
     }
 
     @Override
     public boolean equals(Object o) {
-        return proxy.equals(o);
+        return impl.equals(o);
     }
 
     @Override
     public int hashCode() {
-        return proxy.hashCode();
+        return impl.hashCode();
     }
 
 }
