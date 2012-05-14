6afe81b7235876b6bd0d8004ebbffcf60339adf2
Lenik <pub.lenik@bodz.net>
2012-05-14 17:15:14 +0800

    Refactor variant map: getIng/Long... is-a getScalar.

--- bas/layer-2/bas-script/src/main/java/net/bodz/bas/variant/map/IVariantLookupMap.java
+++ bas/layer-2/bas-script/src/main/java/net/bodz/bas/variant/map/IVariantLookupMap.java
@@ -14,13 +14,31 @@ public interface IVariantLookupMap<K>
 
     Object get(K key, Object defaultValue);
 
-    String getString(K key, String defaultString);
+    /**
+     * Get the value as a scalar.
+     * 
+     * @return If the value is non-array, the value is returned. Otherwise, return the first element
+     *         in the array. If the array is empty, returns <code>null</code>, too.
+     */
+    Object getScalar(K key);
+
+    /**
+     * Get the value as a scalar.
+     * 
+     * @return If the value is non-array, the value is returned. Otherwise, return the first element
+     *         in the array. If the array is empty, returns <code>null</code>, too.
+     * 
+     *         If there is no entry for the key, defaultValue is returned.
+     */
+    Object getScalar(K key, Object defaultValue);
 
     String getString(K key);
 
-    String[] getStringArray(K key, String[] defaultValue);
+    String getString(K key, String defaultString);
 
     String[] getStringArray(K key);
+
+    String[] getStringArray(K key, String[] defaultValue);
 
     // see {@link VariantLookupMapGenerator}
 
