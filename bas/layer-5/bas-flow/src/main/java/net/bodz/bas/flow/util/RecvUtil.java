336cad96bb982dd020ff44539127118444658960
Lenik <pub.lenik@bodz.net>
2012-05-19 15:01:52 +0800

    Added TraitEntryLoader: used to get class local IMethod.

--- bas/layer-5/bas-flow/src/main/java/net/bodz/bas/flow/util/RecvUtil.java
+++ bas/layer-5/bas-flow/src/main/java/net/bodz/bas/flow/util/RecvUtil.java
@@ -6,22 +6,17 @@ import net.bodz.bas.c.type.ClassLocal;
 import net.bodz.bas.c.type.ClassLocals;
 import net.bodz.bas.flow.IReceiverEx;
 import net.bodz.bas.potato.traits.IMethod;
-import net.bodz.bas.trait.Traits;
+import net.bodz.bas.potato.traits.util.TraitEntryLoader;
 
 public class RecvUtil {
 
     private static final ClassLocal<IMethod> recvLocal = ClassLocals.createMap(//
-            RecvUtil.class.getCanonicalName(), entryLoader);
+            RecvUtil.class.getCanonicalName(), TraitEntryLoader.forClass(IMethod.class));
 
     public static void recvEx(IReceiverEx exReceiver, Object data)
             throws IOException, ReflectiveOperationException {
         Class<? extends IReceiverEx> clazz = exReceiver.getClass();
         IMethod recv = recvLocal.get(clazz);
-        if (recv == null) {
-            // new MethodKey()
-            recv = Traits.getTrait(clazz, IMethod.class);
-            recvLocal.put(clazz, recv);
-        }
         // TODO - unwrap IOException
         recv.invoke(exReceiver, data);
     }
