2bdce937644cc2605f0bd31de495f41b15c3a84f
Lenik <pub.lenik@bodz.net>
2012-05-21 17:36:49 +0800

    Moved @TargetSide to ..meta.lang.

--- bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/stereo/TypeIndex.java
+++ bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/stereo/TypeIndex.java
@@ -1,11 +1,21 @@
 package net.bodz.bas.meta.stereo;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to scan service classes and generate the index file.
+ * 
+ * The annotation is inheritable, so only need to annotate on the base type.
+ * 
+ * The result index file is <code>META-INF/services/&lt;base-type&gt;</code> by default.
+ */
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface TypeIndex {
 
     String section() default "services";
--- bas/layer-5/bas-ui/src/main/java/net/bodz/bas/ui/BindProperty.java
+++ bas/layer-5/bas-ui/src/main/java/net/bodz/bas/ui/BindProperty.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import net.bodz.bas.meta.TargetSide;
+import net.bodz.bas.meta.lang.TargetSide;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target( { ElementType.FIELD, ElementType.METHOD })
