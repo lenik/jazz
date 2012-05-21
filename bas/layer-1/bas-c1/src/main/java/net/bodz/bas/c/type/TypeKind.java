9a37a0f3e285570ae43e1e9894e263700e1b83c2
Lenik <pub.lenik@bodz.net>
2012-05-21 17:35:38 +0800

    Added TypeKind.isBoolean, isInteger.

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/type/TypeKind.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/type/TypeKind.java
@@ -2,10 +2,36 @@ package net.bodz.bas.c.type;
 
 public class TypeKind {
 
-    public static boolean isNormalClass(Class<?> clazz) {
-        if (clazz.isArray() || clazz.isInterface() || clazz.isPrimitive())
+    /**
+     * Normal class type is a type which is not primitive, interface, or array.
+     * 
+     * @return <code>true</code> For normal class.
+     */
+    public static boolean isNormalClass(Class<?> type) {
+        if (type.isArray() || type.isInterface() || type.isPrimitive())
             return false;
         return true;
+    }
+
+    /**
+     * Whether the type is boolean or its boxed type.
+     */
+    public static boolean isBoolean(Class<?> type) {
+        return type == boolean.class || type == Boolean.class;
+    }
+
+    /**
+     * Whether the type is byte, short, int, long, or their boxed type.
+     */
+    public static boolean isInteger(Class<?> type) {
+        return type == byte.class //
+                || type == short.class //
+                || type == int.class //
+                || type == long.class //
+                || type == Byte.class //
+                || type == Short.class //
+                || type == Integer.class //
+                || type == Long.class;
     }
 
 }
