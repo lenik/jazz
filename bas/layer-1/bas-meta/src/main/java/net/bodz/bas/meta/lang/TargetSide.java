4c5a8301ca943ad8add7e0130740c55c74c2ab8c
Lenik <pub.lenik@bodz.net>
2012-05-21 17:36:49 +0800

    Moved @TargetSide to ..meta.lang.

--- bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/lang/TargetSide.java
+++ bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/lang/TargetSide.java
@@ -1,4 +1,4 @@
-package net.bodz.bas.meta;
+package net.bodz.bas.meta.lang;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
-@Target( { ElementType.ANNOTATION_TYPE })
+@Target({ ElementType.ANNOTATION_TYPE })
 public @interface TargetSide {
 
     int BEAN = 0;
