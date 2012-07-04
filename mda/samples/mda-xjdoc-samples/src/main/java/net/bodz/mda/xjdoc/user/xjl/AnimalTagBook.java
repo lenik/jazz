1a6a4012e3b0adefd084010499fad07c5b49b01a
Lenik <pub.lenik@bodz.net>
2012-07-04 09:21:29 +0800

    QdoxDog review.

---
+++ mda/samples/mda-xjdoc-samples/src/main/java/net/bodz/mda/xjdoc/user/xjl/AnimalTagBook.java
@@ -0,0 +1,13 @@
+package net.bodz.mda.xjdoc.user.xjl;
+
+import net.bodz.mda.xjdoc.tags.DocTagType;
+import net.bodz.mda.xjdoc.tags.TagBook;
+
+public class AnimalTagBook
+        extends TagBook {
+
+    public AnimalTagBook() {
+        setTagType("color", DocTagType.INSTANCE);
+    }
+
+}
