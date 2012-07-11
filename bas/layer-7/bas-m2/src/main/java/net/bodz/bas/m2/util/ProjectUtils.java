de670cd076cfc3ebefc2019dbb89a9ac0213f1e5
Lenik <pub.lenik@bodz.net>
2012-07-11 16:58:56 +0800

    Reused Abstract*Mojo by symlink to shared/mojo.

---
+++ bas/layer-7/bas-m2/src/main/java/net/bodz/bas/m2/util/ProjectUtils.java
@@ -0,0 +1,21 @@
+package net.bodz.bas.m2.util;
+
+import net.bodz.bas.err.UnexpectedException;
+
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
+import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
+
+public class ProjectUtils {
+
+    public static MavenProject getProject(ExpressionEvaluator expressionEvaluator) {
+        MavenProject project;
+        try {
+            project = (MavenProject) expressionEvaluator.evaluate("${project}");
+        } catch (ExpressionEvaluationException e) {
+            throw new UnexpectedException("Failed to evaluate expression: ${project}", e);
+        }
+        return project;
+    }
+
+}
