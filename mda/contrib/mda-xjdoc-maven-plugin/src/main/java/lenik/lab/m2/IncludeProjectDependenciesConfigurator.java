fb4fd266009ff30538942925447a7203747b30a8
Lenik <pub.lenik@bodz.net>
2012-07-11 16:59:19 +0800

    IncludeProjectDependenciesConfigurator: upgraded for Maven-3.

--- mda/contrib/mda-xjdoc-maven-plugin/src/main/java/lenik/lab/m2/IncludeProjectDependenciesConfigurator.java
+++ mda/contrib/mda-xjdoc-maven-plugin/src/main/java/lenik/lab/m2/IncludeProjectDependenciesConfigurator.java
@@ -4,15 +4,19 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
+import java.util.Set;
 
 import net.bodz.bas.log.api.Logger;
 import net.bodz.bas.log.api.LoggerFactory;
+import net.bodz.bas.m2.util.ProjectUtils;
 
-import org.codehaus.classworlds.ClassRealm;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
 import org.codehaus.plexus.component.configurator.BasicComponentConfigurator;
 import org.codehaus.plexus.component.configurator.ComponentConfigurationException;
 import org.codehaus.plexus.component.configurator.ConfigurationListener;
-import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluationException;
 import org.codehaus.plexus.component.configurator.expression.ExpressionEvaluator;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
 
@@ -35,27 +39,53 @@ public class IncludeProjectDependenciesConfigurator
             ExpressionEvaluator expressionEvaluator, ClassRealm containerRealm, ConfigurationListener listener)
             throws ComponentConfigurationException {
 
+        MavenProject project = ProjectUtils.getProject(expressionEvaluator);
+
         // Add project dependencies to the container realm.
-        List<String> runtimeClasspathElements;
         try {
-            runtimeClasspathElements = (List<String>) expressionEvaluator
-                    .evaluate("${project.runtimeClasspathElements}");
-        } catch (ExpressionEvaluationException e) {
-            throw new ComponentConfigurationException(
-                    "Failed to evaluate expression: ${project.runtimeClasspathElements}", e);
+            List<String> runtimeClasspathElements = project.getRuntimeClasspathElements();
+            // List<String> testClasspathElements = project.getTestClasspathElements();
+            Set<Artifact> artifacts = project.getArtifacts();
+
+            addClasspaths(containerRealm, runtimeClasspathElements);
+            addArtifacts(containerRealm, artifacts);
+        } catch (DependencyResolutionRequiredException e) {
+            throw new ComponentConfigurationException(e);
         }
-        for (String path : runtimeClasspathElements) {
+        super.configureComponent(component, configuration, expressionEvaluator, containerRealm, listener);
+    }
+
+    void addClasspaths(ClassRealm classRealm, List<String> classpaths)
+            throws ComponentConfigurationException {
+        for (String path : classpaths) {
             URL url;
             try {
                 url = new File(path).toURI().toURL();
             } catch (MalformedURLException e) {
                 throw new ComponentConfigurationException("Bad classpath: " + path, e);
             }
-            logger.info("Add-Constituent: ", url);
-            containerRealm.addConstituent(url);
+            logger.info("Add-Classpath: ", url);
+            classRealm.addURL(url);
         }
+    }
 
-        super.configureComponent(component, configuration, expressionEvaluator, containerRealm, listener);
+    void addArtifacts(ClassRealm classRealm, Set<Artifact> artifacts)
+            throws ComponentConfigurationException {
+        for (Artifact artifact : artifacts) {
+            File file = artifact.getFile();
+            if (file == null) {
+                logger.error("Artifact.file is null: artifact");
+                continue;
+            }
+            URL url;
+            try {
+                url = file.toURI().toURL();
+            } catch (MalformedURLException e) {
+                throw new ComponentConfigurationException("Bad classpath: " + file, e);
+            }
+            logger.info("Add-Artifact: ", file);
+            classRealm.addURL(url);
+        }
     }
 
 }
