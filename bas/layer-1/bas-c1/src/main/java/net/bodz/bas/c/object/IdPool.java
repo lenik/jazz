7db7dd29a92f33fc28202702f8d3be6a50d91c40
Lenik <pub.lenik@bodz.net>
2012-07-19 10:28:38 +0800

    Rewrite IdPool (aka InstanceId), based on Class-Locals.

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/object/IdPool.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/object/IdPool.java
@@ -1,51 +1,76 @@
 package net.bodz.bas.c.object;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.IdentityHashMap;
+
+import net.bodz.bas.c.java.util.IMapEntryLoader;
+import net.bodz.bas.c.type.ClassLocal;
+import net.bodz.bas.c.type.ClassLocals;
+import net.bodz.bas.err.LazyLoadException;
 
 public class IdPool {
 
-    private final Class<?> clazz;
-    int index;
+    public static final String NULL_ID = "(null)";
+
+    final Class<?> clazz;
+    int counter;
+    IdentityHashMap<Object, Integer> allocation;
 
     public IdPool(Class<?> clazz) {
         this.clazz = clazz;
+        this.allocation = new IdentityHashMap<>();
     }
 
-    private static Map<Class<?>, Integer> map;
-    static {
-        map = new HashMap<Class<?>, Integer>();
+    public int next() {
+        return ++counter;
     }
 
-    public int getInstanceIndex() {
-        if (index == 0) {
-            Integer nextId = map.get(clazz);
-            if (nextId == null) {
-                index = 1;
-                map.put(clazz, 2);
-            } else {
-                index = nextId;
-                map.put(clazz, index + 1);
-            }
+    public int getId(Object obj) {
+        if (obj == null)
+            return 0;
+        Integer id = allocation.get(obj);
+        if (id == null) {
+            id = next();
+            allocation.put(obj, id);
         }
-        return index;
+        return id;
     }
 
-    public String getSimpleId() {
-        return getClass().getSimpleName() + "@" + getInstanceIndex();
+    public String getSimpleId(Object obj) {
+        if (obj == null)
+            return NULL_ID;
+        int id = getId(obj);
+        return id + "@" + obj.getClass().getSimpleName();
     }
 
-    public String getId() {
-        return getClass().getName() + "@" + getInstanceIndex();
+    public String getCanonicalId(Object obj) {
+        if (obj == null)
+            return NULL_ID;
+        int id = getId(obj);
+        return id + "@" + obj.getClass().getCanonicalName();
     }
 
-    public String getNativeId() {
-        return getClass().getName() + "@" + System.identityHashCode(null);
+    public String getSimpleSystemId(Object obj) {
+        if (obj == null)
+            return NULL_ID;
+        int h = System.identityHashCode(obj);
+        String hex = Integer.toHexString(h);
+        return hex + "@" + obj.getClass().getSimpleName();
     }
 
-    @Override
-    public String toString() {
-        return getSimpleId();
+    public static IdPool getInstance(Class<?> clazz) {
+        return poolMap.load(clazz);
+    }
+
+    static final ClassLocal<IdPool> poolMap;
+    static {
+        poolMap = ClassLocals.createMap(IdPool.class.getName(), //
+                new IMapEntryLoader<Class<?>, IdPool>() {
+                    @Override
+                    public IdPool loadValue(Class<?> type)
+                            throws LazyLoadException {
+                        return new IdPool(type);
+                    }
+                });
     }
 
 }
--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/object/ObjectInfo.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/object/ObjectInfo.java
@@ -2,13 +2,29 @@ package net.bodz.bas.c.object;
 
 public class ObjectInfo {
 
-    public static String getObjectId(Object object) {
+    static final String NULL_ID = "(null)";
+
+    public static String getSystemId(Object object) {
         if (object == null)
-            return "null";
+            return NULL_ID;
         String typeName = object.getClass().getName();
         int hash = System.identityHashCode(object);
         String id = String.format("%s@0x%x", typeName, hash);
         return id;
+    }
+
+    public static String getSimpleId(Object object) {
+        if (object == null)
+            return NULL_ID;
+        IdPool pool = IdPool.getInstance(object.getClass());
+        return pool.getSimpleId(object);
+    }
+
+    public static String getCanonicalId(Object object) {
+        if (object == null)
+            return NULL_ID;
+        IdPool pool = IdPool.getInstance(object.getClass());
+        return pool.getCanonicalId(object);
     }
 
 }
