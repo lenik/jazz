31c64a9855d9b8b1865c68de71cea6ea4bb59d72
Lenik <pub.lenik@bodz.net>
2012-08-07 17:02:21 +0800

    bas-geom: Vector2f rename methods with copy to xxx_().

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/javax/vecmath/Vector2f.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/javax/vecmath/Vector2f.java
@@ -28,6 +28,22 @@ public class Vector2f
         super(init);
     }
 
+    @Override
+    public Vector2f clone() {
+        return new Vector2f(x, y);
+    }
+
+    public final void product(Vector2f v) {
+        float _x = x * v.y;
+        float _y = -y * v.x;
+        x = _x;
+        y = _y;
+    }
+
+    public final Vector2f product_(Vector2f v) {
+        return new Vector2f(x * v.y, -y * v.x);
+    }
+
     public final float angle() {
         return (float) Math.atan2(y, x);
     }
@@ -62,70 +78,220 @@ public class Vector2f
         y = _y;
     }
 
-    public final void normal() {
-        rotate((float) (Math.PI / 2f));
+    public final Vector2f rotate_(float angle) {
+        rotate(angle);
+        return this;
     }
-
-    public final void normalOrig() {
-        rotate((float) (-Math.PI / 2f));
-    }
-
-    public final void product(Vector2f v) {
-        float _x = x * v.y;
-        float _y = -y * v.x;
-        x = _x;
-        y = _y;
-    }
-
-    // toNews
-
-    public final Vector2f toScale(float k) {
-        Vector2f out = new Vector2f(this);
-        out.scale(k);
-        return out;
-    }
-
-    public final Vector2f toNegate() {
-        Vector2f out = new Vector2f(this);
-        out.negate();
-        return out;
-    }
-
-    public final Vector2f toNormalize() {
-        Vector2f out = new Vector2f(this);
-        out.normalize();
-        return out;
-    }
-
-    public final Vector2f toAngle(float angle) {
-        Vector2f out = new Vector2f(this);
-        out.angle(angle);
-        return out;
-    }
-
-    public final Vector2f toRotate(float angle) {
-        Vector2f out = new Vector2f(this);
-        out.rotate(angle);
-        return out;
-    }
-
-    // getNews
 
     public final Vector2f getNormal() {
-        return toRotate((float) (Math.PI / 2f));
+        return clone().rotate_((float) (Math.PI / 2f));
     }
 
     public final Vector2f getNormalOrig() {
-        return toRotate((float) (-Math.PI / 2f));
+        return clone().rotate_((float) (-Math.PI / 2f));
     }
 
-    public final Vector2f getProduct(Vector2f v) {
-        return new Vector2f(x * v.y, -y * v.x);
+    // ------------------- xxx_(...) for method chaining ------------------------
+
+    /**
+     * Sets the value of this tuple to the negation of tuple t1.
+     * 
+     * @param t1
+     *            the source tuple
+     */
+    public final Vector2f negate_(Tuple2f t1) {
+        super.negate(t1);
+        return this;
     }
 
-    @Override
-    public Vector2f clone() {
-        return new Vector2f(x, y);
+    /**
+     * Negates the value of this vector in place.
+     */
+    public final Vector2f negate_() {
+        super.negate();
+        return this;
+    }
+
+    /**
+     * Sets the value of this tuple to the scalar multiplication of tuple t1.
+     * 
+     * @param s
+     *            the scalar value
+     * @param t1
+     *            the source tuple
+     */
+    public final Vector2f scale_(float s, Tuple2f t1) {
+        super.scale(s, t1);
+        return this;
+    }
+
+    /**
+     * Sets the value of this tuple to the scalar multiplication of itself.
+     * 
+     * @param s
+     *            the scalar value
+     */
+    public final Vector2f scale_(float s) {
+        super.scale(s);
+        return this;
+    }
+
+    /**
+     * Sets the value of this tuple to the scalar multiplication of tuple t1 and then adds tuple t2
+     * (this = s*t1 + t2).
+     * 
+     * @param s
+     *            the scalar value
+     * @param t1
+     *            the tuple to be multipled
+     * @param t2
+     *            the tuple to be added
+     */
+    public final Vector2f scaleAdd_(float s, Tuple2f t1, Tuple2f t2) {
+        super.scaleAdd(s, t1, t2);
+        return this;
+    }
+
+    /**
+     * Sets the value of this tuple to the scalar multiplication of itself and then adds tuple t1
+     * (this = s*this + t1).
+     * 
+     * @param s
+     *            the scalar value
+     * @param t1
+     *            the tuple to be added
+     */
+    public final Vector2f scaleAdd_(float s, Tuple2f t1) {
+        super.scaleAdd(s, t1);
+        return this;
+    }
+
+    /**
+     * Clamps the tuple parameter to the range [low, high] and places the values into this tuple.
+     * 
+     * @param min
+     *            the lowest value in the tuple after clamping
+     * @param max
+     *            the highest value in the tuple after clamping
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector2f clamp_(float min, float max, Tuple2f t) {
+        super.clamp(min, max, t);
+        return this;
+    }
+
+    /**
+     * Clamps the minimum value of the tuple parameter to the min parameter and places the values
+     * into this tuple.
+     * 
+     * @param min
+     *            the lowest value in the tuple after clamping
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector2f clampMin_(float min, Tuple2f t) {
+        super.clampMin(min, t);
+        return this;
+    }
+
+    /**
+     * Clamps the maximum value of the tuple parameter to the max parameter and places the values
+     * into this tuple.
+     * 
+     * @param max
+     *            the highest value in the tuple after clamping
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector2f clampMax_(float max, Tuple2f t) {
+        super.clampMax(max, t);
+        return this;
+    }
+
+    /**
+     * Sets each component of the tuple parameter to its absolute value and places the modified
+     * values into this tuple.
+     * 
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector2f absolute_(Tuple2f t) {
+        super.absolute(t);
+        return this;
+    }
+
+    /**
+     * Clamps this tuple to the range [low, high].
+     * 
+     * @param min
+     *            the lowest value in this tuple after clamping
+     * @param max
+     *            the highest value in this tuple after clamping
+     */
+    public final Vector2f clamp_(float min, float max) {
+        super.clamp(min, max);
+        return this;
+    }
+
+    /**
+     * Clamps the minimum value of this tuple to the min parameter.
+     * 
+     * @param min
+     *            the lowest value in this tuple after clamping
+     */
+    public final Vector2f clampMin_(float min) {
+        super.clampMin(min);
+        return this;
+    }
+
+    /**
+     * Clamps the maximum value of this tuple to the max parameter.
+     * 
+     * @param max
+     *            the highest value in the tuple after clamping
+     */
+    public final Vector2f clampMax_(float max) {
+        super.clampMax(max);
+        return this;
+    }
+
+    /**
+     * Sets each component of this tuple to its absolute value.
+     */
+    public final Vector2f absolute_() {
+        super.absolute();
+        return this;
+    }
+
+    /**
+     * Linearly interpolates between tuples t1 and t2 and places the result into this tuple: this =
+     * (1-alpha)*t1 + alpha*t2.
+     * 
+     * @param t1
+     *            the first tuple
+     * @param t2
+     *            the second tuple
+     * @param alpha
+     *            the alpha interpolation parameter
+     */
+    public final Vector2f interpolate_(Tuple2f t1, Tuple2f t2, float alpha) {
+        super.interpolate(t1, t2, alpha);
+        return this;
+    }
+
+    /**
+     * Linearly interpolates between this tuple and tuple t1 and places the result into this tuple:
+     * this = (1-alpha)*this + alpha*t1.
+     * 
+     * @param t1
+     *            the first tuple
+     * @param alpha
+     *            the alpha interpolation parameter
+     */
+    public final Vector2f interpolate_(Tuple2f t1, float alpha) {
+        super.interpolate(t1, alpha);
+        return this;
     }
 
 }
--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/javax/vecmath/Vector3f.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/javax/vecmath/Vector3f.java
@@ -42,7 +42,244 @@ public class Vector3f
         super(vector2f.x, vector2f.y, 1.0f);
     }
 
-    /*
-     * public Vector3f(Point2f point2f) { super(point2f.x(), point2f.y(), 1.0f); }
+    @Override
+    public Vector3f clone() {
+        return new Vector3f(this);
+    }
+
+    // ------------------- xxx_(...) for method chaining ------------------------
+
+    /**
+     * Sets the value of this tuple to the negation of tuple t1.
+     * 
+     * @param t1
+     *            the source tuple
      */
+    public final Vector3f negate_(Tuple3f t1) {
+        super.negate(t1);
+        return this;
+    }
+
+    /**
+     * Negates the value of this tuple in place.
+     */
+    public final Vector3f negate_() {
+        super.negate();
+        return this;
+    }
+
+    /**
+     * Sets the value of this vector to the scalar multiplication of tuple t1.
+     * 
+     * @param s
+     *            the scalar value
+     * @param t1
+     *            the source tuple
+     */
+    public final Vector3f scale_(float s, Tuple3f t1) {
+        super.scale(s, t1);
+        return this;
+    }
+
+    /**
+     * Sets the value of this tuple to the scalar multiplication of the scale factor with this.
+     * 
+     * @param s
+     *            the scalar value
+     */
+    public final Vector3f scale_(float s) {
+        super.scale(s);
+        return this;
+    }
+
+    /**
+     * Sets the value of this tuple to the scalar multiplication of tuple t1 and then adds tuple t2
+     * _(this = s*t1 + t2).
+     * 
+     * @param s
+     *            the scalar value
+     * @param t1
+     *            the tuple to be scaled and added
+     * @param t2
+     *            the tuple to be added without a scale
+     */
+    public final Vector3f scaleAdd_(float s, Tuple3f t1, Tuple3f t2) {
+        super.scaleAdd(s, t1, t2);
+        return this;
+    }
+
+    /**
+     * Sets the value of this tuple to the scalar multiplication of itself and then adds tuple t1
+     * _(this = s*this + t1).
+     * 
+     * @param s
+     *            the scalar value
+     * @param t1
+     *            the tuple to be added
+     */
+    public final Vector3f scaleAdd_(float s, Tuple3f t1) {
+        super.scaleAdd(s, t1);
+        return this;
+    }
+
+    /**
+     * Clamps the tuple parameter to the range [low, high] and places the values into this tuple.
+     * 
+     * @param min
+     *            the lowest value in the tuple after clamping
+     * @param max
+     *            the highest value in the tuple after clamping
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector3f clamp_(float min, float max, Tuple3f t) {
+        super.clamp(min, max, t);
+        return this;
+    }
+
+    /**
+     * Clamps the minimum value of the tuple parameter to the min parameter and places the values
+     * into this tuple.
+     * 
+     * @param min
+     *            the lowest value in the tuple after clamping
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector3f clampMin_(float min, Tuple3f t) {
+        super.clampMin(min, t);
+        return this;
+    }
+
+    /**
+     * Clamps the maximum value of the tuple parameter to the max parameter and places the values
+     * into this tuple.
+     * 
+     * @param max
+     *            the highest value in the tuple after clamping
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector3f clampMax_(float max, Tuple3f t) {
+        super.clampMax(max, t);
+        return this;
+    }
+
+    /**
+     * Sets each component of the tuple parameter to its absolute value and places the modified
+     * values into this tuple.
+     * 
+     * @param t
+     *            the source tuple, which will not be modified
+     */
+    public final Vector3f absolute_(Tuple3f t) {
+        super.absolute(t);
+        return this;
+    }
+
+    /**
+     * Clamps this tuple to the range [low, high].
+     * 
+     * @param min
+     *            the lowest value in this tuple after clamping
+     * @param max
+     *            the highest value in this tuple after clamping
+     */
+    public final Vector3f clamp_(float min, float max) {
+        super.clamp(min, max);
+        return this;
+    }
+
+    /**
+     * Clamps the minimum value of this tuple to the min parameter.
+     * 
+     * @param min
+     *            the lowest value in this tuple after clamping
+     */
+    public final Vector3f clampMin_(float min) {
+        super.clampMin(min);
+        return this;
+    }
+
+    /**
+     * Clamps the maximum value of this tuple to the max parameter.
+     * 
+     * @param max
+     *            the highest value in the tuple after clamping
+     */
+    public final Vector3f clampMax_(float max) {
+        super.clampMax(max);
+        return this;
+    }
+
+    /**
+     * Sets each component of this tuple to its absolute value.
+     */
+    public final Vector3f absolute_() {
+        super.absolute();
+        return this;
+    }
+
+    /**
+     * Linearly interpolates between tuples t1 and t2 and places the result into this tuple: this =
+     * (1-alpha)*t1 + alpha*t2.
+     * 
+     * @param t1
+     *            the first tuple
+     * @param t2
+     *            the second tuple
+     * @param alpha
+     *            the alpha interpolation parameter
+     */
+    public final Vector3f interpolate_(Tuple3f t1, Tuple3f t2, float alpha) {
+        super.interpolate(t1, t2, alpha);
+        return this;
+    }
+
+    /**
+     * Linearly interpolates between this tuple and tuple t1 and places the result into this tuple:
+     * this = (1-alpha)*this + alpha*t1.
+     * 
+     * @param t1
+     *            the first tuple
+     * @param alpha
+     *            the alpha interpolation parameter
+     */
+    public final Vector3f interpolate_(Tuple3f t1, float alpha) {
+        super.interpolate(t1, alpha);
+        return this;
+    }
+
+    /**
+     * Sets this vector to be the vector cross product of vectors v1 and v2.
+     * 
+     * @param v1
+     *            the first vector
+     * @param v2
+     *            the second vector
+     */
+    public final Vector3f cross_(Vector3f v1, Vector3f v2) {
+        super.cross(v1, v2);
+        return this;
+    }
+
+    /**
+     * Sets the value of this vector to the normalization of vector v1.
+     * 
+     * @param v1
+     *            the un-normalized vector
+     */
+    public final Vector3f normalize_(Vector3f v1) {
+        super.normalize(v1);
+        return this;
+    }
+
+    /**
+     * Normalizes this vector in place.
+     */
+    public final Vector3f normalize_() {
+        super.normalize();
+        return this;
+    }
+
 }
