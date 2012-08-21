60500c9f7ff65455d1be8f7e05cf8912fc859bcc
Lenik <pub.lenik@bodz.net>
2012-08-21 17:33:10 +0800

    Re-added @TargetType, AnnotatedElements (Ns.getName)

---
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/reflect/AnnotatedElements.java
@@ -0,0 +1,18 @@
+package net.bodz.bas.c.reflect;
+
+import java.lang.reflect.AnnotatedElement;
+import java.lang.reflect.Member;
+
+public class AnnotatedElements {
+
+    public static String getName(AnnotatedElement element) {
+        if (element instanceof Class<?>)
+            return ((Class<?>) element).getName();
+        if (element instanceof Member) {
+            Member member = (Member) element;
+            return member.getName();
+        }
+        return null;
+    }
+
+}
