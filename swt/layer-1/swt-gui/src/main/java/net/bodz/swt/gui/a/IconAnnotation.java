796c105263a242bc4aae6eff9702db0d95339f4c
Lenik <pub.lenik@bodz.net>
2012-08-21 17:34:05 +0800

    Split A_gui => Font/Icon/ColorAnnotation.

---
+++ swt/layer-1/swt-gui/src/main/java/net/bodz/swt/gui/a/IconAnnotation.java
@@ -0,0 +1,38 @@
+package net.bodz.swt.gui.a;
+
+import java.lang.reflect.AnnotatedElement;
+
+import javax.swing.Icon;
+
+import org.eclipse.swt.graphics.ImageData;
+
+import net.bodz.bas.err.CreateException;
+import net.bodz.bas.jvm.stack.Caller;
+import net.bodz.bas.model.IFactory;
+import net.bodz.swt.c.resources.SWTResources;
+
+public class IconAnnotation {
+
+    public static ImageData getIcon(AnnotatedElement aobject) {
+        Icon aicon = aobject.getAnnotation(Icon.class);
+        if (aicon == null)
+            return null;
+        IFactory<?> iconFactory = getIconFactory(aicon);
+        if (iconFactory == null) { // load resource-path
+            String[] resPaths = aicon.value();
+            if (resPaths.length == 0)
+                return null;
+            String resPath = resPaths[0];
+            ClassLoader loader = Caller.getCallerClassLoader(1);
+            ImageData image = SWTResources.getImageDataRes(loader, resPath);
+            return image;
+        }
+        try {
+            ImageData imageData = (ImageData) iconFactory.create();
+            return imageData;
+        } catch (CreateException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
