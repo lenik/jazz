113237bcd08cb5d087a5488f9c36686ef41349b1
Lenik <pub.lenik@bodz.net>
2012-11-03 09:54:26 +0800

    Prepare geom_i: added basic types.

---
+++ bas/layer-2/bas-geom/src/main/java/net/bodz/bas/geom_i/base/Point2i.java
@@ -0,0 +1,16 @@
+package net.bodz.bas.geom_i.base;
+
+public class Point2i {
+
+    public int x;
+    public int y;
+
+    public Point2i() {
+    }
+
+    public Point2i(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
