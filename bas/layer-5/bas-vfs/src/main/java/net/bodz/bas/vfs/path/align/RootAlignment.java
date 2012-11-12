294d666ef17b1cbb7c06eb1d439ab3e8e60033e9
Lenik <pub.lenik@bodz.net>
2012-11-12 16:37:02 +0800

    bas-vfs: IPathAlignment should be serializable.

--- bas/layer-5/bas-vfs/src/main/java/net/bodz/bas/vfs/path/align/RootAlignment.java
+++ bas/layer-5/bas-vfs/src/main/java/net/bodz/bas/vfs/path/align/RootAlignment.java
@@ -5,6 +5,8 @@ import net.bodz.bas.vfs.path.IPath;
 public class RootAlignment
         extends TruncateOnlyAlignment {
 
+    private static final long serialVersionUID = 1L;
+
     @Override
     public IPath align(IPath context) {
         return context.getRoot();
--- bas/layer-5/bas-vfs/src/main/java/net/bodz/bas/vfs/path/align/TruncateOnlyAlignment.java
+++ bas/layer-5/bas-vfs/src/main/java/net/bodz/bas/vfs/path/align/TruncateOnlyAlignment.java
@@ -3,4 +3,6 @@ package net.bodz.bas.vfs.path.align;
 public abstract class TruncateOnlyAlignment
         extends AbstractPathAlignment {
 
+    private static final long serialVersionUID = 1L;
+
 }
