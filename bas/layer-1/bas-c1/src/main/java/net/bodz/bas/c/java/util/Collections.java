bf77b3b73258b15105f6c7eefce60a6f67f7dd37
Lenik <pub.lenik@bodz.net>
2012-11-15 22:41:06 +0800

    bas-c1: Added Collections.toList/Set(enumeration).

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/java/util/Collections.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/java/util/Collections.java
@@ -18,9 +18,17 @@ public class Collections
         return toArrayList(elements);
     }
 
+    public static <T> List<T> toList(Enumeration<T> enumeration) {
+        return toArrayList(enumeration);
+    }
+
     @SafeVarargs
     public static <T> ArrayList<T> toArrayList(T... elements) {
         return push(new ArrayList<T>(elements.length), elements);
+    }
+
+    public static <T> ArrayList<T> toArrayList(Enumeration<T> enumeration) {
+        return push(new ArrayList<T>(), enumeration);
     }
 
     @SafeVarargs
@@ -28,9 +36,17 @@ public class Collections
         return push(new LinkedList<T>(), elements);
     }
 
+    public static <T> LinkedList<T> toLinkedList(Enumeration<T> enumeration) {
+        return push(new LinkedList<T>(), enumeration);
+    }
+
     @SafeVarargs
     public static <T> Set<T> toSet(T... elements) {
         return toHashSet(elements);
+    }
+
+    public static <T> Set<T> toSet(Enumeration<T> enumeration) {
+        return toHashSet(enumeration);
     }
 
     @SafeVarargs
@@ -38,15 +54,31 @@ public class Collections
         return push(new HashSet<T>(), elements);
     }
 
+    public static <T> HashSet<T> toHashSet(Enumeration<T> enumeration) {
+        return push(new HashSet<T>(), enumeration);
+    }
+
     @SafeVarargs
     public static <T> TreeSet<T> toTreeSet(T... elements) {
         return push(new TreeSet<T>(), elements);
+    }
+
+    public static <T> TreeSet<T> toTreeSet(Enumeration<T> enumeration) {
+        return push(new TreeSet<T>(), enumeration);
     }
 
     @SafeVarargs
     public static <T, C extends Collection<T>> C push(C collection, T... elements) {
         for (T element : elements)
             collection.add(element);
+        return collection;
+    }
+
+    public static <T, C extends Collection<T>> C push(C collection, Enumeration<T> enumeration) {
+        while (enumeration.hasMoreElements()) {
+            T element = enumeration.nextElement();
+            collection.add(element);
+        }
         return collection;
     }
 
