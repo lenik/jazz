733d0c235a5cc01f78bb521e3da8b1a6f2197e30
Lenik <pub.lenik@bodz.net>
2012-12-01 15:08:00 +0800

    bas-gui: Abstract IColor implementation.

--- bas/layer-5/bas-gui/src/main/java/net/bodz/bas/gui/style/IColor.java
+++ bas/layer-5/bas-gui/src/main/java/net/bodz/bas/gui/style/IColor.java
@@ -15,7 +15,17 @@ public interface IColor
 
     IColor_RGBA64 toRGBA64();
 
-    IColor_RGB3f toRGB48f();
+    IColor_RGB3f toRGB3f();
+
+    IColor_RGBA4f toRGBA4f();
+
+    IColor_HSL24 toHSL24();
+
+    IColor_HSLA32 toHSLA32();
+
+    IColor_HSL3f toHSL3f();
+
+    IColor_HSLA4f toHSLA4f();
 
     IColor_CMYK32 toCMYK32();
 
