f141510d1c6297e4206c94569b4fa5b474d00b4b
Lenik <pub.lenik@bodz.net>
2012-12-14 14:00:16 +0800

    bas-t2: IMutableTreeNode.resolve should return this node for empty path.

--- bas/layer-2/bas-t2/src/main/java/net/bodz/bas/t/tree/IMutableTreeNode.java
+++ bas/layer-2/bas-t2/src/main/java/net/bodz/bas/t/tree/IMutableTreeNode.java
@@ -17,6 +17,14 @@ public interface IMutableTreeNode
 
     ITreeNode removeChild(String key);
 
+    /**
+     * Resolve the node by path.
+     * 
+     * @param path
+     *            Path of the node to be resolved. Can't be <code>null</code>.
+     * @return The resolved node. All missing nodes along the path should be created implicitly. If
+     *         path is empty, returns this node.
+     */
     ITreeNode resolve(String path);
 
     void clear();
