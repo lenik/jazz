4ca7db8dfdb0795f51e67110727b41d3db272694
Lenik <pub.lenik@bodz.net>
2012-12-14 16:04:58 +0800

    bas-c3: Renamed URLScanner to URLResourceScanner.

--- bas/layer-3/bas-c3/src/test/java/net/bodz/bas/c/java/net/URLResourceScannerTest.java
+++ bas/layer-3/bas-c3/src/test/java/net/bodz/bas/c/java/net/URLResourceScannerTest.java
@@ -8,19 +8,19 @@ import org.junit.Assert;
 import net.bodz.bas.c.java.util.MapDump;
 import net.bodz.bas.c.string.StringPart;
 
-public class URLScannerTest
+public class URLResourceScannerTest
         extends Assert {
 
     public static void main(String[] args)
             throws Exception {
-        Class<?> clazz = URLScanner.class;
+        Class<?> clazz = URLResourceScanner.class;
         URL self = clazz.getResource(clazz.getSimpleName() + ".class");
 
         String _parent = StringPart.beforeLast(self.toString(), '/');
         // _parent = StringPart.beforeLast(_parent, '/');
         URL parent = new URL(_parent);
 
-        URLScanner scanner = new URLScanner(true);
+        URLResourceScanner scanner = new URLResourceScanner(true);
         Map<String, URL> map = scanner.scan(parent);
 
         System.out.println(MapDump.dump(map));
