a4ec37359a74302ea62561e3ebfea76c8248f641
Lenik <pub.lenik@bodz.net>
2012-12-17 13:19:46 +0800

    bas-vfs: Added treeLineChars option to FileTreeDumper.

--- bas/layer-5/bas-vfs/src/test/java/net/bodz/bas/vfs/util/FileTreeDumperTest.java
+++ bas/layer-5/bas-vfs/src/test/java/net/bodz/bas/vfs/util/FileTreeDumperTest.java
@@ -3,16 +3,18 @@ package net.bodz.bas.vfs.util;
 import org.junit.Assert;
 import org.junit.Test;
 
+import net.bodz.bas.c.m2.MavenProjectOrigin;
 import net.bodz.bas.sio.Stdio;
 import net.bodz.bas.vfs.IFile;
-import net.bodz.bas.vfs.VFS;
+import net.bodz.bas.vfs.impl.jdk.JdkFile;
 
 public class FileTreeDumperTest
         extends Assert {
 
     @Test
     public void testDump1() {
-        IFile start = VFS.resolve("/etc");
+        MavenProjectOrigin orig = MavenProjectOrigin.fromClass(FileTreeDumperTest.class);
+        IFile start = new JdkFile(orig.getProjectDir());
         FileTreeDumper dumper = new FileTreeDumper();
         dumper.dump(Stdio.cout, start);
     }
