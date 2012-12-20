ee479cf0f3c2c9d8a94a0f36f0ac68a3b04ad858
Lenik <pub.lenik@bodz.net>
2012-12-20 22:21:30 +0800

    bas-io: Added PathResource.

---
+++ bas/layer-2/bas-io/src/main/java/net/bodz/bas/io/resource/builtin/PathResource.java
@@ -0,0 +1,64 @@
+package net.bodz.bas.io.resource.builtin;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.channels.ByteChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.Path;
+
+import net.bodz.bas.io.resource.AbstractInputOutputStreamResource;
+
+public class PathResource
+        extends AbstractInputOutputStreamResource
+        implements IChannelResource {
+
+    private Path path;
+
+    public PathResource(Path path) {
+        if (path == null)
+            throw new NullPointerException("path");
+        this.path = path;
+    }
+
+    @Override
+    protected InputStream _newInputStream(OpenOption... options)
+            throws IOException {
+        return Files.newInputStream(path, options);
+    }
+
+    @Override
+    protected OutputStream _newOutputStream(OpenOption... options)
+            throws IOException {
+        return Files.newOutputStream(path, options);
+    }
+
+    @Override
+    protected BufferedReader _newBufferedReader(OpenOption... options)
+            throws IOException {
+        return Files.newBufferedReader(path, getCharset());
+    }
+
+    @Override
+    protected BufferedWriter _newBufferedWriter(OpenOption... options)
+            throws IOException {
+        return Files.newBufferedWriter(path, getCharset(), options);
+    }
+
+    @Override
+    public final ByteChannel newByteChannel(OpenOption... options)
+            throws IOException {
+        return newSeekableByteChannel(options);
+    }
+
+    @Override
+    public SeekableByteChannel newSeekableByteChannel(OpenOption... options)
+            throws IOException {
+        return Files.newByteChannel(path, options);
+    }
+
+}
