ec2a461e01fb53fa57d626fac79744c2b3efeb8a
Lenik <pub.lenik@bodz.net>
2012-12-22 10:27:45 +0800

    bas-io: Added FileResource tests.

---
+++ bas/layer-2/bas-io/src/test/java/net/bodz/bas/io/resource/builtin/FileResourceTest.java
@@ -0,0 +1,15 @@
+package net.bodz.bas.io.resource.builtin;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileResourceTest
+        extends Assert {
+
+    @Test
+    public void testCreateParents() {
+        
+        fail("Not yet implemented");
+    }
+
+}
