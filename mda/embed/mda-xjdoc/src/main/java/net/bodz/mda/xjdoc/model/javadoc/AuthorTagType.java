c088c00f2351f7636b59defbd218a5bb6725095f
Lenik <pub.lenik@bodz.net>
2012-12-30 11:43:03 +0800

    mda-xjdoc: Use simple strings in Author, added AuthorTagType.

---
+++ mda/embed/mda-xjdoc/src/main/java/net/bodz/mda/xjdoc/model/javadoc/AuthorTagType.java
@@ -0,0 +1,28 @@
+package net.bodz.mda.xjdoc.model.javadoc;
+
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.err.ParseException;
+import net.bodz.mda.xjdoc.tagtype.AbstractScalarTagType;
+
+public class AuthorTagType
+        extends AbstractScalarTagType<Author> {
+
+    @Override
+    protected Author parse(String s)
+            throws ParseException {
+        return Author.parse(s);
+    }
+
+    @Override
+    protected String format(Author value)
+            throws FormatException {
+        return value.toString();
+    }
+
+    static AuthorTagType instance = new AuthorTagType();
+
+    public static AuthorTagType getInstance() {
+        return instance;
+    }
+
+}
