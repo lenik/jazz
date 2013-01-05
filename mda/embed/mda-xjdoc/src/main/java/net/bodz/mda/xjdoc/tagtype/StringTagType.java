ae733a02510ff4183800615219482e9bf145a68d
Lenik <pub.lenik@bodz.net>
2013-01-05 13:43:37 +0800

    mda-xjdoc: Quote strings in StringTagType.

--- mda/embed/mda-xjdoc/src/main/java/net/bodz/mda/xjdoc/tagtype/StringTagType.java
+++ mda/embed/mda-xjdoc/src/main/java/net/bodz/mda/xjdoc/tagtype/StringTagType.java
@@ -1,16 +1,40 @@
 package net.bodz.mda.xjdoc.tagtype;
 
+import java.io.IOException;
+
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.rtx.INegotiation;
+import net.bodz.bas.text.flatf.IFlatfOutput;
+
 public class StringTagType
-        extends AbstractScalarTagType<String> {
+        extends AbstractTagType {
 
     @Override
-    protected String parse(String s) {
-        return s;
+    public Object parseJavadoc(String tagNameSpec, Object cont, String string, INegotiation negotiation)
+            throws ParseException {
+        // TODO normalize-space?
+        return string;
     }
 
     @Override
-    protected String format(String value) {
-        return value;
+    public void writeJavadoc(String rootTagName, IJavadocWriter writer, Object value, INegotiation negotiation)
+            throws FormatException, IOException {
+        String string = (String) value;
+        // TODO Line-wrap...
+        writer.writeTag(rootTagName, string);
+    }
+
+    @Override
+    public Object parseEntry(Object cont, String suffix, String string, INegotiation negotiation)
+            throws ParseException {
+        
+        return null;
+    }
+
+    @Override
+    public void writeEntries(IFlatfOutput out, String prefix, Object value, INegotiation negotiation)
+            throws FormatException, IOException {
     }
 
     static final StringTagType INSTANCE = new StringTagType();
