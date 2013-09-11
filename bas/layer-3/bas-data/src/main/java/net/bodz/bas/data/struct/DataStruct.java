26d8dc8f45c2a02a5f0df682dba25da37f10901a
Lenik <pub.lenik@bodz.net>
2013-09-11 11:06:36 +0800

    bas-data: Added IDataStruct.transfer(fmt).

--- bas/layer-3/bas-data/src/main/java/net/bodz/bas/data/struct/DataStruct.java
+++ bas/layer-3/bas-data/src/main/java/net/bodz/bas/data/struct/DataStruct.java
@@ -2,8 +2,14 @@ package net.bodz.bas.data.struct;
 
 import java.io.IOException;
 
+import net.bodz.bas.err.UnexpectedException;
+import net.bodz.bas.io.BByteIn;
+import net.bodz.bas.io.BByteOut;
 import net.bodz.bas.io.IDataIn;
-import net.bodz.bas.io.IDataOut;
+import net.bodz.bas.io.data.DataInImplBE;
+import net.bodz.bas.io.data.DataInImplLE;
+import net.bodz.bas.io.data.DataOutImplBE;
+import net.bodz.bas.io.data.DataOutImplLE;
 import net.bodz.bas.text.rst.IElementHandler;
 import net.bodz.bas.text.rst.IRstOutput;
 import net.bodz.bas.text.rst.IRstSerializable;
@@ -21,19 +27,43 @@ public abstract class DataStruct
     }
 
     @Override
-    public void readObject(IDataIn in)
-            throws IOException {
-    }
-
-    @Override
-    public void writeObject(IDataOut out)
-            throws IOException {
-    }
-
-    @Override
     public void writeObject(IRstOutput out)
             throws IOException {
         ReflectRstDumper.dump(out, this);
+    }
+
+    @SuppressWarnings("resource")
+    @Override
+    public IDataIn transfer(int format) {
+        int size = sizeof();
+        BByteOut bo = new BByteOut(size);
+
+        try {
+            switch (format) {
+            case FORMAT_LE:
+                writeObject(DataOutImplLE.from(bo));
+                break;
+            case FORMAT_BE:
+                writeObject(DataOutImplBE.from(bo));
+                break;
+            default:
+                throw new IllegalArgumentException("Bad format: " + format);
+            }
+        } catch (IOException e) {
+            throw new UnexpectedException(e);
+        }
+
+        byte[] buf = bo.toByteArray();
+        BByteIn bi = new BByteIn(buf);
+
+        switch (format) {
+        case FORMAT_LE:
+            return DataInImplLE.from(bi);
+        case FORMAT_BE:
+            return DataInImplBE.from(bi);
+        default:
+            throw new UnexpectedException();
+        }
     }
 
     @Override
