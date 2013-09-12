b478c6d8fa133dfbacad4d25a64b272070c95e79
Lenik <pub.lenik@bodz.net>
2013-09-12 19:22:54 +0800

    bas-data: Changed IRstFieldOverride to IReflectRstOverrides.

--- bas/layer-3/bas-data/src/main/java/net/bodz/bas/text/rst/RstObject.java
+++ bas/layer-3/bas-data/src/main/java/net/bodz/bas/text/rst/RstObject.java
@@ -3,9 +3,11 @@ package net.bodz.bas.text.rst;
 import java.io.IOException;
 import java.lang.reflect.Field;
 
+import net.bodz.bas.c.object.IEmptyConsts;
+
 public abstract class RstObject
         extends ReflectElementHandler
-        implements IRstSerializable, IRstFieldOverride {
+        implements IRstSerializable, IReflectRstOverrides {
 
     @Override
     public void writeObject(IRstOutput out)
@@ -14,14 +16,19 @@ public abstract class RstObject
     }
 
     @Override
-    public boolean writeObjectFieldOverride(IRstOutput out, Field field)
-            throws IOException {
-        return false;
+    public IElementHandler getElementHandler() {
+        return this;
     }
 
     @Override
-    public IElementHandler getElementHandler() {
-        return this;
+    public String[] getElementArguments() {
+        return IEmptyConsts.emptyStringArray;
+    }
+
+    @Override
+    public boolean writeObjectFieldOverride(IRstOutput out, Field field)
+            throws IOException {
+        return false;
     }
 
 }
