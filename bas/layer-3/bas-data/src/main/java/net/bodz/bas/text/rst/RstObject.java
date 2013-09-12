7255a7eca93e65e2b37af7d8bdda216013ed50c2
Lenik <pub.lenik@bodz.net>
2013-09-13 00:03:24 +0800

    bas-data: RstObject.toString returns the rst script by default.

--- bas/layer-3/bas-data/src/main/java/net/bodz/bas/text/rst/RstObject.java
+++ bas/layer-3/bas-data/src/main/java/net/bodz/bas/text/rst/RstObject.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 
 import net.bodz.bas.c.object.IEmptyConsts;
+import net.bodz.bas.err.UnexpectedException;
+import net.bodz.bas.io.BCharOut;
 
 public abstract class RstObject
         extends ReflectElementHandler
@@ -29,6 +31,19 @@ public abstract class RstObject
     public boolean writeObjectFieldOverride(IRstOutput out, Field field)
             throws IOException {
         return false;
+    }
+
+    @Override
+    public String toString() {
+        BCharOut buf = new BCharOut(1024);
+        IRstOutput out = RstOutputImpl.from(buf);
+        try {
+            writeObject(out);
+        } catch (IOException e) {
+            throw new UnexpectedException(e.getMessage(), e);
+        }
+        String rst = buf.toString();
+        return rst;
     }
 
 }
