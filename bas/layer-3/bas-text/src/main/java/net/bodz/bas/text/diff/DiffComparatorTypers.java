58069b17eaf3587c377ab3702aa578292fe17b1a
Lenik <pub.lenik@bodz.net>
2013-09-17 17:15:58 +0800

    bas-typer: Changed @TyperFamilyClass to @typer.family.

--- bas/layer-3/bas-text/src/main/java/net/bodz/bas/text/diff/DiffComparatorTypers.java
+++ bas/layer-3/bas-text/src/main/java/net/bodz/bas/text/diff/DiffComparatorTypers.java
@@ -1,6 +1,10 @@
 package net.bodz.bas.text.diff;
 
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.rtx.IOptions;
 import net.bodz.bas.typer.std.AbstractCommonTypers;
+import net.bodz.bas.typer.std.IInstanceStore;
+import net.bodz.bas.typer.std.IParser;
 
 public class DiffComparatorTypers
         extends AbstractCommonTypers<IDiffComparator> {
@@ -12,7 +16,22 @@ public class DiffComparatorTypers
 
     @Override
     protected Object queryInt(int typerIndex) {
+        switch (typerIndex) {
+        case IParser.typerIndex:
+        case IInstanceStore.typerIndex:
+            return this;
+        }
         return null;
+    }
+
+    @Override
+    public IDiffComparator parse(String name, IOptions options)
+            throws ParseException {
+        IDiffComparator instance = getInstance(name);
+        if (instance == null)
+            throw new ParseException("Unknown comparator: " + name);
+        else
+            return instance;
     }
 
 }
--- bas/layer-3/bas-text/src/main/java/net/bodz/bas/text/diff/IDiffComparator.java
+++ bas/layer-3/bas-text/src/main/java/net/bodz/bas/text/diff/IDiffComparator.java
@@ -2,9 +2,9 @@ package net.bodz.bas.text.diff;
 
 import java.util.List;
 
-import net.bodz.bas.meta.lang.TyperFamilyClass;
+import net.bodz.bas.meta.lang.typer;
 
-@TyperFamilyClass(DiffComparatorTypers.class)
+@typer.family(DiffComparatorTypers.class)
 public interface IDiffComparator {
 
     List<DiffEntry> compareDiff(List<?> a, List<?> b);
