349023dac504789b2ee5514484c7c713586de482
Lenik <pub.lenik@bodz.net>
2013-09-17 17:21:50 +0800

    bas-program: Moved BuildInfo/ProgramName/StartMode-Utils to "fn" inner classes.

--- bas/layer-7/bas-program/src/main/java/net/bodz/bas/program/meta/BuildInfo.java
+++ bas/layer-7/bas-program/src/main/java/net/bodz/bas/program/meta/BuildInfo.java
@@ -1,7 +1,14 @@
 package net.bodz.bas.program.meta;
 
+import java.io.IOException;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.net.URL;
+import java.util.Properties;
+
+import net.bodz.bas.c.object.Nullables;
+import net.bodz.bas.io.res.tools.StreamLoading;
+import net.bodz.bas.vfs.impl.url.URLFile;
 
 @Retention(RetentionPolicy.RUNTIME)
 public @interface BuildInfo {
@@ -10,5 +17,24 @@ public @interface BuildInfo {
      * Resource name, which contains counters
      */
     String value();
+
+    class fn {
+
+        /**
+         * @return <code>null</code> if build info is unknown.
+         * @throws IOException
+         */
+        public static Properties resolve(Class<?> clazz)
+                throws IOException {
+            String resname = Nullables.getAnnotation(clazz, BuildInfo.class).value();
+            if (resname == null)
+                return null;
+            URL url = clazz.getResource(resname);
+            if (url == null)
+                throw new NullPointerException("BuildInfo resource isn't existed: " + resname);
+            return URLFile.resolve(url).tooling()._for(StreamLoading.class).loadProperties();
+        }
+
+    }
 
 }
--- bas/layer-7/bas-program/src/main/java/net/bodz/bas/program/meta/ProgramName.java
+++ bas/layer-7/bas-program/src/main/java/net/bodz/bas/program/meta/ProgramName.java
@@ -5,10 +5,31 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import net.bodz.bas.c.annotation.AnnotationParseUtil;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ProgramName {
 
     String value();
+
+    class fn
+            extends AnnotationParseUtil {
+
+        public static String getValue(Class<?> type) {
+            return getValue(type, null);
+        }
+
+        public static String getValue(Class<?> type, Boolean toUpperCase) {
+            ProgramName pn = type.getAnnotation(ProgramName.class);
+            if (pn != null)
+                return pn.value();
+            String name = type.getSimpleName();
+            if (toUpperCase != null)
+                name = toUpperCase ? name.toUpperCase() : name.toLowerCase();
+            return name;
+        }
+
+    }
 
 }
