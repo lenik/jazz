616f9c69dbd0dc34fbc3bfda0c898383e8e19939
Lenik <pub.lenik@bodz.net>
2013-09-24 20:39:37 +0800

    bas-data: Moved transfer() to IDataStruct.fn.

--- bas/layer-3/bas-data/src/main/java/net/bodz/bas/data/struct/IDataStruct.java
+++ bas/layer-3/bas-data/src/main/java/net/bodz/bas/data/struct/IDataStruct.java
@@ -3,8 +3,15 @@ package net.bodz.bas.data.struct;
 import java.io.IOException;
 import java.io.Serializable;
 
+import net.bodz.bas.err.UnexpectedException;
+import net.bodz.bas.io.BByteIn;
+import net.bodz.bas.io.BByteOut;
 import net.bodz.bas.io.IDataIn;
 import net.bodz.bas.io.IDataOut;
+import net.bodz.bas.io.data.DataInImplBE;
+import net.bodz.bas.io.data.DataInImplLE;
+import net.bodz.bas.io.data.DataOutImplBE;
+import net.bodz.bas.io.data.DataOutImplLE;
 
 public interface IDataStruct
         extends Serializable {
@@ -21,5 +28,42 @@ public interface IDataStruct
     int FORMAT_BE = 1;
 
     IDataIn transfer(int format);
+
+    class fn {
+
+        @SuppressWarnings("resource")
+        public static IDataIn transfer(IDataStruct struct, int format) {
+            int size = struct.size();
+            BByteOut bo = new BByteOut(size);
+
+            try {
+                switch (format) {
+                case FORMAT_LE:
+                    struct.writeObject(DataOutImplLE.from(bo));
+                    break;
+                case FORMAT_BE:
+                    struct.writeObject(DataOutImplBE.from(bo));
+                    break;
+                default:
+                    throw new IllegalArgumentException("Bad format: " + format);
+                }
+            } catch (IOException e) {
+                throw new UnexpectedException(e);
+            }
+
+            byte[] buf = bo.toByteArray();
+            BByteIn bi = new BByteIn(buf);
+
+            switch (format) {
+            case FORMAT_LE:
+                return DataInImplLE.from(bi);
+            case FORMAT_BE:
+                return DataInImplBE.from(bi);
+            default:
+                throw new UnexpectedException();
+            }
+        }
+
+    }
 
 }
--- bas/layer-3/bas-data/src/main/java/net/bodz/bas/data/struct/RstDataStruct.java
+++ bas/layer-3/bas-data/src/main/java/net/bodz/bas/data/struct/RstDataStruct.java
@@ -1,15 +1,6 @@
 package net.bodz.bas.data.struct;
 
-import java.io.IOException;
-
-import net.bodz.bas.err.UnexpectedException;
-import net.bodz.bas.io.BByteIn;
-import net.bodz.bas.io.BByteOut;
 import net.bodz.bas.io.IDataIn;
-import net.bodz.bas.io.data.DataInImplBE;
-import net.bodz.bas.io.data.DataInImplLE;
-import net.bodz.bas.io.data.DataOutImplBE;
-import net.bodz.bas.io.data.DataOutImplLE;
 import net.bodz.bas.text.rst.RstObject;
 
 public abstract class RstDataStruct
@@ -23,38 +14,9 @@ public abstract class RstDataStruct
         return new ReflectSizeComputer(this).sizeof();
     }
 
-    @SuppressWarnings("resource")
     @Override
     public IDataIn transfer(int format) {
-        int size = size();
-        BByteOut bo = new BByteOut(size);
-
-        try {
-            switch (format) {
-            case FORMAT_LE:
-                writeObject(DataOutImplLE.from(bo));
-                break;
-            case FORMAT_BE:
-                writeObject(DataOutImplBE.from(bo));
-                break;
-            default:
-                throw new IllegalArgumentException("Bad format: " + format);
-            }
-        } catch (IOException e) {
-            throw new UnexpectedException(e);
-        }
-
-        byte[] buf = bo.toByteArray();
-        BByteIn bi = new BByteIn(buf);
-
-        switch (format) {
-        case FORMAT_LE:
-            return DataInImplLE.from(bi);
-        case FORMAT_BE:
-            return DataInImplBE.from(bi);
-        default:
-            throw new UnexpectedException();
-        }
+        return IDataStruct.fn.transfer(this, format);
     }
 
 }
