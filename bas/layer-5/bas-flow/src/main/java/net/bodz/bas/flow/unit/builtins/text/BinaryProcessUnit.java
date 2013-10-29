7dbfbc43a06b9df462d890c856c8b8b338096727
Lenik <pub.lenik@bodz.net>
2013-10-29 18:48:43 +0800

    bas-flow: Implement recv(Object) in subclasses to call the actual recv(*).

--- bas/layer-5/bas-flow/src/main/java/net/bodz/bas/flow/unit/builtins/text/BinaryProcessUnit.java
+++ bas/layer-5/bas-flow/src/main/java/net/bodz/bas/flow/unit/builtins/text/BinaryProcessUnit.java
@@ -3,10 +3,28 @@ package net.bodz.bas.flow.unit.builtins.text;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+import net.bodz.bas.err.NotImplementedException;
 import net.bodz.bas.flow.unit.GenericUnit_11;
 
 public abstract class BinaryProcessUnit
         extends GenericUnit_11 {
+
+    @Override
+    public void recv(Object data)
+            throws IOException {
+
+        if (data instanceof byte[]) {
+            recv((byte[]) data);
+            return;
+        }
+
+        if (data instanceof ByteBuffer) {
+            recv((ByteBuffer) data);
+            return;
+        }
+
+        throw new NotImplementedException();
+    }
 
     public abstract void recv(byte[] bytes, int start, int end)
             throws IOException;
--- bas/layer-5/bas-flow/src/main/java/net/bodz/bas/flow/utils/RecvUtil.java
+++ bas/layer-5/bas-flow/src/main/java/net/bodz/bas/flow/utils/RecvUtil.java
@@ -2,16 +2,19 @@ package net.bodz.bas.flow.utils;
 
 import java.io.IOException;
 
+import net.bodz.bas.c.java.util.IMapEntryLoader;
 import net.bodz.bas.c.type.LazyTypeMap;
 import net.bodz.bas.c.type.TypeMapRegistry;
+import net.bodz.bas.err.LazyLoadException;
 import net.bodz.bas.flow.stream.IReceiverEx;
 import net.bodz.bas.potato.element.IMethod;
-import net.bodz.bas.potato.util.TyperEntryLoader;
+import net.bodz.bas.potato.element.IType;
+import net.bodz.bas.typer.Typers;
 
 public class RecvUtil {
 
     private static final LazyTypeMap<IMethod> clsRecvMethod = TypeMapRegistry.createMap(//
-            RecvUtil.class.getCanonicalName(), TyperEntryLoader.forClass(IMethod.class));
+            RecvUtil.class.getCanonicalName(), new RecvMethodEntryLoader());
 
     public static void recvEx(IReceiverEx exReceiver, Object data)
             throws IOException, ReflectiveOperationException {
@@ -19,6 +22,24 @@ public class RecvUtil {
         IMethod recvMethod = clsRecvMethod.getOrLoad(clazz);
         // TODO - unwrap IOException
         recvMethod.invoke(exReceiver, data);
+    }
+
+}
+
+class RecvMethodEntryLoader
+        implements IMapEntryLoader<Class<?>, IMethod> {
+
+    @Override
+    public IMethod loadValue(Class<?> key)
+            throws LazyLoadException {
+        IType type = Typers.getTyper(key, IType.class);
+
+        IMethod recvMethod = type.getMethod("recv", Object.class);
+        if (recvMethod != null)
+            return recvMethod;
+
+        throw new LazyLoadException(String.format("Class %s doesn't have a recv method.", //
+                key.getName()));
     }
 
 }
