acdbd53547c9e60b2eae23d8b270d9dbf2c79fc4
Lenik <pub.lenik@bodz.net>
2013-11-06 18:01:05 +0800

    bas-vfs: Implemented *FileAttributes/View on MutableFile.

---
+++ bas/layer-5/bas-vfs/src/main/java/net/bodz/bas/vfs/CommonFileAttributes.java
@@ -0,0 +1,9 @@
+package net.bodz.bas.vfs;
+
+import java.nio.file.attribute.DosFileAttributes;
+import java.nio.file.attribute.PosixFileAttributes;
+
+public interface CommonFileAttributes
+        extends DosFileAttributes, PosixFileAttributes {
+
+}
