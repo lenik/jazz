06b06fe9eb9082f231fd98f6c4754498e19b8504
Lenik <pub.lenik@bodz.net>
2014-02-25 17:28:06 +0800

    bas-repr: Removed IRequestDispatch, attribute with ITokenQueue/IPathArrival.

--- bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/ITokenQueue.java
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/ITokenQueue.java
@@ -2,35 +2,40 @@ package net.bodz.bas.repr.path;
 
 /**
  * Records the state of token preprocessing.
+ *
+ * When the dispatch started, the token queue contains tokens to be dispatched, and after dispatch
+ * is completed, all processed tokens are consumed, rest only the unprocessed tokens.
  */
 public interface ITokenQueue {
+
+    String ATTRIBUTE_KEY = ITokenQueue.class.getCanonicalName();
 
     String INDEX = ""; // "<<index>>";
 
     /**
      * Get the available count of remaining tokens.
-     * 
+     *
      * @return Count of the remaining tokens.
      */
     int available();
 
     /**
      * Join the remaining tokens to a path string.
-     * 
+     *
      * @return The joined remaining path, without leading slash('/').
      */
     String getRemainingPath();
 
     /**
      * Test if the queue is empty.
-     * 
+     *
      * @return <code>true</code> If the queue is empty.
      */
     boolean isEmpty();
 
     /**
      * Skip a number of tokens.
-     * 
+     *
      * @param n
      *            Number of tokens to skip.
      */
@@ -38,9 +43,9 @@ public interface ITokenQueue {
 
     /**
      * Shift out a number of head tokens.
-     * 
+     *
      * A trailing slash (<code>'/'</code>) should be translated into {@link #INDEX}.
-     * 
+     *
      * @return A token array consists of n tokens.
      * @throws IndexOutOfBoundsException
      *             If <code>n</code> is out of range.
@@ -54,69 +59,69 @@ public interface ITokenQueue {
 
     /**
      * Shift out the head token.
-     * 
+     *
      * A trailing slash (<code>'/'</code>) should be translated into {@link #INDEX}.
-     * 
+     *
      * @return <code>null</code> If no more token.
      */
     String shift();
 
     /**
      * Shift out the head token as an int.
-     * 
+     *
      * If the head token isn't int, shift doesn't happen and <code>null</code> is returned.
-     * 
+     *
      * @return <code>null</code> If no more token, or the head token isn't an int number.
      */
     Integer shiftInt();
 
     /**
      * Shift out the head token as a long.
-     * 
+     *
      * If the head token isn't long, shift doesn't happen and <code>null</code> is returned.
-     * 
+     *
      * @return <code>null</code> If no more token, or the head token isn't a long number.
      */
     Long shiftLong();
 
     /**
      * Peek at the head token.
-     * 
+     *
      * @return <code>null</code> If no more token.
      */
     String peek();
 
     /**
      * Peek at the n-th token from the head.
-     * 
+     *
      * @return <code>null</code> If the token doesn't exist.
      */
     String peek(int offset);
 
     /**
      * Peek at the head token as integer.
-     * 
+     *
      * @return <code>null</code> If no more token, or the head token isn't a int number.
      */
     Integer peekInt();
 
     /**
      * Peek at the n-th token from the head as int.
-     * 
+     *
      * @return <code>null</code> If the token doesn't exist, or it's not a int integer.
      */
     Integer peekInt(int offset);
 
     /**
      * Peek at the head token as long.
-     * 
+     *
      * @return <code>null</code> If no more token, or the head token isn't a long number.
      */
     Long peekLong();
 
     /**
      * Peek at the n-th token from the head as long.
-     * 
+     *
      * @return <code>null</code> If the token doesn't exist, or it's not a long integer.
      */
     Long peekLong(int offset);
