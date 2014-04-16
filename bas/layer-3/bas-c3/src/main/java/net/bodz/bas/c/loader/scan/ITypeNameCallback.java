e2425cfe89e34e439b6d898f5c07cf308a13c594
Lenik <pub.lenik@bodz.net>
2014-04-16 11:07:31 +0800

    bas-c3: tcoll print single line logs, ignore anonymous classes.

--- bas/layer-3/bas-c3/src/main/java/net/bodz/bas/c/loader/scan/ITypeNameCallback.java
+++ bas/layer-3/bas-c3/src/main/java/net/bodz/bas/c/loader/scan/ITypeNameCallback.java
@@ -2,6 +2,9 @@ package net.bodz.bas.c.loader.scan;
 
 public interface ITypeNameCallback {
 
+    /**
+     * @return <code>true</code> to continue, <code>false</code> to cancel.
+     */
     boolean typeName(String fqcn);
 
 }
