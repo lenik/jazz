f006885f84ccaac57c8c5058bcb8b8f3e8566b7e
Lenik <pub.lenik@bodz.net>
2014-04-17 18:14:07 +0800

    bas-repr: Added ResourcePathDispatcher.

---
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/builtin/ResourcePathDispatcher.java
@@ -0,0 +1,44 @@
+package net.bodz.bas.repr.path.builtin;
+
+import java.net.URL;
+
+import net.bodz.bas.io.res.builtin.URLResource;
+import net.bodz.bas.repr.path.AbstractPathDispatcher;
+import net.bodz.bas.repr.path.IPathArrival;
+import net.bodz.bas.repr.path.ITokenQueue;
+import net.bodz.bas.repr.path.PathArrival;
+import net.bodz.bas.repr.path.PathDispatchException;
+
+public class ResourcePathDispatcher
+        extends AbstractPathDispatcher {
+
+    @Override
+    public int getPriority() {
+        return 10;
+    }
+
+    @Override
+    public IPathArrival dispatch(IPathArrival previous, ITokenQueue tokens)
+            throws PathDispatchException {
+        Object obj = previous.getTarget();
+        if (obj == null)
+            throw new PathDispatchException("null target.");
+
+        String remaining = tokens.getRemainingPath();
+        if (remaining == null || remaining.isEmpty()) // XXX can it be empty?
+            return previous;
+
+        // the file name must have extension.
+        if (!remaining.contains("."))
+            return null;
+
+        Class<?> clazz = obj.getClass();
+        URL url = clazz.getResource(remaining);
+        if (url == null)
+            return null;
+
+        URLResource resource = new URLResource(url);
+        return new PathArrival(previous, resource, tokens.shiftAll(), "");
+    }
+
+}
