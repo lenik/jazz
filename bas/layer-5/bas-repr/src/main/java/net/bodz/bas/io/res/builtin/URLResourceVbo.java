d2448db2ba332a994b0b65f746c6ab326ecbf89b
Lenik <pub.lenik@bodz.net>
2014-04-17 18:10:54 +0800

    bas-repr: Added URLResourceVbo.

---
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/io/res/builtin/URLResourceVbo.java
@@ -0,0 +1,57 @@
+package net.bodz.bas.io.res.builtin;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.bodz.bas.c.java.io.FilePath;
+import net.bodz.bas.c.java.io.FileURL;
+import net.bodz.bas.io.IByteOut;
+import net.bodz.bas.io.res.IStreamResource;
+import net.bodz.bas.io.res.tools.StreamReading;
+import net.bodz.bas.potato.ref.IRefEntry;
+import net.bodz.bas.repr.html.AbstractHtmlViewBuilder;
+import net.bodz.bas.repr.html.IHtmlOutputContext;
+import net.bodz.bas.repr.viz.ViewBuilderException;
+import net.bodz.bas.rtx.IOptions;
+import net.bodz.bas.std.rfc.mime.ContentType;
+import net.bodz.bas.std.rfc.mime.ContentTypes;
+
+public class URLResourceVbo
+        extends AbstractHtmlViewBuilder<URLResource> {
+
+    public URLResourceVbo() {
+        super(IStreamResource.class);
+    }
+
+    @Override
+    public IHtmlOutputContext buildHtmlView(IHtmlOutputContext ctx, IRefEntry<URLResource> entry, IOptions options)
+            throws ViewBuilderException, IOException {
+        HttpServletResponse response = ctx.getResponse();
+
+        URLResource resource = entry.get();
+        URL url = resource.getURL();
+
+        String fileName = url.getFile();
+        String extension = FilePath.getExtension(fileName);
+        ContentType contentType = ContentType.forExtension(extension);
+        if (contentType == null)
+            contentType = ContentTypes.application_octet_stream;
+        response.setContentType(contentType.getName());
+
+        Long length = FileURL.length(url, null);
+        if (length == null)
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        if (length <= Integer.MAX_VALUE)
+            response.setContentLength(length.intValue());
+
+        IByteOut byteOut = ctx.getByteOut();
+        for (byte[] block : resource.to(StreamReading.class).blocks())
+            byteOut.write(block);
+        byteOut.flush();
+
+        return ctx;
+    }
+
+}
