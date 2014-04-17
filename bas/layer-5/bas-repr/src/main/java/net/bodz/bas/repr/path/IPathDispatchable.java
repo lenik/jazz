0c357a7d9968ecab9ff4b3e9aa3c704dfb26596f
Lenik <pub.lenik@bodz.net>
2014-04-17 18:13:33 +0800

    bas-repr: Added PathArrival.shift() helper function.

--- bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/IPathDispatchable.java
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/IPathDispatchable.java
@@ -8,7 +8,7 @@ public interface IPathDispatchable {
      * @param previous
      *            Non-null previous arrival info. For the initial dispatching, the previous arrival
      *            info contains the start object to be dispatched from.
-     * @param pathTokens
+     * @param tokens
      *            Tokens to be consumed by the dispatcher. It's the caller's responsibility to check
      *            whether all tokens are processed.
      * @return The arrival info, <code>null</code> if the next path token is unknown to the
@@ -16,7 +16,7 @@ public interface IPathDispatchable {
      * @throws NullPointerException
      *             If either <code>previous</code> or <code>pathTokens</code> is <code>null</code>.
      */
-    IPathArrival dispatch(IPathArrival previous, ITokenQueue pathTokens)
+    IPathArrival dispatch(IPathArrival previous, ITokenQueue tokens)
             throws PathDispatchException;
 
 }
