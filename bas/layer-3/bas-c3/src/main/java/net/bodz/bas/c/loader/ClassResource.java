21436ab0eb02af34168c80ff5e1868fe290489b2
Lenik <pub.lenik@bodz.net>
2014-04-23 08:14:28 +0800

    bas-c3: ClassResource.get* return null if not existed.

--- bas/layer-3/bas-c3/src/main/java/net/bodz/bas/c/loader/ClassResource.java
+++ bas/layer-3/bas-c3/src/main/java/net/bodz/bas/c/loader/ClassResource.java
@@ -1,5 +1,7 @@
 package net.bodz.bas.c.loader;
 
+import java.net.URL;
+
 import net.bodz.bas.io.res.builtin.URLResource;
 
 public class ClassResource
@@ -7,17 +9,28 @@ public class ClassResource
 
     /**
      * The class bytes resource of the <code>clazz</code>.
+     * 
+     * @return <code>null</code> if the resource isn't existed.
      */
     public static URLResource getClassBytes(Class<?> clazz) {
-        return new URLResource(getClassBytesURL(clazz));
+        URL url = getClassBytesURL(clazz);
+        if (url == null)
+            return null;
+        else
+            return new URLResource(url);
     }
 
     /**
      * @param extension
      *            don't include the dot(.)
+     * @return <code>null</code> if the data resource isn't existed.
      */
     public static URLResource getData(Class<?> clazz, String extension) {
-        return new URLResource(getDataURL(clazz, extension));
+        URL url = getDataURL(clazz, extension);
+        if (url == null)
+            return null;
+        else
+            return new URLResource(url);
     }
 
 }
