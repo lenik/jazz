9890da74c272e3d1dfd9fafdc85443bac82e66c6
Lenik <pub.lenik@bodz.net>
2014-04-24 22:00:55 +0800

    bas-html: Generated HTML5 event methods.

--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/io/html/AbstractHtmlTagBuilder.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/io/html/AbstractHtmlTagBuilder.java
@@ -81,4 +81,298 @@ public abstract class AbstractHtmlTagBuilder<self_t extends IXmlTagBuilder>
         return attr("translate", translate);
     }
 
+    // ________________________________________________________________________
+    // ⇱ Part: Event Handlers
+    //
+
+    @Override
+    public self_t onblur(String handler) {
+        return attr("onblur", handler);
+    }
+
+    @Override
+    public self_t oncancel(String handler) {
+        return attr("oncancel", handler);
+    }
+
+    @Override
+    public self_t oncanplay(String handler) {
+        return attr("oncanplay", handler);
+    }
+
+    @Override
+    public self_t oncanplaythrough(String handler) {
+        return attr("oncanplaythrough", handler);
+    }
+
+    @Override
+    public self_t onchange(String handler) {
+        return attr("onchange", handler);
+    }
+
+    @Override
+    public self_t onclick(String handler) {
+        return attr("onclick", handler);
+    }
+
+    @Override
+    public self_t onclose(String handler) {
+        return attr("onclose", handler);
+    }
+
+    @Override
+    public self_t oncuechange(String handler) {
+        return attr("oncuechange", handler);
+    }
+
+    @Override
+    public self_t ondblclick(String handler) {
+        return attr("ondblclick", handler);
+    }
+
+    @Override
+    public self_t ondrag(String handler) {
+        return attr("ondrag", handler);
+    }
+
+    @Override
+    public self_t ondragend(String handler) {
+        return attr("ondragend", handler);
+    }
+
+    @Override
+    public self_t ondragenter(String handler) {
+        return attr("ondragenter", handler);
+    }
+
+    @Override
+    public self_t ondragexit(String handler) {
+        return attr("ondragexit", handler);
+    }
+
+    @Override
+    public self_t ondragleave(String handler) {
+        return attr("ondragleave", handler);
+    }
+
+    @Override
+    public self_t ondragover(String handler) {
+        return attr("ondragover", handler);
+    }
+
+    @Override
+    public self_t ondragstart(String handler) {
+        return attr("ondragstart", handler);
+    }
+
+    @Override
+    public self_t ondrop(String handler) {
+        return attr("ondrop", handler);
+    }
+
+    @Override
+    public self_t ondurationchange(String handler) {
+        return attr("ondurationchange", handler);
+    }
+
+    @Override
+    public self_t onemptied(String handler) {
+        return attr("onemptied", handler);
+    }
+
+    @Override
+    public self_t onended(String handler) {
+        return attr("onended", handler);
+    }
+
+    @Override
+    public self_t onerror(String handler) {
+        return attr("onerror", handler);
+    }
+
+    @Override
+    public self_t onfocus(String handler) {
+        return attr("onfocus", handler);
+    }
+
+    @Override
+    public self_t oninput(String handler) {
+        return attr("oninput", handler);
+    }
+
+    @Override
+    public self_t oninvalid(String handler) {
+        return attr("oninvalid", handler);
+    }
+
+    @Override
+    public self_t onkeydown(String handler) {
+        return attr("onkeydown", handler);
+    }
+
+    @Override
+    public self_t onkeypress(String handler) {
+        return attr("onkeypress", handler);
+    }
+
+    @Override
+    public self_t onkeyup(String handler) {
+        return attr("onkeyup", handler);
+    }
+
+    @Override
+    public self_t onload(String handler) {
+        return attr("onload", handler);
+    }
+
+    @Override
+    public self_t onloadeddata(String handler) {
+        return attr("onloadeddata", handler);
+    }
+
+    @Override
+    public self_t onloadedmetadata(String handler) {
+        return attr("onloadedmetadata", handler);
+    }
+
+    @Override
+    public self_t onloadstart(String handler) {
+        return attr("onloadstart", handler);
+    }
+
+    @Override
+    public self_t onmousedown(String handler) {
+        return attr("onmousedown", handler);
+    }
+
+    @Override
+    public self_t onmouseenter(String handler) {
+        return attr("onmouseenter", handler);
+    }
+
+    @Override
+    public self_t onmouseleave(String handler) {
+        return attr("onmouseleave", handler);
+    }
+
+    @Override
+    public self_t onmousemove(String handler) {
+        return attr("onmousemove", handler);
+    }
+
+    @Override
+    public self_t onmouseout(String handler) {
+        return attr("onmouseout", handler);
+    }
+
+    @Override
+    public self_t onmouseover(String handler) {
+        return attr("onmouseover", handler);
+    }
+
+    @Override
+    public self_t onmouseup(String handler) {
+        return attr("onmouseup", handler);
+    }
+
+    @Override
+    public self_t onmousewheel(String handler) {
+        return attr("onmousewheel", handler);
+    }
+
+    @Override
+    public self_t onpause(String handler) {
+        return attr("onpause", handler);
+    }
+
+    @Override
+    public self_t onplay(String handler) {
+        return attr("onplay", handler);
+    }
+
+    @Override
+    public self_t onplaying(String handler) {
+        return attr("onplaying", handler);
+    }
+
+    @Override
+    public self_t onprogress(String handler) {
+        return attr("onprogress", handler);
+    }
+
+    @Override
+    public self_t onratechange(String handler) {
+        return attr("onratechange", handler);
+    }
+
+    @Override
+    public self_t onreset(String handler) {
+        return attr("onreset", handler);
+    }
+
+    @Override
+    public self_t onresize(String handler) {
+        return attr("onresize", handler);
+    }
+
+    @Override
+    public self_t onscroll(String handler) {
+        return attr("onscroll", handler);
+    }
+
+    @Override
+    public self_t onseeked(String handler) {
+        return attr("onseeked", handler);
+    }
+
+    @Override
+    public self_t onseeking(String handler) {
+        return attr("onseeking", handler);
+    }
+
+    @Override
+    public self_t onselect(String handler) {
+        return attr("onselect", handler);
+    }
+
+    @Override
+    public self_t onshow(String handler) {
+        return attr("onshow", handler);
+    }
+
+    @Override
+    public self_t onstalled(String handler) {
+        return attr("onstalled", handler);
+    }
+
+    @Override
+    public self_t onsubmit(String handler) {
+        return attr("onsubmit", handler);
+    }
+
+    @Override
+    public self_t onsuspend(String handler) {
+        return attr("onsuspend", handler);
+    }
+
+    @Override
+    public self_t ontimeupdate(String handler) {
+        return attr("ontimeupdate", handler);
+    }
+
+    @Override
+    public self_t ontoggle(String handler) {
+        return attr("ontoggle", handler);
+    }
+
+    @Override
+    public self_t onvolumechange(String handler) {
+        return attr("onvolumechange", handler);
+    }
+
+    @Override
+    public self_t onwaiting(String handler) {
+        return attr("onwaiting", handler);
+    }
+
 }
--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/io/html/tag/HtmlScriptBuilder.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/io/html/tag/HtmlScriptBuilder.java
@@ -9,11 +9,23 @@ public class HtmlScriptBuilder
         super(o);
     }
 
+    public HtmlScriptBuilder javascript(String src) {
+        type("text/javascript");
+        text(src);
+        return this;
+    }
+
     public HtmlScriptBuilder javascriptSrc(String href) {
         if (href == null)
             throw new NullPointerException("href");
         type("text/javascript"); // This is also the default.
         src(href);
+        return this;
+    }
+
+    public HtmlScriptBuilder vbscript(String src) {
+        type("text/vbscript");
+        text(src);
         return this;
     }
 
