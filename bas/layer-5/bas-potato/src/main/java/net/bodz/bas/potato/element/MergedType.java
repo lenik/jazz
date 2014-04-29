a65f742c3f020bf80341ae93b75419776f798d6f
Lenik <pub.lenik@bodz.net>
2014-04-29 13:46:29 +0800

    bas-potato: Added MergedType as the default (instead of LinkedType).

---
+++ bas/layer-5/bas-potato/src/main/java/net/bodz/bas/potato/element/MergedType.java
@@ -0,0 +1,113 @@
+package net.bodz.bas.potato.element;
+
+import java.lang.annotation.Annotation;
+import java.util.Arrays;
+import java.util.List;
+
+import net.bodz.mda.xjdoc.model.IJavaElementDoc;
+
+public class MergedType
+        extends AbstractType {
+
+    private final Class<?> clazz;
+
+    private IPropertyMap propertyMap;
+    private IMethodMap methodMap;
+    private IConstructorMap constructorMap;
+    private IEventMap eventMap;
+
+    public MergedType(Class<?> clazz, IType[] types, IJavaElementDoc xjdoc) {
+        this(clazz, Arrays.asList(types), xjdoc);
+    }
+
+    public MergedType(Class<?> clazz, List<IType> types, IJavaElementDoc xjdoc) {
+        super(clazz, clazz.getName());
+        this.clazz = clazz;
+
+        boolean sort = false;
+
+        MutablePropertyMap propertyMap = new MutablePropertyMap(sort);
+        MutableMethodMap methodMap = new MutableMethodMap(sort);
+        MutableConstructorMap constructorMap = new MutableConstructorMap();
+        MutableEventMap eventMap = new MutableEventMap(sort);
+
+        for (IType type : types) {
+            for (IProperty property : type.getProperties())
+                if (!propertyMap.containsProperty(property.getName()))
+                    propertyMap.addProperty(property);
+
+            for (IMethod method : type.getMethods())
+                if (!methodMap.containsMethod(method.getSignature()))
+                    methodMap.addMethod(method);
+
+            for (IConstructor ctor : type.getConstructors())
+                if (!constructorMap.containsConstructor(ctor.getSignature()))
+                    constructorMap.addConstructor(ctor);
+
+            for (IEvent event : type.getEvents())
+                if (!eventMap.containsEvent(event.getName()))
+                    eventMap.addEvent(event);
+        }
+
+        this.propertyMap = propertyMap;
+        this.methodMap = methodMap;
+        this.constructorMap = constructorMap;
+        this.eventMap = eventMap;
+
+        setXjdoc(xjdoc);
+    }
+
+    /** ⇱ Implementation Of {@link IType}. */
+    /* _____________________________ */static section.iface __TYPE__;
+
+    @Override
+    public Class<?> getType() {
+        return clazz;
+    }
+
+    @Override
+    public IPropertyMap getPropertyMap() {
+        return propertyMap;
+    }
+
+    @Override
+    public IMethodMap getMethodMap() {
+        return methodMap;
+    }
+
+    @Override
+    public IConstructorMap getConstructorMap() {
+        return constructorMap;
+    }
+
+    @Override
+    public IEventMap getEventMap() {
+        return eventMap;
+    }
+
+    /** ⇱ Implementaton Of {@link IAnnotated}. */
+    /* _____________________________ */static section.iface __ANNOTATION__;
+
+    // TODO Should merge annotations from all types.
+
+    @Override
+    public boolean isAnnotationPresent(Class<? extends Annotation> annotationClass) {
+        return clazz.isAnnotationPresent(annotationClass);
+    }
+
+    @Override
+    public <A extends Annotation> A getAnnotation(Class<A> annotationClass) {
+        return clazz.getAnnotation(annotationClass);
+    }
+
+    @Override
+    public Annotation[] getAnnotations() {
+        return clazz.getAnnotations();
+    }
+
+    @Override
+    public Annotation[] getDeclaredAnnotations() {
+        return clazz.getDeclaredAnnotations();
+    }
+
+}
