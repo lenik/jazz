d98c98ea5094c070142daa123b39d46188743217
Lenik <pub.lenik@bodz.net>
2014-05-08 23:33:58 +0800

    bas-program: Added ff2tm - Xjdoc2TextMap.

---
+++ bas/layer-7/bas-program/src/main/java/net/bodz/bas/shell/util/Xjdoc2TextMap.java
@@ -0,0 +1,64 @@
+package net.bodz.bas.shell.util;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import net.bodz.bas.i18n.dom.iString;
+import net.bodz.bas.io.IPrintOut;
+import net.bodz.bas.program.meta.ProgramName;
+import net.bodz.bas.program.skel.BatchEditCLI;
+import net.bodz.bas.program.skel.FileHandler;
+import net.bodz.bas.text.textmap.TextMapFormat;
+import net.bodz.mda.xjdoc.FlatfDocLoader;
+import net.bodz.mda.xjdoc.model.IElementDoc;
+
+@ProgramName("ff2tm")
+public class Xjdoc2TextMap
+        extends BatchEditCLI {
+
+    FlatfDocLoader flatfDocLoader = new FlatfDocLoader();
+    TextMapFormat textMapFormat = new TextMapFormat();
+
+    public Xjdoc2TextMap() {
+        textMapFormat.keyStartPattern = "@";
+        textMapFormat.keyEndPattern = "";
+        textMapFormat.indented = true;
+    }
+
+    @Override
+    public void processFile(FileHandler handler)
+            throws Exception {
+        Map<String, iString> map = new LinkedHashMap<>();
+
+        IElementDoc doc = flatfDocLoader.load(handler.getInputSource());
+        for (Entry<String, Object> entry : doc.getTagMap().entrySet()) {
+            String key = entry.getKey();
+            Object value = entry.getValue();
+            map.put(key, iString.fn.val(value));
+        }
+
+        iString text = doc.getText();
+        map.put("text", text);
+
+        String keyStart = textMapFormat.keyStartPattern;
+        String keyEnd = textMapFormat.keyEndPattern;
+
+        IPrintOut out = handler.getOutputTarget().newPrintOut();
+        for (Entry<String, iString> entry : map.entrySet()) {
+            out.println(keyStart + entry.getKey() + keyEnd);
+            iString istr = entry.getValue();
+            String plstr = istr.toParaLangString("\n    ");
+            out.println("    " + plstr);
+        }
+        out.close();
+
+        handler.save();
+    }
+
+    public static void main(String[] args)
+            throws Exception {
+        new Xjdoc2TextMap().execute(args);
+    }
+
+}
