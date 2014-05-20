78c7d6aadb2da5a2c552de39aa7d388d2ef4973c
Lenik <pub.lenik@bodz.net>
2014-05-20 17:14:22 +0800

    bas-site: Implement IContent/IPathDispatchable on BasicSite.
    - With support of sitemap.xml.

--- bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/BasicSite.java
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/BasicSite.java
@@ -1,8 +1,51 @@
 package net.bodz.bas.site;
 
-import net.bodz.mda.xjdoc.model.javadoc.XjdocObject;
+import javax.servlet.http.HttpServletRequest;
 
-public class BasicSite
-        extends XjdocObject {
+import net.bodz.bas.http.ctx.CurrentHttpService;
+import net.bodz.bas.repr.content.AbstractXjdocContent;
+import net.bodz.bas.repr.path.IPathArrival;
+import net.bodz.bas.repr.path.IPathDispatchable;
+import net.bodz.bas.repr.path.ITokenQueue;
+import net.bodz.bas.repr.path.PathArrival;
+import net.bodz.bas.repr.path.PathDispatchException;
+import net.bodz.bas.site.org.ICrawlable;
+import net.bodz.bas.site.org.Sitemap;
+import net.bodz.bas.site.org.SitemapGenerator;
+
+public abstract class BasicSite
+        extends AbstractXjdocContent
+        implements IPathDispatchable, ICrawlable {
+
+    public abstract String getHomeUrl();
+
+    public Sitemap getSitemap() {
+        return new SitemapGenerator(getHomeUrl()).traverse(this);
+    }
+
+    /** ⇱ Implementation Of {@link IPathDispatchable}. */
+    /* _____________________________ */static section.iface __PATH_DISP__;
+
+    @Override
+    public synchronized IPathArrival dispatch(IPathArrival previous, ITokenQueue tokens)
+            throws PathDispatchException {
+        String token = tokens.peek();
+        switch (token) {
+        case "sitemap.xml":
+            return PathArrival.shift(previous, getSitemap(), tokens);
+
+        case "robots.txt":
+            break;
+
+        case "intl":
+            String lang = tokens.peek(1);
+            if (lang != null) {
+                HttpServletRequest request = CurrentHttpService.getRequest();
+                request.setAttribute("LOCALE", lang);
+            }
+            break;
+        }
+        return null;
+    }
 
 }
