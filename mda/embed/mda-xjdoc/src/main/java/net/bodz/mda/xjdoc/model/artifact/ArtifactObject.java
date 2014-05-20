97fbcef0613cf0d2f24489596281c0fec9f9a5b3
Lenik <pub.lenik@bodz.net>
2014-05-21 00:48:31 +0800

    mda-xjdoc: Made XjdocObject.loadXjdoc() overridable.

--- mda/embed/mda-xjdoc/src/main/java/net/bodz/mda/xjdoc/model/artifact/ArtifactObject.java
+++ mda/embed/mda-xjdoc/src/main/java/net/bodz/mda/xjdoc/model/artifact/ArtifactObject.java
@@ -1,5 +1,9 @@
 package net.bodz.mda.xjdoc.model.artifact;
 
+import java.io.IOException;
+
+import net.bodz.bas.err.ParseException;
+import net.bodz.mda.xjdoc.ClassDocLoadException;
 import net.bodz.mda.xjdoc.model.IElementDoc;
 import net.bodz.mda.xjdoc.model.javadoc.XjdocObject;
 
@@ -15,7 +19,8 @@ public abstract class ArtifactObject
     }
 
     @Override
-    public ArtifactDoc loadXjdoc() {
+    protected ArtifactDoc loadXjdoc()
+            throws ClassDocLoadException, ParseException, IOException {
         IElementDoc xjdoc = super.loadXjdoc();
         // xjdoc.as(ArtifactDoc.class);
         ArtifactDoc artifactDoc = new ArtifactDoc(xjdoc);
