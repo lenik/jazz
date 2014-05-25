30f0dbeecfffdfba6515e4273e5f50fd98d594d0
Lenik <pub.lenik@bodz.net>
2014-05-25 21:21:49 +0800

    bas-repr: IPathArrival: added previous(target), consumedFullPath, toList.

--- bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/IPathArrival.java
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/IPathArrival.java
@@ -1,52 +1,60 @@
 package net.bodz.bas.repr.path;
 
+import java.util.List;
+
 import net.bodz.bas.meta.decl.Shortcut;
 
 public interface IPathArrival {
 
     /**
      * Get the previous arrival node.
-     *
+     * 
      * @return Previous arrival node, <code>null</code> if none.
      */
     IPathArrival getPrevious();
 
     IPathArrival getPrevious(int n);
 
+    IPathArrival getPrevious(Object target);
+
     /**
      * The partial path tokens consumed by this context.
-     *
+     * 
      * @return Non-empty string array contains of the consumed path tokens.
      */
     String[] getConsumedTokens();
 
     /**
      * The partial path consumed by this context.
-     *
+     * 
      * @return Non-<code>null</code> consumed path.
      */
     String getConsumedPath();
 
+    String getConsumedFullPath();
+
     /**
      * Get the remaining path.
-     *
+     * 
      * @return <code>null</code> if this is the final arrival.
      */
     String getRemainingPath();
 
     /**
      * Get the target object.
-     *
+     * 
      * @return The dispatched object, only the final target can be <code>null</code>.
      */
     Object getTarget();
 
     /**
      * Get the last non-<code>null</code> target in the arrival chain.
-     *
+     * 
      * @return <code>null</code> if none.
      */
     @Shortcut
     Object getLastNonNullTarget();
+
+    List<IPathArrival> toList();
 
 }
