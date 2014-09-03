59352ee8a27c6922c141886fad04077237d0c113
Lenik <pub.lenik@bodz.net>
2014-09-03 11:09:51 +0800

    bas-site: Renamed contentInfo to content.

--- bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/org/ICrawler.java
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/org/ICrawler.java
@@ -4,6 +4,6 @@ import net.bodz.bas.repr.content.IContent;
 
 public interface ICrawler {
 
-    void follow(String subpath, IContent contentInfo);
+    void follow(String subpath, IContent content);
 
 }
