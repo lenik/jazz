0590f6f51f287ff1135745b5de9b8eceda7ee34d
Lenik <pub.lenik@bodz.net>
2014-09-03 10:57:03 +0800

    bas-http: Added ResourceTransferer.transfer(..., filename) for content-disposition.

--- bas/layer-3/bas-http/src/main/java/net/bodz/bas/http/ResourceTransferer.java
+++ bas/layer-3/bas-http/src/main/java/net/bodz/bas/http/ResourceTransferer.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.bodz.bas.c.java.io.FileURL;
 import net.bodz.bas.io.res.builtin.URLResource;
+import net.bodz.bas.std.rfc.http.ContentDisposition;
 import net.bodz.bas.std.rfc.http.ContentRange;
 import net.bodz.bas.std.rfc.mime.ContentType;
 
@@ -34,8 +35,13 @@ public class ResourceTransferer {
 
     public void transfer(URLResource resource)
             throws IOException {
+        transfer(resource, null, null);
+    }
+
+    public void transfer(URLResource resource, String filename, String description)
+            throws IOException {
         URL url = resource.getURL();
-        String path = url.getFile();
+        String path = url.getPath();
         // if (!resource.canRead()) {
         // resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Not readable.");
         // return;
@@ -50,10 +56,35 @@ public class ResourceTransferer {
                 resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
                 return;
             }
+
+            /**
+             * @see RFC 2616 14.29
+             *      <p>
+             *      HTTP/1.1 servers SHOULD send Last-Modified whenever feasible
+             */
             resp.setDateHeader("Last-Modified", lastModified);
+
+            /**
+             * @see RFC 2616 14.21
+             *      <p>
+             *      To mark a response as "never expires," an origin server sends an Expires date
+             *      approximately one year from the time the response is sent. HTTP/1.1 servers
+             *      SHOULD NOT send Expires dates more than one year in the future.
+             */
             resp.setDateHeader("Expires", lastModified + maxAge * 1000L);
         }
-        // resp.setHeader("E-Tag", eTag);
+
+        /**
+         * @see RFC 2616 13.3.2
+         *      <p>
+         *      The ETag response-header field value, an entity tag, provides for an "opaque" cache
+         *      validator. This might allow more reliable validation in situations where it is
+         *      inconvenient to store modification dates, where the one-second resolution of HTTP
+         *      date values is not sufficient, or where the origin server wishes to avoid certain
+         *      paradoxes that might arise from the use of modification dates.
+         */
+        String eTag = null;
+        // if (eTag != null) resp.setHeader("E-Tag", eTag);
 
         ContentType contentType = ContentType.forPath(path);
         if (contentType != null)
@@ -65,6 +96,21 @@ public class ResourceTransferer {
                 resp.setContentLength(length.intValue());
             else
                 resp.setHeader("Content-Length", String.valueOf(length));
+
+        /**
+         * @see RFC 2183 2.10
+         */
+        if (filename != null) {
+            // String filename = FilePath.getBaseName(path);
+            String contentDisposition = ContentDisposition.format(filename, true, false);
+
+            if (contentDisposition != null) {
+                resp.setHeader("Content-Disposition", contentDisposition);
+
+                if (description != null)
+                    resp.setHeader("Content-Description", description);
+            }
+        }
 
         ContentRange contentRange = null;
         String contentRangeHeader = req.getHeader("Content-Range");
