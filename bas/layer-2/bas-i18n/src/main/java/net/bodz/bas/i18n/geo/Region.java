f71e39317e0b8e026a4f9e1d3b6590cac90dfa3b
Lenik <pub.lenik@bodz.net>
2014-09-03 10:53:26 +0800

    bas-i18n: Added ..geo.Region, and china-2013 region db.

---
+++ bas/layer-2/bas-i18n/src/main/java/net/bodz/bas/i18n/geo/Region.java
@@ -0,0 +1,81 @@
+package net.bodz.bas.i18n.geo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Region {
+
+    private final String code;
+    private final String localeName;
+
+    private String enName;
+    private String zhName;
+
+    private final Region parent;
+    private final List<Region> children = new ArrayList<Region>();;
+
+    public Region(String code, String localeName, Region parent) {
+        this.code = code;
+        this.localeName = localeName;
+        this.parent = parent;
+        if (parent != null)
+            parent.children.add(this);
+    }
+
+    public List<Region> bottomUp() {
+        List<Region> list = new ArrayList<>();
+        Region node = this;
+        while (node != null) {
+            list.add(node);
+            node = node.getParent();
+        }
+        return list;
+    }
+
+    public List<Region> topDown() {
+        List<Region> list = bottomUp();
+        Collections.reverse(list);
+        return list;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String joinId() {
+        StringBuilder sb = new StringBuilder(16);
+        for (Region r : topDown())
+            sb.append(r.code);
+        return sb.toString();
+    }
+
+    public String getLocaleName() {
+        return localeName;
+    }
+
+    public String getEnName() {
+        return enName;
+    }
+
+    public void setEnName(String enName) {
+        this.enName = enName;
+    }
+
+    public String getZhName() {
+        return zhName;
+    }
+
+    public void setZhName(String zhName) {
+        this.zhName = zhName;
+    }
+
+    public Region getParent() {
+        return parent;
+    }
+
+    public List<Region> getChildren() {
+        return children;
+    }
+
+}
---
+++ bas/layer-2/bas-i18n/src/main/java/net/bodz/bas/i18n/geo/Regions.java
@@ -0,0 +1,98 @@
+package net.bodz.bas.i18n.geo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.bodz.bas.c.java.nio.Charsets;
+import net.bodz.bas.err.ParseException;
+
+public class Regions {
+
+    private static final Region china = new Region("", "中国", null);
+    private static final Map<String, Region> chinaIndex = new HashMap<String, Region>();
+
+    public static Region getChina() {
+        return china;
+    }
+
+    public static Region getChinaRegion(String joinId) {
+        return chinaIndex.get(joinId);
+    }
+
+    static {
+        try {
+            loadChinaRegions();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private static void loadChinaRegions()
+            throws IOException, ParseException {
+        URL csv = Regions.class.getResource("china-2013.csv");
+        InputStream in = csv.openStream();
+
+        try {
+            Reader reader = new InputStreamReader(in, Charsets.UTF8);
+            BufferedReader br = new BufferedReader(reader);
+            String line;
+            int lineNo = 1;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (line.isEmpty())
+                    continue;
+                if (line.startsWith("#"))
+                    continue;
+
+                int colon = line.indexOf(':');
+                if (colon == -1)
+                    throw new ParseException("Expected colon(:) at line " + lineNo);
+                lineNo++;
+
+                String id = line.substring(0, colon);
+                String zhName = line.substring(colon + 1);
+                Region parent;
+                String code;
+
+                // AABBCCDDD
+                switch (id.length()) {
+                case 2:
+                    parent = china;
+                    code = id;
+                    break;
+
+                case 4:
+                    parent = chinaIndex.get(id.substring(0, 2));
+                    code = id.substring(2);
+                    break;
+
+                case 6:
+                    parent = chinaIndex.get(id.substring(0, 4));
+                    code = id.substring(4);
+                    break;
+
+                case 9:
+                    parent = chinaIndex.get(id.substring(0, 6));
+                    code = id.substring(6);
+                    break;
+
+                default:
+                    throw new ParseException("Bad ID: " + id);
+                }
+
+                Region region = new Region(code, zhName, parent);
+                region.setZhName(zhName);
+                chinaIndex.put(id, region);
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+}
