b6378080abcc0c8378aeb60570c8587b06867b11
Lenik <pub.lenik@bodz.net>
2014-12-07 23:00:03 +0800

    bas-potato: Added simple interface IPropertyAccessor.

--- bas/layer-5/bas-potato/src/main/java/net/bodz/bas/potato/element/IProperty.java
+++ bas/layer-5/bas-potato/src/main/java/net/bodz/bas/potato/element/IProperty.java
@@ -5,7 +5,7 @@ import java.beans.PropertyDescriptor;
 import net.bodz.bas.t.event.IPropertyChangeListener;
 
 public interface IProperty
-        extends IPotatoElement {
+        extends IPotatoElement, IPropertyAccessor {
 
     /**
      * The property type.
@@ -17,30 +17,6 @@ public interface IProperty
     boolean isReadable();
 
     boolean isWritable();
-
-    /**
-     * Get the property value from a particular instance.
-     * 
-     * @throws NullPointerException
-     *             If <code>instance</code> is <code>null</code> and the property is not static.
-     * @throws ClassCastException
-     *             If <code>instance</code> isn't of the declaring potato type
-     */
-    Object getValue(Object instance)
-            throws ReflectiveOperationException;
-
-    /**
-     * Set the property value to a particular instance.
-     * 
-     * @throws NullPointerException
-     *             If <code>instance</code> is <code>null</code> and the property is not static.
-     * @throws ClassCastException
-     *             If <code>instance</code> isn't of the declaring potato type
-     * @throws IllegalArgumentException
-     *             If <code>value</code> isn't instance of type of this property.
-     */
-    void setValue(Object instance, Object value)
-            throws ReflectiveOperationException;
 
     /**
      * A bound property can cause property change event.
