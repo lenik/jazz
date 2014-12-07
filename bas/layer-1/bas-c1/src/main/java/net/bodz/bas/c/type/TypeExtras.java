c488a3df33183ae86e20ab75bbc1269fde88330f
Lenik <pub.lenik@bodz.net>
2014-12-07 21:30:11 +0800

    bas-c1: Added TypeExtras for feature/attributes registry.

---
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/type/TypeExtras.java
@@ -0,0 +1,70 @@
+package net.bodz.bas.c.type;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class TypeExtras {
+
+    private final Class<?> type;
+    private Map<String, Object> attributes = new HashMap<>();
+    private Map<Class<?>, Object> features = new HashMap<>();
+
+    public TypeExtras(Class<?> type) {
+        if (type == null)
+            throw new NullPointerException("type");
+        this.type = type;
+    }
+
+    public Class<?> getType() {
+        return type;
+    }
+
+    public Map<String, Object> getAttributeMap() {
+        return attributes;
+    }
+
+    public Set<String> getAttributeNames() {
+        return attributes.keySet();
+    }
+
+    public <T> T getAttribute(String name) {
+        return (T) attributes.get(name);
+    }
+
+    public void setAttribute(String name, Object value) {
+        attributes.put(name, value);
+    }
+
+    public void removeAttribute(String name) {
+        attributes.remove(name);
+    }
+
+    public <T> T getFeature(Class<T> featureClass) {
+        Object value = features.get(featureClass);
+        return featureClass.cast(value);
+    }
+
+    public <T> void setFeature(Class<T> featureClass, T implementation) {
+        if (featureClass == null)
+            throw new NullPointerException("featureClass");
+        if (implementation == null)
+            throw new NullPointerException("implementation");
+        features.put(featureClass, implementation);
+    }
+
+    public void removeFeature(Class<?> featureClass) {
+        features.remove(featureClass);
+    }
+
+    static final LazyTypeMap<TypeExtras> clsExtras;
+    static {
+        clsExtras = TypeMapRegistry.createMap(TypeExtras.class);
+    }
+
+    public static TypeExtras of(Class<?> clazz) {
+        TypeExtras extras = clsExtras.getOrLoad(clazz);
+        return extras;
+    }
+
+}
