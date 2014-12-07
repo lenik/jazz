28c2c41c056023c612328f2d4f9107ea4b459be3
Lenik <pub.lenik@bodz.net>
2014-12-07 21:53:06 +0800

    bas-html: Added Html-Tag: Br.void, Hr.void, Table.width, Table.data*, Th.data*.

--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/dom/tag/HtmlTableTag.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/dom/tag/HtmlTableTag.java
@@ -9,6 +9,28 @@ public class HtmlTableTag
         super(parent, tagName);
     }
 
+    public HtmlTableTag dataToggle(String val) {
+        attr("data-toggle", val);
+        return this;
+    }
+
+    /**
+     * Bootstrap: Example
+     * 
+     * <pre>
+     * &lt;table data-url="data.json" data-toggle="table" &gt;
+     *     &lt;thead&gt;
+     *      &lt;tr&gt;
+     *          &lt;th data-field="firstname" ...&gt;
+     *      &lt;/tr&gt;
+     *     &lt;/thead&gt;
+     * &lt;/table&gt;
+     */
+    public HtmlTableTag dataUrl(String val) {
+        attr("data-url", val);
+        return this;
+    }
+
     public HtmlTableTag width(String val) {
         attr("width", val);
         return this;
--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/dom/tag/HtmlThTag.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/dom/tag/HtmlThTag.java
@@ -9,4 +9,50 @@ public class HtmlThTag
         super(parent, tagName);
     }
 
+    /**
+     * Bootstrap: Example
+     * 
+     * <pre>
+     * &lt;table data-url="data.json" data-toggle="table" &gt;
+     *     &lt;thead&gt;
+     *      &lt;tr&gt;
+     *          &lt;th data-field="firstname" ...&gt;
+     *      &lt;/tr&gt;
+     *     &lt;/thead&gt;
+     * &lt;/table&gt;
+     */
+    public HtmlThTag dataField(String val) {
+        if (val == null)
+            throw new NullPointerException("val");
+        attr("data-field", val);
+        return this;
+    }
+
+    public HtmlThTag dataAlign(String val) {
+        if (val == null)
+            throw new NullPointerException("val");
+        attr("data-align", val);
+        return this;
+    }
+
+    /** header align */
+    public HtmlThTag dataHalign(String val) {
+        if (val == null)
+            throw new NullPointerException("val");
+        attr("data-halign", val);
+        return this;
+    }
+
+    public HtmlThTag dataHeight(String val) {
+        if (val == null)
+            throw new NullPointerException("val");
+        attr("data-height", val);
+        return this;
+    }
+
+    public HtmlThTag dataSortable(boolean val) {
+        attr("data-sortable", val);
+        return this;
+    }
+
 }
