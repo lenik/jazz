4e1da06f73a2005a4e98577785423e4422b36683
Lenik <pub.lenik@bodz.net>
2014-12-08 11:22:13 +0800

    bas-ui: Refactor UiObject to be XjdocObject based.

--- bas/layer-5/bas-ui/src/main/java/net/bodz/bas/ui/dom1/UiObject.java
+++ bas/layer-5/bas-ui/src/main/java/net/bodz/bas/ui/dom1/UiObject.java
@@ -1,8 +1,27 @@
 package net.bodz.bas.ui.dom1;
 
-import net.bodz.mda.xjdoc.model.javadoc.AbstractXjdocElement;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.ui.style.IUiElementStyleDeclaration;
+import net.bodz.bas.ui.style.MutableUiStyleDeclaration;
+import net.bodz.mda.xjdoc.model.IElementDoc;
+import net.bodz.mda.xjdoc.model.javadoc.XjdocObject;
 
 public class UiObject
-        extends AbstractXjdocElement {
+        extends XjdocObject
+        implements IUiElement {
+
+    @Override
+    public IUiElementStyleDeclaration getStyle() {
+        IElementDoc xjdoc = getXjdoc();
+        String styleCss = (String) xjdoc.getTag("style");
+
+        MutableUiStyleDeclaration decl = new MutableUiStyleDeclaration();
+        try {
+            decl.parseCss(styleCss);
+        } catch (ParseException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        return decl;
+    }
 
 }
