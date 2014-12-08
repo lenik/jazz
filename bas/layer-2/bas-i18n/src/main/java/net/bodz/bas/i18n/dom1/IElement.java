e916bbb3ddebda373dff83a9fe0afcfa999d893c
Lenik <pub.lenik@bodz.net>
2014-12-08 11:48:15 +0800

    bas-i18n: Added IElement.fn.labelName(), .description().

--- bas/layer-2/bas-i18n/src/main/java/net/bodz/bas/i18n/dom1/IElement.java
+++ bas/layer-2/bas-i18n/src/main/java/net/bodz/bas/i18n/dom1/IElement.java
@@ -12,14 +12,14 @@ public interface IElement {
 
     /**
      * The unique name of the potato element.
-     *
+     * 
      * @return non-<code>null</code> name.
      */
     String getName();
 
     /**
      * The human readable display name.
-     *
+     * 
      * @return non-<code>null</code> string of display name.
      */
     iString getLabel();
@@ -38,7 +38,7 @@ public interface IElement {
      * The detail level controls whether this element is displayed. When user specifies a display
      * detail level, then only elements with detail level smaller then the the display detail level
      * is displayed.
-     *
+     * 
      * @see DetailLevel#CRITICAL
      * @see DetailLevel#NORMAL
      * @see DetailLevel#EXTEND
@@ -51,10 +51,54 @@ public interface IElement {
     /**
      * Returns the Java language modifiers for this element, as an integer. The {@link Modifier}
      * class should be used to decode the modifiers in the integer.
-     *
+     * 
      * @return the Java language modifiers for the underlying element.
      * @see java.lang.reflect.Modifier
      */
     int getModifiers();
+
+    class fn {
+
+        public static final String labelName(IElement element) {
+            return labelName(element, false);
+        }
+
+        public static final String labelName(IElement element, boolean allowEmpty) {
+            if (element == null)
+                return null;
+
+            iString label = element.getLabel();
+            if (label != null) {
+                String labelStr = label.toString();
+                if (labelStr != null)
+                    if (allowEmpty || !labelStr.isEmpty())
+                        return labelStr;
+            }
+
+            return element.getName();
+        }
+
+        public static final String description(IElement element) {
+            return description(element, null);
+        }
+
+        /**
+         * @return <code>null</code> if the element is <code>null</code>.
+         */
+        public static final String description(IElement element, String nullStr) {
+            if (element == null)
+                return null;
+
+            iString description = element.getDescription();
+            if (description != null) {
+                String descriptionStr = description.toString();
+                if (descriptionStr != null)
+                    return descriptionStr;
+            }
+
+            return nullStr;
+        }
+
+    }
 
 }
