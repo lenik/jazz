70ce2fd5dfeca57a4c80d65e0742203497a34730
Lenik <pub.lenik@bodz.net>
2014-12-08 22:00:50 +0800

    mda-xjdoc: Added IXjdocAware.fn.injectFields(...).

--- mda/embed/mda-xjdoc/src/main/java/net/bodz/mda/xjdoc/model/javadoc/IXjdocAware.java
+++ mda/embed/mda-xjdoc/src/main/java/net/bodz/mda/xjdoc/model/javadoc/IXjdocAware.java
@@ -1,6 +1,14 @@
 package net.bodz.mda.xjdoc.model.javadoc;
 
-import net.bodz.bas.meta.decl.Internal;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map.Entry;
+
+import net.bodz.bas.meta.bean.Internal;
+import net.bodz.mda.xjdoc.IXjdocProvider;
+import net.bodz.mda.xjdoc.Xjdocs;
+import net.bodz.mda.xjdoc.model.ClassDoc;
+import net.bodz.mda.xjdoc.model.FieldDoc;
 import net.bodz.mda.xjdoc.model.IElementDoc;
 
 public interface IXjdocAware {
@@ -9,5 +17,50 @@ public interface IXjdocAware {
     IElementDoc getXjdoc();
 
     void setXjdoc(IElementDoc xjdoc);
+
+    class fn {
+
+        public static String injectFields(Class<?> clazz, boolean includeNonPublic) {
+            return injectFields(Xjdocs.getDefaultProvider(), clazz, includeNonPublic);
+        }
+
+        public static String injectFields(IXjdocProvider xjdocLoader, Class<?> clazz, boolean includeNonPublic) {
+            int modifiers = clazz.getModifiers();
+            if (!(includeNonPublic || Modifier.isPublic(modifiers)))
+                return "Non-public: " + clazz;
+
+            ClassDoc classDoc = xjdocLoader.getClassDoc(clazz);
+
+            for (Entry<String, FieldDoc> entry : classDoc.getFieldDocs().entrySet()) {
+                String fieldName = entry.getKey();
+                FieldDoc fieldDoc = entry.getValue();
+
+                Field field;
+                try {
+                    field = clazz.getField(fieldName);
+                } catch (NoSuchFieldException e) {
+                    return "No such field: " + fieldName;
+                }
+
+                modifiers = field.getModifiers();
+                if (!Modifier.isStatic(modifiers))
+                    return "Non-static: " + field;
+
+                if (!(includeNonPublic || Modifier.isPublic(modifiers)))
+                    return "Non-public: " + field;
+
+                Class<?> fieldType = field.getType();
+                if (IXjdocAware.class.isAssignableFrom(fieldType))
+                    try {
+                        IXjdocAware xjdocAware = (IXjdocAware) field.get(null);
+                        xjdocAware.setXjdoc(fieldDoc);
+                    } catch (IllegalAccessException e) {
+                        return "Can't access field: " + e.getMessage();
+                    }
+            } // for fieldDocs
+            return null;
+        }
+
+    }
 
 }
