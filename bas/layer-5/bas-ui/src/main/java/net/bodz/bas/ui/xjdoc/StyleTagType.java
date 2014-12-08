70677afa7aa97399d7f21f410544ea102e1268f0
Lenik <pub.lenik@bodz.net>
2014-12-08 11:22:51 +0800

    bas-ui: Changed tag type of @style to TextMap<String>.

--- bas/layer-5/bas-ui/src/main/java/net/bodz/bas/ui/xjdoc/StyleTagType.java
+++ bas/layer-5/bas-ui/src/main/java/net/bodz/bas/ui/xjdoc/StyleTagType.java
@@ -1,20 +1,26 @@
 package net.bodz.bas.ui.xjdoc;
 
+import net.bodz.bas.c.java.util.TextMap;
 import net.bodz.bas.err.NotImplementedException;
 import net.bodz.bas.err.ParseException;
 import net.bodz.mda.xjdoc.tagtype.AbstractScalarTagType;
 
 public class StyleTagType
-        extends AbstractScalarTagType<String> {
+        extends AbstractScalarTagType<TextMap<String>> {
 
     @Override
-    protected String parse(String s)
+    public Class<?> getValueType() {
+        return TextMap.class;
+    }
+
+    @Override
+    protected TextMap<String> parse(String s)
             throws ParseException {
         throw new NotImplementedException();
     }
 
     @Override
-    protected String format(String value) {
+    protected String format(TextMap<String> value) {
         throw new NotImplementedException();
     }
 
