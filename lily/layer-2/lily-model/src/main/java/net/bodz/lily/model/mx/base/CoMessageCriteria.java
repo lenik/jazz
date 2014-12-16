9beec0036fa4d4c6667075968fa9891d81b1a8db
Lenik <pub.lenik@bodz.net>
2014-12-16 19:02:07 +0800

    lily-model: Renamed favorites to likerCount.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/mx/base/CoMessageCriteria.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/mx/base/CoMessageCriteria.java
@@ -7,7 +7,31 @@ import net.bodz.lily.model.base.CoEntityCriteria;
 public class CoMessageCriteria
         extends CoEntityCriteria {
 
-    IntRange votesRange;
-    IntRange favoritesRange;
+    IntRange voteCountRange;
+
+    IntRange likerCountRange;
+
+    /**
+     * Vote Count Range
+     */
+    public IntRange getVoteCountRange() {
+        return voteCountRange;
+    }
+
+    public void setVoteCountRange(IntRange voteCountRange) {
+        this.voteCountRange = voteCountRange;
+    }
+
+    /**
+     * Liker Count Range
+     */
+
+    public IntRange getLikerCountRange() {
+        return likerCountRange;
+    }
+
+    public void setLikerCountRange(IntRange likerCountRange) {
+        this.likerCountRange = likerCountRange;
+    }
 
 }
