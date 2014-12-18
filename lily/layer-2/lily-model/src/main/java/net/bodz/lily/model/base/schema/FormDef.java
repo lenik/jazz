93d3834a9140978eaf598859750e43959f15f370
Lenik <pub.lenik@bodz.net>
2014-12-18 15:21:45 +0800

    lily-model: Added AbstractDefinitionVbo.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/schema/FormDef.java
@@ -0,0 +1,8 @@
+package net.bodz.lily.model.base.schema;
+
+public class FormDef
+        extends AbstractDefinition<FormDef> {
+
+    private static final long serialVersionUID = 1L;
+
+}
--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/schema/impl/AttributeDefVbo.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/schema/impl/AttributeDefVbo.java
@@ -2,19 +2,16 @@ package net.bodz.lily.model.base.schema.impl;
 
 import java.io.IOException;
 
-import net.bodz.bas.html.AbstractHtmlViewBuilder;
 import net.bodz.bas.html.IHtmlViewContext;
 import net.bodz.bas.repr.viz.ViewBuilderException;
 import net.bodz.bas.rtx.IOptions;
-import net.bodz.bas.site.IBasicSiteAnchors;
 import net.bodz.bas.ui.dom1.IUiRef;
 
 import net.bodz.lily.model.base.schema.AttributeDef;
 
 public class AttributeDefVbo
-        extends AbstractHtmlViewBuilder<AttributeDef>
-        implements IBasicSiteAnchors {
-    
+        extends AbstractDefinitionVbo<AttributeDef> {
+
     public AttributeDefVbo() {
         super(AttributeDef.class);
     }
