d49434014b0c9db3eeb1668b93cc5dee078e0329
Lenik <pub.lenik@bodz.net>
2014-12-29 18:59:17 +0800

    bas-html: PathDispatchServlet set queryContext in the ctx.

--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/AbstractHtmlViewContext.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/AbstractHtmlViewContext.java
@@ -3,10 +3,21 @@ package net.bodz.bas.html;
 import javax.servlet.http.HttpServletRequest;
 
 import net.bodz.bas.rtx.AbstractQueryable;
+import net.bodz.bas.rtx.IQueryable;
 
 public abstract class AbstractHtmlViewContext
         extends AbstractQueryable
         implements IHtmlViewContext {
+
+    private IQueryable queryContext;
+
+    public IQueryable getQueryContext() {
+        return queryContext;
+    }
+
+    public void setQueryContext(IQueryable queryContext) {
+        this.queryContext = queryContext;
+    }
 
     @Override
     public <spec_t> spec_t query(Class<spec_t> specificationClass) {
@@ -15,6 +26,12 @@ public abstract class AbstractHtmlViewContext
         Object requestAttribute = request.getAttribute(specificationClass.getName());
         if (requestAttribute != null)
             return specificationClass.cast(requestAttribute);
+
+        if (queryContext != null) {
+            spec_t impl = queryContext.query(specificationClass);
+            if (impl != null)
+                return impl;
+        }
 
         return super.query(specificationClass);
     }
@@ -26,6 +43,12 @@ public abstract class AbstractHtmlViewContext
         Object requestAttribute = request.getAttribute(specificationId);
         if (requestAttribute != null)
             return requestAttribute;
+
+        if (queryContext != null) {
+            Object impl = queryContext.query(specificationId);
+            if (impl != null)
+                return impl;
+        }
 
         return super.query(specificationId);
     }
