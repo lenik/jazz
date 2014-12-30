0705cc2c8d69130d030df32cdd453ad98523335c
Lenik <pub.lenik@bodz.net>
2014-12-30 16:27:14 +0800

    bas-html: Added HtmlThTag.data-* attributes. (datatables)

--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/dom/tag/HtmlThTag.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/dom/tag/HtmlThTag.java
@@ -10,7 +10,27 @@ public class HtmlThTag
     }
 
     /**
-     * Bootstrap: Example
+     * True to show a radio. The radio column has fixed width.
+     * 
+     * @default false
+     */
+    public HtmlThTag dataRadio(String val) {
+        attr("data-radio", val);
+        return this;
+    }
+
+    /**
+     * True to show a checkbox. The checkbox column has fixed width.
+     * 
+     * @default false
+     */
+    public HtmlThTag dataCheckbox(String val) {
+        attr("data-checkbox", val);
+        return this;
+    }
+
+    /**
+     * The column field name.
      * 
      * <pre>
      * &lt;table data-url="data.json" data-toggle="table" &gt;
@@ -22,36 +42,163 @@ public class HtmlThTag
      * &lt;/table&gt;
      */
     public HtmlThTag dataField(String val) {
-        if (val == null)
-            throw new NullPointerException("val");
         attr("data-field", val);
         return this;
     }
 
+    /**
+     * The column title text.
+     */
+    public HtmlThTag dataTitle(String val) {
+        attr("data-title", val);
+        return this;
+    }
+
+    /**
+     * The column class name.
+     */
+    public HtmlThTag dataClass(String val) {
+        attr("data-class", val);
+        return this;
+    }
+
+    /**
+     * Indicate how to align the column data. "left", "right", "center" can be used.
+     */
     public HtmlThTag dataAlign(String val) {
-        if (val == null)
-            throw new NullPointerException("val");
         attr("data-align", val);
         return this;
     }
 
-    /** header align */
+    /**
+     * Indicate how to align the table header. "left", "right", "center" can be used.
+     */
     public HtmlThTag dataHalign(String val) {
-        if (val == null)
-            throw new NullPointerException("val");
         attr("data-halign", val);
         return this;
     }
 
-    public HtmlThTag dataHeight(String val) {
-        if (val == null)
-            throw new NullPointerException("val");
-        attr("data-height", val);
+    /**
+     * Indicate how to align the cell data. "top", "middle", "bottom" can be used.
+     */
+    public HtmlThTag dataValign(String val) {
+        attr("data-valign", val);
         return this;
     }
 
+    /**
+     * The width of column. If not defined, the width will auto expand to fit its contents.
+     */
+    public HtmlThTag dataWidth(String val) {
+        attr("data-width", val);
+        return this;
+    }
+
+    /**
+     * True to allow the column can be sorted.
+     * 
+     * @default false
+     */
     public HtmlThTag dataSortable(boolean val) {
         attr("data-sortable", val);
+        return this;
+    }
+
+    /**
+     * The default sort order, can only be "asc" or "desc".
+     * 
+     * @default asc
+     */
+    public HtmlThTag dataOrder(String val) {
+        attr("data-order", val);
+        return this;
+    }
+
+    /**
+     * False to hide the columns item.
+     * 
+     * @default true
+     */
+    public HtmlThTag dataVisible(String val) {
+        attr("data-visible", val);
+        return this;
+    }
+
+    /**
+     * False to disable the switchable of columns item.
+     * 
+     * @default true
+     */
+    public HtmlThTag dataSwitchable(String val) {
+        attr("data-switchable", val);
+        return this;
+    }
+
+    /**
+     * True to select checkbox or radiobox when the column is clicked.
+     * 
+     * @default true
+     */
+    public HtmlThTag dataClickToStart(String val) {
+        attr("data-click-to-start", val);
+        return this;
+    }
+
+    /**
+     * The cell formatter function, take three parameters:
+     * <ul>
+     * <li>value: the field value.
+     * <li>row: the row record data.
+     * <li>index: the row index.
+     * </ul>
+     */
+    public HtmlThTag dataFormatter(String val) {
+        attr("data-formatter", val);
+        return this;
+    }
+
+    /**
+     * The cell events listener when you use formatter function, take three parameters:
+     * <ul>
+     * <li>event: the jQuery event.
+     * <li>value: the field value.
+     * <li>row: the row record data.
+     * <li>index: the row index.
+     * </ul>
+     */
+    public HtmlThTag dataEvents(String val) {
+        attr("data-events", val);
+        return this;
+    }
+
+    /**
+     * The custom field sort function that used to do local sorting, take two parameters:
+     * <ul>
+     * <li>a: the first field value.
+     * <li>b: the second field value.
+     * </ul>
+     */
+    public HtmlThTag dataSorter(String val) {
+        attr("data-sorter", val);
+        return this;
+    }
+
+    /**
+     * The cell style formatter function, take three parameters:
+     * <ul>
+     * <li>value: the field value.
+     * <li>row: the row record data.
+     * <li>index: the row index.
+     * <li>Support classes or css.
+     * </ul>
+     */
+    public HtmlThTag dataCellStyle(String val) {
+        attr("data-cell-style", val);
+        return this;
+    }
+
+    public HtmlThTag dataHeight(String val) {
+        attr("data-height", val);
         return this;
     }
 
