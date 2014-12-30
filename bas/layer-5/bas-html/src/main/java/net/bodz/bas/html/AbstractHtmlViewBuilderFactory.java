b6bdafcf45a5d021e7c75e3e1f890c47b03a5659
Lenik <pub.lenik@bodz.net>
2014-12-30 23:32:18 +0800

    bas-repr: Added IViewBuilderFactory.getViewBuilder(IUiRef, ...).

--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/AbstractHtmlViewBuilderFactory.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/AbstractHtmlViewBuilderFactory.java
@@ -2,6 +2,7 @@ package net.bodz.bas.html;
 
 import net.bodz.bas.repr.viz.AbstractViewBuilderFactory;
 import net.bodz.bas.repr.viz.IViewBuilder;
+import net.bodz.bas.ui.dom1.IUiRef;
 
 public abstract class AbstractHtmlViewBuilderFactory
         extends AbstractViewBuilderFactory
@@ -10,6 +11,11 @@ public abstract class AbstractHtmlViewBuilderFactory
     @Override
     public <T> IHtmlViewBuilder<T> getViewBuilder(Class<? extends T> type, String... features) {
         return (IHtmlViewBuilder<T>) super.getViewBuilder(type);
+    }
+
+    @Override
+    public <T> IHtmlViewBuilder<T> getViewBuilder(IUiRef<? extends T> ref, String... features) {
+        return (IHtmlViewBuilder<T>) super.getViewBuilder(ref);
     }
 
     @Override
