df90ad505e01dfd320a05e3b3edd75fb7725d5b1
Lenik <pub.lenik@bodz.net>
2015-01-06 01:30:55 +0800

    bas-html: beginForm() returns IHtmlTag, to support (pseudo) inner-form.

--- bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/viz/util/DefaultForm_htm.java
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/viz/util/DefaultForm_htm.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import net.bodz.bas.err.IllegalConfigException;
 import net.bodz.bas.html.dom.IHtmlTag;
 import net.bodz.bas.html.dom.tag.HtmlTableTag;
+import net.bodz.bas.html.dom.tag.HtmlTdTag;
 import net.bodz.bas.html.dom.tag.HtmlTrTag;
 import net.bodz.bas.html.viz.IHtmlViewBuilderFactory;
 import net.bodz.bas.html.viz.IHtmlViewContext;
@@ -16,22 +17,38 @@ import net.bodz.bas.repr.viz.ViewBuilderException;
 import net.bodz.bas.rtx.IOptions;
 import net.bodz.bas.ui.dom1.IUiRef;
 
-public class SimpleForm_htm
-        extends AbstractForm_htm<Object> {
+public class DefaultForm_htm<T>
+        extends AbstractForm_htm<T> {
 
-    public SimpleForm_htm() {
+    private boolean embedded = false;
+
+    public DefaultForm_htm() {
         super(Object.class);
     }
 
+    public DefaultForm_htm(boolean embedded) {
+        this();
+        this.embedded = embedded;
+    }
+
     @Override
-    protected void nullInstance(IHtmlTag out, IUiRef<Object> ref) {
+    protected void nullInstance(IHtmlTag out, IUiRef<T> ref) {
         out.text("just null.");
+    }
+
+    @Override
+    protected IHtmlTag beginForm(IHtmlViewContext ctx, IHtmlTag out, IUiRef<T> ref, IOptions options)
+            throws ViewBuilderException, IOException {
+        if (embedded)
+            return out;
+        else
+            return out.form();
     }
 
     @Override
     protected IHtmlTag beginCategory(IHtmlViewContext ctx, IHtmlTag out, FieldCategory category)
             throws ViewBuilderException {
-        HtmlTableTag table = out.table().border("1");
+        HtmlTableTag table = out.table().border("0").class_("field-category");
         return table;
     }
 
@@ -40,17 +57,19 @@ public class SimpleForm_htm
     }
 
     @Override
-    protected IHtmlTag beginField(IHtmlViewContext ctx, IHtmlTag out, IFieldDecl fieldDecl)
+    protected IHtmlTag beginField(IHtmlViewContext ctx, IHtmlTag table, IFieldDecl fieldDecl)
             throws ViewBuilderException {
-        HtmlTrTag tr = out.tr();
-        tr.td().text(fieldDecl.getLabel());
+        HtmlTrTag tr = table.tr().class_("field-row");
+        tr.td().class_("field-label").text(fieldDecl.getLabel());
         return tr;
     }
 
     @Override
-    protected void fieldBody(IHtmlViewContext ctx, IHtmlTag out, IUiRef<Object> instanceRef, IFieldDecl fieldDecl,
+    protected void fieldBody(IHtmlViewContext ctx, IHtmlTag tr, IUiRef<T> instanceRef, IFieldDecl fieldDecl,
             IOptions options)
             throws ViewBuilderException, IOException {
+        HtmlTdTag bodyTd = tr.td().class_("field-body");
+
         IProperty property = fieldDecl.getProperty();
         UiPropertyRef<Object> propertyRef = new UiPropertyRef<Object>(instanceRef, property);
 
@@ -58,7 +77,7 @@ public class SimpleForm_htm
         if (factory == null)
             throw new IllegalConfigException(IHtmlViewBuilderFactory.class + " isn't set.");
         else
-            factory.buildView(ctx, out, propertyRef);
+            factory.buildView(ctx, bodyTd, propertyRef);
     }
 
     @Override
