89815039b7be0faf182dd8fa53ebcf13d9f6592d
Lenik <pub.lenik@bodz.net>
2015-01-06 17:16:10 +0800

    bas-repr: Added StdGroup.Graph.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoNodeChain.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoNodeChain.java
@@ -3,7 +3,7 @@ package net.bodz.lily.model.base;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class CoNodeChain<T extends CoNode<T>>
+public class CoNodeChain<T extends CoNode<T, ?>>
         extends ArrayList<T> {
 
     private static final long serialVersionUID = 1L;
