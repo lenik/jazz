dab72b3b27001402c7c823c649a4915e6c4c9c51
Lenik <pub.lenik@bodz.net>
2015-01-22 19:08:14 +0800

    bas-http: ResourceTransferer handle URL directly. Resolved Range/Content-Range.

--- bas/layer-3/bas-http/src/main/java/net/bodz/bas/http/ResourceTransferer.java
+++ bas/layer-3/bas-http/src/main/java/net/bodz/bas/http/ResourceTransferer.java
@@ -1,5 +1,6 @@
 package net.bodz.bas.http;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -9,12 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.bodz.bas.c.java.io.FileURL;
-import net.bodz.bas.io.res.builtin.URLResource;
+import net.bodz.bas.log.Logger;
+import net.bodz.bas.log.LoggerFactory;
 import net.bodz.bas.std.rfc.http.ContentDisposition;
 import net.bodz.bas.std.rfc.http.ContentRange;
 import net.bodz.bas.std.rfc.mime.ContentType;
 
 public class ResourceTransferer {
+
+    static final Logger logger = LoggerFactory.getLogger(ResourceTransferer.class);
 
     private HttpServletRequest req;
     private HttpServletResponse resp;
@@ -33,64 +37,34 @@ public class ResourceTransferer {
         this.maxAge = maxAge;
     }
 
-    public void transfer(URLResource resource)
+    public void transfer(URL url)
             throws IOException {
-        transfer(resource, null, null);
+        String filename = null;
+        String description = null;
+        String mode = req.getParameter("mode");
+        if (mode != null)
+            switch (mode) {
+            case "attachment":
+                File file = FileURL.toFile(url, null);
+                if (file != null)
+                    filename = file.getName();
+            }
+
+        transfer(url, filename, description);
     }
 
-    public void transfer(URLResource resource, String filename, String description)
+    public void transfer(URL url, String filename, String description)
             throws IOException {
-        URL url = resource.getURL();
-        String path = url.getPath();
         // if (!resource.canRead()) {
         // resp.sendError(HttpServletResponse.SC_FORBIDDEN, "Not readable.");
         // return;
         // }
 
-        resp.setHeader("Cache-Control", "max-age=" + maxAge);
-
-        long lastModified = resource.lastModified();
-        if (lastModified > 0) {
-            long ifModifiedSince = req.getDateHeader("If-Modified-Since");
-            if (ifModifiedSince > 0 && ifModifiedSince >= lastModified) {
-                resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
-                return;
-            }
-
-            /**
-             * @see RFC 2616 14.29
-             *      <p>
-             *      HTTP/1.1 servers SHOULD send Last-Modified whenever feasible
-             */
-            resp.setDateHeader("Last-Modified", lastModified);
-
-            /**
-             * @see RFC 2616 14.21
-             *      <p>
-             *      To mark a response as "never expires," an origin server sends an Expires date
-             *      approximately one year from the time the response is sent. HTTP/1.1 servers
-             *      SHOULD NOT send Expires dates more than one year in the future.
-             */
-            resp.setDateHeader("Expires", lastModified + maxAge * 1000L);
-        }
-
-        /**
-         * @see RFC 2616 13.3.2
-         *      <p>
-         *      The ETag response-header field value, an entity tag, provides for an "opaque" cache
-         *      validator. This might allow more reliable validation in situations where it is
-         *      inconvenient to store modification dates, where the one-second resolution of HTTP
-         *      date values is not sufficient, or where the origin server wishes to avoid certain
-         *      paradoxes that might arise from the use of modification dates.
-         */
-        String eTag = null;
-        // if (eTag != null) resp.setHeader("E-Tag", eTag);
-
-        ContentType contentType = ContentType.forPath(path);
+        ContentType contentType = ContentType.forPath(url.getPath());
         if (contentType != null)
             resp.setContentType(contentType.getName());
 
-        Long length = FileURL.length(resource.getURL(), null);
+        Long length = FileURL.length(url, null);
         if (length != null)
             if (length <= Integer.MAX_VALUE)
                 resp.setContentLength(length.intValue());
@@ -112,33 +86,75 @@ public class ResourceTransferer {
             }
         }
 
-        ContentRange contentRange = null;
-        String contentRangeHeader = req.getHeader("Content-Range");
-        if (contentRangeHeader != null) {
+        resp.setHeader("Cache-Control", "max-age=" + maxAge);
+
+        Long lastModified = FileURL.lastModified(url, null);
+        if (lastModified != null) {
+            long ifModifiedSince = req.getDateHeader("If-Modified-Since");
+            if (ifModifiedSince > 0 && ifModifiedSince >= lastModified) {
+                resp.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+                return;
+            }
+
+            /**
+             * @see RFC 2616 14.29
+             *      <p>
+             *      HTTP/1.1 servers SHOULD send Last-Modified whenever feasible
+             */
+            resp.setDateHeader("Last-Modified", lastModified);
+
+            /**
+             * @see RFC 2616 14.21
+             *      <p>
+             *      To mark a response as "never expires," an origin server sends an Expires date
+             *      approximately one year from the time the response is sent. HTTP/1.1 servers
+             *      SHOULD NOT send Expires dates more than one year in the future.
+             */
+            resp.setDateHeader("Expires", System.currentTimeMillis() + maxAge * 1000L);
+        }
+
+        /**
+         * @see RFC 2616 13.3.2
+         *      <p>
+         *      The ETag response-header field value, an entity tag, provides for an "opaque" cache
+         *      validator. This might allow more reliable validation in situations where it is
+         *      inconvenient to store modification dates, where the one-second resolution of HTTP
+         *      date values is not sufficient, or where the origin server wishes to avoid certain
+         *      paradoxes that might arise from the use of modification dates.
+         */
+        String eTag = null;
+        // if (eTag != null) resp.setHeader("E-Tag", eTag);
+
+        ContentRange range = null;
+        String rangeHeader = req.getHeader("Range");
+        if (rangeHeader != null) {
             if (length == null) {
                 resp.sendError(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE, //
                         "Length is unknown.");
                 return;
             }
-            contentRange = ContentRange.parseAndSet(length, contentRangeHeader, resp);
-            if (contentRange == null)
+            range = ContentRange.parseAndSet(length, rangeHeader, resp);
+            if (range == null)
                 return;
         }
 
         long start = 0;
-        long remaining = -1;
-        if (length != null) {
-            long end = length;
-            if (contentRange != null)
-                end = contentRange.end;
-            remaining = end - start;
+        long end = length;
+        if (range != null) {
+            start = range.start;
+            end = range.end;
         }
+        long remaining = end - start;
+        long transferred = 0;
 
         ServletOutputStream out = resp.getOutputStream();
-        InputStream in = resource.newInputStream();
+        InputStream in = url.openStream();
+
         try {
+            in.skip(start);
+
             byte[] block = new byte[4096];
-            while (true) {
+            while (remaining != 0) {
                 int cb = block.length;
                 if (remaining != -1 && remaining < cb)
                     cb = (int) remaining;
@@ -155,10 +171,16 @@ public class ResourceTransferer {
 
                 if (remaining != -1)
                     remaining -= cb;
+                transferred += cb;
             }
         } finally {
-            in.close();
-            out.close();
+            logger.debug("Transferred: " + transferred + " for chunk " + range);
+            if (range != null)
+                logger.debug("    expected size " + (range.end - range.start));
+            if (in != null)
+                in.close();
+            if (out != null)
+                out.close();
         }
     }
 
--- bas/layer-3/bas-http/src/main/java/net/bodz/bas/std/rfc/http/ContentRange.java
+++ bas/layer-3/bas-http/src/main/java/net/bodz/bas/std/rfc/http/ContentRange.java
@@ -13,18 +13,71 @@ public class ContentRange
     public static final int BYTE = 0;
 
     public int unit = BYTE;
-    public long start;
-    public long end;
+    public Long start;
+    public Long end;
+    public Long lengthTotal;
 
-    public long instanceLength;
-    public boolean unlimited;
+    @Override
+    public String toString() {
+        return "[ " + start + ", " + end + " ) / " + lengthTotal;
+    }
 
     /**
+     * @param str
+     *            Non-<code>null</code> Range field to be parsed.
+     * @param posBase
+     *            0-based or 1-based.
+     * @return Error message. <code>null</code> if succeeded.
+     */
+    public String parseRange(String str, int posBase) {
+        if (str == null)
+            throw new NullPointerException("str");
+
+        str = str.trim();
+        int EQ = str.indexOf('=');
+        if (EQ == -1)
+            return "no eq('=')";
+
+        int unit = BYTE;
+        String unitSpec = str.substring(0, EQ).trim();
+        switch (unitSpec) {
+        case "bytes":
+            unit = BYTE;
+            break;
+        default:
+            return "bad unit: " + unitSpec;
+        }
+
+        str = str.substring(EQ + 1);
+        int dash = str.indexOf('-');
+        if (dash == -1)
+            return "no dash";
+
+        String first = str.substring(0, dash).trim();
+        String last = str.substring(dash + 1).trim();
+        try {
+            this.unit = unit;
+            this.start = first.isEmpty() ? null : Long.parseLong(first) - posBase;
+            this.end = last.isEmpty() ? null : Long.parseLong(last) - posBase + 1;
+            return null;
+        } catch (NumberFormatException e) {
+            return "illegal number format.";
+        }
+    }
+
+    /**
+     * Format: <code>Content-Range: bytes FIRST-LAST/TOTAL</code>.
+     * <p>
+     * Examples:
+     * <ul>
+     * <li><code>bytes 21010-47021/47022</code>
+     * </ul>
+     *
      * @param str
      *            Non-<code>null</code> Content-Range field to be parsed.
      * @return Error message. <code>null</code> if succeeded.
      */
-    public String parse(String str) {
+    public String parseContentRange(String str) {
         if (str == null)
             throw new NullPointerException("str");
 
@@ -58,39 +111,56 @@ public class ContentRange
         String first = respSpec.substring(0, dash);
         String last = respSpec.substring(dash + 1);
         try {
-            long start = Long.parseLong(first);
-            long end = Long.parseLong(last) + 1;
-            boolean unlimited = "*".equals(lengthSpec);
-            long length = unlimited ? 0 : Long.parseLong(lengthSpec);
-
             this.unit = unit;
-            this.start = start;
-            this.end = end;
-            this.instanceLength = length;
-            this.unlimited = unlimited;
+            this.start = Long.parseLong(first);
+            this.end = Long.parseLong(last) + 1;
+
+            boolean lengthUnknown = "*".equals(lengthSpec);
+            this.lengthTotal = lengthUnknown ? null : Long.parseLong(lengthSpec);
             return null;
         } catch (NumberFormatException e) {
             return "illegal number format.";
         }
     }
 
-    public static ContentRange parseAndSet(long length, String contentRangeHeader, HttpServletResponse resp)
+    public String toContentRange() {
+        return toContentRange(0);
+    }
+
+    public String toContentRange(int posBase) {
+        long first = start + posBase;
+        long last = end - 1 + posBase;
+        return "bytes " + first + "-" + last + "/" + lengthTotal;
+    }
+
+    public static ContentRange parseAndSet(long lengthAvailable, String rangeHeader, HttpServletResponse resp)
             throws IOException {
-        ContentRange contentRange = new ContentRange();
-        String error = contentRange.parse(contentRangeHeader);
+        ContentRange range = new ContentRange();
+
+        String error = range.parseRange(rangeHeader, 1);
         if (error != null) {
-            resp.setHeader("Content-Range", "*/" + length);
+            resp.setHeader("Content-Range", "*/" + lengthAvailable);
             resp.sendError(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE, //
-                    error + ": " + contentRangeHeader);
+                    error + ": " + rangeHeader);
             return null;
-        } else if (contentRange.end >= length) {
-            resp.sendError(HttpServletResponse.SC_REQUESTED_RANGE_NOT_SATISFIABLE, //
-                    "out of length");
-            return null;
-        } else {
-            resp.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-            return contentRange;
         }
+
+        if (range.end == null)
+            range.end = lengthAvailable;
+
+        if (range.start == null) {
+            range.start = lengthAvailable - range.end;
+            range.end = lengthAvailable;
+        }
+
+        if (range.end > lengthAvailable)
+            range.end = lengthAvailable;
+
+        range.lengthTotal = lengthAvailable;
+
+        resp.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+        resp.setHeader("Content-Range", range.toContentRange());
+        return range;
     }
 
 }
