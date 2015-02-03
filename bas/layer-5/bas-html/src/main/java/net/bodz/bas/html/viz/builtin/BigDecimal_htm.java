83c14ea14aca8bfbeeb8b8758103d6b615f41f98
Lenik <pub.lenik@bodz.net>
2015-02-03 16:46:35 +0800

    bas-html: Added BigDecimal_htm.

---
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/viz/builtin/BigDecimal_htm.java
@@ -0,0 +1,23 @@
+package net.bodz.bas.html.viz.builtin;
+
+import java.math.BigDecimal;
+
+import net.bodz.bas.html.dom.IHtmlTag;
+import net.bodz.bas.html.dom.tag.HtmlInputTag;
+import net.bodz.bas.potato.ref.UiPropertyRef;
+import net.bodz.bas.repr.form.IFieldDecl;
+
+public class BigDecimal_htm
+        extends AbstractNumericForm_htm<BigDecimal> {
+
+    public BigDecimal_htm() {
+        super(BigDecimal.class);
+    }
+
+    @Override
+    protected HtmlInputTag createInput(IHtmlTag out, UiPropertyRef<BigDecimal> ref, IFieldDecl fieldDecl) {
+        HtmlInputTag input = out.input().type("number");
+        return input;
+    }
+
+}
