bc6ed7d9d8c54b9e170500e920861027f7d4a7ce
Lenik <pub.lenik@bodz.net>
2015-03-28 07:34:14 +0800

    lily-model: Added special password for maintaince login.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/security/impl/UserCriteria.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/security/impl/UserCriteria.java
@@ -1,5 +1,6 @@
 package net.bodz.lily.model.base.security.impl;
 
+import net.bodz.bas.c.string.StringUtil;
 import net.bodz.bas.err.ParseException;
 
 import net.bodz.lily.model.base.CoObjectCriteria;
@@ -12,6 +13,21 @@ public class UserCriteria
         extends CoObjectCriteria {
 
     public String password;
+
+    public String getCodeName1() {
+        String s = getCodeName();
+        if (s != null)
+            s = StringUtil.enc1(s);
+        return s;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     protected void populate(QVariantMap<String> map)
