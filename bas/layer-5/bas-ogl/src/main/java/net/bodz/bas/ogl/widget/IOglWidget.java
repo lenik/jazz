d8eb1e7a74605141830656030b1bb61555165abf
Lenik <pub.lenik@bodz.net>
2015-04-30 17:50:06 +0800

    bas-ogl: Init Ogl widget.

---
+++ bas/layer-5/bas-ogl/src/main/java/net/bodz/bas/ogl/widget/IOglWidget.java
@@ -0,0 +1,9 @@
+package net.bodz.bas.ogl.widget;
+
+import net.bodz.bas.ogl.shape.IOglShape;
+import net.bodz.bas.ui.layout.IAutoLayoutWidget;
+
+public interface IOglWidget
+        extends IOglShape, IAutoLayoutWidget {
+
+}
