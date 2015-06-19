e6a95e1f1a733aa76d5c84e40b97db32bd1ea2bd
Lenik <pub.lenik@bodz.net>
2015-06-19 14:17:15 +0800

    lily-model: Added ILazyLoading helper.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/ILazyLoading.java
@@ -0,0 +1,23 @@
+package net.bodz.lily.model.base;
+
+import java.util.Collection;
+
+public interface ILazyLoading {
+
+    int SIZE_UNKNOWN = -1;
+    long SIZEL_UNKNOWN = -1L;
+
+    class SizeFn {
+
+        public static int getSize(Collection<?> collection, int size) {
+            if (collection == null)
+                return size == SIZE_UNKNOWN ? 0 : size;
+            if (!collection.isEmpty())
+                return collection.size();
+            if (size != SIZE_UNKNOWN)
+                return size;
+            return 0;
+        }
+    }
+
+}
