5ecb46a02a409b30eab941711f6fa088b42eaa02
Lenik <pub.lenik@bodz.net>
2016-02-21 12:47:19 +0800

    lily-model: Added res.art, res.people std models.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/res/art/SupplyMethodTypers.java
@@ -0,0 +1,12 @@
+package net.bodz.lily.model.res.art;
+
+import net.bodz.bas.t.predef.PredefTypers;
+
+public class SupplyMethodTypers
+        extends PredefTypers<SupplyMethod, Integer> {
+
+    public SupplyMethodTypers() {
+        super(SupplyMethod.class);
+    }
+
+}
