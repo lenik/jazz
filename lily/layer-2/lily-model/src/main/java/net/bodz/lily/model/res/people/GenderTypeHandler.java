a444afeb2cd73f48b2e41efa7046ebcab152bc3e
Lenik <pub.lenik@bodz.net>
2016-02-21 12:47:19 +0800

    lily-model: Added res.art, res.people std models.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/res/people/GenderTypeHandler.java
@@ -0,0 +1,51 @@
+package net.bodz.lily.model.res.people;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.ibatis.type.JdbcType;
+
+import net.bodz.bas.db.ibatis.TypeHandlerImpl;
+
+public class GenderTypeHandler
+        extends TypeHandlerImpl<Gender> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Gender parameter, JdbcType jdbcType)
+            throws SQLException {
+        ps.setString(i, parameter.getKey().toString());
+    }
+
+    @Override
+    public Gender getNullableResult(ResultSet rs, String columnName)
+            throws SQLException {
+        String s = rs.getString(columnName);
+        if (s == null)
+            return Gender.UNKNOWN;
+        else
+            return Gender.METADATA.ofKey(s.charAt(0));
+    }
+
+    @Override
+    public Gender getNullableResult(ResultSet rs, int columnIndex)
+            throws SQLException {
+        String s = rs.getString(columnIndex);
+        if (s == null)
+            return Gender.UNKNOWN;
+        else
+            return Gender.METADATA.ofKey(s.charAt(0));
+    }
+
+    @Override
+    public Gender getNullableResult(CallableStatement cs, int columnIndex)
+            throws SQLException {
+        String s = cs.getString(columnIndex);
+        if (s == null)
+            return Gender.UNKNOWN;
+        else
+            return Gender.METADATA.ofKey(s.charAt(0));
+    }
+
+}
