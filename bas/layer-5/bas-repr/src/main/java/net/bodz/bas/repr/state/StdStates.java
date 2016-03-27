4e321fb7ad2c9f038a7ff66bcca552387fea4625
Lenik <pub.lenik@bodz.net>
2016-03-27 16:35:18 +0800

    bas-repr: Added StdStates.WAIT. START->INIT, ACCEPTED->OK.

--- bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/state/StdStates.java
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/state/StdStates.java
@@ -3,13 +3,16 @@ package net.bodz.bas.repr.state;
 public class StdStates
         implements IStateConsts {
 
-    public static final int ID_START = 0;
-    public static final int ID_ACCEPTED = 1;
+    public static final int ID_INIT = 0;
+    public static final int ID_OK = 1;
+    public static final int ID_WAIT = 2;
+    public static final int ID_USER = 100;
     public static final int ID_ERROR = -1;
     public static final int ID_EPSILON = -2;
 
-    public static final State START = new State(ID_START, "start", StateType.NONTERM);
-    public static final State ACCEPTED = new State(ID_ACCEPTED, "accepted", StateType.ACCEPTED);
+    public static final State INIT = new State(ID_INIT, "init", StateType.NONTERM);
+    public static final State OK = new State(ID_OK, "ok", StateType.ACCEPTED);
+    public static final State WAIT = new State(ID_WAIT, "wait", StateType.ACCEPTED);
     public static final State ERROR = new State(ID_ERROR, "error", StateType.ERROR);
     public static final State EPSILON = new State(ID_EPSILON, "epsilon", StateType.NONTERM);
 
