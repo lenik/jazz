7b9a7466bda5a4768c2ed37e0dbfcfb0b8f4a2ec
Lenik <pub.lenik@bodz.net>
2016-03-27 16:34:08 +0800

    bas-repr: Added @GroupDef.

---
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/form/meta/GroupDef.java
@@ -0,0 +1,12 @@
+package net.bodz.bas.repr.form.meta;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ ElementType.ANNOTATION_TYPE })
+public @interface GroupDef {
+
+}
