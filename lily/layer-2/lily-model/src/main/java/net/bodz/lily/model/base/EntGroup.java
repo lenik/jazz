f832daf5a0247c30853fd18e9978f7bd8fb03f9d
Lenik <pub.lenik@bodz.net>
2016-03-27 16:36:01 +0800

    lily-model: Added @EntGroup.*.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/EntGroup.java
@@ -0,0 +1,46 @@
+package net.bodz.lily.model.base;
+
+import net.bodz.bas.meta.decl.Priority;
+import net.bodz.bas.repr.form.meta.GroupDef;
+
+@GroupDef
+public @interface EntGroup {
+
+    /** 会计 */
+    @Priority(0)
+    @interface Accounting {
+    }
+
+    /** 颜色管理 */
+    @interface ColorManagement {
+    }
+
+    /** 通信 */
+    @interface Communication {
+    }
+
+    /** 定制扩展 */
+    @interface CustomExtension {
+    }
+
+    /** 包装 */
+    @interface Packaging {
+    }
+
+    /** 位置 */
+    @interface Position {
+    }
+
+    /** 交易 */
+    @interface Trade {
+    }
+
+    /** 运输 */
+    @interface Transportation {
+    }
+
+    /** 用户交互 */
+    @interface UserInteraction {
+    }
+
+}
