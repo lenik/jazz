75cfaff4a1f7e221f28b2af9ca831109991615d3
Lenik <pub.lenik@bodz.net>
2016-03-27 16:38:40 +0800

    lily-model:  Added ArticleDoc.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/mx/doc/ArticleDoc.java
@@ -0,0 +1,10 @@
+package net.bodz.lily.model.mx.doc;
+
+import net.bodz.lily.model.mx.forum.TopicPost;
+
+public class ArticleDoc
+        extends TopicPost {
+
+    private static final long serialVersionUID = 1L;
+
+}
