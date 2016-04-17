461b2f9ac558b8845b35290cc361c5c1aa71a131
Lenik <pub.lenik@bodz.net>
2016-04-17 12:50:05 +0800

    lily-site: Added std menu/toolbars.

---
+++ lily/layer-5/lily-site/src/main/java/net/bodz/lily/site/ui/cmd/CollaborationToolbar.java
@@ -0,0 +1,14 @@
+package net.bodz.lily.site.ui.cmd;
+
+import net.bodz.bas.ui.model.cmd.UiToolbarDecl;
+
+/**
+ *  transfer/send
+ *  "new chat window"
+ *  verify
+ *  share...
+ */
+public class CollaborationToolbar
+        extends UiToolbarDecl {
+
+}
---
+++ lily/layer-5/lily-site/src/main/java/net/bodz/lily/site/ui/cmd/SelectionMenu.java
@@ -0,0 +1,15 @@
+package net.bodz.lily.site.ui.cmd;
+
+import net.bodz.bas.ui.model.cmd.UiMenuDecl;
+
+/**
+        Open
+        Delete
+        Verify
+        -
+        Share...
+ */
+public class SelectionMenu
+        extends UiMenuDecl {
+
+}
