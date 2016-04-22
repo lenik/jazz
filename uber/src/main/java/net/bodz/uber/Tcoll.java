7f738e39e4308f9ba12f1d565cae7b9d9abf69f4
Lenik <pub.lenik@bodz.net>
2016-04-22 10:25:48 +0800

    Run tcoll.

---
+++ uber/src/main/java/net/bodz/uber/Tcoll.java
@@ -0,0 +1,12 @@
+package net.bodz.uber;
+
+import net.bodz.bas.shell.util.TypeCollectorCLI;
+
+public class Tcoll {
+
+    public static void main(String[] args)
+            throws Exception {
+        TypeCollectorCLI.main(args);
+    }
+
+}
