2f6a5c641c1b168a609b2c12f14270bcfa4534ea
Lenik <pub.lenik@bodz.net>
2016-04-22 09:36:38 +0800

    bas-c3: TypePoMapTest: add more tests.

--- bas/layer-3/bas-c3/src/test/java/net/bodz/bas/c/type/TypePoMapTest.java
+++ bas/layer-3/bas-c3/src/test/java/net/bodz/bas/c/type/TypePoMapTest.java
@@ -1,8 +1,11 @@
 package net.bodz.bas.c.type;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,16 +17,34 @@ public class TypePoMapTest
     {
         orig.put(Number.class, "Number");
         orig.put(Integer.class, "Integer");
+        orig.put(Long.class, "Long");
+        orig.put(Double.class, "Double");
+        orig.put(Boolean.class, "Boolean");
         orig.put(String.class, "String");
     }
 
     @Test
-    public void testFloor() {
+    public void testMeet() {
         TypePoMap<Object> map = new TypePoMap<Object>();
         map.putAll(orig);
         assertEquals("Number", map.meet(Float.class));
         assertEquals("Integer", map.meet(Integer.class));
         assertNull(map.meet(Date.class));
+    }
+
+    @Test
+    public void testJoin() {
+        TypePoMap<Object> map = new TypePoMap<Object>();
+        map.putAll(orig);
+
+        Set<Object> set = new HashSet<>();
+        for (Object o : map.join(Number.class))
+            set.add(o);
+
+        Set<String> expected = new HashSet<>(Arrays.asList(//
+                "Number", "Integer", "Long", "Double"));
+
+        assertEquals(expected, set);
     }
 
 }
