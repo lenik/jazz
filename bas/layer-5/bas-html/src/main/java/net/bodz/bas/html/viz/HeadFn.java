d0966ffbdef4892fd91d2845b2b72b4515c9a9a8
Lenik <pub.lenik@bodz.net>
2016-04-24 11:23:55 +0800

    bas-html: Separated html view utilities to fn.*..

---
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/viz/HeadFn.java
@@ -0,0 +1,55 @@
+package net.bodz.bas.html.viz;
+
+import java.io.IOException;
+import java.util.Map.Entry;
+
+import net.bodz.bas.html.artifact.ArtifactType;
+import net.bodz.bas.html.artifact.IArtifact;
+import net.bodz.bas.html.artifact.IArtifactManager;
+import net.bodz.bas.html.artifact.MutableWebArtifact;
+import net.bodz.bas.html.dom.IHtmlHeadData;
+import net.bodz.bas.html.io.IHtmlOut;
+
+public class HeadFn {
+
+    public static void writeHeadData(IHtmlViewContext ctx, IHtmlOut head)
+            throws IOException {
+        writeMetas(ctx, head);
+        writeImports(ctx, head);
+    }
+
+    public static void writeMetas(IHtmlViewContext ctx, IHtmlOut head)
+            throws IOException {
+        IHtmlHeadData headData = ctx.getHeadData();
+
+        String title = headData.getTitle().trim();
+        if (title != null)
+            head.title().text(title);
+
+        head.meta().httpEquiv(IHtmlHeadData.HTTP_CONTENT_TYPE).content(ctx.getResponse().getContentType());
+        head.meta().name(IHtmlHeadData.META_GENERATOR).content("Jazz BAS Repr/HTML 2.0");
+
+        for (Entry<String, String> entry : headData.getHttpEquivMetaMap().entrySet())
+            head.meta().httpEquiv(entry.getKey()).content(entry.getValue());
+
+        for (Entry<String, String> entry : headData.getMetaMap().entrySet())
+            head.meta().name(entry.getKey()).content(entry.getValue());
+    }
+
+    public static void writeImports(IHtmlViewContext ctx, IHtmlOut head)
+            throws IOException {
+        IArtifactManager artifactManager = ctx.query(IArtifactManager.class);
+        IHtmlHeadData headData = ctx.getHeadData();
+
+        for (IArtifact artifact : artifactManager.getClosure(headData, ArtifactType.STYLESHEET, null)) {
+            MutableWebArtifact wa = (MutableWebArtifact) artifact;
+            head.link().css(wa.getAnchor().toString());
+        }
+
+        for (IArtifact artifact : artifactManager.getClosure(headData, ArtifactType.SCRIPT, null)) {
+            MutableWebArtifact wa = (MutableWebArtifact) artifact;
+            head.script().javascriptSrc(wa.getAnchor().toString());
+        }
+    }
+
+}
---
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/viz/fn/RedirectFn.java
@@ -0,0 +1,39 @@
+package net.bodz.bas.html.viz.fn;
+
+import java.io.IOException;
+
+import net.bodz.bas.http.viz.IHttpViewContext;
+import net.bodz.bas.repr.path.IPathArrival;
+import net.bodz.bas.ui.dom1.IUiRef;
+
+public class RedirectFn {
+
+    public static boolean addSlash(IHttpViewContext ctx, IUiRef<?> ref)
+            throws IOException {
+        Object obj = ref.get();
+        IPathArrival arrival = ctx.query(IPathArrival.class);
+        boolean arrivedHere = arrival.getPrevious(obj).getRemainingPath() == null;
+        return arrivedHere && addSlash(ctx);
+    }
+
+    public static boolean addSlash(IHttpViewContext ctx)
+            throws IOException {
+        String uri = ctx.getRequest().getRequestURI();
+        if (uri.endsWith("/"))
+            return false;
+
+        StringBuffer url = ctx.getRequest().getRequestURL();
+        url.append('/');
+
+        String queryString = ctx.getRequest().getQueryString();
+        if (queryString != null) {
+            url.append('?');
+            url.append(queryString);
+        }
+
+        ctx.getResponse().sendRedirect(url.toString());
+        ctx.stop();
+        return true;
+    }
+
+}
