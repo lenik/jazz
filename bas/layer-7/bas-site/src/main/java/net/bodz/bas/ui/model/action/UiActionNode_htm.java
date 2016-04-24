9ece09638b7584ba7bd9fbcbaa1b75d47e55321a
Lenik <pub.lenik@bodz.net>
2016-04-24 11:31:07 +0800

    bas-site: Added UiActionNode_htm merge-parent feature.

--- bas/layer-7/bas-site/src/main/java/net/bodz/bas/ui/model/action/UiActionNode_htm.java
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/ui/model/action/UiActionNode_htm.java
@@ -8,18 +8,28 @@ import net.bodz.bas.html.io.tag.HtmlUl;
 import net.bodz.bas.html.viz.AbstractHtmlViewBuilder;
 import net.bodz.bas.html.viz.IHtmlViewContext;
 import net.bodz.bas.http.ui.cmd.UiServletAction;
+import net.bodz.bas.log.Logger;
+import net.bodz.bas.log.LoggerFactory;
 import net.bodz.bas.repr.viz.ViewBuilderException;
 import net.bodz.bas.ui.dom1.IUiElement;
 import net.bodz.bas.ui.dom1.IUiRef;
 import net.bodz.bas.ui.dom1.UiVar;
-import net.bodz.bas.ui.model.action.UiActionNode;
-import net.bodz.bas.ui.model.action.UiLocationDecl;
 
 public class UiActionNode_htm
         extends AbstractHtmlViewBuilder<UiActionNode> {
 
+    static final Logger logger = LoggerFactory.getLogger(UiActionNode_htm.class);
+
+    boolean debugView = false;
+    String menuBarClass = "ZwkMenuBar p1";
+    String menuClass = "ZwkMenu p1";
+
     public UiActionNode_htm() {
         super(UiActionNode.class);
+    }
+
+    boolean isDebugView() {
+        return debugView;
     }
 
     @Override
@@ -28,13 +38,6 @@ public class UiActionNode_htm
         UiActionNode node = ref.get();
         IUiElement element = node.getElement();
 
-        if (node.isRoot()) {
-            // root node
-        } else {
-            out = out.li();
-            out.text(element.getLabel());
-        }
-
         boolean mergeChildren = false;
         boolean mergeToParent = false;
 
@@ -42,6 +45,16 @@ public class UiActionNode_htm
             UiLocationDecl decl = (UiLocationDecl) element;
             mergeChildren = decl.isMergeChildren();
             mergeToParent = decl.isMergeToParent();
+        }
+
+        boolean pseudo = node.isRoot() || mergeToParent;
+        if (!pseudo) {
+            // root node doesn't have label, image, at all.
+            out = out.li();
+
+            if (isDebugView()) {
+                out.attr("j:class", element.getClass().getSimpleName());
+            }
         }
 
         if (element instanceof UiServletAction) {
@@ -57,14 +70,18 @@ public class UiActionNode_htm
                 out.attrs(attributeMap);
         }
 
+        if (!pseudo)
+            out.text(element.getLabel());
+
         if (!node.isEmpty()) {
             IHtmlOut sub = out;
-            if (!mergeToParent) {
+            if (mergeToParent) {
+            } else {
                 HtmlUl ul = out.ul();
                 if (node.isRoot())
-                    ul.class_("ZwkMenuBar p1 -top");
+                    ul.class_(menuBarClass + " -top");
                 else
-                    ul.class_("ZwkMenu p1");
+                    ul.class_(menuClass);
                 sub = ul;
             }
 
@@ -72,10 +89,14 @@ public class UiActionNode_htm
                 if (mergeChildren) {
                     for (UiActionNode childChild : child.getChildren())
                         buildHtmlView(ctx, sub, UiVar.wrap(childChild));
+                    out.hr();
                 } else {
                     buildHtmlView(ctx, sub, UiVar.wrap(child));
                 }
             }
+
+            if (mergeToParent)
+                out.hr();
         }
 
         return out;
