8329ca61af8852c113ca610eb1abb98e4ef5a6b4
Lenik <pub.lenik@bodz.net>
2016-04-26 15:20:02 +0800

    bas-repr: Added State.declaringClass.

--- bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/state/State.java
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/state/State.java
@@ -9,32 +9,39 @@ public class State
         extends XjdocObject {
 
     private final int id;
+    private final Class<?> declaringClass;
     private final String name;
     private final StateType type;
 
-    public State(int id, String name, StateType type) {
+    public State(int id, Class<?> declaringClass, String name, StateType type) {
+        if (declaringClass == null)
+            throw new NullPointerException("declaringClass");
         if (name == null)
             throw new NullPointerException("name");
         if (type == null)
             throw new NullPointerException("type");
         this.id = id;
+        this.declaringClass = declaringClass;
         this.name = name;
         this.type = type;
     }
 
-    public State(String name, StateType type) {
+    public State(Class<?> declaringClass, String name, StateType type) {
+        if (declaringClass == null)
+            throw new NullPointerException("declaringClass");
         if (name == null)
             throw new NullPointerException("name");
         if (type == null)
             throw new NullPointerException("type");
 
-        CRC32 crc32 = new CRC32();
-        byte[] nameBytes = name.getBytes(Charsets.UTF8);
-        crc32.update(nameBytes);
-        this.id = (int) crc32.getValue();
-
+        this.declaringClass = declaringClass;
         this.name = name;
         this.type = type;
+
+        CRC32 crc32 = new CRC32();
+        byte[] bytes = getQName().getBytes(Charsets.UTF8);
+        crc32.update(bytes);
+        this.id = (int) crc32.getValue();
     }
 
     public int getId() {
@@ -48,6 +55,14 @@ public class State
 
     public StateType getType() {
         return type;
+    }
+
+    public String getQName() {
+        StringBuilder qName = new StringBuilder();
+        qName.append(declaringClass.getName());
+        qName.append("::");
+        qName.append(name);
+        return qName.toString();
     }
 
     @Override
--- bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/state/StdStates.java
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/state/StdStates.java
@@ -10,10 +10,10 @@ public class StdStates
     public static final int ID_ERROR = -1;
     public static final int ID_EPSILON = -2;
 
-    public static final State INIT = new State(ID_INIT, "init", StateType.NONTERM);
-    public static final State OK = new State(ID_OK, "ok", StateType.ACCEPTED);
-    public static final State WAIT = new State(ID_WAIT, "wait", StateType.ACCEPTED);
-    public static final State ERROR = new State(ID_ERROR, "error", StateType.ERROR);
-    public static final State EPSILON = new State(ID_EPSILON, "epsilon", StateType.NONTERM);
+    public static final State INIT = new State(ID_INIT, StdStates.class, "init", StateType.NONTERM);
+    public static final State OK = new State(ID_OK, StdStates.class, "ok", StateType.ACCEPTED);
+    public static final State WAIT = new State(ID_WAIT, StdStates.class, "wait", StateType.ACCEPTED);
+    public static final State ERROR = new State(ID_ERROR, StdStates.class, "error", StateType.ERROR);
+    public static final State EPSILON = new State(ID_EPSILON, StdStates.class, "epsilon", StateType.NONTERM);
 
 }
