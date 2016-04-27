184ffc78d1226654d3d39cd7363b544e07a9cbef
Lenik <pub.lenik@bodz.net>
2016-04-27 21:29:57 +0800

    bas-db: Added @IncludeMapperXml to index shared xmls.

---
+++ bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/ibatis/IncludeMapperXml.java
@@ -0,0 +1,22 @@
+package net.bodz.bas.db.ibatis;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import net.bodz.bas.meta.codegen.IndexedType;
+import net.bodz.bas.meta.codegen.PublishDir;
+
+/**
+ * Specify extra mapper XML files to be included.
+ */
+@Documented
+@IndexedType(publishDir = PublishDir.features)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface IncludeMapperXml {
+
+    int priority() default 0;
+
+    String[] value() default {};
+
+}
