4b052d7a43690b7f34de6f48ca69c0b169e4499a
Lenik <pub.lenik@bodz.net>
2016-04-27 10:17:22 +0800

    bas-db: Renamed TableUtils._name() to aTableName().

--- bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/meta/TableUtils.java
+++ bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/meta/TableUtils.java
@@ -6,7 +6,7 @@ import net.bodz.bas.c.string.Strings;
 
 public class TableUtils {
 
-    public static String _name(Class<?> type) {
+    public static String aTableName(Class<?> type) {
         Table aTable = type.getAnnotation(Table.class);
         if (aTable == null)
             return null;
@@ -17,27 +17,27 @@ public class TableUtils {
     }
 
     public static String tablename(Class<?> type) {
-        String name = _name(type);
+        String name = aTableName(type);
         return name != null ? name : type.getSimpleName().toLowerCase();
     }
 
     public static String TABLENAME(Class<?> type) {
-        String name = _name(type);
+        String name = aTableName(type);
         return name != null ? name : type.getSimpleName().toUpperCase();
     }
 
     public static String tableName(Class<?> type) {
-        String name = _name(type);
+        String name = aTableName(type);
         return name != null ? name : Strings.lcfirst(type.getSimpleName());
     }
 
     public static String table_name(Class<?> type) {
-        String name = _name(type);
+        String name = aTableName(type);
         return name != null ? name : Strings.hyphenatize(type.getSimpleName()).toLowerCase();
     }
 
     public static String TABLE_NAME(Class<?> type) {
-        String name = _name(type);
+        String name = aTableName(type);
         return name != null ? name : Strings.hyphenatize(type.getSimpleName()).toUpperCase();
     }
 
