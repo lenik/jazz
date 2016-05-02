28a6168b737676270367ec1b963f7eeaee356649
Lenik <pub.lenik@bodz.net>
2016-05-02 09:07:09 +0800

    bas-fmt: CsvRow --o IJsonSerializable, and convert empty nums to null.

---
+++ bas/layer-3/bas-fmt/src/main/java/net/bodz/bas/fmt/json/IJsonSerializable.java
@@ -0,0 +1,17 @@
+package net.bodz.bas.fmt.json;
+
+import org.json.JSONObject;
+import org.json.JSONWriter;
+
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.meta.source.SerializableForm;
+
+@SerializableForm
+public interface IJsonSerializable {
+
+    void readObject(JSONObject json)
+            throws ParseException;
+
+    void writeObject(JSONWriter out);
+
+}
