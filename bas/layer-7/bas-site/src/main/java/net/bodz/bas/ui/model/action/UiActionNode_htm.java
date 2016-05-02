6d05665b66355e12188e8928264d234e741f00b0
Lenik <pub.lenik@bodz.net>
2016-05-02 09:25:57 +0800

    bas-site: Added menu icons.

--- bas/layer-7/bas-site/src/main/java/net/bodz/bas/ui/model/action/UiActionNode_htm.java
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/ui/model/action/UiActionNode_htm.java
@@ -1,6 +1,8 @@
 package net.bodz.bas.ui.model.action;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import net.bodz.bas.html.io.IHtmlOut;
@@ -11,16 +13,25 @@ import net.bodz.bas.http.ui.cmd.UiServletAction;
 import net.bodz.bas.log.Logger;
 import net.bodz.bas.log.LoggerFactory;
 import net.bodz.bas.repr.viz.ViewBuilderException;
+import net.bodz.bas.site.IBasicSiteAnchors;
+import net.bodz.bas.site.artifact.ZwkArtifacts;
 import net.bodz.bas.ui.dom1.IUiElement;
 import net.bodz.bas.ui.dom1.IUiRef;
 import net.bodz.bas.ui.dom1.UiVar;
+import net.bodz.mda.xjdoc.model.IElementDoc;
+import net.bodz.mda.xjdoc.model.javadoc.IXjdocAware;
 
 public class UiActionNode_htm
-        extends AbstractHtmlViewBuilder<UiActionNode> {
+        extends AbstractHtmlViewBuilder<UiActionNode>
+        implements IBasicSiteAnchors {
 
     static final Logger logger = LoggerFactory.getLogger(UiActionNode_htm.class);
 
     boolean debugView = false;
+
+    Integer minPriority;
+    Integer maxPriority;
+
     String menuBarClass = "ZwkMenuBar p1";
     String menuClass = "ZwkMenu p1";
 
@@ -33,10 +44,18 @@ public class UiActionNode_htm
     }
 
     @Override
+    public void precompile(IHtmlViewContext ctx, IUiRef<UiActionNode> ref) {
+        super.precompile(ctx, ref);
+        ctx.getHeadData().addDependency(ZwkArtifacts.zwkMenus.page(1));
+    }
+
+    @Override
     public IHtmlOut buildHtmlViewStart(IHtmlViewContext ctx, IHtmlOut out, IUiRef<UiActionNode> ref)
             throws ViewBuilderException, IOException {
         UiActionNode node = ref.get();
         IUiElement element = node.getElement();
+
+        IElementDoc xjdoc = (element instanceof IXjdocAware) ? ((IXjdocAware) element).getXjdoc() : null;
 
         boolean mergeChildren = false;
         boolean mergeToParent = false;
@@ -66,12 +85,23 @@ public class UiActionNode_htm
                     out.attr(scriptId, script);
             }
             Map<String, String> attributeMap = action.getAttributeMap();
-            if (attributeMap != null)
+            if (attributeMap != null && !attributeMap.isEmpty())
                 out.attrs(attributeMap);
         }
 
-        if (!pseudo)
+        if (!pseudo) {
+            // IImageData image = element.getStyle().getImage(ImageUsage.NORMAL);
+            String icon = xjdoc == null ? null : (String) xjdoc.getFirstTag("icon");
+            if (icon != null) {
+                if (icon.startsWith("\"")) {
+                    String iconChar = icon.substring(1, icon.length() - 1);
+                    out.span().class_("icon").text(iconChar);
+                } else {
+                    out.img().class_("icon").src(_webApp_.join(icon));
+                }
+            }
             out.text(element.getLabel());
+        }
 
         if (!node.isEmpty()) {
             IHtmlOut sub = out;
@@ -86,10 +116,19 @@ public class UiActionNode_htm
             }
 
             for (UiActionNode child : node.getChildren()) {
+                if (!isIncluded(child))
+                    continue;
                 if (mergeChildren) {
+                    List<UiActionNode> selection = new ArrayList<>();
                     for (UiActionNode childChild : child.getChildren())
-                        buildHtmlView(ctx, sub, UiVar.wrap(childChild));
-                    out.hr();
+                        if (isIncluded(childChild))
+                            selection.add(childChild);
+
+                    if (!selection.isEmpty()) {
+                        for (UiActionNode childChild : selection)
+                            buildHtmlView(ctx, sub, UiVar.wrap(childChild));
+                        out.hr();
+                    }
                 } else {
                     buildHtmlView(ctx, sub, UiVar.wrap(child));
                 }
@@ -100,6 +139,31 @@ public class UiActionNode_htm
         }
 
         return out;
+    }
+
+    public Integer getMinPriority() {
+        return minPriority;
+    }
+
+    public void setMinPriority(Integer minPriority) {
+        this.minPriority = minPriority;
+    }
+
+    public Integer getMaxPriority() {
+        return maxPriority;
+    }
+
+    public void setMaxPriority(Integer maxPriority) {
+        this.maxPriority = maxPriority;
+    }
+
+    boolean isIncluded(UiActionNode node) {
+        IUiElement element = node.getElement();
+        if (minPriority != null && element.getPriority() < minPriority)
+            return false;
+        if (maxPriority != null && element.getPriority() > maxPriority)
+            return false;
+        return true;
     }
 
 }
