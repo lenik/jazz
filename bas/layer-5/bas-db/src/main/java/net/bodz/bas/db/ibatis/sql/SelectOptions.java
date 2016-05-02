87a0db1f100798649e5510d87950934ecfed9cca
Lenik <pub.lenik@bodz.net>
2016-05-02 09:09:11 +0800

    bas-db: IMapperTemplate: named params all(opt), filter(m, opt).

---
+++ bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/ibatis/sql/SelectOptions.java
@@ -0,0 +1,11 @@
+package net.bodz.bas.db.ibatis.sql;
+
+public class SelectOptions {
+
+    private final Pagination page = new Pagination();
+
+    public Pagination getPage() {
+        return page;
+    }
+
+}
