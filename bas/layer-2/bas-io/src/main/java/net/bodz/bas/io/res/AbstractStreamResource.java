e85d9d08bb4c22b2b62d2dd6d2d0d98033181c57
Lenik <pub.lenik@bodz.net>
2016-05-02 09:03:29 +0800

    bas-io: AbstractStreamResource: add read(), write() shortcuts.

--- bas/layer-2/bas-io/src/main/java/net/bodz/bas/io/res/AbstractStreamResource.java
+++ bas/layer-2/bas-io/src/main/java/net/bodz/bas/io/res/AbstractStreamResource.java
@@ -1,7 +1,18 @@
 package net.bodz.bas.io.res;
 
+import net.bodz.bas.io.res.tools.StreamReading;
+import net.bodz.bas.io.res.tools.StreamWriting;
+
 public abstract class AbstractStreamResource
         extends StreamResourceTemplate
         implements IStreamResource {
+
+    public StreamReading read() {
+        return new StreamReading(this);
+    }
+
+    public StreamWriting write() {
+        return new StreamWriting(this);
+    }
 
 }
