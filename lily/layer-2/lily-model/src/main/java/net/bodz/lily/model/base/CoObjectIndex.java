66a40cc6581e437b80e9a5302398146da3dedc23
Lenik <pub.lenik@bodz.net>
2016-05-02 09:09:49 +0800

    bas-db: Added IDataContextAware.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoObjectIndex.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoObjectIndex.java
@@ -3,9 +3,11 @@ package net.bodz.lily.model.base;
 import net.bodz.bas.c.java.io.FilePath;
 import net.bodz.bas.c.string.StringPred;
 import net.bodz.bas.db.ctx.DataContext;
+import net.bodz.bas.db.ctx.IDataContextAware;
 import net.bodz.bas.db.ibatis.IMapperTemplate;
 import net.bodz.bas.err.IllegalUsageException;
 import net.bodz.bas.html.viz.HtmlViewOptions;
+import net.bodz.bas.http.ctx.RequestScope;
 import net.bodz.bas.i18n.dom.iString;
 import net.bodz.bas.meta.codegen.IndexedType;
 import net.bodz.bas.meta.decl.ObjectType;
@@ -17,16 +19,18 @@ import net.bodz.bas.repr.path.ITokenQueue;
 import net.bodz.bas.repr.path.PathArrival;
 import net.bodz.bas.repr.path.PathDispatchException;
 import net.bodz.bas.std.rfc.mime.ContentTypes;
+import net.bodz.lily.entity.Instantiables;
 
+@RequestScope
 @IndexedType
 public abstract class CoObjectIndex
-        implements IPathDispatchable {
+        implements IPathDispatchable, IDataContextAware {
 
     private Class<?> objectType;
     private HtmlViewOptions viewOptions = new HtmlViewOptions();
     private boolean pathEnd;
 
-    private DataContext dataContext;
+    protected DataContext dataContext;
 
     public CoObjectIndex() {
         this.objectType = getClass().getAnnotation(ObjectType.class).value();
@@ -40,14 +44,21 @@ public abstract class CoObjectIndex
         return viewOptions;
     }
 
+    @Override
     public DataContext getDataContext() {
         if (dataContext == null)
             throw new IllegalStateException("DataContext isn't set.");
         return dataContext;
     }
 
+    @Override
     public void setDataContext(DataContext dataContext) {
         this.dataContext = dataContext;
+        if (dataContext != null)
+            afterDataContextSet();
+    }
+
+    protected void afterDataContextSet() {
     }
 
     @Override
@@ -99,11 +110,9 @@ public abstract class CoObjectIndex
     @Override
     public String toString() {
         IType type = PotatoTypes.getInstance().forClass(getClass());
-        if (type == null)
-            throw new NullPointerException("type");
         iString label = type.getLabel();
         if (label == null)
-            throw new NullPointerException("label");
+            throw new IllegalUsageException("Index title (label) isn't specified on " + getClass());
         return label.toString();
     }
 
