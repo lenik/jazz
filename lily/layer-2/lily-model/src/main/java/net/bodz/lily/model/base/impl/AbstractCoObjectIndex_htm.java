91abd8490c62dca6a696508130c67abe8e6154e1
Lenik <pub.lenik@bodz.net>
2016-06-03 23:09:53 +0800

    lily-model: Added AbstractCoObjectIndex_htm.fn.co.initMask(m, ctx).

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/impl/AbstractCoObjectIndex_htm.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/impl/AbstractCoObjectIndex_htm.java
@@ -2,12 +2,18 @@ package net.bodz.lily.model.base.impl;
 
 import javax.servlet.http.HttpServletRequest;
 
+import net.bodz.bas.err.ParseException;
 import net.bodz.bas.html.viz.AbstractHtmlViewBuilder;
 import net.bodz.bas.html.viz.HtmlViewOptions;
+import net.bodz.bas.html.viz.IHtmlViewContext;
+import net.bodz.bas.repr.viz.ViewBuilderException;
 import net.bodz.bas.std.rfc.mime.ContentType;
+import net.bodz.bas.t.variant.VariantMaps;
+import net.bodz.lily.model.base.CoObject;
 import net.bodz.lily.model.base.CoObjectIndex;
+import net.bodz.lily.model.base.CoObjectMask;
 
-public abstract class AbstractCoObjectIndex_htm<T extends CoObjectIndex>
+public abstract class AbstractCoObjectIndex_htm<T extends CoObjectIndex<E>, E extends CoObject>
         extends AbstractHtmlViewBuilder<T> {
 
     public AbstractCoObjectIndex_htm(Class<?> valueClass) {
@@ -32,6 +38,27 @@ public abstract class AbstractCoObjectIndex_htm<T extends CoObjectIndex>
             return origin;
         else
             return super.isOrigin(value);
+    }
+
+    protected static interface fn
+            extends AbstractHtmlViewBuilder.fn {
+
+        class co {
+
+            public static <M extends CoObjectMask> M initMask(M mask, IHtmlViewContext ctx)
+                    throws ViewBuilderException {
+                HttpServletRequest request = ctx.getRequest();
+                // HttpSession session = request.getSession();
+                try {
+                    mask.readObject(VariantMaps.fromRequest(request));
+                } catch (ParseException e) {
+                    throw new ViewBuilderException(e.getMessage(), e);
+                }
+                return mask;
+            }
+
+        }
+
     }
 
 }
