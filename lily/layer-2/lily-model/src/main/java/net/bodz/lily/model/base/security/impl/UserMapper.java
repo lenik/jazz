047cff911f976afa8897e07630d67bfb8a99485c
Lenik <pub.lenik@bodz.net>
2016-06-03 23:07:51 +0800

    lily-model: Added UserMapper.findForLogin().

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/security/impl/UserMapper.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/security/impl/UserMapper.java
@@ -1,9 +1,15 @@
 package net.bodz.lily.model.base.security.impl;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import net.bodz.bas.db.ibatis.IMapperTemplate;
 import net.bodz.lily.model.base.security.User;
 
 public interface UserMapper
         extends IMapperTemplate<User, UserMask> {
+
+    List<User> findForLogin(@Param("m") UserMask mask);
 
 }
