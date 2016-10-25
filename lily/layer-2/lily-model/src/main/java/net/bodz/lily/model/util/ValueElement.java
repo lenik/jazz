bfae370cc476a12fe194d87cc3a9ffcbc00cc0b3
Lenik <pub.lenik@bodz.net>
2016-10-25 08:47:07 +0800

    lily-model: Added @AccessControl and ValueElement.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/util/ValueElement.java
@@ -0,0 +1,104 @@
+package net.bodz.lily.model.util;
+
+import org.json.JSONObject;
+import org.json.JSONWriter;
+
+import net.bodz.bas.err.NotImplementedException;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.fmt.json.IJsonSerializable;
+import net.bodz.bas.i18n.dom.iString;
+import net.bodz.bas.i18n.dom1.MutableElement;
+
+public class ValueElement
+        extends MutableElement
+        implements IJsonSerializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Class<? extends Number> valueType;
+    private Number value = 0;
+
+    /**
+     * For jQuery-flot charts.
+     */
+    public static final int FLOT = 0;
+
+    private int jsonFormat = FLOT;
+
+    public Number getValue() {
+        return value;
+    }
+
+    public void setValue(Number value) {
+        this.value = value;
+    }
+
+    @Override
+    public void readObject(JSONObject json)
+            throws ParseException {
+        switch (jsonFormat) {
+        case FLOT:
+            for (Object key : json.keySet()) {
+                String name = (String) key;
+                String str = (String) json.get(name);
+                switch (name) {
+                case "label":
+                    setLabel(iString.fn.val(str));
+                    break;
+                case "data":
+                    value = parseValue(str);
+                    break;
+                }
+            }
+            return;
+
+        default:
+            assert false;
+        }
+    }
+
+    @Override
+    public void writeObject(JSONWriter out) {
+        switch (jsonFormat) {
+        case FLOT:
+            out.object();
+            {
+                out.key("label");
+                out.value(getLabel());
+                out.key("data");
+                out.value(value);
+            }
+            out.endObject();
+            return;
+
+        default:
+            assert false;
+        }
+    }
+
+    public Number parseValue(String str)
+            throws ParseException {
+        if (str == null)
+            return null;
+
+        if (valueType == null) {
+            try {
+                if (str.contains(".")) {
+                    return Double.valueOf(str);
+                } else {
+                    long l = Long.valueOf(str);
+                    if (l >= Integer.MIN_VALUE && l <= Integer.MAX_VALUE)
+                        return (int) l;
+                    else
+                        return l;
+                }
+            } catch (NumberFormatException e) {
+                throw new ParseException(e.getMessage(), e);
+            }
+        }
+
+        // if (valueType == Integer.class) ...
+        throw new NotImplementedException();
+    }
+
+}
