f2a6c409a893c3db00b40f6f3f9be7bd2c5ff217
Lenik <pub.lenik@bodz.net>
2016-10-25 08:38:48 +0800

    bas: polishing.

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/java/security/Cryptos.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/java/security/Cryptos.java
@@ -1,9 +1,8 @@
 package net.bodz.bas.c.java.security;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
-import net.bodz.bas.err.UnexpectedException;
 
 public class Cryptos {
 
@@ -12,7 +11,7 @@ public class Cryptos {
             MessageDigest digest = MessageDigest.getInstance(alg);
             return digest;
         } catch (NoSuchAlgorithmException e) {
-            throw new UnexpectedException(String.format("Message digest %s isn\'t installed", alg));
+            throw new IllegalArgumentException(String.format("Message digest %s isn\'t installed", alg));
         }
     }
 
@@ -32,6 +31,46 @@ public class Cryptos {
     public static byte[] sha1(byte[] bin) {
         MessageDigest sha1 = getSHA1();
         return sha1.digest(bin);
+    }
+
+    public static String md5(String s) {
+        return md5(s, "utf-8");
+    }
+
+    public static String md5(String s, String encoding) {
+        try {
+            byte[] bin = s.getBytes(encoding);
+            byte[] md5 = md5(bin);
+            return toHex(md5);
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException("Invalid encoding", e);
+        }
+    }
+
+    public static String sha1(String s) {
+        return sha1(s, "utf-8");
+    }
+
+    public static String sha1(String s, String encoding) {
+        try {
+            byte[] bin = s.getBytes(encoding);
+            byte[] sha1 = sha1(bin);
+            return toHex(sha1);
+        } catch (UnsupportedEncodingException e) {
+            throw new IllegalArgumentException("Invalid encoding", e);
+        }
+    }
+
+    static char[] hextab = "0123456789abcdef".toCharArray();
+
+    static String toHex(byte... bin) {
+        StringBuilder sb = new StringBuilder(bin.length * 2);
+        for (int i = 0; i < bin.length; i++) {
+            int by = bin[i] & 0xff;
+            sb.append(hextab[by >> 4]);
+            sb.append(hextab[by & 0xf]);
+        }
+        return sb.toString();
     }
 
 }
