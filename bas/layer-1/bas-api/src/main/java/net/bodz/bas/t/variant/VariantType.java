50a3fb2b4396bc30b88abee9ea863499c110cd2b
Lenik <pub.lenik@bodz.net>
2016-12-29 20:44:50 +0800

    bas-api: Add VariantType.

---
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/t/variant/VariantType.java
@@ -0,0 +1,29 @@
+package net.bodz.bas.t.variant;
+
+public enum VariantType {
+
+    BYTE,
+
+    SHORT,
+
+    INT,
+
+    LONG,
+
+    FLOAT,
+
+    DOUBLE,
+
+    BOOLEAN,
+
+    CHAR,
+
+    STRING,
+
+    OBJECT,
+
+    BYTEARRAY,
+    
+    DATE,
+    
+}
