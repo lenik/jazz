d7a396148baf18c969256320b2c900f1683b3e81
Lenik <pub.lenik@bodz.net>
2016-12-29 20:08:28 +0800

    bas-meta: Add @ToDo, @NotTested.

---
+++ bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/source/NotTested.java
@@ -0,0 +1,10 @@
+package net.bodz.bas.meta.source;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@ToDo
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotTested {
+
+}
