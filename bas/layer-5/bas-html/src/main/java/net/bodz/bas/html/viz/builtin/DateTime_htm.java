8bc06ce160122e47413e006bfbccd9da9c030a8c
Lenik <pub.lenik@bodz.net>
2017-07-17 00:10:01 +0800

    bas-html: Joda DateTime htms.

---
+++ bas/layer-5/bas-html/src/main/java/net/bodz/bas/html/viz/builtin/DateTime_htm.java
@@ -0,0 +1,44 @@
+package net.bodz.bas.html.viz.builtin;
+
+import java.io.IOException;
+
+import org.joda.time.DateTime;
+
+import net.bodz.bas.html.io.IHtmlOut;
+import net.bodz.bas.html.io.tag.HtmlInput;
+import net.bodz.bas.html.util.FieldDeclToHtml;
+import net.bodz.bas.html.viz.IHtmlViewContext;
+import net.bodz.bas.potato.ref.UiPropertyRef;
+import net.bodz.bas.repr.form.FieldDeclBuilder;
+import net.bodz.bas.repr.form.IFieldDecl;
+import net.bodz.bas.repr.viz.ViewBuilderException;
+
+public class DateTime_htm
+        extends AbstractFormInput_htm<DateTime> {
+
+    static FieldDeclBuilder fieldDeclBuilder = new FieldDeclBuilder();
+
+    public DateTime_htm() {
+        super(DateTime.class);
+    }
+
+    @Override
+    public void buildHtmlView(IHtmlViewContext ctx, IHtmlOut out, UiPropertyRef<DateTime> ref, IFieldDecl fieldDecl)
+            throws ViewBuilderException, IOException {
+        DateTime dateTime = ref.get();
+
+        HtmlInput input = out.input().class_("noprint");
+
+        FieldDeclToHtml.apply(input, fieldDecl);
+
+        // When not supported, the browser defaults to the text input type.
+        // input.type("datetime"); // deprecated
+        input.type("date");
+        if (dateTime != null) {
+            String str = dateTime.toString("yyyy-MM-dd");
+            input.value(str);
+            out.span().class_("print").text(str);
+        }
+    }
+
+}
