d2decea08b9e836a2876e45cbaac331804ff56a1
Lenik <pub.lenik@bodz.net>
2017-07-22 07:54:50 +0800

    lily-model: Add SalesCategory/Phase.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/violet/sale/SalesPhase.java
@@ -0,0 +1,23 @@
+package net.bodz.violet.sale;
+
+import javax.persistence.Table;
+
+import net.bodz.lily.entity.IdType;
+import net.bodz.lily.model.base.CoCode;
+
+@IdType(Integer.class)
+@Table(name = "salephase")
+public class SalesPhase
+        extends CoCode<SalesPhase> {
+
+    private static final long serialVersionUID = 1L;
+
+    public SalesPhase() {
+        super();
+    }
+
+    public SalesPhase(SalesPhase parent) {
+        super(parent);
+    }
+
+}
