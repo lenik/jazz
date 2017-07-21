d7ecaee40c50a2b9cf3da40285b7eababab63e92
Lenik <pub.lenik@bodz.net>
2017-07-21 10:32:44 +0800

    bas-repr: ClassResourcePathDispatcher with higher priority.

--- bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/builtin/BuiltinPathDispatcherPriorities.java
+++ bas/layer-5/bas-repr/src/main/java/net/bodz/bas/repr/path/builtin/BuiltinPathDispatcherPriorities.java
@@ -2,14 +2,16 @@ package net.bodz.bas.repr.path.builtin;
 
 public class BuiltinPathDispatcherPriorities {
 
-    public static final int PRIORITY_OVERRIDEN = 0;
-    public static final int PRIORITY_CLASS_RESOURCE = 10;
-    public static final int PRIORITY_TREE_NODE = 11;
-    public static final int PRIORITY_MAP = 20;
-    public static final int PRIORITY_LIST = 20;
+    public static final int PRIORITY_CLASS_RESOURCE = 1;
+    public static final int PRIORITY_OVERRIDEN = 2;
+
+    public static final int PRIORITY_TREE_NODE = 20;
+    public static final int PRIORITY_LIST = 21;
+    public static final int PRIORITY_MAP = 22;
+
     public static final int PRIORITY_PROPERTY = 40;
-    public static final int PRIORITY_FIELD = 41;
-    public static final int PRIORITY_INVOCATION = 50;
+    public static final int PRIORITY_INVOCATION = 41;
+    public static final int PRIORITY_FIELD = 50;
     public static final int PRIORITY_METHOD = 51;
 
 }
