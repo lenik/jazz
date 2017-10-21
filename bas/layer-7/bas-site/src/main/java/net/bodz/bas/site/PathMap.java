caf7abfad5c81ea816e0e66c6d97bd4addc89808
Lenik <pub.lenik@bodz.net>
2017-10-22 07:28:36 +0800

    bas-site: Add PathMap.install(key, service).

--- bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/PathMap.java
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/PathMap.java
@@ -16,15 +16,19 @@ public class PathMap
     public void install(Map<String, ?> map) {
         for (Map.Entry<String, ?> entry : map.entrySet()) {
             String key = entry.getKey();
-            if (containsKey(key)) {
-                Object existing = get(key);
-                logger.error("Path already defined and skipped: %s of %s => %s", //
-                        key, existing == null ? null : existing.getClass(), existing);
-                continue;
-            }
-            Object value = entry.getValue();
-            put(key, value);
+            install(key, entry.getValue());
         }
+    }
+
+    public boolean install(String key, Object service) {
+        if (containsKey(key)) {
+            Object existing = get(key);
+            logger.error("Path already defined and skipped: %s of %s => %s", //
+                    key, existing == null ? null : existing.getClass(), existing);
+            return false;
+        }
+        put(key, service);
+        return true;
     }
 
 }
