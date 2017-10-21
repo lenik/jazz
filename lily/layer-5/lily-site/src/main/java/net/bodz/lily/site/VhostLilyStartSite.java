14dcea348fe6ea70ff29c4c1b6b23ceac0734ae6
Lenik <pub.lenik@bodz.net>
2017-10-22 07:32:24 +0800

    lily-site: Made LilyStartSite.dataContext accessible in subclass.

--- lily/layer-5/lily-site/src/main/java/net/bodz/lily/site/VhostLilyStartSite.java
+++ lily/layer-5/lily-site/src/main/java/net/bodz/lily/site/VhostLilyStartSite.java
@@ -1,10 +1,14 @@
 package net.bodz.lily.site;
 
+import net.bodz.bas.log.Logger;
+import net.bodz.bas.log.LoggerFactory;
 import net.bodz.bas.site.vhost.IVirtualHost;
 import net.bodz.bas.site.vhost.VhostDataContexts;
 
 public abstract class VhostLilyStartSite
         extends LilyStartSite {
+
+    static final Logger logger = LoggerFactory.getLogger(VhostLilyStartSite.class);
 
     IVirtualHost vhost;
 
