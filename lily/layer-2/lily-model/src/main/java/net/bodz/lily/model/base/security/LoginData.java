d3f95a8c8a1ccd9214771e3b2732c3404b75a10f
Lenik <pub.lenik@bodz.net>
2017-10-22 07:29:48 +0800

    lily-model: Mod. user login.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/security/LoginData.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/security/LoginData.java
@@ -3,17 +3,41 @@ package net.bodz.lily.model.base.security;
 import javax.servlet.http.HttpSession;
 
 import net.bodz.bas.http.ctx.CurrentHttpService;
-import net.bodz.bas.site.vhost.IVirtualHost;
 import net.bodz.bas.t.preorder.PrefixMap;
 
 public class LoginData {
 
     public static final String ATTRIBUTE_KEY = LoginData.class.getName();
 
-    public IVirtualHost virtualHost;
-    public User user;
+    User user;
+    PrefixMap<String> permissions = new PrefixMap<>();
 
-    public PrefixMap<String> permissions;
+    public LoginData() {
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public PrefixMap<String> getPermissions() {
+        return permissions;
+    }
+
+    public void saveInSession() {
+        HttpSession session = CurrentHttpService.getSessionOpt();
+        if (session == null)
+            throw new IllegalStateException("No session");
+        else
+            saveInSession(session);
+    }
+
+    public void saveInSession(HttpSession session) {
+        session.setAttribute(LoginData.ATTRIBUTE_KEY, this);
+    }
 
     public static LoginData fromSession() {
         HttpSession session = CurrentHttpService.getSessionOpt();
@@ -25,6 +49,16 @@ public class LoginData {
 
     public static LoginData fromSession(HttpSession session) {
         return (LoginData) session.getAttribute(LoginData.ATTRIBUTE_KEY);
+    }
+
+    public static void removeFromSession() {
+        HttpSession session = CurrentHttpService.getSessionOpt();
+        if (session != null)
+            removeFromSession(session);
+    }
+
+    public static void removeFromSession(HttpSession session) {
+        session.removeAttribute(LoginData.ATTRIBUTE_KEY);
     }
 
 }
