3d6d63ef1f4453b01a97804bf99835aad728861b
Lenik <pub.lenik@bodz.net>
2017-11-29 11:57:31 +0800

    lily: Add lily-uber.

---
+++ lily/build/lily-uber/src/main/java/net/bodz/lily/MainTypeCollector.java
@@ -0,0 +1,13 @@
+package net.bodz.lily;
+
+import net.bodz.bas.shell.util.TypeCollectorApp;
+
+public class MainTypeCollector
+        extends TypeCollectorApp {
+
+    public static void main(String[] args)
+            throws Exception {
+        new MainTypeCollector().execute(args);
+    }
+
+}
