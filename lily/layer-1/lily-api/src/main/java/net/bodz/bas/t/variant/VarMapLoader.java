46b196028bbc2cdd55cb5a91997c80a6c0c6d54c
Lenik <pub.lenik@bodz.net>
2017-11-29 23:46:47 +0800

    lily-api: VarMapLoader try convert in bi-di, load nested maps.

--- lily/layer-1/lily-api/src/main/java/net/bodz/bas/t/variant/VarMapLoader.java
+++ lily/layer-1/lily-api/src/main/java/net/bodz/bas/t/variant/VarMapLoader.java
@@ -1,5 +1,8 @@
 package net.bodz.bas.t.variant;
 
+import java.util.List;
+
+import net.bodz.bas.c.primitive.Primitives;
 import net.bodz.bas.err.LoaderException;
 import net.bodz.bas.log.Logger;
 import net.bodz.bas.log.LoggerFactory;
@@ -18,6 +21,7 @@ public class VarMapLoader {
 
     boolean autoCreate = true;
     char fieldSep = '.';
+    boolean skipNull = true;
 
     public void load(Class<?> clazz, Object obj, ILookupMap<String, ?> map)
             throws LoaderException {
@@ -36,6 +40,10 @@ public class VarMapLoader {
 
     void load(IType type, Object obj, String path, Object anyVal)
             throws LoaderException {
+        if (anyVal == null)
+            if (skipNull)
+                return;
+
         int pos = path.indexOf(fieldSep);
         String remain = null;
         if (pos != -1) {
@@ -46,11 +54,16 @@ public class VarMapLoader {
         IProperty property = type.getProperty(path);
         if (property == null) {
             // nothing yet.
-            logger.warn("Ignored non-existed property: " + path);
+            // logger.warn("Ignored non-existed property: " + path);
             return;
         }
 
         if (remain == null) {
+            if (!property.isWritable()) {
+                // can be transient or derived, and all
+                // logger.warn("Ignored readonly property: " + path);
+                return;
+            }
             loadProperty(obj, property, anyVal);
             return;
         }
@@ -94,16 +107,56 @@ public class VarMapLoader {
             }
     }
 
-    public void loadProperty(Object obj, IProperty property, Object anyVal)
+    public void loadProperty(Object obj, IProperty property, Object rval)
             throws LoaderException {
         if (obj == null)
             throw new NullPointerException("obj");
         if (property == null)
             throw new NullPointerException("property");
         Class<?> ltype = property.getPropertyType();
-        Class<?> rtype = anyVal.getClass();
-        IVarConverter<Object> converter = (IVarConverter<Object>) VarConverters.getConverter(rtype);
-        Object lval = converter.to(anyVal, ltype);
+        ltype = Primitives.box(ltype);
+        Class<?> rtype = rval.getClass();
+
+        Object lval;
+        do {
+            if (ltype == rtype) {
+                lval = rval;
+                break;
+            }
+
+            IVarConverter<Object> rconv = (IVarConverter<Object>) VarConverters.getConverter(rtype);
+            if (rconv != null && rconv.canConvertTo(ltype)) {
+                lval = rconv.to(rval, ltype);
+                break;
+            }
+
+            IVarConverter<Object> lconv = (IVarConverter<Object>) VarConverters.getConverter(ltype);
+            if (lconv != null && lconv.canConvertFrom(rtype)) {
+                lval = lconv.from(rtype, rval);
+                break;
+            }
+
+            try {
+                lval = property.getValue(obj);
+            } catch (ReflectiveOperationException e) {
+                throw new LoaderException("Failed to access object property: " + e.getMessage(), e);
+            }
+
+            if (rval instanceof ILookupMap) {
+                ILookupMap<String, ?> rmap = ILookupMap.class.cast(rval);
+                load(ltype, lval, rmap);
+                return;
+            }
+
+            if (rval instanceof List<?>) {
+                // TODO lval.autoCreate();
+            }
+
+            logger.warn(String.format(//
+                    "No converter for property %s from %s: %s.", property, rtype, rval));
+            return;
+        } while (false);
+
         try {
             property.setValue(obj, lval);
         } catch (ReflectiveOperationException e) {
