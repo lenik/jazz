0bf7a8bbc1b91a295b9c131b5f0f6daf52304c2e
Lenik <pub.lenik@bodz.net>
2017-11-29 23:21:55 +0800

    bas-t2: Separated VarConvExtensions to avoid init order problems.

--- bas/layer-2/bas-t2/src/main/java/net/bodz/bas/t/variant/conv/VarConverters.java
+++ bas/layer-2/bas-t2/src/main/java/net/bodz/bas/t/variant/conv/VarConverters.java
@@ -1,24 +1,17 @@
 package net.bodz.bas.t.variant.conv;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.NavigableSet;
-import java.util.ServiceLoader;
-import java.util.Set;
-import java.util.TreeSet;
 
 import net.bodz.bas.c.primitive.Primitives;
 import net.bodz.bas.c.type.IndexedTypes;
-import net.bodz.bas.err.IllegalUsageException;
+import net.bodz.bas.c.type.SingletonUtil;
 import net.bodz.bas.meta.codegen.IndexedTypeLoader;
-import net.bodz.bas.t.order.PriorityComparator;
 
 @IndexedTypeLoader
 public class VarConverters {
 
     static Map<Class<?>, IVarConverter<?>> map = new HashMap<>();
-    static Map<Class<?>, NavigableSet<IVarConverterExtension<?>>> extensions = new HashMap<>();
 
     static {
         load();
@@ -26,13 +19,7 @@ public class VarConverters {
 
     static void load() {
         for (Class<?> converterClass : IndexedTypes.list(IVarConverter.class, false)) {
-            IVarConverter<?> instance;
-            try {
-                Field field = converterClass.getField("instance");
-                instance = (IVarConverter<?>) field.get(null);
-            } catch (ReflectiveOperationException e) {
-                throw new IllegalUsageException("Failed to get the static converter instance.", e);
-            }
+            IVarConverter<?> instance = (IVarConverter<?>) SingletonUtil.getInstanceField(converterClass);
             Class<?> type = instance.getType();
             IVarConverter<?> prev = map.get(type);
             if (prev != null) {
@@ -41,16 +28,6 @@ public class VarConverters {
             }
             map.put(type, instance);
         }
-
-        for (IVarConverterExtension<?> ext : ServiceLoader.load(IVarConverterExtension.class)) {
-            Class<?> type = ext.getType();
-            NavigableSet<IVarConverterExtension<?>> set = extensions.get(type);
-            if (set == null) {
-                set = new TreeSet<>(PriorityComparator.INSTANCE);
-                extensions.put(type, set);
-            }
-            set.add(ext);
-        }
     }
 
     public static <T> IVarConverter<T> getConverter(Class<?> type) {
@@ -58,16 +35,6 @@ public class VarConverters {
             type = Primitives.box(type);
         IVarConverter<T> converter = (IVarConverter<T>) map.get(type);
         return converter;
-    }
-
-    @SuppressWarnings("unchecked")
-    public static <T> Set<IVarConverterExtension<T>> getExtensions(Class<T> type) {
-        NavigableSet<IVarConverterExtension<T>> set = (NavigableSet<IVarConverterExtension<T>>) (Object) extensions
-                .get(type);
-        if (set == null)
-            // Java 8: set = Collections.emptySortedSet();
-            set = new TreeSet<>();
-        return set;
     }
 
 }
