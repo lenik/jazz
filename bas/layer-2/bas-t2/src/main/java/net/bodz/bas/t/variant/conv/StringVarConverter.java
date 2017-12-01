8c6ff1649caf1365a0c070261745952ec0590692
Lenik <pub.lenik@bodz.net>
2017-12-01 22:58:19 +0800

    bas-t2: StringVarConverter take care of NaN and Infinity.

--- bas/layer-2/bas-t2/src/main/java/net/bodz/bas/t/variant/conv/StringVarConverter.java
+++ bas/layer-2/bas-t2/src/main/java/net/bodz/bas/t/variant/conv/StringVarConverter.java
@@ -22,13 +22,25 @@ public class StringVarConverter
 
     @Override
     public Number toNumber(String value) {
+        switch (value) {
+        case "NaN":
+            return Double.NaN;
+        case "Infinity":
+            return Double.POSITIVE_INFINITY;
+        case "-Infinity":
+            return Double.NEGATIVE_INFINITY;
+        }
+
+        boolean negative = value.startsWith("-");
+
         StringBuilder sb = new StringBuilder(value.length());
         int len = value.length();
         boolean exp = false;
         boolean decimal = false;
         boolean start0 = false;
         int radix = 10;
-        for (int i = 0; i < len; i++) {
+        int pos = 0;
+        for (int i = negative ? 1 : 0; i < len; i++, pos++) {
             char ch = value.charAt(i);
             switch (ch) {
             case ' ':
@@ -40,17 +52,17 @@ public class StringVarConverter
             case '.':
                 decimal = true;
             case '0':
-                if (i == 0) {
+                if (pos == 0) {
                     start0 = true;
                     radix = 8;
                 }
                 break;
             case 'b':
-                if (start0 && i == 1)
+                if (start0 && pos == 1)
                     radix = 2;
                 break;
             case 'x':
-                if (start0 && i == 1)
+                if (start0 && pos == 1)
                     radix = 16;
                 break;
             case 'e':
@@ -62,9 +74,15 @@ public class StringVarConverter
         String compact = sb.toString();
         try {
             if (decimal || exp) {
-                return Double.parseDouble(compact);
+                double fval = Double.parseDouble(compact);
+                if (negative)
+                    fval = -fval;
+                return fval;
             } else {
-                return Long.parseLong(compact, radix);
+                long lval = Long.parseLong(compact, radix);
+                if (negative)
+                    lval = -lval;
+                return lval;
             }
         } catch (NumberFormatException e) {
             throw new TypeConvertException("Failed to parse number: " + value, e);
