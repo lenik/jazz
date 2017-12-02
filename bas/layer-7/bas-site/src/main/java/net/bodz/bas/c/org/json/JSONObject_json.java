37e1e9fd78464fc6f44a7ab58ff46770846ea3cc
Lenik <pub.lenik@bodz.net>
2017-12-02 23:58:42 +0800

    bas-site: Auto render JSONObject, JSONStringer. Add HttpPayload.

---
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/c/org/json/JSONObject_json.java
@@ -0,0 +1,40 @@
+package net.bodz.bas.c.org.json;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONObject;
+
+import net.bodz.bas.http.viz.AbstractHttpViewBuilder;
+import net.bodz.bas.http.viz.IHttpViewContext;
+import net.bodz.bas.repr.viz.ViewBuilderException;
+import net.bodz.bas.std.rfc.mime.ContentType;
+import net.bodz.bas.std.rfc.mime.ContentTypes;
+import net.bodz.bas.ui.dom1.IUiRef;
+
+public class JSONObject_json
+        extends AbstractHttpViewBuilder<JSONObject> {
+
+    public JSONObject_json() {
+        super(JSONObject.class);
+    }
+
+    @Override
+    public ContentType getContentType(HttpServletRequest request, JSONObject value) {
+        return ContentTypes.application_json;
+    }
+
+    @Override
+    public Object buildHttpViewStart(IHttpViewContext ctx, HttpServletResponse resp, IUiRef<JSONObject> ref)
+            throws ViewBuilderException, IOException {
+        JSONObject jsonObj = ref.get();
+        String json = jsonObj.toString();
+        PrintWriter writer = resp.getWriter();
+        writer.print(json);
+        return null;
+    }
+
+}
