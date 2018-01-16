63e9e35e2d92c83d839e983159f0e24b08cf389a
Lenik <pub.lenik@bodz.net>
2018-01-17 00:23:26 +0800

    -- collect.

--- lily/build/lily-uber/src/test/java/net/bodz/lily/TestTypeCollector.java
+++ lily/build/lily-uber/src/test/java/net/bodz/lily/TestTypeCollector.java
@@ -1,9 +1,17 @@
 package net.bodz.lily;
 
+import java.util.List;
+
 import net.bodz.bas.shell.util.TypeCollectorApp;
 
 public class TestTypeCollector
         extends TypeCollectorApp {
+
+    @Override
+    protected void buildPackageList(List<String> packages) {
+        super.buildPackageList(packages);
+        packages.add("net.bodz.violet");
+    }
 
     public static void main(String[] args)
             throws Exception {
