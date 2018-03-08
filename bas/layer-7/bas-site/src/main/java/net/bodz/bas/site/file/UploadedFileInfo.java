a0fd9e2e6a7b2780a6137abcb88b2b74118cea7f
Lenik <pub.lenik@bodz.net>
2018-03-09 00:31:22 +0800

    bas-site: UploadedFileInfo make it a bean.

--- bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/file/UploadedFileInfo.java
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/site/file/UploadedFileInfo.java
@@ -7,17 +7,73 @@ import net.bodz.bas.fmt.json.JsonSupport;
 public class UploadedFileInfo
         extends JsonSupport {
 
-    public String name;
-    public String path;
-    public long size;
-    public String url;
-    public String thumbnail;
-    public String deleteUrl;
-    public String deleteType = "DELETE";
+    String name;
+    String path;
+    long size;
+    String url;
+    String thumbnail;
+    String deleteUrl;
+    String deleteType = "DELETE";
 
     public UploadedFileInfo(FileItem item) {
         name = item.getName();
         size = item.getSize();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getDeleteUrl() {
+        return deleteUrl;
+    }
+
+    public void setDeleteUrl(String deleteUrl) {
+        this.deleteUrl = deleteUrl;
+    }
+
+    public String getDeleteType() {
+        return deleteType;
+    }
+
+    public void setDeleteType(String deleteType) {
+        this.deleteType = deleteType;
     }
 
 }
