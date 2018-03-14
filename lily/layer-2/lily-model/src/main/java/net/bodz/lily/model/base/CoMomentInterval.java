056ce498869af7c41b746af8f4da17c896fb2be6
Lenik <pub.lenik@bodz.net>
2018-03-14 21:28:36 +0800

    lily-model: Add CoObject.setId_ for general.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoMomentInterval.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoMomentInterval.java
@@ -7,20 +7,17 @@ import org.joda.time.DateTime;
 import net.bodz.bas.db.ibatis.IncludeMapperXml;
 import net.bodz.bas.html.io.IHtmlOut;
 import net.bodz.bas.html.viz.IHtmlViewContext;
-import net.bodz.bas.repr.form.meta.FormInput;
 import net.bodz.bas.repr.form.meta.OfGroup;
 import net.bodz.bas.repr.form.meta.StdGroup;
-import net.bodz.lily.entity.IId;
 import net.bodz.lily.entity.IMomentInterval;
 
 @IncludeMapperXml
 public abstract class CoMomentInterval<Id>
-        extends CoObject
-        implements IMomentInterval, IId<Id> {
+        extends CoEntity<Id>
+        implements IMomentInterval {
 
     private static final long serialVersionUID = 1L;
 
-    private Id id;
     private DateTime beginDate;
     private DateTime endDate;
 
@@ -28,28 +25,12 @@ public abstract class CoMomentInterval<Id>
         beginDate = getCreationDate();
     }
 
-    @Override
-    public Class<Id> idType() {
-        return IId.fn._getIdType(getClass());
-    }
-
-    @FormInput(readOnly = true)
-    @Override
-    public Id getId() {
-        return id;
-    }
-
-    @Override
-    public void setId(Id id) {
-        this.id = id;
-    }
-
     /**
      * <p lang="zh">
      * 事件的开始时间（具体的发生时间，或期待的发生时间）。
      * <p>
      * 当未指定时，表示尚未开始或开始于无限远以前。
-     * 
+     *
      * @label Begin Date
      * @label.zh 开始时间
      */
@@ -69,7 +50,7 @@ public abstract class CoMomentInterval<Id>
      * 事件的结束时间（具体的完成时间，或期待的完成时间）。
      * <p>
      * 当未指定时，表示尚未结束或结束于无限远以后。
-     * 
+     *
      * @label End Date
      * @label.zh 结束时间
      */
