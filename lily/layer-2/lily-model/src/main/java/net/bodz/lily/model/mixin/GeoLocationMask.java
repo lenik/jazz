0db8cc9f5cef021a0a3892d474e5e7ba8c1b5c86
Lenik <pub.lenik@bodz.net>
2018-07-23 19:13:09 +0800

    lily-model: Add GeoLocation mixin class.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/mixin/GeoLocationMask.java
@@ -0,0 +1,81 @@
+package net.bodz.lily.model.mixin;
+
+import java.util.Map;
+
+import net.bodz.bas.err.LoaderException;
+import net.bodz.bas.t.range.DoubleRange;
+import net.bodz.bas.t.variant.IVarMapSerializable;
+import net.bodz.bas.t.variant.IVariantMap;
+
+/**
+ * @see GeoLocation
+ */
+public class GeoLocationMask
+        implements IVarMapSerializable {
+
+    GeoLocation refPoint;
+    Double distance;
+
+    DoubleRange longitudeRange;
+    DoubleRange latitudeRange;
+    Integer regionId;
+
+    public GeoLocation getRefPoint() {
+        return refPoint;
+    }
+
+    public void setRefPoint(GeoLocation refPoint) {
+        this.refPoint = refPoint;
+    }
+
+    public Double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(Double distance) {
+        this.distance = distance;
+    }
+
+    public DoubleRange getLongitudeRange() {
+        return longitudeRange;
+    }
+
+    public void setLongitudeRange(DoubleRange longitudeRange) {
+        this.longitudeRange = longitudeRange;
+    }
+
+    public DoubleRange getLatitudeRange() {
+        return latitudeRange;
+    }
+
+    public void setLatitudeRange(DoubleRange latitudeRange) {
+        this.latitudeRange = latitudeRange;
+    }
+
+    public Integer getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(Integer regionId) {
+        this.regionId = regionId;
+    }
+
+    /** ⇱ Implementation Of {@link IVarMapSerializable}. */
+    /* _____________________________ */static section.iface __VarMap__;
+
+    @Override
+    public void readObject(IVariantMap<String> map)
+            throws LoaderException {
+        distance = map.getDouble("distance");
+        regionId = map.getInt("region.id");
+    }
+
+    @Override
+    public void writeObject(Map<String, Object> map) {
+        if (distance != null)
+            map.put("distance", distance);
+        if (regionId != null)
+            map.put("region.id", regionId);
+    }
+
+}
