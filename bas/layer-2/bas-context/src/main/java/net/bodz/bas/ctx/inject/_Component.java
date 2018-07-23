85be01260bbe81d3c49c5dba928171ca5cf5bc5b
Lenik <pub.lenik@bodz.net>
2018-07-23 16:28:26 +0800

    -- merge fix.

--- bas/layer-2/bas-context/src/main/java/net/bodz/bas/ctx/inject/_Component.java
+++ bas/layer-2/bas-context/src/main/java/net/bodz/bas/ctx/inject/_Component.java
@@ -6,11 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 
 import net.bodz.bas.meta.codegen.IndexedType;
 import net.bodz.bas.meta.codegen.PublishDir;
-import net.bodz.bas.meta.meta.MetaAnnotation;
+import net.bodz.bas.meta.meta.Implicit;
 
 @Documented
 @IndexedType(publishDir = PublishDir.features, includeClass = false, includeAnnotation = true)
-@MetaAnnotation
+@Implicit
 @Retention(RetentionPolicy.RUNTIME)
 public @interface _Component {
 
