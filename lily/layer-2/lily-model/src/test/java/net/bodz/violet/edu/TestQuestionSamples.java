4f5815161f59d3ebeb958f3311fc186b63a385d6
Lenik <pub.lenik@bodz.net>
2018-08-01 18:12:50 +0800

    lily-model: Made testq sample.

--- lily/layer-2/lily-model/src/test/java/net/bodz/violet/edu/TestQuestionSamples.java
+++ lily/layer-2/lily-model/src/test/java/net/bodz/violet/edu/TestQuestionSamples.java
@@ -2,10 +2,12 @@ package net.bodz.violet.edu;
 
 public class TestQuestionSamples {
 
-    public static TestQuestion build() {
+    public static TestQuestion build(Course course) {
         TestQuestion a = new TestQuestion();
         a.setLabel("testQuestion-1");
-        a.setDescription("A testQuestion named testQuestion-1.");
+        a.setSubject("A testQuestion named testQuestion-1.");
+        a.setCourse(course);
+        a.setAnswer("answer-1");
         return a;
     }
 
