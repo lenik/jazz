6dc29c6c0a36fe2a4b018972f68c332bf6f34c9b
Lenik <pub.lenik@bodz.net>
2018-08-01 18:12:39 +0800

    lily-model: Add TableProfiles to pickSome/Any.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/util/mapper/TableProfiles.java
@@ -0,0 +1,64 @@
+package net.bodz.lily.util.mapper;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+import net.bodz.bas.c.type.TypeParam;
+import net.bodz.bas.db.ctx.DataContext;
+import net.bodz.bas.db.ibatis.IMapperTemplate;
+import net.bodz.bas.db.ibatis.sql.SelectOptions;
+import net.bodz.bas.t.range.LongRange;
+import net.bodz.lily.model.base.CoObjectMask;
+
+public class TableProfiles {
+
+    private DataContext context;
+
+    public TableProfiles(DataContext context) {
+        this.context = context;
+    }
+
+    public <entity_t, mask_t extends CoObjectMask, mapper_t extends IMapperTemplate<entity_t, mask_t>> //
+    entity_t pickAny(Class<mapper_t> mapperClass, String tableName) {
+        List<entity_t> some = pickSome(mapperClass, tableName, 1);
+        return some.isEmpty() ? null : some.get(0);
+    }
+
+    public <entity_t, mask_t extends CoObjectMask, mapper_t extends IMapperTemplate<entity_t, mask_t>> //
+    List<entity_t> pickSome(Class<mapper_t> mapperClass, String tableName, int limit) {
+        Class<mask_t> maskClass = TypeParam.infer1(mapperClass, IMapperTemplate.class, 1);
+
+        mapper_t mapper = context.getMapper(mapperClass);
+        ProfileMapper prof = context.getMapper(ProfileMapper.class);
+
+        LongRange range = prof.findIdRange(tableName);
+        if (range == null)
+            return Collections.emptyList();
+        range.end++; // so exclusive.
+
+        Random random = new Random();
+        long w = range.end - range.start;
+        long any = Math.abs(random.nextLong() % w);
+        if (any < 0)
+            any += w;
+        any += range.start;
+
+        mask_t m;
+        try {
+            m = maskClass.newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        LongRange idRange = new LongRange();
+        idRange.setEnd(any + 1);
+        m.setIdRange(idRange);
+
+        SelectOptions opts = new SelectOptions();
+        opts.order("id", false);
+        opts.page(limit, 0);
+        List<entity_t> list = mapper.filter(m, opts);
+        return list;
+    }
+
+}
