42b4bd5076ddf3102f82ec96e9fbd9ba2cb739bd
Lenik <pub.lenik@bodz.net>
2018-08-27 09:24:44 +0800

    lily-model: -- updates.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoNodeMask.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/CoNodeMask.java
@@ -3,7 +3,16 @@ package net.bodz.lily.model.base;
 public class CoNodeMask
         extends CoObjectMask {
 
+    public Long parentId;
     public Integer maxDepth;
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
 
     public Integer getMaxDepth() {
         return maxDepth;
