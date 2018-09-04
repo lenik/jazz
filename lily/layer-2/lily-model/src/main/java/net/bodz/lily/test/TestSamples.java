8215cb00df281211813c75818636d1ed87477917
Lenik <pub.lenik@bodz.net>
2018-09-04 14:22:44 +0800

    -- indexed.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/test/TestSamples.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/test/TestSamples.java
@@ -2,7 +2,10 @@ package net.bodz.lily.test;
 
 import java.util.Random;
 
-public class TestSamples {
+import net.bodz.bas.meta.codegen.IndexedType;
+
+@IndexedType
+public abstract class TestSamples {
 
     protected static final Random random = new Random();
 
