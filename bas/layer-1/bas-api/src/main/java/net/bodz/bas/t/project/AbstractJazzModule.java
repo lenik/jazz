4594b27ce0699305818722260834e2f875154f98
Lenik <pub.lenik@bodz.net>
2018-09-05 18:21:00 +0800

    bas-api: AbstractJazzModule loads class list.

--- bas/layer-1/bas-api/src/main/java/net/bodz/bas/t/project/AbstractJazzModule.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/t/project/AbstractJazzModule.java
@@ -1,5 +1,11 @@
 package net.bodz.bas.t.project;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -15,13 +21,35 @@ public abstract class AbstractJazzModule
 
     private Set<String> managedClassNames = new TreeSet<>();
 
+    static Charset utf8 = Charset.forName("utf-8");
+
     public AbstractJazzModule() {
+        String fqcn = getClass().getName();
         String baseName = getClass().getSimpleName();
         if (baseName.endsWith("Module"))
             baseName = baseName.substring(0, baseName.length() - 6);
 
         this.name = baseName;
         this.packageName = getClass().getPackage().getName();
+
+        URL listFile = getClass().getResource("/META-INF/modules/" + fqcn);
+        if (listFile != null)
+            try {
+                InputStream in = listFile.openStream();
+                InputStreamReader isr = new InputStreamReader(in, utf8);
+                BufferedReader reader = new BufferedReader(isr);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (line.isEmpty())
+                        continue;
+                    if (line.startsWith("#"))
+                        continue;
+                    managedClassNames.add(line);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
     }
 
     @Override
