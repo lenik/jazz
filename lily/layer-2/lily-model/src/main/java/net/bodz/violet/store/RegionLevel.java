a08b138580f56ff0269d43fd3e811ec97ea37a94
Lenik <pub.lenik@bodz.net>
2019-01-25 09:27:33 +0900

    lily-model: Add RichProperties.

--- lily/layer-2/lily-model/src/main/java/net/bodz/violet/store/RegionLevel.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/violet/store/RegionLevel.java
@@ -4,6 +4,7 @@ import javax.persistence.Table;
 
 import net.bodz.lily.entity.IdType;
 import net.bodz.lily.model.base.CoCode;
+import net.bodz.lily.template.RichProperties;
 
 /**
  * 区域级别
@@ -19,5 +20,12 @@ public class RegionLevel
     public static final int ID_Place = 1;
     public static final int ID_Room = 2;
     public static final int ID_Shelf = 3;
+
+    RichProperties properties = new RichProperties();
+
+    @Override
+    public RichProperties getProperties() {
+        return properties;
+    }
 
 }
