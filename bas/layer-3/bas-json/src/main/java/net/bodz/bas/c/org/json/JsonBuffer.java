0171a6eb474f75cb796589742cc8ddf0f6930ab2
Lenik <pub.lenik@bodz.net>
2019-09-24 21:53:08 +0800

    bas-json: Add JsonWriter.buffer() shortcut.

---
+++ bas/layer-3/bas-json/src/main/java/net/bodz/bas/c/org/json/JsonBuffer.java
@@ -0,0 +1,37 @@
+package net.bodz.bas.c.org.json;
+
+import java.io.StringWriter;
+
+import org.json.JSONException;
+
+import net.bodz.bas.fmt.json.JsonObject;
+
+public class JsonBuffer
+        extends JsonWriter {
+
+    StringWriter sw;
+
+    public JsonBuffer() {
+        this(new StringWriter());
+    }
+
+    JsonBuffer(StringWriter buf) {
+        super(buf);
+        this.sw = buf;
+    }
+
+    @Override
+    public String toString() {
+        return sw.toString();
+    }
+
+    public JsonObject reconstruct() {
+        String json = sw.toString();
+        try {
+            return new JsonObject(json);
+        } catch (JSONException e) {
+            throw new RuntimeException("Failed to re-construct " + json, e);
+        }
+    }
+
+}
