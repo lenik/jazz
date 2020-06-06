68880e4394df5500e827345fb37508a5f0e6063a
Lenik <pub.lenik@bodz.net>
2020-06-06 20:58:43 +0800

    lily-model: Moved VeriCodeGen* to lily-impl.

--- lily/layer-5/lily-impl/src/test/java/net/bodz/lily/security/login/VerificationCodeChallengerTest.java
+++ lily/layer-5/lily-impl/src/test/java/net/bodz/lily/security/login/VerificationCodeChallengerTest.java
@@ -1,28 +1,30 @@
-package net.bodz.lily.security;
+package net.bodz.lily.security.login;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-public class VerificationCodeGeneratorTest
+import net.bodz.lily.security.login.VerificationCodeChallenger;
+
+public class VerificationCodeChallengerTest
         extends Assert {
 
     int window = 300;
-    int slots = 3;
+    int slots = VerificationCodeChallenger.NSLOT;
     int delta = window / slots;
-    VerificationCodeGenerator vcg = new VerificationCodeGenerator(window, slots);
+    VerificationCodeChallenger vcg = new VerificationCodeChallenger("KeY", window);
 
     @Test
-    public void testSearchBackwards()
+    public void testRcheck()
             throws InterruptedException {
         long time = System.currentTimeMillis();
-        String code = vcg.generate(time, 10);
+        String code = vcg.computeAt(time);
         int safeSlots = 1;
         for (int i = 0; i <= (slots + safeSlots); i++) {
-            int s = vcg.searchBackwards(time + delta * i, 10, code);
+            int s = vcg.rcheckAt(time + delta * i, code);
             assertEquals(i, s);
         }
         long boundary = time + delta * (slots + safeSlots + 1);
-        int s = vcg.searchBackwards(boundary, 10, code);
+        int s = vcg.rcheckAt(boundary, code);
         assertEquals(-1, s);
     }
 
@@ -31,11 +33,11 @@ public class VerificationCodeGeneratorTest
         int window = 100;
         int slots = 3;
         int delta = window / slots;
-        VerificationCodeGenerator vcg = new VerificationCodeGenerator(window, slots);
+        VerificationCodeChallenger vcg = new VerificationCodeChallenger("KeY", window);
         for (int i = 0; i < slots * 10; i++) {
             long time = System.currentTimeMillis();
             long idx = vcg.getIndex(time);
-            String s = vcg.generate(time, 10);
+            String s = vcg.computeAt(time);
             System.out.println(idx + ": " + s);
             Thread.sleep(delta / 3);
         }
@@ -44,20 +46,19 @@ public class VerificationCodeGeneratorTest
     public static void main(String[] args)
             throws Exception {
         int window = 10000;
-        int slots = 10;
-        VerificationCodeGenerator vcg = new VerificationCodeGenerator(window, slots);
+        VerificationCodeChallenger vcg = new VerificationCodeChallenger("KeY", window);
 
         long time = System.currentTimeMillis();
         long index = vcg.getIndex(time);
-        String code = vcg.generate(time, 10);
+        String code = vcg.computeAt(time);
         System.out.printf("Current time %d, index %d, code %s.\n", time, index, code);
 
         for (int i = 0; i < 10; i++) {
-            String c = vcg.generateForIndex(index + i, 10);
+            String c = vcg.mix(index + i);
             System.out.printf("    idx+%d: %s\n", i, c);
         }
 
-        int s = vcg.searchBackwards(time, 10, "927827");
+        int s = vcg.rcheckAt(time, "491727");
         System.out.println("Last slot: " + s);
     }
 
