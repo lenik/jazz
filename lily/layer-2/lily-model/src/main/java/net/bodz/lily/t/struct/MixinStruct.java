dc6ff01a27c51a3f901ce665f64969bd096375ac
Lenik <pub.lenik@bodz.net>
2020-06-22 06:33:26 +0800

    bas-json: Add JsonStruct.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/t/struct/MixinStruct.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/t/struct/MixinStruct.java
@@ -1,34 +1,10 @@
 package net.bodz.lily.t.struct;
 
-import java.io.Serializable;
-
-import net.bodz.bas.fmt.json.IJsonSerializable;
-import net.bodz.bas.fmt.json.JsonFn;
+import net.bodz.bas.fmt.json.JsonStruct;
 
 public abstract class MixinStruct
-        implements IJsonSerializable, Serializable {
+        extends JsonStruct {
 
     private static final long serialVersionUID = 1L;
-
-    @Override
-    public String toString() {
-        return JsonFn.toJson(this);
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj.getClass() != this.getClass())
-            return false;
-        MixinStruct o = (MixinStruct) obj;
-        String js1 = JsonFn.toJson(this);
-        String js2 = JsonFn.toJson(o);
-        return js1.equals(js2);
-    }
-
-    @Override
-    public int hashCode() {
-        String json = JsonFn.toJson(this);
-        return json.hashCode();
-    }
 
 }
