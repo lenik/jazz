d9dd5f1983746dc3d0c55073dbef44a398f171b1
Lenik <pub.lenik@bodz.net>
2020-06-22 06:33:26 +0800

    bas-json: Add JsonStruct.

---
+++ bas/layer-3/bas-json/src/main/java/net/bodz/bas/fmt/json/JsonStruct.java
@@ -0,0 +1,39 @@
+package net.bodz.bas.fmt.json;
+
+import java.io.Serializable;
+
+public abstract class JsonStruct
+        implements IJsonSerializable, Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj != this)
+            return false;
+        if (obj.getClass() != this.getClass())
+            return false;
+        JsonStruct o = (JsonStruct) obj;
+        return jsonEquals(o);
+    }
+
+    public boolean jsonEquals(JsonStruct o) {
+        String js1 = JsonFn.toJson(this);
+        String js2 = JsonFn.toJson(o);
+        if (js1.equals(js2))
+            return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        String json = JsonFn.toJson(this);
+        return json.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return JsonFn.toJson(this);
+    }
+
+}
