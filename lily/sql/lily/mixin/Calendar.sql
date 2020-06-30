7e2493fdfdb3daaa08a9989a5a64ad1355f21c6b
Lenik <pub.lenik@bodz.net>
2020-07-01 01:00:41 +0800

    lily: Set default CalendarDate fields be 0 or -1.

--- lily/sql/lily/mixin/Calendar.sql
+++ lily/sql/lily/mixin/Calendar.sql
@@ -1,8 +1,8 @@
-        $1year        smallint not null default -1,    --
-        $1month       smallint not null default -1,    -- 1 for jan
-        $1day         smallint not null default -1,    -- 1..
-        $1week        smallint not null default -1,    -- 1..54
-        $1weekday     smallint not null default -1,    -- 1..7 for mon..sun
+        $1year        smallint not null default 0,      --
+        $1month       smallint not null default 0,      -- 1 for jan
+        $1day         smallint not null default 0,      -- 1..
+        $1week        smallint not null default 0,      -- 1..54
+        $1weekday     smallint not null default 0,      -- 1..7 for mon..sun
         $1hour        smallint not null default -1,    -- 0-23
         $1minute      smallint not null default -1,    -- 0-59
         $1second      smallint not null default -1,    -- 0-59
