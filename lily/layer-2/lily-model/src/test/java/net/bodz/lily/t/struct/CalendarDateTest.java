85d1fd2beb257a36196e1fa4b3fa68f79b0d386e
Lenik <pub.lenik@bodz.net>
2020-07-20 21:31:15 +0800

    lily-model: CalendarDate.

--- lily/layer-2/lily-model/src/test/java/net/bodz/lily/t/struct/CalendarDateTest.java
+++ lily/layer-2/lily-model/src/test/java/net/bodz/lily/t/struct/CalendarDateTest.java
@@ -1,5 +1,6 @@
 package net.bodz.lily.t.struct;
 
+import org.joda.time.DateTime;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -14,6 +15,22 @@ public class CalendarDateTest
         CalendarDate date = new CalendarDate();
         date.setAsString("1922-4-05w7-1 13:04:56");
         System.out.println(date);
+    }
+
+    public static void main(String[] args)
+            throws ParseException {
+        CalendarDate wed = new CalendarDate();
+        wed.setAsString("--w-3 ::");
+        DateTime time = new DateTime();
+        for (int i = 0; i < 100; i++) {
+            DateTime future = wed.alignFrom(time);
+            time = future.plusDays(1);
+
+            int week = future.getWeekOfWeekyear();
+            int weekDay = future.getDayOfWeek();
+
+            System.out.println(future + " Week " + week + "/" + weekDay);
+        }
     }
 
 }
