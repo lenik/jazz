9bba44c6ee1c13cffe0e0ad6d1406189ea48433f
Lenik <pub.lenik@bodz.net>
2020-08-17 05:56:16 +0800

    lily-impl: Moved login apis to lily-model.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/security/login/ILoginCryptoConsts.java
@@ -0,0 +1,17 @@
+package net.bodz.lily.security.login;
+
+import net.bodz.bas.crypto.trans.IFlyingSupport;
+import net.bodz.bas.crypto.trans.fn.PartialMd5Transformer;
+import net.bodz.bas.sugar.IConstants;
+
+public interface ILoginCryptoConsts
+        extends IConstants, IFlyingSupport {
+
+    PartialMd5Transformer SHORT_TEXT = tr.partialMd5(6, 10);
+
+    class X {
+        void f() {
+        }
+    }
+
+}
