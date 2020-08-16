8061378aa8020e81d2252f68282d1a27f71c22f4
Lenik <pub.lenik@bodz.net>
2020-08-17 05:54:46 +0800

    bas-crypto: Some moved from lily-impl.

--- bas/layer-1/bas-api/src/main/java/net/bodz/bas/fn/AbstractTransformer.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/fn/AbstractTransformer.java
@@ -3,8 +3,8 @@ package net.bodz.bas.fn;
 public abstract class AbstractTransformer<S, T>
         implements ITransformer<S, T> {
 
-    public <Y> ITransformer<S, Y> join(ITransformer<? super T, Y> o) {
-        return new ConjunctTransformer<>(this, o);
+    public <Y> ITransformer<S, Y> join(ITransformer<? super T, Y> second) {
+        return new ConjunctTransformer<>(this, second);
     }
 
 }
