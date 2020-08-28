e89f740855474e5dc2d644cf9c61374e89ad7ad1
Lenik <pub.lenik@bodz.net>
2020-08-28 22:01:38 +0800

    bas-c1: Add Iso8601-compat, required by android 5.

---
+++ bas/layer-1/bas-c1/src/test/java/net/bodz/bas/c/java/util/Iso8601DateFormatCompatTest.java
@@ -0,0 +1,32 @@
+package net.bodz.bas.c.java.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.junit.Test;
+
+import net.bodz.bas.c.java.util.Iso8601DateFormatCompat;
+
+public class Iso8601DateFormatCompatTest {
+
+    @Test
+    public void testSamples()
+            throws ParseException {
+        // SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        SimpleDateFormat sdf = new Iso8601DateFormatCompat();
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+
+        assertEquals("1970-01-01T08:00:00.000+08:00", sdf.format(new Date(0)));
+        assertEquals(0L, sdf.parse("1970-01-01T08:00:00.000+08:00").getTime());
+
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+
+        assertEquals("1970-01-01T00:00:00.000Z", sdf.format(new Date(0)));
+        assertEquals(0L, sdf.parse("1970-01-01T00:00:00.000Z").getTime());
+    }
+
+}
