bff5ba63c25e176f86fd2242eeb887b80e2f05dd
Lenik <pub.lenik@bodz.net>
2021-09-05 00:17:33 +0800

    bas-script: Refactor ValueFn.dumpValue to ValueDumper. To allow mixed contents.

---
+++ bas/layer-5/bas-script/src/main/java/net/bodz/bas/script/js/ValueDumper.java
@@ -0,0 +1,160 @@
+package net.bodz.bas.script.js;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.graalvm.polyglot.SourceSection;
+import org.graalvm.polyglot.Value;
+import org.graalvm.polyglot.proxy.Proxy;
+
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.fmt.json.IJsonOut;
+import net.bodz.bas.fmt.json.obj.BeanJsonDumper;
+
+public class ValueDumper
+        extends BeanJsonDumper {
+
+    public ValueDumper(IJsonOut out) {
+        super(out);
+    }
+
+    @Override
+    protected void formatObjectMembers(Class<?> type, Object obj, int depth)
+            throws IOException, FormatException {
+        if (type != Value.class) {
+            super.formatObjectMembers(type, obj, depth);
+            return;
+        }
+
+        Value value = (Value) obj;
+
+        if (value.canExecute())
+            out.entry("canExecute", true);
+
+        if (value.canInstantiate())
+            out.entry("canInstantiate", true);
+
+        if (value.isBoolean())
+            out.entry("a-boolean", value.asBoolean());
+
+        if (value.isDate())
+            out.entry("a-date", value.asDate());
+
+        if (value.isDuration())
+            out.entry("a-duration", value.asDuration());
+
+        if (value.isException()) {
+            try {
+                value.throwException();
+            } catch (Throwable e) {
+                out.entry("a-exception", e);
+            }
+        }
+
+        if (value.isInstant())
+            out.entry("a-instant", value.asInstant());
+
+        if (value.isMetaObject()) {
+            out.entry("metaQualifiedName", value.getMetaQualifiedName());
+            out.entry("metaSimpleName", value.getMetaSimpleName());
+            // out.key("metaObject");
+            // Value metaObject = value.getMetaObject();
+            // dumper.dumpBoxed(metaObject);
+        }
+
+        if (value.isNativePointer())
+            out.entry("a-nativePointer", value.asNativePointer());
+
+        if (value.isNull())
+            out.entry("a-null", true);
+
+        if (value.isNumber())
+            out.entry("a-number", value.as(Number.class));
+
+        if (value.isProxyObject()) {
+            out.key("proxyObject");
+            Proxy proxyObject = value.asProxyObject();
+
+            String name = "~proxyObject";
+            markset.enter(name);
+            _dumpOnce(true, proxyObject, depth + 1, name);
+            markset.leave();
+        }
+
+        if (value.isString())
+            out.entry("a-string", value.asString());
+
+        if (value.isTime())
+            out.entry("a-time", value.asTime());
+
+        if (value.isTimeZone())
+            out.entry("a-timeZone", value.asTimeZone());
+
+        List<String> fits = new ArrayList<>();
+        {
+            if (value.fitsInByte())
+                fits.add("byte");
+            if (value.fitsInShort())
+                fits.add("short");
+            if (value.fitsInInt())
+                fits.add("int");
+            if (value.fitsInLong())
+                fits.add("long");
+            if (value.fitsInFloat())
+                fits.add("float");
+            if (value.fitsInDouble())
+                fits.add("double");
+            if (!fits.isEmpty())
+                out.entry("fits", fits.toString());
+        }
+
+        if (value.hasArrayElements()) {
+            out.key("array");
+            out.object();
+            long size = value.getArraySize();
+            out.entry("size", size);
+            out.key("elements");
+            out.array();
+            for (long i = 0; i < size; i++) {
+                String name = "[" + i + "]";
+                Value element = value.getArrayElement(i);
+                dumpBoxedWithName(name, element);
+            }
+            out.endArray();
+            out.endObject();
+        }
+
+        if (value.hasMembers()) {
+            out.key("members");
+            out.object();
+            for (String key : value.getMemberKeys()) {
+                out.key(key);
+                Value member = value.getMember(key);
+                dumpBoxedWithName(key, member);
+            }
+            out.endObject();
+        }
+
+        if (value.isHostObject()) {
+            Object hostObject = value.asHostObject();
+            out.key("hostObject");
+            dumpBoxedWithName("~hostObject", hostObject);
+        }
+
+        SourceSection sourceLocation = value.getSourceLocation();
+        if (sourceLocation != null) {
+            out.key("sourceLocation");
+            out.object();
+            out.entry("start", //
+                    sourceLocation.getStartLine() + ":" + sourceLocation.getStartColumn() + "/"
+                            + sourceLocation.getCharIndex());
+            out.entry("end", sourceLocation.getEndLine() + ":" + sourceLocation.getEndColumn() + "/"
+                    + sourceLocation.getCharEndIndex());
+            out.entry("length", sourceLocation.getCharLength());
+            out.entry("chars", sourceLocation.getCharacters());
+            out.endObject();
+        }
+    }
+
+}
--- bas/layer-5/bas-script/src/main/java/net/bodz/bas/script/js/ValueFn.java
+++ bas/layer-5/bas-script/src/main/java/net/bodz/bas/script/js/ValueFn.java
@@ -1,162 +1,31 @@
 package net.bodz.bas.script.js;
 
 import java.io.IOException;
-import java.io.StringWriter;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.graalvm.polyglot.SourceSection;
 import org.graalvm.polyglot.Value;
-import org.graalvm.polyglot.proxy.Proxy;
 
-import net.bodz.bas.c.org.json.JsonWriter;
+import net.bodz.bas.c.org.json.JsonBuffer;
 import net.bodz.bas.err.FormatException;
 import net.bodz.bas.err.UnexpectedException;
 import net.bodz.bas.fmt.json.IJsonOut;
-import net.bodz.bas.fmt.json.obj.BeanJsonDumper;
 
 public class ValueFn {
 
     public static String dumpBoxed(Value value)
             throws FormatException {
-        StringWriter buf = new StringWriter(10000);
-        JsonWriter out = new JsonWriter(buf);
+        JsonBuffer out = new JsonBuffer();
         try {
             dumpBoxed(out, value);
         } catch (IOException e) {
             throw new UnexpectedException(e);
         }
-        return buf.toString();
+        return out.toString();
     }
 
     public static void dumpBoxed(IJsonOut out, Value value)
             throws IOException, FormatException {
-        BeanJsonDumper dumper = new BeanJsonDumper(out);
-        out.object();
-
-        if (value.canExecute())
-            out.entry("canExecute", true);
-
-        if (value.canInstantiate())
-            out.entry("canInstantiate", true);
-
-        if (value.isBoolean())
-            out.entry("a-boolean", value.asBoolean());
-
-        if (value.isDate())
-            out.entry("a-date", value.asDate());
-
-        if (value.isDuration())
-            out.entry("a-duration", value.asDuration());
-
-        if (value.isException()) {
-            try {
-                value.throwException();
-            } catch (Throwable e) {
-                out.entry("a-exception", e);
-            }
-        }
-
-        if (value.isInstant())
-            out.entry("a-instant", value.asInstant());
-
-        if (value.isMetaObject()) {
-            out.entry("metaQualifiedName", value.getMetaQualifiedName());
-            out.entry("metaSimpleName", value.getMetaSimpleName());
-            // out.key("metaObject");
-            // Value metaObject = value.getMetaObject();
-            // dumper.dumpBoxed(metaObject);
-        }
-
-        if (value.isNativePointer())
-            out.entry("a-nativePointer", value.asNativePointer());
-
-        if (value.isNull())
-            out.entry("a-null", true);
-
-        if (value.isNumber())
-            out.entry("a-number", value.as(Number.class));
-
-        if (value.isProxyObject()) {
-            out.key("proxyObject");
-            Proxy proxyObject = value.asProxyObject();
-            dumper.dumpBoxed(proxyObject);
-        }
-
-        if (value.isString())
-            out.entry("a-string", value.asString());
-
-        if (value.isTime())
-            out.entry("a-time", value.asTime());
-
-        if (value.isTimeZone())
-            out.entry("a-timeZone", value.asTimeZone());
-
-        List<String> fits = new ArrayList<>();
-        {
-            if (value.fitsInByte())
-                fits.add("byte");
-            if (value.fitsInShort())
-                fits.add("short");
-            if (value.fitsInInt())
-                fits.add("int");
-            if (value.fitsInLong())
-                fits.add("long");
-            if (value.fitsInFloat())
-                fits.add("float");
-            if (value.fitsInDouble())
-                fits.add("double");
-            if (!fits.isEmpty())
-                out.entry("fits", fits.toString());
-        }
-
-        if (value.hasArrayElements()) {
-            out.key("array");
-            out.object();
-            long size = value.getArraySize();
-            out.entry("size", size);
-            out.key("elements");
-            out.array();
-            for (long i = 0; i < size; i++) {
-                Value element = value.getArrayElement(i);
-                dumpBoxed(out, element);
-            }
-            out.endArray();
-            out.endObject();
-        }
-
-        if (value.hasMembers()) {
-            out.key("members");
-            out.object();
-            for (String key : value.getMemberKeys()) {
-                out.key(key);
-                Value member = value.getMember(key);
-                dumpBoxed(out, member);
-            }
-            out.endObject();
-        }
-
-        if (value.isHostObject()) {
-            Object hostObject = value.asHostObject();
-            out.key("hostObject");
-            dumper.dumpBoxed(hostObject);
-        }
-
-        SourceSection sourceLocation = value.getSourceLocation();
-        if (sourceLocation != null) {
-            out.key("sourceLocation");
-            out.object();
-            out.entry("start", //
-                    sourceLocation.getStartLine() + ":" + sourceLocation.getStartColumn() + "/"
-                            + sourceLocation.getCharIndex());
-            out.entry("end", sourceLocation.getEndLine() + ":" + sourceLocation.getEndColumn() + "/"
-                    + sourceLocation.getCharEndIndex());
-            out.entry("length", sourceLocation.getCharLength());
-            out.entry("chars", sourceLocation.getCharacters());
-            out.endObject();
-        }
-
-        out.endObject();
+        ValueDumper dumper = new ValueDumper(out);
+        dumper.dumpBoxed(value);
     }
 
     public static Object convert(Value value) {
