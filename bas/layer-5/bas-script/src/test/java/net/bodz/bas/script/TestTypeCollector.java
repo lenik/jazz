fb5c8bbbceedaef5f4f4462695e32dbc807238f5
Lenik <pub.lenik@bodz.net>
2021-09-04 20:31:50 +0800

    bas-script: Add ValueFn.dump(Value) and SimpleJsRunner.

---
+++ bas/layer-5/bas-script/src/test/java/net/bodz/bas/script/TestTypeCollector.java
@@ -0,0 +1,13 @@
+package net.bodz.bas.script;
+
+import net.bodz.bas.shell.util.TypeCollectorApp;
+
+public class TestTypeCollector
+        extends TypeCollectorApp {
+
+    public static void main(String[] args)
+            throws Exception {
+        new TestTypeCollector().execute(args);
+    }
+
+}
