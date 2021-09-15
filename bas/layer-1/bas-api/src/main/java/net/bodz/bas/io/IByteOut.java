e5e9e5a92dd6f9ff0ec336545daad61769c3ce71
Lenik <pub.lenik@bodz.net>
2021-09-16 06:55:27 +0800

    bas-api: IByteOut as functional interface.

--- bas/layer-1/bas-api/src/main/java/net/bodz/bas/io/IByteOut.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/io/IByteOut.java
@@ -3,22 +3,32 @@ package net.bodz.bas.io;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
+@FunctionalInterface
 public interface IByteOut
-        extends ISimpleByteOut, IFlushable, ICloseable {
+        extends
+            ISimpleByteOut,
+            IFlushable,
+            ICloseable {
 
     /**
      * @throws NullPointerException
      *             If <code>buf</code> is <code>null</code>.
      */
-    void write(byte[] buf)
-            throws IOException;
+    default void write(byte[] buf)
+            throws IOException {
+        write(buf, 0, buf.length);
+    }
 
     /**
      * @throws NullPointerException
      *             If <code>buf</code> is <code>null</code>.
      */
-    void write(byte[] buf, int off, int len)
-            throws IOException;
+    default void write(byte[] buf, int off, int len)
+            throws IOException {
+        int end = off + len;
+        for (int i = off; i < end; i++)
+            write(buf[i]);
+    }
 
     /**
      * @param buf
@@ -26,8 +36,25 @@ public interface IByteOut
      * @throws NullPointerException
      *             If <code>buffer</code> is <code>null</code>.
      */
-    void write(ByteBuffer buf)
-            throws IOException;
+    default void write(ByteBuffer buf)
+            throws IOException {
+        fn.write(this, buf);
+    }
+
+    @Override
+    default void flush(boolean sync)
+            throws IOException {
+    }
+
+    @Override
+    default void close()
+            throws IOException {
+    }
+
+    @Override
+    default boolean isClosed() {
+        return false;
+    }
 
     IByteOut NULL = IDataOut.NULL_LE;
 
