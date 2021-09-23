d41810509f08b92d76119fb7a1660ac8b0255623
Lenik <pub.lenik@bodz.net>
2021-09-23 20:14:14 +0800

    bas-db: Config BoneCP pool size.

--- bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/jdbc/BoneCPDataSourceProvider.java
+++ bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/jdbc/BoneCPDataSourceProvider.java
@@ -7,7 +7,9 @@ import net.bodz.bas.rtx.IQueryable;
 import com.jolbox.bonecp.BoneCPDataSource;
 
 public class BoneCPDataSourceProvider
-        implements IDataSourceProvider, IDataSourcePropertyNames {
+        implements
+            IDataSourceProvider,
+            IDataSourcePropertyNames {
 
     private BoneCPDataSource dataSource;
 
@@ -23,6 +25,14 @@ public class BoneCPDataSourceProvider
         dataSource.setJdbcUrl(args.getConnectionUrl());
         dataSource.setUsername(args.getUserName());
         dataSource.setPassword(args.getPassword());
+
+        // dataSource.setIdleConnectionTestPeriodInMinutes(2); // 4 hr
+        // dataSource.setIdleMaxAge(2, TimeUnit.HOURS); // 1 hr
+        // dataSource.setMaxConnectionsPerPartition(60); // 10
+        // dataSource.setMinConnectionsPerPartition(20); // 0
+        // dataSource.setPartitionCount(3); // 1
+        // dataSource.setAcquireIncrement(10); // 2
+        // dataSource.setStatementsCacheSize(50); // 0
     }
 
     @Override
