74af3dacd1c102e56683e6058508df89364f2fe0
Lenik <pub.lenik@bodz.net>
2021-10-08 00:06:12 +0800

    bas-db: Enable p6spy by default.

--- bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/jdbc/BoneCPDataSourceProvider.java
+++ bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/jdbc/BoneCPDataSourceProvider.java
@@ -7,13 +7,14 @@ import javax.sql.DataSource;
 import net.bodz.bas.rtx.IQueryable;
 
 import com.jolbox.bonecp.BoneCPDataSource;
+import com.p6spy.engine.spy.P6DataSource;
 
 public class BoneCPDataSourceProvider
         implements
             IDataSourceProvider,
             IDataSourcePropertyNames {
 
-    private BoneCPDataSource dataSource;
+    private DataSource dataSource;
 
     public BoneCPDataSourceProvider(IQueryable queryable) {
         this(queryable.query(ConnectOptions.class));
@@ -22,7 +23,8 @@ public class BoneCPDataSourceProvider
     public BoneCPDataSourceProvider(ConnectOptions args) {
         if (args == null)
             throw new NullPointerException("args");
-        dataSource = new BoneCPDataSource();
+
+        BoneCPDataSource dataSource = new BoneCPDataSource();
         dataSource.setDriverClass(args.getType().getDriverClassName());
         dataSource.setJdbcUrl(args.getConnectionUrl());
         dataSource.setUsername(args.getUserName());
@@ -39,6 +41,9 @@ public class BoneCPDataSourceProvider
         // dataSource.setPartitionCount(3); // 1
         // dataSource.setAcquireIncrement(10); // 2
         // dataSource.setStatementsCacheSize(50); // 0
+
+        P6DataSource p6 = new P6DataSource(dataSource);
+        this.dataSource = p6;
     }
 
     @Override
