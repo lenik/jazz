68f6877d8fd4e0e65481875c6f108cfdecd60398
Lenik <pub.lenik@bodz.net>
2022-07-20 20:30:24 +0800

    bas-db: Refactor DataContexts to DataHub.

---
+++ bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/ctx/DataHub.java
@@ -0,0 +1,69 @@
+package net.bodz.bas.db.ctx;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.TreeMap;
+
+import net.bodz.bas.db.jdbc.ConnectOptions;
+import net.bodz.bas.meta.codegen.IndexedType;
+import net.bodz.bas.meta.decl.Namespace;
+import net.bodz.bas.meta.decl.Priority;
+import net.bodz.bas.t.order.IPriority;
+import net.bodz.bas.t.order.PriorityComparator;
+
+/**
+ * Marker class.
+ */
+@Priority
+@Namespace
+@IndexedType(includeAbstract = false)
+public abstract class DataHub
+        implements
+            IPriority {
+
+    int priority = Priority.DEFAULT;
+
+    public DataHub() {
+        Priority aPriority = getClass().getAnnotation(Priority.class);
+        if (aPriority != null)
+            priority = aPriority.value();
+    }
+
+    @Override
+    public int getPriority() {
+        return priority;
+    }
+
+    public abstract DataContext getMain();
+
+    protected static final Map<String, ConnectOptions> nameMap = new TreeMap<>();
+
+    protected static ConnectOptions declare(String name) {
+        ConnectOptions o = new ConnectOptions();
+        nameMap.put(name, o);
+        return o;
+    }
+
+    private static List<DataHub> hubs;
+
+    synchronized static void loadHubs() {
+        if (hubs == null) {
+            hubs = new ArrayList<>();
+            for (DataHub hub : ServiceLoader.load(DataHub.class))
+                hubs.add(hub);
+            Collections.sort(hubs, PriorityComparator.INSTANCE);
+        }
+    }
+
+    public static DataHub getPreferredHub() {
+        loadHubs();
+        if (hubs.isEmpty())
+            return null;
+        else
+            return hubs.get(0);
+    }
+
+}
---
+++ bas/layer-5/bas-db/src/main/java/net/bodz/bas/db/ctx/TestDataHub.java
@@ -0,0 +1,9 @@
+package net.bodz.bas.db.ctx;
+
+import net.bodz.bas.meta.decl.Priority;
+
+@Priority(Priority.HIGH)
+public abstract class TestDataHub
+        extends DataHub {
+
+}
