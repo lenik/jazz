22e7d6162680f7de11d8472ceebb90cb11c3d7bc
Lenik <pub.lenik@bodz.net>
2022-07-20 08:47:08 +0800

    bas-log: Moved ILogEntry, LogLevel to bas-api.

--- bas/layer-1/bas-api/src/main/java/net/bodz/bas/err/ExceptionLog.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/err/ExceptionLog.java
@@ -1,38 +1,17 @@
 package net.bodz.bas.err;
 
-import java.io.Serializable;
 import java.util.ArrayList;
 
-class LogItem
-        implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    long time;
-    Throwable exception;
-
-    public LogItem(long time, Throwable exception) {
-        this.time = time;
-        this.exception = exception;
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public Throwable getException() {
-        return exception;
-    }
-
-}
+import net.bodz.bas.log.LogLevel;
+import net.bodz.bas.log.LogRecord;
 
 public class ExceptionLog
-        extends ArrayList<LogItem> {
+        extends ArrayList<LogRecord> {
 
     private static final long serialVersionUID = 1L;
 
     public void log(Throwable exception) {
-        LogItem item = new LogItem(System.currentTimeMillis(), exception);
+        LogRecord item = new LogRecord(LogLevel.ERROR, 0, exception);
         add(item);
     }
 
