7191379d7edab69407d014da1d69afb788776ef2
Lenik <pub.lenik@bodz.net>
2022-07-20 20:18:15 +0800

    bas-meta: Enlarged @Priority values.

--- bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/decl/Priority.java
+++ bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/decl/Priority.java
@@ -1,20 +1,22 @@
 package net.bodz.bas.meta.decl;
 
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
  * @see net.bodz.bas.t.order.IPriority
  */
+@Inherited
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Priority {
 
-    int URGENT = -10;
-    int HIGH = -5;
-    int MEDIUM = 0;
-    int LOW = 5;
-    int VERYLOW = 10;
+    int URGENT = -100;
+    int HIGH = -10;
+    int DEFAULT = 0;
+    int LOW = 10;
+    int VERYLOW = 100;
 
-    int value();
+    int value() default DEFAULT;
 
 }
