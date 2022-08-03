8895805ac1b805e872ad86bc77e3a1004849b2b2
Lenik <pub.lenik@bodz.net>
2022-08-03 10:34:25 +0800

    bas-fmt: Add IElement.getAttributeVar().

--- bas/layer-3/bas-fmt/src/main/java/net/bodz/bas/fmt/xml/xq/IElement.java
+++ bas/layer-3/bas-fmt/src/main/java/net/bodz/bas/fmt/xml/xq/IElement.java
@@ -2,9 +2,13 @@ package net.bodz.bas.fmt.xml.xq;
 
 import org.w3c.dom.Element;
 
+import net.bodz.bas.t.variant.IVariant;
+
 public interface IElement
         extends
             IXmlSelection,
             Element {
+
+    IVariant getAttributeVar(String name);
 
 }
