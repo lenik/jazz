820a6c71a2653f79d20a22fdcc52da6019a1692f
Lenik <pub.lenik@bodz.net>
2022-08-09 22:24:33 +0800

    lily: Add TextPattern.

---
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/model/base/TextPattern.java
@@ -0,0 +1,51 @@
+package net.bodz.lily.model.base;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.fmt.json.IJsonForm;
+import net.bodz.bas.fmt.json.IJsonOut;
+import net.bodz.bas.fmt.xml.IXmlForm;
+import net.bodz.bas.json.JsonObject;
+
+public class TextPattern
+        implements
+            Serializable,
+            IJsonForm,
+            IXmlForm {
+
+    String pattern;
+
+    public String getPattern() {
+        return pattern;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public void setStartsWith(String pattern) {
+        this.pattern = pattern + "%";
+    }
+
+    public void setEndsWith(String pattern) {
+        this.pattern = "%" + pattern;
+    }
+
+    public void setContains(String pattern) {
+        this.pattern = "%" + pattern + "%";
+    }
+
+    @Override
+    public void readObject(JsonObject o)
+            throws ParseException {
+    }
+
+    @Override
+    public void writeObject(IJsonOut out)
+            throws IOException, FormatException {
+    }
+
+}
