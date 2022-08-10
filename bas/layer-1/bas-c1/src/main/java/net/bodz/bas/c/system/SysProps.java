e2253a3226440fa91db82d821f70ca072b3de83c
Lenik <pub.lenik@bodz.net>
2022-08-10 23:32:18 +0800

    bas-c1: Add SysProps.dataDir.

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/system/SysProps.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/system/SysProps.java
@@ -9,11 +9,19 @@ public class SysProps {
     public static final File userWorkDir;
     public static final File userHome;
 
+    public static final File dataDir;
+
     static {
         fileSep = SystemProperties.getFileSeparator();
         pathSep = SystemProperties.getPathSeparator();
         userWorkDir = new File(SystemProperties.getUserDir());
         userHome = new File(SystemProperties.getUserHome());
+
+        String dataDirProperty = System.getProperty("data.dir");
+        if (dataDirProperty == null)
+            dataDir = new File(userHome, "data");
+        else
+            dataDir = new File(dataDirProperty);
     }
 
 }
