1b7dfdb31a3b722ebc771ee23dd95ed5554978dc
Lenik <pub.lenik@bodz.net>
2022-08-23 23:11:22 +0800

    bas-text: Refactor supports around gnudiff.

--- bas/layer-3/bas-text/src/main/java/net/bodz/bas/c/string/StringDiff.java
+++ bas/layer-3/bas-text/src/main/java/net/bodz/bas/c/string/StringDiff.java
@@ -1,10 +1,11 @@
 package net.bodz.bas.c.string;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
+import net.bodz.bas.err.UnexpectedException;
 import net.bodz.bas.io.BCharOut;
-import net.bodz.bas.io.IPrintOut;
 import net.bodz.bas.text.diff.DiffComparators;
 import net.bodz.bas.text.diff.DiffEntry;
 import net.bodz.bas.text.diff.IDiffComparator;
@@ -12,23 +13,30 @@ import net.bodz.bas.text.diff.IDiffFormat;
 
 public class StringDiff {
 
-    public static void diff(List<?> a, List<?> b, IPrintOut out, IDiffFormat format) {
+    public static void diff(List<?> a, List<?> b, Appendable out, IDiffFormat format)
+            throws IOException {
         IDiffComparator alg = DiffComparators.gnudiff;
         List<DiffEntry> diffs = alg.compareDiff(a, b);
         format.printDiffs(out, a, b, diffs);
     }
 
-    public static void diff(List<?> a, List<?> b, IPrintOut out) {
+    public static void diff(List<?> a, List<?> b, Appendable out)
+            throws IOException {
         diff(a, b, out, IDiffFormat.SIMPLE);
     }
 
     public static String diff(List<?> a, List<?> b) {
         BCharOut buffer = new BCharOut();
-        diff(a, b, buffer);
+        try {
+            diff(a, b, buffer);
+        } catch (IOException e) {
+            throw new UnexpectedException(e.getMessage(), e);
+        }
         return buffer.toString();
     }
 
-    public static void diff(String a, String b, IPrintOut out) {
+    public static void diff(String a, String b, Appendable out)
+            throws IOException {
         String[] av = a.split("\n");
         String[] bv = b.split("\n");
         diff(Arrays.asList(av), Arrays.asList(bv), out);
@@ -36,7 +44,11 @@ public class StringDiff {
 
     public static String diff(String a, String b) {
         BCharOut buffer = new BCharOut();
-        diff(a, b, buffer);
+        try {
+            diff(a, b, buffer);
+        } catch (IOException e) {
+            throw new UnexpectedException(e.getMessage(), e);
+        }
         return buffer.toString();
     }
 
--- bas/layer-3/bas-text/src/main/java/net/bodz/bas/text/diff/IDiffFormat.java
+++ bas/layer-3/bas-text/src/main/java/net/bodz/bas/text/diff/IDiffFormat.java
@@ -1,8 +1,8 @@
 package net.bodz.bas.text.diff;
 
+import java.io.IOException;
 import java.util.List;
 
-import net.bodz.bas.io.IPrintOut;
 import net.bodz.bas.meta.lang.typer;
 import net.bodz.bas.text.diff.gnudiff.DiffPrint;
 
@@ -15,7 +15,8 @@ public interface IDiffFormat {
     IDiffFormat NORMAL = new NormalDiffFormat();
     IDiffFormat UNIFIED = new UnifiedDiffFormat();
 
-    void printDiffs(IPrintOut out, List<?> a, List<?> b, List<DiffEntry> diffs);
+    void printDiffs(Appendable out, List<?> a, List<?> b, List<DiffEntry> diffs)
+            throws IOException;
 
 }
 
@@ -23,54 +24,66 @@ class SimpleDiffFormat
         extends AbstractDiffFormat {
 
     @Override
-    protected void formatHunk(List<DiffEntry> diffs, int off, int len) {
+    protected void formatHunk(List<DiffEntry> diffs, int off, int len)
+            throws IOException {
         super.formatHunk(diffs, off, len);
     }
 
     @Override
-    protected void hunkSeparator() {
-        out.println();
+    protected void hunkSeparator()
+            throws IOException {
+        out.append("\n");
     }
 
     @Override
-    protected void edit0(char op, int index) {
-        out.printf(" %6d%c%s\n", index, op, al.get(index));
+    protected void edit0(char op, int index)
+            throws IOException {
+        out.append(String.format(" %6d%c%s\n", index, op, al.get(index)));
     }
 
     @Override
-    protected void edit1(char op, int index) {
-        out.printf("*%6d%c%s\n", index, op, bl.get(index));
+    protected void edit1(char op, int index)
+            throws IOException {
+        out.append(String.format("*%6d%c%s\n", index, op, bl.get(index)));
     }
 }
 
 class EDDiffFormat
-        implements IDiffFormat {
+        implements
+            IDiffFormat {
     @Override
-    public void printDiffs(IPrintOut out, List<?> a, List<?> b, List<DiffEntry> diff) {
+    public void printDiffs(Appendable out, List<?> a, List<?> b, List<DiffEntry> diff)
+            throws IOException {
         new DiffPrint.EdPrint(a, b, out).print_script(diff);
     }
 }
 
 class ContextDiffFormat
-        implements IDiffFormat {
+        implements
+            IDiffFormat {
     @Override
-    public void printDiffs(IPrintOut out, List<?> a, List<?> b, List<DiffEntry> diff) {
+    public void printDiffs(Appendable out, List<?> a, List<?> b, List<DiffEntry> diff)
+            throws IOException {
         new DiffPrint.ContextPrint(a, b, out).print_script(diff);
     }
 }
 
 class NormalDiffFormat
-        implements IDiffFormat {
+        implements
+            IDiffFormat {
     @Override
-    public void printDiffs(IPrintOut out, List<?> a, List<?> b, List<DiffEntry> diff) {
+    public void printDiffs(Appendable out, List<?> a, List<?> b, List<DiffEntry> diff)
+            throws IOException {
         new DiffPrint.NormalPrint(a, b, out).print_script(diff);
     }
 }
 
 class UnifiedDiffFormat
-        implements IDiffFormat {
+        implements
+            IDiffFormat {
     @Override
-    public void printDiffs(IPrintOut out, List<?> a, List<?> b, List<DiffEntry> diff) {
+    public void printDiffs(Appendable out, List<?> a, List<?> b, List<DiffEntry> diff)
+            throws IOException {
         new DiffPrint.UnifiedPrint(a, b, out).print_script(diff);
     }
 }
