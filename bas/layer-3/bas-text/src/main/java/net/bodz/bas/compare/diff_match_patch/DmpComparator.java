909a4927e11810c0d444d8aec809e46688117b69
Lenik <pub.lenik@bodz.net>
2022-08-26 18:56:04 +0800

    -- index

--- bas/layer-3/bas-text/src/main/java/net/bodz/bas/compare/diff_match_patch/DmpComparator.java
+++ bas/layer-3/bas-text/src/main/java/net/bodz/bas/compare/diff_match_patch/DmpComparator.java
@@ -4,14 +4,13 @@ import java.util.List;
 
 import net.bodz.bas.compare.IListComparator;
 import net.bodz.bas.compare.IListCompareResult;
-
-import name.fraser.neil.plaintext.diff_match_patch;
+import net.bodz.bas.compare.dmp.diff_match_patch_compat;
 
 public class DmpComparator
         implements
             IListComparator<String, String> {
 
-    diff_match_patch dmp = new diff_match_patch();
+    diff_match_patch_compat dmp = new diff_match_patch_compat();
 
     @Override
     public IListCompareResult<String, String> compare(List<? extends String> source, List<? extends String> target) {
