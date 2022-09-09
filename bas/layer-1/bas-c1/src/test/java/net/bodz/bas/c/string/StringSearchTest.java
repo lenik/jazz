d2e9c808502bc441386d61140f4b7409fa0d751a
Lenik <pub.lenik@bodz.net>
2022-09-09 19:06:36 +0800

    bas-c1: Add StringSearchTest to test splitByNoncont().

--- bas/layer-1/bas-c1/src/test/java/net/bodz/bas/c/string/StringSearchTest.java
+++ bas/layer-1/bas-c1/src/test/java/net/bodz/bas/c/string/StringSearchTest.java
@@ -3,12 +3,30 @@ package net.bodz.bas.c.string;
 import java.util.Arrays;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 public class StringSearchTest
         extends Assert {
 
-    public static void main(String[] args) {
+    @Test
+    public void testSplitByNoncont() {
+        checkSplit("f", "f");
+        checkSplit("foo", "foo");
+        checkSplit("Foo", "Foo");
+        checkSplit("foo-Bar", "fooBar");
+        checkSplit("A-Dog", "ADog");
+        checkSplit("AB-Dog", "ABDog");
+        checkSplit("ABC-Dog", "ABCDog");
+        checkSplit("ABC", "ABC");
+    }
 
+    void checkSplit(String expected, String s) {
+        String[] array = StringSearch.splitByNoncontinuous(s, Character.UPPERCASE_LETTER);
+        String actual = StringArray.join("-", array);
+        assertEquals(expected, actual);
+    }
+
+    public static void main(String[] args) {
         String a = "heyI'amLENIKButIWantYouToKNOW!...That'sTHE123Foo";
         String[] v = StringSearch.splitByNoncontinuous(a, Character.UPPERCASE_LETTER);
         System.out.println(Arrays.asList(v));
