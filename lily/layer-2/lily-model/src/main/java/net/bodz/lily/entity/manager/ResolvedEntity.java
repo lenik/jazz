5899b48d7f973e2bdad97106ff1f1d2f1b0cf48b
Lenik <pub.lenik@bodz.net>
2022-10-11 16:57:13 +0800

    lily: Echo entity's json in ResolvedEntity.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/entity/manager/ResolvedEntity.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/entity/manager/ResolvedEntity.java
@@ -1,6 +1,17 @@
 package net.bodz.lily.entity.manager;
 
-public class ResolvedEntity {
+import java.io.IOException;
+
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.fmt.json.IJsonForm;
+import net.bodz.bas.fmt.json.IJsonOut;
+import net.bodz.bas.fmt.json.JsonFormOptions;
+import net.bodz.bas.json.JsonObject;
+
+public class ResolvedEntity
+        implements
+            IJsonForm {
 
     public String idFieldStrings[];
     public Object idFields[];
@@ -9,5 +20,25 @@ public class ResolvedEntity {
     public Object entity;
 
     public String preferredExtension;
+
+    @Override
+    public void jsonIn(JsonObject o, JsonFormOptions opts)
+            throws ParseException {
+    }
+
+    @Override
+    public void jsonOut(IJsonOut out, JsonFormOptions opts)
+            throws IOException, FormatException {
+        if (entity == null) {
+            out.entry("error", "null entity.");
+            return;
+        }
+        if (!(entity instanceof IJsonForm)) {
+            out.entry("error", "can't convert to JSON: " + entity);
+            return;
+        }
+        IJsonForm jsonEntity = (IJsonForm) entity;
+        jsonEntity.jsonOut(out, opts);
+    }
 
 }
