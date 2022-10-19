0544f5345ce3e277b9b1cf33157e9316b0fc96f3
Lenik <pub.lenik@bodz.net>
2022-10-19 20:48:51 +0800

    bas-c1: Phrase fields null for null.

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/string/Phrase.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/string/Phrase.java
@@ -9,37 +9,45 @@ public class Phrase {
 
     public static Phrase foo_bar(String s) {
         Phrase phrase = new Phrase();
-        phrase.foo_bar = s;
-        phrase.FOO_BAR = s.toUpperCase();
-        phrase.fooBar = StringId.UL.toCamel(s);
-        phrase.FooBar = Strings.ucfirst(phrase.fooBar);
+        if (s != null) {
+            phrase.foo_bar = s;
+            phrase.FOO_BAR = s.toUpperCase();
+            phrase.fooBar = StringId.UL.toCamel(s);
+            phrase.FooBar = Strings.ucfirst(phrase.fooBar);
+        }
         return phrase;
     }
 
     public static Phrase fooBar(String s) {
         Phrase phrase = new Phrase();
-        phrase.foo_bar = StringId.UL.breakCamel(s);
-        phrase.FOO_BAR = phrase.foo_bar.toUpperCase();
-        phrase.fooBar = s;
-        phrase.FooBar = Strings.ucfirst(phrase.fooBar);
+        if (s != null) {
+            phrase.foo_bar = StringId.UL.breakCamel(s);
+            phrase.FOO_BAR = phrase.foo_bar.toUpperCase();
+            phrase.fooBar = s;
+            phrase.FooBar = Strings.ucfirst(phrase.fooBar);
+        }
         return phrase;
     }
 
     public static Phrase FooBar(String s) {
         Phrase phrase = new Phrase();
-        phrase.FooBar = s;
-        phrase.fooBar = Strings.lcfirst(s);
-        phrase.foo_bar = StringId.UL.breakCamel(phrase.fooBar);
-        phrase.FOO_BAR = phrase.foo_bar.toUpperCase();
+        if (s != null) {
+            phrase.FooBar = s;
+            phrase.fooBar = Strings.lcfirst(s);
+            phrase.foo_bar = StringId.UL.breakCamel(phrase.fooBar);
+            phrase.FOO_BAR = phrase.foo_bar.toUpperCase();
+        }
         return phrase;
     }
 
     public static Phrase dual(String foo_bar, String fooBar) {
         Phrase phrase = new Phrase();
-        phrase.foo_bar = foo_bar;
-        phrase.FOO_BAR = foo_bar.toUpperCase();
-        phrase.fooBar = fooBar;
-        phrase.FooBar = Strings.ucfirst(fooBar);
+        if (foo_bar != null) {
+            phrase.foo_bar = foo_bar;
+            phrase.FOO_BAR = foo_bar.toUpperCase();
+            phrase.fooBar = fooBar;
+            phrase.FooBar = Strings.ucfirst(fooBar);
+        }
         return phrase;
     }
 
