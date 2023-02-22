7045695b067ac4c8ce70ffd23849637de29a3c59
Lenik <pub.lenik@bodz.net>
2023-02-22 20:27:51 +0800

    swt-c1: Add EventsFn.addSelectionListener for ToolItem, Table.

--- swt/layer-1/swt-c1/src/main/java/net/bodz/swt/c/functional/EventsFn.java
+++ swt/layer-1/swt-c1/src/main/java/net/bodz/swt/c/functional/EventsFn.java
@@ -2,6 +2,8 @@ package net.bodz.swt.c.functional;
 
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.ToolItem;
 
 public class EventsFn {
 
@@ -11,6 +13,14 @@ public class EventsFn {
 
     public static void addSelectionListener(Combo button, SelectionFunc fn) {
         button.addSelectionListener(fn);
+    }
+
+    public static void addSelectionListener(ToolItem toolItem, SelectionFunc fn) {
+        toolItem.addSelectionListener(fn);
+    }
+
+    public static void addSelectionListener(Table table, SelectionFunc fn) {
+        table.addSelectionListener(fn);
     }
 
 }
