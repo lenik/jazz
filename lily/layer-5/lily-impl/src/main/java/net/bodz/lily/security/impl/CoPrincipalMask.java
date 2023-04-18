ae49e84cb82e25181b32c9abf611a8aed1d0633a
Lenik <pub.lenik@bodz.net>
2023-04-19 01:29:58 +0800

    lily: Add necessary idRange in mask types.

--- lily/layer-5/lily-impl/src/main/java/net/bodz/lily/security/impl/CoPrincipalMask.java
+++ lily/layer-5/lily-impl/src/main/java/net/bodz/lily/security/impl/CoPrincipalMask.java
@@ -1,12 +1,23 @@
 package net.bodz.lily.security.impl;
 
 import net.bodz.bas.c.string.StringUtil;
+import net.bodz.bas.t.range.IntegerRange;
 import net.bodz.lily.model.base.CoObjectMask;
 
 public class CoPrincipalMask
         extends CoObjectMask {
 
+    IntegerRange idRange;
+
     String name;
+
+    public IntegerRange getIdRange() {
+        return idRange;
+    }
+
+    public void setIdRange(IntegerRange idRange) {
+        this.idRange = idRange;
+    }
 
     public String getName() {
         return name;
