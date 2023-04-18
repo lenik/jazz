170139157e71c4942481f15c7c6317bf2794e560
Lenik <pub.lenik@bodz.net>
2023-04-18 19:31:09 +0800

    bas-std: Add ILoadableContent.

---
+++ bas/layer-2/bas-std/src/main/java/net/bodz/bas/util/cache/LoadContentException.java
@@ -0,0 +1,24 @@
+package net.bodz.bas.util.cache;
+
+public class LoadContentException
+        extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public LoadContentException() {
+        super();
+    }
+
+    public LoadContentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public LoadContentException(String message) {
+        super(message);
+    }
+
+    public LoadContentException(Throwable cause) {
+        super(cause);
+    }
+
+}
