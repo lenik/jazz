48918903184979861f8bc067d95048e20775ca98
Lenik <pub.lenik@bodz.net>
2023-04-27 20:06:38 +0800

    lily-site: Add tool.EventLogger.

---
+++ lily/layer-5/lily-site/src/main/java/net/bodz/lily/tool/log/EventLog.java
@@ -0,0 +1,195 @@
+package net.bodz.lily.tool.log;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.fmt.json.IJsonForm;
+import net.bodz.bas.fmt.json.IJsonOut;
+import net.bodz.bas.fmt.json.JsonFn;
+import net.bodz.bas.fmt.json.JsonFormOptions;
+import net.bodz.bas.json.JsonObject;
+import net.bodz.bas.site.json.JsonVarMap;
+import net.bodz.lily.model.base.CoEntity;
+import net.bodz.lily.security.User;
+
+public class EventLog
+        implements
+            Serializable,
+            IJsonForm {
+
+    private static final long serialVersionUID = 1L;
+
+    long time;
+    User user;
+    Class<?> entity;
+    long id;
+    String message;
+    Throwable exception;
+
+    public EventLog() {
+    }
+
+    public EventLog(User user) {
+        this.time = System.currentTimeMillis();
+        this.user = user;
+    }
+
+    public EventLog(User user, CoEntity<? extends Number> entity, String message, Throwable exception) {
+        this.time = System.currentTimeMillis();
+        this.user = user;
+        this.entity = entity.getClass();
+        this.id = entity.id().longValue();
+        this.message = message;
+        this.exception = exception;
+    }
+
+    public EventLog time(long time) {
+        this.time = time;
+        return this;
+    }
+
+    public EventLog user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public EventLog entity(CoEntity<? extends Number> entity) {
+        this.entity = entity.getClass();
+        this.id = entity.id().longValue();
+        return this;
+    }
+
+    public EventLog message(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public EventLog message(String format, Object... args) {
+        this.message = String.format(format, args);
+        return this;
+    }
+
+    public EventLog exception(Throwable exception) {
+        this.exception = exception;
+        return this;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Class<?> getEntity() {
+        return entity;
+    }
+
+    public void setEntity(Class<?> entity) {
+        this.entity = entity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void jsonIn(JsonObject o, JsonFormOptions opts)
+            throws ParseException {
+        JsonVarMap map = new JsonVarMap(o);
+        time = map.getLong("time", 0);
+        message = map.getString("message");
+
+        if (o.has("entity")) {
+            JsonObject $entity = o.getJsonObject("entity");
+            String type = $entity.getString("type");
+            try {
+                entity = Class.forName(type);
+            } catch (ClassNotFoundException e) {
+                throw new ParseException(e.getMessage(), e);
+            }
+            id = $entity.getLong("id");
+        }
+
+        if (o.has("user")) {
+            JsonObject $user = o.getJsonObject("user");
+            map = new JsonVarMap($user);
+            User user = new User();
+            user.id(map.getInt("id"));
+            user.setName(map.getString("name"));
+            user.setFullName(map.getString("fullName"));
+            this.user = user;
+        }
+
+        // Throwable exception;
+    }
+
+    @Override
+    public void jsonOut(IJsonOut out, JsonFormOptions opts)
+            throws IOException {
+        out.entry("time", time);
+
+        if (entity != null) {
+            out.key("entity");
+            out.object();
+            out.entry("type", entity.getCanonicalName());
+            out.entry("id", id);
+            out.endObject();
+        }
+
+        if (user != null) {
+            out.key("user");
+            out.object();
+            out.entry("id", user.id());
+            out.entry("name", user.getName());
+            out.entry("fullName", user.getFullName());
+            out.endObject();
+        }
+
+        if (message != null)
+            out.entry("message", message);
+
+        if (exception != null) {
+            out.key("exception");
+            out.object();
+            out.entry("type", exception.getClass().getCanonicalName());
+            out.entry("message", exception.getMessage());
+            out.endObject();
+        }
+    }
+
+    @Override
+    public String toString() {
+        String json;
+        try {
+            json = JsonFn.toJson(this, JsonFormOptions.COMPACT);
+        } catch (FormatException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+        return json;
+    }
+
+}
---
+++ lily/layer-5/lily-site/src/main/java/net/bodz/lily/tool/log/EventLogger.java
@@ -0,0 +1,146 @@
+package net.bodz.lily.tool.log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.bodz.bas.c.java.util.Dates;
+import net.bodz.bas.c.org.json.JsonBuffer;
+import net.bodz.bas.c.system.SystemProperties;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.err.UnexpectedException;
+import net.bodz.bas.fmt.json.JsonFormOptions;
+import net.bodz.bas.json.JsonObject;
+import net.bodz.bas.repr.path.IPathArrival;
+import net.bodz.bas.repr.path.IPathDispatchable;
+import net.bodz.bas.repr.path.ITokenQueue;
+import net.bodz.bas.repr.path.PathArrival;
+import net.bodz.bas.repr.path.PathDispatchException;
+import net.bodz.bas.servlet.ctx.CurrentHttpService;
+import net.bodz.bas.site.json.HttpPayload;
+import net.bodz.bas.site.json.JsonResult;
+import net.bodz.bas.t.variant.IVariantMap;
+import net.bodz.lily.model.base.CoEntity;
+import net.bodz.lily.security.User;
+
+public class EventLogger
+        implements
+            IPathDispatchable {
+
+    File logDir;
+    LinkedList<EventLog> logs;
+    EventLog pend;
+
+    public EventLogger() {
+        logs = new LinkedList<>();
+
+        String home = SystemProperties.getUserHome();
+        File dataDir = new File(home, "data");
+        logDir = new File(dataDir, "log");
+    }
+
+    public LinkedList<EventLog> getLogs() {
+        return logs;
+    }
+
+    public EventLog log(User user)
+            throws IOException {
+        flush();
+        EventLog item = new EventLog(user);
+        this.pend = item;
+        return item;
+    }
+
+    public EventLog log(User user, CoEntity<? extends Number> entity, String message)
+            throws IOException {
+        flush();
+        EventLog item = new EventLog(user, entity, message, null);
+        logs.add(item);
+        this.pend = item;
+        return item;
+    }
+
+    public synchronized void flush()
+            throws IOException {
+        if (pend != null) {
+            save(pend.getTime(), Arrays.asList(pend));
+            pend = null;
+        }
+    }
+
+    void save(long time, List<EventLog> logs)
+            throws IOException {
+        String yearMonth = Dates.YYYY_MM.format(time);
+        File subDir = new File(logDir, yearMonth);
+        subDir.mkdirs();
+
+        String date = Dates.YYYY_MM_DD.format(time);
+        File logFile = new File(subDir, date);
+        FileOutputStream fout = new FileOutputStream(logFile, true);
+        PrintStream out = new PrintStream(fout, true, "utf-8");
+
+        for (EventLog log : logs)
+            out.println(log.toString());
+
+        out.close();
+    }
+
+    @Override
+    public IPathArrival dispatch(IPathArrival previous, ITokenQueue tokens, IVariantMap<String> q)
+            throws PathDispatchException {
+        Object target = null;
+
+        String token = tokens.peek();
+        if (token == null)
+            return null;
+
+        switch (token) {
+        case "add":
+            JsonResult result = new JsonResult();
+            JsonObject jsonObj = HttpPayload.getJsonPayload(CurrentHttpService.getRequest());
+            try {
+                EventLog log = new EventLog();
+                log.jsonIn(jsonObj, JsonFormOptions.DEFAULT);
+                log.setTime(System.currentTimeMillis());
+                logs.add(log);
+            } catch (ParseException e) {
+                throw new PathDispatchException("Parse error:" + e.getMessage(), e);
+            }
+            result.succeed();
+            target = result;
+            break;
+
+        case "data.json":
+            JsonBuffer jsbuf = new JsonBuffer();
+            jsbuf.array();
+            Iterator<EventLog> iter = logs.descendingIterator();
+            while (iter.hasNext()) {
+                EventLog log = iter.next();
+                try {
+                    log.jsonOut(jsbuf, JsonFormOptions.DEFAULT, true);
+                } catch (Exception e) {
+                    throw new UnexpectedException(e.getMessage(), e);
+                }
+            }
+            jsbuf.endArray();
+            target = jsbuf;
+            break;
+        }
+
+        if (target != null)
+            return PathArrival.shift(previous, this, target, tokens);
+        return null;
+    }
+
+    private static EventLogger instance = new EventLogger();
+
+    public static EventLogger getInstance() {
+        return instance;
+    }
+
+}
