0d1eadcf3fd8e27798edbbb63c0ce4c4e12504c5
Lenik <pub.lenik@bodz.net>
2023-05-16 10:35:07 +0800

    lily: Add IId.idComponents and idPath.

--- bas/layer-1/bas-api/src/main/java/net/bodz/lily/entity/IId.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/lily/entity/IId.java
@@ -1,5 +1,10 @@
 package net.bodz.lily.entity;
 
+import java.util.Arrays;
+import java.util.List;
+
+import net.bodz.bas.c.object.Nullables;
+
 public interface IId<Id> {
 
     Class<Id> idType();
@@ -7,5 +12,27 @@ public interface IId<Id> {
     Id id();
 
     void id(Id id);
+
+    default List<Object> getIdComponents() {
+        return Arrays.asList(id());
+    }
+
+    default String getIdPath() {
+        List<Object> components = getIdComponents();
+        int n = components.size();
+        if (n == 0)
+            return null;
+        if (n == 1) {
+            Object single = components.get(0);
+            return Nullables.toString(single);
+        }
+        StringBuilder sb = new StringBuilder(n * 16);
+        for (int i = 0; i < n; i++) {
+            if (i != 0)
+                sb.append('/');
+            sb.append(components.get(i));
+        }
+        return sb.toString();
+    }
 
 }
