c47baf11dcfbb175d3003607caf1c9696d6cccdc
Lenik <pub.lenik@bodz.net>
2023-06-19 15:42:42 +0800

    lily: Remove old user/group -Mask duplicated.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/security/dao/UserSecretMask.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/security/dao/UserSecretMask.java
@@ -3,4 +3,65 @@ package net.bodz.lily.security.dao;
 public class UserSecretMask
         extends _UserSecretMask_stuff {
 
+    String userName;
+    String phone;
+    String email;
+
+    public UserSecretMask() {
+    }
+
+    public UserSecretMask(Integer userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserSecretMask userId(int id) {
+        setUserId(id);
+        return this;
+    }
+
+    public UserSecretMask userName(String name) {
+        this.userName = name;
+        return this;
+    }
+
+    public UserSecretMask password(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public UserSecretMask phone(String phone) {
+        this.phone = phone;
+        return this;
+    }
+
+    public UserSecretMask email(String email) {
+        this.email = email;
+        return this;
+    }
+
 }
