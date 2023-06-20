2bf59a6af0bed9a5718101d4cbf38339ff773bd2
Lenik <pub.lenik@bodz.net>
2023-06-20 21:21:49 +0800

    lily: ILoginManager as services. Default VhostLoginManagerProvider.

--- lily/layer-2/lily-model/src/main/java/net/bodz/lily/security/login/LoginManagers.java
+++ lily/layer-2/lily-model/src/main/java/net/bodz/lily/security/login/LoginManagers.java
@@ -1,16 +1,37 @@
 package net.bodz.lily.security.login;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ServiceLoader;
 
-import net.bodz.bas.servlet.ctx.CurrentHttpService;
+import net.bodz.bas.t.order.PriorityComparator;
 
 public class LoginManagers {
 
-    public static ILoginManager fromRequest() {
-        HttpServletRequest request = CurrentHttpService.getRequestOpt();
-        if (request == null)
-            return null;
+    static List<ILoginManagerProvider> providers;
 
+    static {
+        providers = new ArrayList<>();
+        for (ILoginManagerProvider provider : ServiceLoader.load(ILoginManagerProvider.class)) {
+            providers.add(provider);
+        }
+        Collections.sort(providers, PriorityComparator.INSTANCE);
+    }
+
+    public static ILoginManager requireLoginManager() {
+        ILoginManager loginManager = getLoginManager();
+        if (loginManager == null)
+            throw new IllegalStateException("No available login manager");
+        return loginManager;
+    }
+
+    public static ILoginManager getLoginManager() {
+        for (ILoginManagerProvider provider : providers) {
+            ILoginManager loginManager = provider.getLoginManager();
+            if (loginManager != null)
+                return loginManager;
+        }
         return null;
     }
 
