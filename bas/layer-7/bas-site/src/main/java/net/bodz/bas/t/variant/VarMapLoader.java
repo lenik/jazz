8aff8c616c47849334175c5b03aec3ce7f1dc2e4
Lenik <pub.lenik@bodz.net>
2023-08-01 17:07:04 +0800

    bas-site: VarMapLoader: more detail message for converter error.

--- bas/layer-7/bas-site/src/main/java/net/bodz/bas/t/variant/VarMapLoader.java
+++ bas/layer-7/bas-site/src/main/java/net/bodz/bas/t/variant/VarMapLoader.java
@@ -1,5 +1,6 @@
 package net.bodz.bas.t.variant;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 import net.bodz.bas.c.primitive.Primitives;
@@ -182,9 +183,9 @@ public class VarMapLoader {
             }
 
             logger.warn(String.format(//
-                    "No converter for property '%s': from %s: value %s.", //
-                    property, rtype.getCanonicalName(), //
-                    rval));
+                    "No converter for property '%s': from value %s (of type %s).", //
+                    property, //
+                    format(rval), rtype.getCanonicalName()));
             return;
         } while (false);
 
@@ -193,6 +194,25 @@ public class VarMapLoader {
         } catch (ReflectiveOperationException e) {
             throw new LoaderException("Failed to set property value: " + e.getMessage(), e);
         }
+    }
+
+    static String format(Object o) {
+        if (o == null)
+            return "null";
+        if (o.getClass().isArray()) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("[");
+            int n = Array.getLength(o);
+            for (int i = 0; i < n; i++) {
+                if (i != 0)
+                    sb.append(", ");
+                String item = format(Array.get(o, i));
+                sb.append(item);
+            }
+            sb.append("]");
+            return sb.toString();
+        } else
+            return o.toString();
     }
 
     static Map<Class<?>, Class<?>> concreteTypes = new HashMap<Class<?>, Class<?>>();
