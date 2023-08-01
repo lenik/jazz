ab13bdef3945f7cbce903ab69cc4d775628c8ce7
Lenik <pub.lenik@bodz.net>
2023-08-01 17:08:51 +0800

    lily: Refactor buildTableSheet(): use separate ExcelBuilder and DefaultListingExcel.

---
+++ lily/layer-5/lily-impl/src/main/java/net/bodz/lily/format/doc/MarkdownDoc.java
@@ -0,0 +1,114 @@
+package net.bodz.lily.format.doc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+
+import net.bodz.bas.c.object.Nullables;
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.err.LoaderException;
+import net.bodz.bas.err.NotImplementedException;
+import net.bodz.bas.err.ParseException;
+
+import com.vladsch.flexmark.docx.converter.DocxRenderer;
+import com.vladsch.flexmark.ext.emoji.EmojiExtension;
+import com.vladsch.flexmark.ext.emoji.EmojiImageType;
+import com.vladsch.flexmark.ext.emoji.EmojiShortcutType;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Document;
+import com.vladsch.flexmark.util.data.MutableDataHolder;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+import com.vladsch.flexmark.util.misc.Extension;
+
+public class MarkdownDoc
+        implements
+            IWordForm {
+
+    String markdown;
+
+    MutableDataHolder options;
+    Document _document;
+
+    public MarkdownDoc(String markdown) {
+        this.markdown = markdown;
+
+//        options = new MutableDataSet() //
+//                .setFrom(ParserEmulationProfile.GITHUB_DOC);
+
+        List<Extension> extensions = new ArrayList<>();
+//        extensions.add(AutolinkExtension.create());
+        extensions.add(EmojiExtension.create());
+        extensions.add(StrikethroughExtension.create());
+//        extensions.add(TaskListExtension.create());
+        extensions.add(TablesExtension.create());
+
+        options = new MutableDataSet().set(Parser.EXTENSIONS, extensions)
+                // set GitHub table parsing options
+                .set(TablesExtension.WITH_CAPTION, false) //
+                .set(TablesExtension.COLUMN_SPANS, true) //
+                .set(TablesExtension.MIN_HEADER_ROWS, 1) //
+                .set(TablesExtension.MAX_HEADER_ROWS, 1) //
+                .set(TablesExtension.MIN_SEPARATOR_DASHES, 1) //
+                .set(TablesExtension.APPEND_MISSING_COLUMNS, true) //
+                .set(TablesExtension.DISCARD_EXTRA_COLUMNS, true) //
+                .set(TablesExtension.HEADER_SEPARATOR_COLUMN_MATCH, true) //
+                //
+                // setup emoji shortcut options
+                // uncomment and change to your image directory for emoji images if you have it
+                // setup
+                // .set(EmojiExtension.ROOT_IMAGE_PATH, emojiInstallDirectory())
+                .set(EmojiExtension.USE_SHORTCUT_TYPE, EmojiShortcutType.GITHUB) //
+                .set(EmojiExtension.USE_IMAGE_TYPE, EmojiImageType.IMAGE_ONLY) //
+        ;
+
+    }
+
+    public String getMarkdown() {
+        return markdown;
+    }
+
+    public void setMarkdown(String markdown) {
+        if (!Nullables.equals(this.markdown, markdown)) {
+            this.markdown = markdown;
+            this._document = null;
+        }
+    }
+
+    public synchronized Document getDocument() {
+        if (_document == null) {
+            Parser parser = Parser.builder(options).build();
+            _document = parser.parse(markdown);
+        }
+        return _document;
+    }
+
+    public MutableDataHolder getOptions() {
+        return options;
+    }
+
+    public void setOptions(MutableDataHolder options) {
+        this.options = options;
+    }
+
+    @Override
+    public void readObject(WordprocessingMLPackage pack)
+            throws ParseException, LoaderException {
+        throw new NotImplementedException("can't load markdown from word document.");
+    }
+
+    @Override
+    public void writeObject(WordprocessingMLPackage pack)
+            throws FormatException {
+        Document document = getDocument();
+        try {
+            DocxRenderer renderer = DocxRenderer.builder(options).build();
+            renderer.render(document, pack);
+        } catch (Exception e) {
+            throw new FormatException(e.getMessage(), e);
+        }
+    }
+
+}
---
+++ lily/layer-5/lily-impl/src/main/java/net/bodz/lily/format/excel/ExcelBuilder.java
@@ -0,0 +1,133 @@
+package net.bodz.lily.format.excel;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import net.bodz.bas.err.FormatException;
+import net.bodz.bas.err.LoaderException;
+import net.bodz.bas.err.ParseException;
+import net.bodz.bas.err.UnexpectedException;
+import net.bodz.bas.repr.content.FileContent;
+import net.bodz.bas.std.rfc.mime.ContentTypes;
+import net.bodz.bas.t.variant.IVarMapForm;
+import net.bodz.bas.t.variant.IVariantMap;
+import net.bodz.bas.t.variant.VarMapLoader;
+
+public abstract class ExcelBuilder
+        implements
+            IVarMapForm {
+
+    // protected Workbook workbook;
+
+    public ExcelBuilder() {
+    }
+
+    protected abstract String getTitle();
+
+    public final Workbook build(IVariantMap<String> q)
+            throws FormatException {
+        return build(false, q);
+    }
+
+    public final synchronized Workbook build(boolean xssf, IVariantMap<String> q)
+            throws FormatException {
+        try {
+            readObject(q);
+        } catch (LoaderException | ParseException e) {
+            throw new IllegalArgumentException("Failed to parse parameters: " + e.getMessage(), e);
+        }
+        return build();
+    }
+
+    public final Workbook build()
+            throws FormatException {
+        return build(false);
+    }
+
+    public final Workbook build(boolean xssf)
+            throws FormatException {
+        Workbook workbook;
+        try {
+            workbook = WorkbookFactory.create(xssf);
+        } catch (IOException e) {
+            throw new FormatException(e.getMessage(), e);
+        }
+        buildWorkbook(workbook);
+        return workbook;
+    }
+
+    public synchronized void buildWorkbook(Workbook workbook)
+            throws FormatException {
+        Sheet sheet = workbook.createSheet();
+
+        buildMainSheet(sheet);
+
+        String title = getTitle();
+        if (title != null) {
+            int sheetIndex = workbook.getSheetIndex(sheet);
+            workbook.setSheetName(sheetIndex, title);
+        }
+    }
+
+    protected abstract void buildMainSheet(Sheet sheet)
+            throws FormatException;
+
+    public byte[] toBytes()
+            throws FormatException {
+        return toBytes(false);
+    }
+
+    public byte[] toBytes(boolean xssf)
+            throws FormatException {
+        Workbook workbook = build(xssf);
+
+        ByteArrayOutputStream buf = new ByteArrayOutputStream(30000);
+        try {
+            workbook.write(buf);
+        } catch (IOException e) {
+            throw new UnexpectedException(e.getMessage(), e);
+        }
+
+        return buf.toByteArray();
+    }
+
+    public String getPreferredFileName(Workbook workbook) {
+        int activeSheetIndex = workbook.getActiveSheetIndex();
+        String sheetName = workbook.getSheetName(activeSheetIndex);
+        String fileName = sheetName;
+        return fileName;
+    }
+
+    public FileContent toContent(boolean xssf)
+            throws FormatException {
+        Workbook workbook = build();
+
+        ByteArrayOutputStream buf = new ByteArrayOutputStream(30000);
+        try {
+            workbook.write(buf);
+        } catch (IOException e) {
+            throw new UnexpectedException(e.getMessage(), e);
+        }
+
+        String fileName = getPreferredFileName(workbook);
+        fileName += (xssf ? ".xlsx" : ".xls");
+        return new FileContent(fileName, ContentTypes.application_vnd_ms_excel, buf.toByteArray());
+    }
+
+    @Override
+    public void readObject(IVariantMap<String> map)
+            throws LoaderException, ParseException {
+        VarMapLoader loader = new VarMapLoader();
+        loader.load(getClass(), this, map);
+    }
+
+    @Override
+    public void writeObject(Map<String, Object> map) {
+    }
+
+}
