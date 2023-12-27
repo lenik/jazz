9c0de1ea9c3637ddc311af71c5b67f1959638fc7
Lenik <pub.lenik@bodz.net>
2023-12-27 23:54:55 +0800

    bas-api: AbstractDecorator: allow null in ctor for delayed creation.

--- bas/layer-1/bas-api/src/main/java/net/bodz/bas/t/model/AbstractDecorator.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/t/model/AbstractDecorator.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 /**
  * Decorator Pattern:
- * 
+ *
  * Decorator is also called "Smart Proxy." This is used when you want to add functionality to an
  * object, but not by extending that object's type. This allows you to do so at runtime.
  */
@@ -16,8 +16,6 @@ public abstract class AbstractDecorator<T>
     protected T _orig;
 
     public AbstractDecorator(T _orig) {
-        if (_orig == null)
-            throw new NullPointerException("_orig");
         this._orig = _orig;
     }
 
