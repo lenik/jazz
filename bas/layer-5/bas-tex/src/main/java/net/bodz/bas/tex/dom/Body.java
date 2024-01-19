330c736420ca72c68249de7943aa3791cdef8059
Lenik <pub.lenik@bodz.net>
2024-01-19 12:39:10 +0800

    bas-c1: Add ITextForm.parseObject(), changed writeObject(ICharOut) to printObject(ITreeOut).

--- bas/layer-5/bas-tex/src/main/java/net/bodz/bas/tex/dom/Body.java
+++ bas/layer-5/bas-tex/src/main/java/net/bodz/bas/tex/dom/Body.java
@@ -2,15 +2,15 @@ package net.bodz.bas.tex.dom;
 
 import java.io.IOException;
 
-import net.bodz.bas.c.string.IIndentedForm;
+import net.bodz.bas.c.string.ITextPrintForm;
 import net.bodz.bas.io.ITreeOut;
 
 public class Body
         implements
-            IIndentedForm {
+            ITextPrintForm {
 
     @Override
-    public void writeObject(ITreeOut out)
+    public void printObject(ITreeOut out)
             throws IOException {
     }
 
