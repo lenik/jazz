adde24677fc555a43c32444224b0146832f20ab3
Lenik <pub.lenik@bodz.net>
2024-01-25 23:56:24 +0800

    bas-io: Enhanced IBitIn, add BitInImpl. (buffered)
    - Add IBitIn.read/readInt/readLong().
    - Add IBitIn.dropToByte().
    - Add IBitIn.readBool().

--- bas/layer-2/bas-io/src/main/java/net/bodz/bas/io/bit/IBitIn.java
+++ bas/layer-2/bas-io/src/main/java/net/bodz/bas/io/bit/IBitIn.java
@@ -1,8 +1,7 @@
 package net.bodz.bas.io.bit;
 
+import java.io.EOFException;
 import java.io.IOException;
-
-import net.bodz.bas.t._bit.ISimpleBits;
 
 public interface IBitIn {
 
@@ -12,11 +11,34 @@ public interface IBitIn {
     int readBit()
             throws IOException;
 
+    default boolean readBool()
+            throws IOException {
+        int bit = readBit();
+        if (bit == -1)
+            throw new EOFException();
+        return bit == 1 ? true : false;
+    }
+
+    int read()
+            throws IOException;
+
+    default int readInt(int n)
+            throws IOException {
+        return (int) readLong(n);
+    }
+
+    long readLong(int n)
+            throws IOException;
+
     /**
      * @return bit count successfully read, -1 if EOF reached.
      */
-    int readBit(ISimpleBits bits)
-            throws IOException;
+//    int readBit(ISimpleBits bits)
+//            throws IOException;
+
+    int availableBits();
+
+    void dropToByte();
 
     void close()
             throws IOException;
