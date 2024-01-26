9d80cc918b4ff7fac515d08183b91f8f4dc5127a
Lenik <pub.lenik@bodz.net>
2024-01-27 01:21:40 +0800

    lily: add createdb/dropdb makefile target.

--- lily/sql/fab.sql
+++ lily/sql/fab.sql
@@ -1,12 +1,13 @@
 --\import lily
 
-create schema if not exists "${1=violet}";
-set search_path = ${1=violet}, lily, public;
-
 -------- Violet --------
 --\import violet
 
--------- Violet :: Edu --------
+create schema if not exists "${1=violet}";
+set search_path = ${1=violet}, lily, public;
+
+-------- Violet :: Fab --------
 --\import violet.fab
 
 set search_path = public;
+
