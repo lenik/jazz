11e71ee9944e9ba132f26e8f8648d5711efbf092
Lenik <pub.lenik@bodz.net>
2024-01-26 17:26:38 +0800

    lily: Polishing person demo.

--- lily/basic-types/src/main/java/net/bodz/lily/schema/contact/dao/PersonCriteriaBuilder.java
+++ lily/basic-types/src/main/java/net/bodz/lily/schema/contact/dao/PersonCriteriaBuilder.java
@@ -1,6 +1,19 @@
 package net.bodz.lily.schema.contact.dao;
 
+import net.bodz.lily.criteria.NumberFieldCriterionBuilder;
+
 public class PersonCriteriaBuilder
         extends _PersonCriteriaBuilder_stuff<PersonCriteriaBuilder> {
+
+    @Override
+    public NumberFieldCriterionBuilder<?, ?> getIdField() {
+        return id;
+    }
+
+    @Override
+    public void query(String text) {
+        super.query(text);
+        or().gender.like(text).end();
+    }
 
 }
