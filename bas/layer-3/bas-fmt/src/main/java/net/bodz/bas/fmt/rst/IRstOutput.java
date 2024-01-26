b7a046d2720c5c6cbb899cd995a2101e29b5d944
Lenik <pub.lenik@bodz.net>
2024-01-26 17:15:06 +0800

    bas-fmt: Add IRstOutput.object(name,child).

--- bas/layer-3/bas-fmt/src/main/java/net/bodz/bas/fmt/rst/IRstOutput.java
+++ bas/layer-3/bas-fmt/src/main/java/net/bodz/bas/fmt/rst/IRstOutput.java
@@ -2,6 +2,7 @@ package net.bodz.bas.fmt.rst;
 
 import java.io.IOException;
 
+import net.bodz.bas.c.string.StringArray;
 import net.bodz.bas.err.FormatException;
 import net.bodz.bas.fmt.api.IDataCodec;
 import net.bodz.bas.fmt.api.StdDataCodec;
@@ -13,14 +14,36 @@ public interface IRstOutput {
 
     FramedMarks getMarks();
 
+    default void element(String name, IRstForm child, Object... args)
+            throws IOException, FormatException {
+        element(name, child, StringArray.of(args));
+    }
+
     void element(String name, IRstForm child, String... args)
             throws IOException, FormatException;
+
+    default void beginElement(String name, Object... args)
+            throws IOException, FormatException {
+        beginElement(name, StringArray.of(args));
+    }
 
     void beginElement(String name, String... args)
             throws IOException;
 
     void endElement()
             throws IOException;
+
+    default void object(String name, IRstForm child, Object... args)
+            throws IOException, FormatException {
+        object(name, child, StringArray.of(args));
+    }
+
+    default void object(String name, IRstForm child, String... args)
+            throws IOException, FormatException {
+        beginElement(name, args);
+        child.writeObject(this);
+        endElement();
+    }
 
     void attributeRaw(String name, String rawData)
             throws IOException;
