eb5a7f7ef9ef247057541997558ff9c76f0d1d60
Lenik <pub.lenik@bodz.net>
2024-02-17 19:05:39 +0800

    lily: generate router.ts by make-router.

---
+++ lily/@lily/packages/basic/test/Index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+import type { ViewHandles } from '@skeljs/core/src/ui/layout/TabViews.vue';
+import TabViews, { getSelectedViewName } from '@skeljs/core/src/ui/layout/TabViews.vue';
+import View from '@skeljs/core/src/ui/layout/View.vue';
+import { links } from './router';
+
+const views: ViewHandles = {
+    index: {
+        selected: true,
+        label: "Directory",
+        icon: "far-list",
+    },
+};
+
+const initialView = getSelectedViewName(views);
+const view = ref(initialView);
+
+</script>
+
+<template>
+    <TabViews :views="views" v-model:view="view">
+        <View name="index" :selection="view">
+            <ul>
+                <li v-for="(it, k) in links" :key="k">
+                    <router-link :to="k"> {{ it }} </router-link>
+                </li>
+            </ul>
+        </View>
+    </TabViews>
+</template>
