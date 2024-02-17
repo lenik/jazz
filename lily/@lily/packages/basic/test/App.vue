8327ac5950a649e415914cfc466631eb7533b9d9
Lenik <pub.lenik@bodz.net>
2024-02-17 19:05:39 +0800

    lily: generate router.ts by make-router.

---
+++ lily/@lily/packages/basic/test/App.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+
+import { provide, ref } from 'vue';
+
+import ModelC from '@skeljs/core/src/ui/layout/ModelC.vue';
+import SiteBar from '@skeljs/core/src/ui/menu/SiteBar.vue';
+import ProjectInfo from '@skeljs/core/src/ui/demo/ProjectInfo.vue';
+import Copyright from '@skeljs/core/src/ui/demo/Copyright.vue';
+import { SERVER_URL } from '@skeljs/dba/src/ui/lily/context';
+import { serverUrl } from './server.config';
+
+const model = defineModel();
+
+interface Props {
+}
+
+const props = withDefaults(defineProps<Props>(), {
+});
+
+const emit = defineEmits<{
+    error: [message: string]
+}>();
+
+// provides
+provide(SERVER_URL, serverUrl);
+
+// property shortcuts
+
+const version = "1.0";
+const stat = {
+    views: 100,
+    stars: 5,
+    upvotes: 26,
+    downvotes: 2,
+};
+
+</script>
+
+<template>
+    <ModelC>
+        <template #site-bar>
+            <SiteBar title='Lily tests' />
+        </template>
+        <template #project-info>
+            <ProjectInfo :stat="stat" label="Lily Test Apps" description="Test applications of lily::basuc. ">
+            </ProjectInfo>
+        </template>
+        <router-view></router-view>
+        <template #footer>
+            <Copyright />
+        </template>
+    </ModelC>
+</template>
+
+<style lang="scss" scoped>
+.sitebar {
+    background: #444;
+}
+
+#v-options {
+    padding: 0;
+}
+</style>
