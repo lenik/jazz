f1a53ec6c0db7e5cd71345e7110526fbabd611d9
Lenik <pub.lenik@bodz.net>
2024-02-19 00:57:00 +0800

    bas-api: Add Primitives.forName().

--- bas/layer-1/bas-api/src/main/java/net/bodz/bas/c/primitive/Primitives.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/c/primitive/Primitives.java
@@ -5,29 +5,27 @@ import java.util.Map;
 
 public class Primitives {
 
-    private static Map<Class<?>, Class<?>> p2w;
-    private static Map<Class<?>, Class<?>> w2p;
+    private static final Map<Class<?>, Class<?>> p2w = new HashMap<>();
+    private static final Map<Class<?>, Class<?>> w2p = new HashMap<>();
+    private static final Map<String, Class<?>> names = new HashMap<>();
+
+    static void add(Class<?> type, Class<?> boxed) {
+        p2w.put(type, boxed);
+        w2p.put(boxed, type);
+        String name = type.getName();
+        names.put(name, type);
+    }
+
     static {
-        p2w = new HashMap<Class<?>, Class<?>>();
-        p2w.put(void.class, Void.class);
-        p2w.put(byte.class, Byte.class);
-        p2w.put(short.class, Short.class);
-        p2w.put(int.class, Integer.class);
-        p2w.put(long.class, Long.class);
-        p2w.put(float.class, Float.class);
-        p2w.put(double.class, Double.class);
-        p2w.put(char.class, Character.class);
-        p2w.put(boolean.class, Boolean.class);
-        w2p = new HashMap<Class<?>, Class<?>>();
-        w2p.put(Void.class, void.class);
-        w2p.put(Byte.class, byte.class);
-        w2p.put(Short.class, short.class);
-        w2p.put(Integer.class, int.class);
-        w2p.put(Long.class, long.class);
-        w2p.put(Float.class, float.class);
-        w2p.put(Double.class, double.class);
-        w2p.put(Character.class, char.class);
-        w2p.put(Boolean.class, boolean.class);
+        add(void.class, Void.class);
+        add(byte.class, Byte.class);
+        add(short.class, Short.class);
+        add(int.class, Integer.class);
+        add(long.class, Long.class);
+        add(float.class, Float.class);
+        add(double.class, Double.class);
+        add(char.class, Character.class);
+        add(boolean.class, Boolean.class);
     }
 
     public static Class<?> box(Class<?> type) {
@@ -37,6 +35,15 @@ public class Primitives {
     public static Class<?> unbox(Class<?> type) {
         Class<?> primitive = w2p.get(type);
         return primitive == null ? type : primitive;
+    }
+
+    public static Class<?> forName(String className)
+            throws ClassNotFoundException {
+        Class<?> clazz = names.get(className);
+        if (clazz != null)
+            return clazz;
+        else
+            return Class.forName(className);
     }
 
 }
