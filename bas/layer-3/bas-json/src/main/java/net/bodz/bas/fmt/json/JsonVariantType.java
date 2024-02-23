17e68d80e446ed2db334bbcb9ce5b72dc3bce7db
Lenik <pub.lenik@bodz.net>
2024-02-23 14:56:18 +0800

    bas-json: make JsonVariant immutable. Add NULL type.

--- bas/layer-3/bas-json/src/main/java/net/bodz/bas/fmt/json/JsonVariantType.java
+++ bas/layer-3/bas-json/src/main/java/net/bodz/bas/fmt/json/JsonVariantType.java
@@ -1,5 +1,8 @@
 package net.bodz.bas.fmt.json;
 
+import net.bodz.bas.json.JsonArray;
+import net.bodz.bas.json.JsonObject;
+
 public enum JsonVariantType {
 
     OBJECT,
@@ -7,5 +10,19 @@ public enum JsonVariantType {
     ARRAY,
 
     SCALAR,
+
+    NULL,
+
+    ;
+
+    public static JsonVariantType getType(Object value) {
+        if (value == null)
+            return JsonVariantType.NULL;
+        if (value instanceof JsonObject)
+            return JsonVariantType.OBJECT;
+        if (value instanceof JsonArray)
+            return JsonVariantType.ARRAY;
+        return JsonVariantType.SCALAR;
+    }
 
 }
