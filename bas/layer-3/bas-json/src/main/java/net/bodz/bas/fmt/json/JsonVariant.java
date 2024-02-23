0ffb876a234486e41d2c9b0fc3ee6934860473f3
Lenik <pub.lenik@bodz.net>
2024-02-23 14:56:18 +0800

    bas-json: make JsonVariant immutable. Add NULL type.

--- bas/layer-3/bas-json/src/main/java/net/bodz/bas/fmt/json/JsonVariant.java
+++ bas/layer-3/bas-json/src/main/java/net/bodz/bas/fmt/json/JsonVariant.java
@@ -8,24 +8,25 @@ import net.bodz.bas.repr.form.SortOrder;
 
 public class JsonVariant {
 
-    JsonVariantType type;
-
-    Object value;
+    final JsonVariantType type;
+    final Object value;
 
     public JsonVariant(JsonVariantType type, Object value) {
-        setValue(type, value);
+        this.type = value == null ? JsonVariantType.NULL : type;
+        this.value = value;
     }
 
     public JsonVariant(Object value) {
-        setValue(value);
+        this.type = JsonVariantType.getType(value);
+        this.value = value;
     }
 
     public JsonVariant(JsonObject value) {
-        setValue(JsonVariantType.OBJECT, value);
+        this(JsonVariantType.OBJECT, value);
     }
 
     public JsonVariant(JsonArray value) {
-        setValue(JsonVariantType.ARRAY, value);
+        this(JsonVariantType.ARRAY, value);
     }
 
     public static JsonVariant of(Object value) {
@@ -44,6 +45,10 @@ public class JsonVariant {
         return type;
     }
 
+    public boolean isNull() {
+        return type == JsonVariantType.NULL;
+    }
+
     public boolean isObject() {
         return type == JsonVariantType.OBJECT;
     }
@@ -57,21 +62,36 @@ public class JsonVariant {
     }
 
     public JsonObject getObject() {
-        if (type != JsonVariantType.OBJECT)
+        switch (type) {
+        case NULL:
+            return null;
+        case OBJECT:
+            return (JsonObject) value;
+        default:
             throw new IllegalUsageException("expect Json Object.");
-        return (JsonObject) value;
+        }
     }
 
     public JsonArray getArray() {
-        if (type != JsonVariantType.ARRAY)
+        switch (type) {
+        case NULL:
+            return null;
+        case ARRAY:
+            return (JsonArray) value;
+        default:
             throw new IllegalUsageException("expect Json Array.");
-        return (JsonArray) value;
+        }
     }
 
     public Object getScalar() {
-        if (type != JsonVariantType.SCALAR)
+        switch (type) {
+        case NULL:
+            return null;
+        case SCALAR:
+            return value;
+        default:
             throw new IllegalUsageException("expect scalar.");
-        return value;
+        }
     }
 
     public JsonObject getObjectFor(String what) {
@@ -96,74 +116,67 @@ public class JsonVariant {
         return value;
     }
 
-    public void setAny(Object value) {
-        this.value = value;
-        if (value instanceof JsonObject)
-            type = JsonVariantType.OBJECT;
-        else if (value instanceof JsonArray)
-            type = JsonVariantType.ARRAY;
-        else
-            type = JsonVariantType.SCALAR;
-    }
-
-    public void setAny(JsonVariantType type, Object value) {
-        if (type == null)
-            throw new NullPointerException("type'");
-        if (value == null)
-            throw new NullPointerException("value");
-        this.type = type;
-        this.value = value;
-    }
-
     @Deprecated
     public Object getValue() {
         return getAny();
     }
 
-    @Deprecated
-    public void setValue(Object value) {
-        setAny(value);
-    }
-
-    @Deprecated
-    public void setValue(JsonVariantType type, Object value) {
-        setAny(type, value);
-    }
-
+    /**
+     * @return <code>null</code> if the item is <code>null</code>.
+     * @throws NullPointerException
+     *             If the object is null.
+     */
     public JsonVariant get(String key) {
+        if (type == null)
+            throw new NullPointerException("null object");
         if (type == JsonVariantType.OBJECT) {
             JsonObject jo = (JsonObject) value;
             Object any = jo.get(key);
+            if (any == null)
+                return null;
             return new JsonVariant(any);
         }
         return null;
     }
 
+    /**
+     * @return <code>null</code> if the array item is <code>null</code>.
+     * @throws IndexOutOfBoundsException
+     * @throws NullPointerException
+     *             If the array is null.
+     */
     public JsonVariant get(int index) {
+        if (type == JsonVariantType.NULL)
+            throw new NullPointerException("null array");
         if (type == JsonVariantType.ARRAY) {
             JsonArray ja = (JsonArray) value;
             Object any = ja.get(index);
+            if (any == null)
+                return null;
             return new JsonVariant(any);
         }
         return null;
     }
 
-    public void sort() {
-        sort(SortOrder.ASCENDING);
+    public JsonVariant sort() {
+        return sort(SortOrder.ASCENDING);
     }
 
-    public void sort(SortOrder order) {
+    public JsonVariant sort(SortOrder order) {
         if (type == JsonVariantType.OBJECT) {
             JsonObject hash = (JsonObject) value;
             JsonObject sorted = new JsonObject(order);
             for (String key : hash.keySet())
                 sorted.put(key, hash.get(key));
-            value = sorted;
+            return of(sorted);
         }
+        return this;
     }
 
     public String toJson() {
         switch (type) {
+        case NULL:
+            return "null";
         case OBJECT:
             JsonObject jo = (JsonObject) value;
             return jo.toString();
@@ -180,7 +193,12 @@ public class JsonVariant {
 
     @Override
     public String toString() {
-        return type + ": " + value;
+        if (type == JsonVariantType.NULL)
+            return "null";
+        else
+            return type + ": " + value;
     }
+
+    public static final JsonVariant NULL = new JsonVariant(JsonVariantType.NULL, null);
 
 }
