17d3c52b66bc46c665f9b7ed5ae8e0004c23a4e9
Lenik <pub.lenik@bodz.net>
2024-03-12 12:50:36 +0800

    lily: Add CoImagedMomentInterval.

---
+++ lily/lily-api/src/main/java/net/bodz/lily/concrete/CoImagedCriteriaBuilder.java
@@ -0,0 +1,6 @@
+package net.bodz.lily.concrete;
+
+public class CoImagedCriteriaBuilder<self_t extends CoImagedCriteriaBuilder<self_t>>
+        extends CoObjectCriteriaBuilder<self_t> {
+
+}
---
+++ lily/lily-api/src/main/java/net/bodz/lily/concrete/CoImagedMomentIntervalCriteriaBuilder.java
@@ -0,0 +1,6 @@
+package net.bodz.lily.concrete;
+
+public class CoImagedMomentIntervalCriteriaBuilder<self_t extends CoImagedMomentIntervalCriteriaBuilder<self_t>>
+        extends CoMomentIntervalCriteriaBuilder<self_t> {
+
+}
