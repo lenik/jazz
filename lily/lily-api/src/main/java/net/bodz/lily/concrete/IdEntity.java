feebd7075ac4c85f97df5095c2d470a896266f6b
Lenik <pub.lenik@bodz.net>
2024-03-20 17:36:18 +0800

    lily: Annotated IdEntity.id with @GeneratedValue.

--- lily/lily-api/src/main/java/net/bodz/lily/concrete/IdEntity.java
+++ lily/lily-api/src/main/java/net/bodz/lily/concrete/IdEntity.java
@@ -1,5 +1,7 @@
 package net.bodz.lily.concrete;
 
+import javax.persistence.GeneratedValue;
+
 import net.bodz.lily.entity.IdColumn;
 import net.bodz.lily.meta.TypeParamType;
 import net.bodz.lily.meta.TypeParameters;
@@ -11,6 +13,7 @@ public abstract class IdEntity<Id>
 
     private static final long serialVersionUID = 1L;
 
+    @GeneratedValue
     public Id getId() {
         return id();
     }
