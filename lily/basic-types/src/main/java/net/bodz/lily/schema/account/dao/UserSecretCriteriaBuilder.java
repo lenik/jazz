524ad4265e5a3cea627c1c999867d7c55fbfbf94
Lenik <pub.lenik@bodz.net>
2024-04-29 15:40:22 +0800

    lily: UserSecretCriteria: add name = userName = field("user".name).

--- lily/basic-types/src/main/java/net/bodz/lily/schema/account/dao/UserSecretCriteriaBuilder.java
+++ lily/basic-types/src/main/java/net/bodz/lily/schema/account/dao/UserSecretCriteriaBuilder.java
@@ -4,6 +4,7 @@ public class UserSecretCriteriaBuilder
         extends _UserSecretCriteriaBuilder_stuff<UserSecretCriteriaBuilder> {
 
     public final StringField userName = string("\"user\".name");
+    public final StringField name = userName;
 
     public final StringField phone = string("phone");
     public final StringField email = string("email");
