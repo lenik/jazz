7971605a32bdee6788bff8b29825b483b7445bd4
Lenik <pub.lenik@bodz.net>
2024-07-15 06:05:25 +0800

    lily: refreshed */router.ts.

--- lily/@lily/packages/fab/test/router.ts
+++ lily/@lily/packages/fab/test/router.ts
@@ -23,30 +23,32 @@ export const links = {
     "FabTrackTestParameterAdmin": "Admin view of: Fab track test parameter",
 };
 
+export const routes = [
+    { path: '/', component: () => import('./Index.vue') },
+    { path: '/Project', component: () => import('@skeljs/core/src/ui/demo/Project.vue') },
+    { path: '/ArtifactModelAdmin', component: () => import('../src/net/bodz/violet/schema/art/ArtifactModelAdmin.vue') },
+    { path: '/FabFnAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabFnAdmin.vue') },
+    { path: '/FabOrderAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabOrderAdmin.vue') },
+    { path: '/FabOrderItemAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabOrderItemAdmin.vue') },
+    { path: '/FabProcessAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabProcessAdmin.vue') },
+    { path: '/FabProcessSerialAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabProcessSerialAdmin.vue') },
+    { path: '/FabStdProcessAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdProcessAdmin.vue') },
+    { path: '/FabStdProcessInputAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdProcessInputAdmin.vue') },
+    { path: '/FabStdTestAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTestAdmin.vue') },
+    { path: '/FabStdTestCategoryAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTestCategoryAdmin.vue') },
+    { path: '/FabStdTestParameterAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTestParameterAdmin.vue') },
+    { path: '/FabStdTesterAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTesterAdmin.vue') },
+    { path: '/FabTaskAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTaskAdmin.vue') },
+    { path: '/FabTaskItemAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTaskItemAdmin.vue') },
+    { path: '/FabTrackAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackAdmin.vue') },
+    { path: '/FabTrackPartyAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackPartyAdmin.vue') },
+    { path: '/FabTrackTestAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackTestAdmin.vue') },
+    { path: '/FabTrackTestParameterAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackTestParameterAdmin.vue') },
+];
+
 const router = createRouter({
     history: createWebHashHistory(),
-    routes: [
-        { path: '/', component: () => import('./Index.vue') },
-        { path: '/Project', component: () => import('@skeljs/core/src/ui/demo/Project.vue') },
-        { path: '/ArtifactModelAdmin', component: () => import('../src/net/bodz/violet/schema/art/ArtifactModelAdmin.vue') },
-        { path: '/FabFnAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabFnAdmin.vue') },
-        { path: '/FabOrderAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabOrderAdmin.vue') },
-        { path: '/FabOrderItemAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabOrderItemAdmin.vue') },
-        { path: '/FabProcessAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabProcessAdmin.vue') },
-        { path: '/FabProcessSerialAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabProcessSerialAdmin.vue') },
-        { path: '/FabStdProcessAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdProcessAdmin.vue') },
-        { path: '/FabStdProcessInputAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdProcessInputAdmin.vue') },
-        { path: '/FabStdTestAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTestAdmin.vue') },
-        { path: '/FabStdTestCategoryAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTestCategoryAdmin.vue') },
-        { path: '/FabStdTestParameterAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTestParameterAdmin.vue') },
-        { path: '/FabStdTesterAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabStdTesterAdmin.vue') },
-        { path: '/FabTaskAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTaskAdmin.vue') },
-        { path: '/FabTaskItemAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTaskItemAdmin.vue') },
-        { path: '/FabTrackAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackAdmin.vue') },
-        { path: '/FabTrackPartyAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackPartyAdmin.vue') },
-        { path: '/FabTrackTestAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackTestAdmin.vue') },
-        { path: '/FabTrackTestParameterAdmin', component: () => import('../src/net/bodz/violet/schema/fab/FabTrackTestParameterAdmin.vue') },
-    ]
+    routes: routes
 });
 
 export default router;
