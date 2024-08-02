4ff0ed112ea2554d8ae0d046cbe4e4434ae32541
Lenik <pub.lenik@bodz.net>
2024-08-02 13:38:58 +0800

    bas-repr-api: Moved @NotNull to bas-meta.

--- bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/decl/Nullable.java
+++ bas/layer-1/bas-meta/src/main/java/net/bodz/bas/meta/decl/Nullable.java
@@ -1,11 +1,14 @@
-package net.bodz.bas.repr.form.meta;
+package net.bodz.bas.meta.decl;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER })
 public @interface Nullable {
 
 }
