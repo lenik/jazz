7e208a2a2df67210d27fb25b49b1ca9341187ff2
Lenik <pub.lenik@bodz.net>
2024-08-20 18:58:43 +0800

    bas-json: Add JsonResult.execute().

--- bas/layer-3/bas-json/src/main/java/net/bodz/bas/site/json/JsonResult.java
+++ bas/layer-3/bas-json/src/main/java/net/bodz/bas/site/json/JsonResult.java
@@ -1,6 +1,7 @@
 package net.bodz.bas.site.json;
 
 import net.bodz.bas.fmt.json.IJsonForm;
+import net.bodz.bas.fn.IExecutableX;
 
 public class JsonResult
         extends AbstractJsonResponse<JsonResult> {
@@ -11,6 +12,26 @@ public class JsonResult
 
     public JsonResult(int status, String message, IJsonForm data) {
         super(status, message, data);
+    }
+
+    public void execute(IExecutableX<Throwable> executable) {
+        try {
+            executable.execute();
+            succeed();
+        } catch (Throwable e) {
+            fail(e, e.getMessage());
+        }
+    }
+
+    public static JsonResult exec(IExecutableX<Throwable> executable) {
+        JsonResult result = new JsonResult();
+        try {
+            executable.execute();
+            result.succeed();
+        } catch (Throwable e) {
+            result.fail(e, e.getMessage());
+        }
+        return result;
     }
 
 }
