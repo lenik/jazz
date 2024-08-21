98e9f48d9e0fb28c895924cc112f3ae0d7e3425a
Lenik <pub.lenik@bodz.net>
2024-08-21 10:29:56 +0800

    bas-api: Add PriorityComparator.getInstance().

--- bas/layer-1/bas-api/src/main/java/net/bodz/bas/t/order/PriorityComparator.java
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/t/order/PriorityComparator.java
@@ -1,9 +1,11 @@
 package net.bodz.bas.t.order;
 
+import java.util.Comparator;
+
 /**
  * For two objects with the same priority, the order is undetermined. (identity hash code is used in
  * this case)
- * 
+ *
  * Override {@link #compare2(Object, Object)} when necessary.
  */
 public class PriorityComparator
@@ -20,7 +22,7 @@ public class PriorityComparator
 
     /**
      * The default implementation compares the two objects by their identity hash code.
-     * 
+     *
      * @param o1
      *            The first non-<code>null</code> object to compare.
      * @param o2
@@ -30,6 +32,11 @@ public class PriorityComparator
     @Override
     protected int compareFallback(IPriority o1, IPriority o2) {
         return compareIdentity(o1, o2);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static final <T> Comparator<T> getInstance() {
+        return (Comparator<T>) INSTANCE;
     }
 
     public static final PriorityComparator INSTANCE = new PriorityComparator();
