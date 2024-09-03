6adf6097b6a24ab4b4ba146e7ad23420589b963d
Lenik <pub.lenik@bodz.net>
2024-09-03 23:21:02 +0800

    bas-c1: IndexedTypes: iterator load class on demand.

--- bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/type/IndexedTypes.java
+++ bas/layer-1/bas-c1/src/main/java/net/bodz/bas/c/type/IndexedTypes.java
@@ -6,7 +6,12 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.lang.reflect.Modifier;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ServiceLoader;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,6 +19,7 @@ import net.bodz.bas.c.java.nio.Charsets;
 import net.bodz.bas.err.IllegalUsageException;
 import net.bodz.bas.meta.codegen.IndexedType;
 import net.bodz.bas.meta.codegen.PublishDir;
+import net.bodz.bas.t.iterator.PrefetchedIterator;
 import net.bodz.bas.t.order.IPriority;
 import net.bodz.bas.t.order.PriorityComparator;
 
@@ -56,67 +62,150 @@ public class IndexedTypes {
         }
 
         String resourceName = dirName + "/" + serviceBaseType.getName();
-        Enumeration<URL> resources = classLoader.getResources(resourceName);
-        Set<Class<? extends T>> classes = new LinkedHashSet<Class<? extends T>>();
 
-        while (resources.hasMoreElements()) {
-            URL url = resources.nextElement();
-            InputStream in = url.openStream();
-            try {
-                InputStreamReader reader = new InputStreamReader(in, Charsets.UTF_8);
-                BufferedReader br = new BufferedReader(reader);
-                String line;
-                while (true) {
-                    try {
-                        line = br.readLine();
-                    } catch (Exception e) { // SecurityException: digest error
-                        logger.severe("Can't read from " + url + ": " + e.getMessage());
-                        e.printStackTrace();
-                        break;
-                    }
-
-                    if (line == null)
-                        break;
-
-                    String className = line.trim();
-
-                    if (className.isEmpty())
-                        continue;
-
-                    if (className.startsWith("#"))
-                        continue;
-
-                    /*
-                     * Optimize: Maybe another class loader different to the resource discoverer
-                     * should be used.
-                     */
-                    Class<?> serviceClass;
-                    try {
-                        serviceClass = Class.forName(className, true, classLoader);
-                    } catch (ExceptionInInitializerError e) {
-                        logger.log(Level.SEVERE, "Failed to resolve service class: " + className, e);
-                        throw e;
-                    }
-
-                    if (!serviceBaseType.isAnnotation())
-                        if (!serviceBaseType.isAssignableFrom(serviceClass))
-                            throw new IllegalUsageException(String.format("Invalid service class %s for %s.", //
-                                    className, serviceBaseType.getName()));
-
-                    if (Modifier.isAbstract(serviceClass.getModifiers()))
-                        if (!includeAbstract)
-                            continue;
-
-                    @SuppressWarnings("unchecked")
-                    Class<? extends T> casted = (Class<? extends T>) serviceClass;
-                    classes.add(casted);
+        return new Iterable<Class<? extends T>>() {
+            @Override
+            public Iterator<Class<? extends T>> iterator() {
+                try {
+                    Enumeration<URL> resources = classLoader.getResources(resourceName);
+                    return new ResolveIterator<T>(serviceBaseType, includeAbstract, classLoader, resources);
+                } catch (IOException e) {
+                    throw new RuntimeException(e.getMessage(), e);
                 }
-            } finally {
-                in.close();
+            }
+        };
+
+    }
+
+    static class ResolveIterator<T>
+            extends PrefetchedIterator<Class<? extends T>> {
+
+        Class<T> serviceBaseType;
+        boolean includeAbstract;
+        ClassLoader classLoader;
+
+        Enumeration<URL> resources;
+        URL currentResource;
+        BufferedReader currentReader;
+        boolean ignoreIOError = false;
+        boolean ignoreROE = true;
+
+        public ResolveIterator(Class<T> serviceBaseType, boolean includeAbstract, ClassLoader classLoader,
+                Enumeration<URL> resources) {
+            this.serviceBaseType = serviceBaseType;
+            this.includeAbstract = includeAbstract;
+            this.classLoader = classLoader;
+            this.resources = resources;
+        }
+
+        void close() {
+            if (currentReader != null) {
+                try {
+                    currentReader.close();
+                } catch (IOException e) {
+                    logger.log(Level.WARNING, "Can't close the reader: " + e.getMessage(), e);
+                    if (ignoreIOError) {
+                        // ignore close error.
+                    } else {
+                        throw new RuntimeException("error close: " + e.getMessage(), e);
+                    }
+                } finally {
+                    currentReader = null;
+                }
             }
         }
 
-        return classes;
+        @Override
+        protected Class<? extends T> fetch() {
+            if (currentReader == null) {
+                while (resources.hasMoreElements()) {
+                    URL resource = resources.nextElement();
+
+                    InputStream in;
+                    try {
+                        in = resource.openStream();
+                    } catch (IOException e) {
+                        logger.severe(String.format(//
+                                "Can't open resource %s to read: %s", currentResource, e.getMessage()));
+                        if (ignoreIOError)
+                            continue;
+                        else
+                            throw new RuntimeException(e);
+                    }
+                    currentResource = resource;
+
+                    InputStreamReader reader = new InputStreamReader(in, Charsets.UTF_8);
+                    currentReader = new BufferedReader(reader);
+                    break;
+                }
+                if (currentReader == null)
+                    return end();
+            }
+
+            String line;
+            while (true) {
+                try {
+                    line = currentReader.readLine();
+                } catch (IOException e) { // SecurityException: digest error
+                    logger.log(Level.SEVERE, "Can't read from " + currentResource + ": " + e.getMessage(), e);
+                    close();
+                    return fetch();
+                }
+
+                // EOF
+                if (line == null) {
+                    close();
+                    return fetch();
+                }
+
+                String className = line.trim();
+                if (className.isEmpty())
+                    continue;
+                if (className.startsWith("#"))
+                    continue;
+
+                /*
+                 * Optimize: Maybe another class loader different to the resource discoverer should
+                 * be used.
+                 */
+                Class<?> serviceClass;
+                try {
+                    serviceClass = Class.forName(className, true, classLoader);
+                } catch (ExceptionInInitializerError e) {
+                    logger.log(Level.SEVERE, "Failed to init service class: " + className, e);
+                    if (ignoreROE)
+                        continue;
+                    else
+                        throw e;
+                } catch (ClassNotFoundException | NoClassDefFoundError e) {
+                    if (ignoreROE) {
+                        logger.log(Level.SEVERE, "No such service class: " + className);
+                        continue;
+                    } else
+                        throw new IllegalUsageException(e);
+                }
+
+                if (! serviceBaseType.isAnnotation())
+                    if (! serviceBaseType.isAssignableFrom(serviceClass)) {
+                        String message = String.format(//
+                                "Invalid service class %s for %s.", className, serviceBaseType.getName());
+                        logger.severe(message);
+                        if (ignoreROE)
+                            continue;
+                        else
+                            throw new IllegalUsageException(message);
+                    }
+
+                if (Modifier.isAbstract(serviceClass.getModifiers()))
+                    if (! includeAbstract)
+                        continue;
+
+                @SuppressWarnings("unchecked")
+                Class<? extends T> casted = (Class<? extends T>) serviceClass;
+                return casted;
+            }
+        } // fetch()
+
     }
 
     public static <S extends IPriority> List<S> loadInOrder(Class<S> service) {
