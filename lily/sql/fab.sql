f0c57e79f5ed59d0f223dc29dc75aa0cdfc72501
Lenik <pub.lenik@bodz.net>
2024-09-07 20:01:46 +0800

    lily: fab.sql: shared create-schema with violet.

--- lily/sql/fab.sql
+++ lily/sql/fab.sql
@@ -3,7 +3,8 @@
 -------- Violet --------
 --\import violet
 
-create schema if not exists "${1=violet}";
+-- shared schema with violet
+-- create schema if not exists "${1=fab}";
 set search_path = ${1=violet}, lily, public;
 
 -------- Violet :: Fab --------
