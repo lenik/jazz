99976d3d4b0dd8dfcb16e0ef7e4c196bc4d47a14
Lenik <pub.lenik@bodz.net>
2025-03-14 08:31:19 +0800

    mda-xjdoc: nullable classDocFile.

--- mda/contrib/mda-xjdoc-maven-plugin/src/main/java/net/bodz/mda/xjdoc/contrib/maven/ClassDocBuilderMojo.java
+++ mda/contrib/mda-xjdoc-maven-plugin/src/main/java/net/bodz/mda/xjdoc/contrib/maven/ClassDocBuilderMojo.java
@@ -77,7 +77,7 @@ public class ClassDocBuilderMojo
 
     /**
      * Xjdoc taglib names.
-     *
+     * <p>
      * This can be the FQCN of the {@link ITagLibrary} implementation, or predefined language name
      * includes:
      * <ul>
@@ -192,7 +192,7 @@ public class ClassDocBuilderMojo
         DirectoryScanner scanner = new DirectoryScanner(srcDir);
         scanner.addFilter(new SuffixFilter(".java"));
 
-        if (! alwaysMake)
+        if (!alwaysMake)
             scanner.addFilter(new IgnoreUpdatedFilter(srcDir, dstDir));
 
         scanner.scan(new FileVisitor() {
@@ -210,9 +210,8 @@ public class ClassDocBuilderMojo
         return jsources;
     }
 
-    class IgnoreUpdatedFilter
-            implements
-                Filter {
+    static class IgnoreUpdatedFilter
+            implements Filter {
 
         File srcDir;
         File dstDir;
@@ -231,7 +230,7 @@ public class ClassDocBuilderMojo
             if (srcPath.length() < srcPrefixLen) // dir, not a file.
                 return false;
             String srcRelativePath = srcPath.substring(srcPrefixLen);
-            if (! srcRelativePath.endsWith(".java"))
+            if (!srcRelativePath.endsWith(".java"))
                 return false;
             String dstRelativePath = srcRelativePath.substring(0, srcRelativePath.length() - 5) + ".ff";
             File dstFile = new File(dstDir, dstRelativePath);
@@ -257,14 +256,9 @@ public class ClassDocBuilderMojo
             String packageName = jsource.getPackageName();
             File outDir = outRoot == null ? null : new File(outRoot, packageName.replace('.', '/'));
 
-            // Not used: souce imports are resolvable in members.
-            // ImportMap sourceFileImports = new ImportMap(packageName);
-            // for (String importFqcn : jsource.getImports())
-            // sourceFileImports.add(importFqcn);
-
             for (JavaClass jclass : QdoxUtils.getAllNestedClasses(jsource)) {
                 String qName = jclass.getFullyQualifiedName();
-                if (! qName.startsWith(packageName + "."))
+                if (!qName.startsWith(packageName + "."))
                     throw new UnexpectedException("Class FQCN doesn't starts with package: " + qName);
                 String baseName = qName.substring(packageName.length() + 1);
                 baseName = baseName.replace('.', '$') + "." + extension;
@@ -303,10 +297,11 @@ public class ClassDocBuilderMojo
         IStreamOutputTarget outTarget;
         if (classDocFile == null) {
             outTarget = new OutputStreamTarget(System.out);
-            System.out.println("FILE: " + classDocFile.getName());
+            System.out.println("CLASS: " + jclass.getName());
         } else {
-            log.debug("Generate " + classDocFile);
-            classDocFile.getParentFile().mkdirs();
+            log.debug("FILE: " + classDocFile);
+            if (!classDocFile.getParentFile().mkdirs())
+                ;
             outTarget = ResFn.file(classDocFile);
         }
         // outTarget.setCharset("utf-8");
