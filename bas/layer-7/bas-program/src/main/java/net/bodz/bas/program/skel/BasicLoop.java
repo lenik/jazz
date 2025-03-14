e0dfbb0acbe212522f791e2f0d0819de6c208948
Lenik <pub.lenik@bodz.net>
2025-03-14 18:49:19 +0800

    bas-program: BasicLoop not quit due to thread.wait.

--- bas/layer-7/bas-program/src/main/java/net/bodz/bas/program/skel/BasicLoop.java
+++ bas/layer-7/bas-program/src/main/java/net/bodz/bas/program/skel/BasicLoop.java
@@ -17,46 +17,46 @@ public abstract class BasicLoop
             throws Exception {
         setUp();
 
-        final Thread thread = Thread.currentThread();
+        final Thread mainThread = Thread.currentThread();
         AtomicBoolean keepWorking = new AtomicBoolean(true);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             logger.debug("Oops! Trapped exit signal...");
-            synchronized (thread) {
-                // itsMe.notify();
+            synchronized (mainThread) {
+                mainThread.notify();
                 keepWorking.set(false);
                 try {
                     logger.debug("waiting for the main thread to finish...");
-                    thread.wait();
+                    while (mainThread.isAlive())
+                        mainThread.wait(1);
                     logger.debug("main thread ended.");
                 } catch (InterruptedException e) {
                     logger.debug(e, "shtudown hook interrupted");
                 }
             }
-        }));
+        }, "hook"));
 
         logger.debug("Starting... Ctrl-C to stop.");
         try {
-            synchronized (thread) {
-                // itsMe.wait();
+            synchronized (mainThread) {
                 int counter = 0;
                 while (keepWorking.get()) {
                     logger.debug("iteration " + counter);
                     if (!iterateMain())
                         break;
                     logger.debug("    wait interval: " + interval);
-                    thread.wait(interval);
+                    mainThread.wait(interval);
                 }
             }
 
             logger.debug("Tear down...");
-            tearDown();  // This stuff takes time
+            tearDown(); // This stuff takes time
             logger.debug("Tear down completed!");
         } catch (InterruptedException e) {
             logger.error(e, "main interrupted");
         }
-        synchronized (thread) {
-            thread.notify(); // Unlock the shutdown hook.
+        synchronized (mainThread) {
+            mainThread.notify(); // Unlock the shutdown hook.
         }
         System.out.println("Main end");
     }
