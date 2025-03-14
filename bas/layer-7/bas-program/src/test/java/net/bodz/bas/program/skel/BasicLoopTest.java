d05985518afe987780e608bc2df7bd7fefc50bbf
Lenik <pub.lenik@bodz.net>
2025-03-14 18:49:19 +0800

    bas-program: BasicLoop not quit due to thread.wait.

--- bas/layer-7/bas-program/src/test/java/net/bodz/bas/program/skel/BasicLoopTest.java
+++ bas/layer-7/bas-program/src/test/java/net/bodz/bas/program/skel/BasicLoopTest.java
@@ -1,5 +1,10 @@
 package net.bodz.bas.program.skel;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+
 import net.bodz.bas.log.Logger;
 import net.bodz.bas.log.LoggerFactory;
 
@@ -8,9 +13,37 @@ public class BasicLoopTest
 
     static final Logger logger = LoggerFactory.getLogger(BasicLoopTest.class);
 
+    public static int getPid() {
+        try {
+            return Integer.parseInt(Files.readSymbolicLink(Paths.get("/proc/self")).toString());
+        } catch (NoSuchFileException e) {
+            return -1;
+        } catch (NumberFormatException e) {
+            return -1;
+        } catch (IOException e) {
+            return -1;
+        }
+    }
+
     @Override
     protected void setUp()
             throws Exception {
+        Thread mainThread = Thread.currentThread();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+                System.err.println("Send interrupt to main thread");
+                mainThread.interrupt();
+
+//                int pid = getPid();
+//                System.err.println("Send interrupt to process " + pid);
+//                Runtime.getRuntime().exec("kill -SIGINT " + pid);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e.getMessage(), e);
+            }
+        }, "kbdsend").start();
     }
 
     @Override
@@ -21,7 +54,7 @@ public class BasicLoopTest
 
     public static void main(String[] args)
             throws Exception {
-        new BasicLoopTest().execute(args);
+        new BasicLoopTest().execute("-v", "-v", "-v", "-v");
     }
 
 }
