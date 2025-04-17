58547f8898476c9bed5e9f61476067d933d33372
Lenik <pub.lenik@bodz.net>
2025-04-17 21:36:35 +0800

    lily-api: Preferred to use FilePath.name/extension.

--- lily/lily-api/src/main/java/net/bodz/lily/entity/format/WordUtils.java
+++ lily/lily-api/src/main/java/net/bodz/lily/entity/format/WordUtils.java
@@ -7,10 +7,10 @@ import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 
+import net.bodz.bas.c.java.io.FilePath;
 import net.bodz.bas.err.UnexpectedException;
 import net.bodz.bas.repr.content.FileContent;
 import net.bodz.bas.std.rfc.mime.ContentTypes;
-import net.bodz.bas.t.tuple.Split;
 
 public class WordUtils {
 
@@ -22,9 +22,8 @@ public class WordUtils {
         String fileName = defaultFileName;
         String title = wordDoc.getTitle();
         if (title != null && !title.trim().isEmpty()) {
-            Split name_ext = Split.nameExtension(defaultFileName);
-            String extension = name_ext.b;
-            fileName = String.format("%s.%s", title.trim(), extension);
+            String dotExtension = FilePath.dotExtension(defaultFileName);
+            fileName = title.trim() + dotExtension;
         }
         return new FileContent(fileName, ContentTypes.application_vnd_ms_excel, buf.toByteArray());
     }
@@ -40,9 +39,8 @@ public class WordUtils {
         String fileName = defaultFileName;
         String title = wordDoc.getProperties().getCoreProperties().getTitle();
         if (title != null && !title.trim().isEmpty()) {
-            Split name_ext = Split.nameExtension(defaultFileName);
-            String extension = name_ext.b;
-            fileName = String.format("%s.%s", title.trim(), extension);
+            String dotExtension = FilePath.dotExtension(defaultFileName);
+            fileName = title.trim() + dotExtension;
         }
         return new FileContent(fileName, ContentTypes.application_vnd_ms_excel, buf.toByteArray());
     }
