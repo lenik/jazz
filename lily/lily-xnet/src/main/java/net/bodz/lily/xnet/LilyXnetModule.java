667776dfe26bffb7d797f3bd2cb4a03f3069b073
Lenik <pub.lenik@bodz.net>
2025-05-09 20:21:14 +0800

    lily-xnet: new module for external networking.

---
+++ lily/lily-xnet/src/main/java/net/bodz/lily/xnet/LilyXnetModule.java
@@ -0,0 +1,11 @@
+package net.bodz.lily.xnet;
+
+import net.bodz.lily.AbstractLilyModule;
+
+/**
+ * Lily Framework Site Template
+ */
+public class LilyXnetModule
+        extends AbstractLilyModule {
+
+}
