7f9e94516877f170064f33ebc5053dc4aac9f78e
Lenik <pub.lenik@bodz.net>
2025-05-28 16:19:34 +0800

    bas-api: Add ClassLiterals.

---
+++ bas/layer-1/bas-api/src/main/java/net/bodz/bas/c/object/ClassLiterals.java
@@ -0,0 +1,23 @@
+package net.bodz.bas.c.object;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+
+@SuppressWarnings("unchecked")
+public interface ClassLiterals {
+
+    Class<Collection<?>> Collection_class = (Class<Collection<?>>) (Class<?>) Collection.class;
+    Class<List<?>> List_class = (Class<List<?>>) (Class<?>) List.class;
+    Class<Set<?>> Set_class = (Class<Set<?>>) (Class<?>) Set.class;
+    Class<SortedSet<?>> SortedSet_class = (Class<SortedSet<?>>) (Class<?>) SortedSet.class;
+
+    Class<Map<?, ?>> Map_class = (Class<Map<?, ?>>) (Class<?>) Map.class;
+    Class<SortedMap<?, ?>> SortedMap_class = (Class<SortedMap<?, ?>>) (Class<?>) SortedMap.class;
+    Class<NavigableMap<?, ?>> NavigableMap_class = (Class<NavigableMap<?, ?>>) (Class<?>) NavigableMap.class;
+
+}
